fntlnz/chronograf

Convert Tickscript tree to typescript

ui/src/kapacitor/components/TickscriptEditor.tsx
@@ -1,20 +1,24 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+import React, {PureComponent} from 'react'
+
 import {Controlled as CodeMirror} from 'react-codemirror2'
 import 'src/external/codemirror'
+
 import {ErrorHandling} from 'src/shared/decorators/errors'
 
+interface Props {
+  onChangeScript: (tickscript: string) => void
+  script: string
+}
+
+const NOOP = () => {}
+
 @ErrorHandling
-class TickscriptEditor extends Component {
+class TickscriptEditor extends PureComponent<Props> {
   constructor(props) {
     super(props)
   }
 
-  updateCode = (_, __, script) => {
-    this.props.onChangeScript(script)
-  }
-
-  render() {
+  public render() {
     const {script} = this.props
 
     const options = {
@@ -31,17 +35,15 @@ class TickscriptEditor extends Component {
           value={script}
           onBeforeChange={this.updateCode}
           options={options}
+          onTouchStart={NOOP}
         />
       </div>
     )
   }
-}
 
-const {func, string} = PropTypes
-
-TickscriptEditor.propTypes = {
-  onChangeScript: func,
-  script: string,
+  private updateCode = (_, __, script) => {
+    this.props.onChangeScript(script)
+  }
 }
 
 export default TickscriptEditor

ui/src/kapacitor/components/TickscriptEditorControls.tsx
@@ -1,0 +1,67 @@
+import React, {PureComponent, MouseEvent, ChangeEvent} from 'react'
+
+import TickscriptType from 'src/kapacitor/components/TickscriptType'
+import MultiSelectDBDropdown from 'src/shared/components/MultiSelectDBDropdown'
+import TickscriptID, {
+  TickscriptStaticID,
+} from 'src/kapacitor/components/TickscriptID'
+
+import {Task} from 'src/types'
+import {DBRP} from 'src/types/kapacitor'
+
+interface DBRPDropdownItem extends DBRP {
+  name: string
+}
+
+interface Props {
+  isNewTickscript: boolean
+  onSelectDbrps: (dbrps: DBRP[]) => void
+  onChangeType: (type: string) => (event: MouseEvent<HTMLLIElement>) => void
+  onChangeID: (e: ChangeEvent<HTMLInputElement>) => void
+  task: Task
+}
+
+class TickscriptEditorControls extends PureComponent<Props> {
+  public render() {
+    const {onSelectDbrps, onChangeType, task} = this.props
+    return (
+      <div className="tickscript-controls">
+        {this.tickscriptID}
+        <div className="tickscript-controls--right">
+          <TickscriptType type={task.type} onChangeType={onChangeType} />
+          <MultiSelectDBDropdown
+            selectedItems={this.addName(task.dbrps)}
+            onApply={onSelectDbrps}
+          />
+        </div>
+      </div>
+    )
+  }
+
+  private get tickscriptID() {
+    const {isNewTickscript, onChangeID, task} = this.props
+
+    if (isNewTickscript) {
+      return <TickscriptID onChangeID={onChangeID} id={task.id} />
+    }
+
+    return <TickscriptStaticID id={this.taskID} />
+  }
+
+  private get taskID() {
+    const {
+      task: {name, id},
+    } = this.props
+    if (name) {
+      return name
+    }
+    return id
+  }
+
+  private addName = (list: DBRP[]): DBRPDropdownItem[] => {
+    const listWithName = list.map(l => ({...l, name: `${l.db}.${l.rp}`}))
+    return listWithName
+  }
+}
+
+export default TickscriptEditorControls

ui/src/kapacitor/components/TickscriptID.tsx
@@ -1,14 +1,18 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+import React, {PureComponent, SFC, ChangeEvent} from 'react'
 import {ErrorHandling} from 'src/shared/decorators/errors'
 
+interface TickscriptIDProps {
+  onChangeID: (e: ChangeEvent<HTMLInputElement>) => void
+  id: string
+}
+
 @ErrorHandling
-class TickscriptID extends Component {
+class TickscriptID extends PureComponent<TickscriptIDProps> {
   constructor(props) {
     super(props)
   }
 
-  render() {
+  public render() {
     const {onChangeID, id} = this.props
 
     return (
@@ -25,19 +29,11 @@ class TickscriptID extends Component {
   }
 }
 
-export const TickscriptStaticID = ({id}) => (
+interface TickscriptStaticIDProps {
+  id: string
+}
+export const TickscriptStaticID: SFC<TickscriptStaticIDProps> = ({id}) => (
   <h1 className="tickscript-controls--name">{id}</h1>
 )
-
-const {func, string} = PropTypes
-
-TickscriptID.propTypes = {
-  onChangeID: func.isRequired,
-  id: string.isRequired,
-}
-
-TickscriptStaticID.propTypes = {
-  id: string.isRequired,
-}
 
 export default TickscriptID

influxdata/ui

src/kapacitor/components/TickscriptEditor.tsx
@@ -1,20 +1,24 @@
 import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+
 import {Controlled as CodeMirror} from 'react-codemirror2'
 import 'src/external/codemirror'
+
 import {ErrorHandling} from 'src/shared/decorators/errors'
 
+interface Props {
+  onChangeScript: (tickscript: string) => void
+  script: string
+}
+
+const NOOP = () => {}
+
 @ErrorHandling
-class TickscriptEditor extends Component {
+class TickscriptEditor extends Component<Props> {
   constructor(props) {
     super(props)
   }
 
-  updateCode = (_, __, script) => {
-    this.props.onChangeScript(script)
-  }
-
-  render() {
+  public render() {
     const {script} = this.props
 
     const options = {
@@ -31,17 +35,15 @@ class TickscriptEditor extends Component {
           value={script}
           onBeforeChange={this.updateCode}
           options={options}
+          onTouchStart={NOOP}
         />
       </div>
     )
   }
-}
 
-const {func, string} = PropTypes
-
-TickscriptEditor.propTypes = {
-  onChangeScript: func,
-  script: string,
+  private updateCode = (_, __, script) => {
+    this.props.onChangeScript(script)
+  }
 }
 
 export default TickscriptEditor

src/kapacitor/components/TickscriptEditorControls.tsx
@@ -1,0 +1,67 @@
+import React, {Component, MouseEvent, ChangeEvent} from 'react'
+
+import TickscriptType from 'src/kapacitor/components/TickscriptType'
+import MultiSelectDBDropdown from 'src/shared/components/MultiSelectDBDropdown'
+import TickscriptID, {
+  TickscriptStaticID,
+} from 'src/kapacitor/components/TickscriptID'
+
+import {Task} from 'src/types'
+import {DBRP} from 'src/types/kapacitor'
+
+interface DBRPDropdownItem extends DBRP {
+  name: string
+}
+
+interface Props {
+  isNewTickscript: boolean
+  onSelectDbrps: (dbrps: DBRP[]) => void
+  onChangeType: (type: string) => (event: MouseEvent<HTMLLIElement>) => void
+  onChangeID: (e: ChangeEvent<HTMLInputElement>) => void
+  task: Task
+}
+
+class TickscriptEditorControls extends Component<Props> {
+  public render() {
+    const {onSelectDbrps, onChangeType, task} = this.props
+    return (
+      <div className="tickscript-controls">
+        {this.tickscriptID}
+        <div className="tickscript-controls--right">
+          <TickscriptType type={task.type} onChangeType={onChangeType} />
+          <MultiSelectDBDropdown
+            selectedItems={this.addName(task.dbrps)}
+            onApply={onSelectDbrps}
+          />
+        </div>
+      </div>
+    )
+  }
+
+  private get tickscriptID() {
+    const {isNewTickscript, onChangeID, task} = this.props
+
+    if (isNewTickscript) {
+      return <TickscriptID onChangeID={onChangeID} id={task.id} />
+    }
+
+    return <TickscriptStaticID id={this.taskID} />
+  }
+
+  private get taskID() {
+    const {
+      task: {name, id},
+    } = this.props
+    if (name) {
+      return name
+    }
+    return id
+  }
+
+  private addName = (list: DBRP[]): DBRPDropdownItem[] => {
+    const listWithName = list.map(l => ({...l, name: `${l.db}.${l.rp}`}))
+    return listWithName
+  }
+}
+
+export default TickscriptEditorControls

src/kapacitor/components/TickscriptID.tsx
@@ -1,14 +1,18 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+import React, {Component, SFC, ChangeEvent} from 'react'
 import {ErrorHandling} from 'src/shared/decorators/errors'
 
+interface TickscriptIDProps {
+  onChangeID: (e: ChangeEvent<HTMLInputElement>) => void
+  id: string
+}
+
 @ErrorHandling
-class TickscriptID extends Component {
+class TickscriptID extends Component<TickscriptIDProps> {
   constructor(props) {
     super(props)
   }
 
-  render() {
+  public render() {
     const {onChangeID, id} = this.props
 
     return (
@@ -25,19 +29,11 @@ class TickscriptID extends Component {
   }
 }
 
-export const TickscriptStaticID = ({id}) => (
+interface TickscriptStaticIDProps {
+  id: string
+}
+export const TickscriptStaticID: SFC<TickscriptStaticIDProps> = ({id}) => (
   <h1 className="tickscript-controls--name">{id}</h1>
 )
-
-const {func, string} = PropTypes
-
-TickscriptID.propTypes = {
-  onChangeID: func.isRequired,
-  id: string.isRequired,
-}
-
-TickscriptStaticID.propTypes = {
-  id: string.isRequired,
-}
 
 export default TickscriptID
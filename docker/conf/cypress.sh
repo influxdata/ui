cc1b9c3373ed94d3b2aa5d4954f1af9cee950057
Andrew Watkins <andrew.watkinz@gmail.com>
2020-07-16 13:49:15 -0700

    chore: pipefail

--- docker/conf/cypress.sh
+++ docker/conf/cypress.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 set +e
+set -o pipefail
+
 cd /repo && \
 yarn cypress run --browser chrome --spec $1 --reporter junit --reporter-options 'mochaFile=junit-results/test-output-[hash].xml'
-retVal=$?
 
 cp -r /repo/junit-results /test-artifacts
-
-exit $retVal

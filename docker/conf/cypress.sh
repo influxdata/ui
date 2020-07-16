d61c84c8814777cb409bf196710c73c659cffe44
Andrew Watkins <andrew.watkinz@gmail.com>
2020-07-16 13:23:52 -0700

    chore: science experiement

--- docker/conf/cypress.sh
+++ docker/conf/cypress.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-cd /repo && \
-yarn cypress run --browser chrome --spec $1 --reporter junit --reporter-options 'mochaFile=junit-results/test-output-[hash].xml'
-retVal=$?
+retVal=(cd /repo && \
+yarn cypress run --browser chrome --spec $1 --reporter junit --reporter-options 'mochaFile=junit-results/test-output-[hash].xml')
 
 cp -r /repo/junit-results /test-artifacts
 

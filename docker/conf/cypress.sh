9771954226ca529fde9e8c1f622501e87c6d79a4
Andrew Watkins <andrew.watkinz@gmail.com>
2020-07-16 13:00:49 -0700

    chore: science experiment

--- docker/conf/cypress.sh
+++ docker/conf/cypress.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 cd /repo && \
-yarn cypress run --browser chrome --spec "$(circleci tests glob "./cypress/e2e/**/*.test.ts" | circleci tests split --split-by=timings | paste -sd "," -)" --reporter junit --reporter-options 'mochaFile=junit-results/test-output-[hash].xml'
+yarn cypress run --browser chrome --spec $1 --reporter junit --reporter-options 'mochaFile=junit-results/test-output-[hash].xml'
 retVal=$?
 
 cp -r /repo/junit-results /test-artifacts

5d76a541bacfce3fdb8836cdedde98e13f2d6dd3
Andrew Watkins <andrew.watkinz@gmail.com>
2020-07-16 14:19:59 -0700

    build: testing machine

--- docker/conf/cypress.sh
+++ docker/conf/cypress.sh
@@ -7,6 +7,4 @@ cd /repo && \
 yarn cypress run --browser chrome --spec $1 --reporter junit --reporter-options 'mochaFile=junit-results/test-output-[hash].xml'
 retVal=$?
 
-cp -r /repo/junit-results /test-artifacts
-
 exit $retVal

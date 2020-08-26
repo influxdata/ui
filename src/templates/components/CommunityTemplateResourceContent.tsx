1c64d141dd08af5f324cc1662743082bbf8be6e9
Bucky Schwarz <d.w.schwarz@gmail.com>
2020-08-26 16:33:03 -0700

    chore(community-templates): rename communitytemplateoverlaycontents => communitytemplateresourcecontent

--- src/templates/components/CommunityTemplateResourceContent.tsx
+++ src/templates/components/CommunityTemplateResourceContent.tsx
@@ -25,7 +25,7 @@ import {event} from 'src/cloud/utils/reporting'
 type ReduxProps = ConnectedProps<typeof connector>
 type Props = ReduxProps
 
-class CommunityTemplateOverlayContentsUnconnected extends PureComponent<Props> {
+class CommunityTemplateResourceContentUnconnected extends PureComponent<Props> {
   render() {
     const {summary} = this.props
     if (!Object.keys(summary).length) {
@@ -251,6 +251,6 @@ const mdtp = {
 
 const connector = connect(mstp, mdtp)
 
-export const CommunityTemplateOverlayContents = connector(
-  CommunityTemplateOverlayContentsUnconnected
+export const CommunityTemplateResourceContent = connector(
+  CommunityTemplateResourceContentUnconnected
 )

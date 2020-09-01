e23f74d9737ecb72c72198cf1365c7356645cbf7
Bucky Schwarz <d.w.schwarz@gmail.com>
2020-09-01 16:17:04 -0700

    fix: don't try to rename a failed template install. make notification stay indefinitely.

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -37,6 +37,7 @@ export const NOTIFICATION_TRANSITION = 250
 export const FIVE_SECONDS = 5000
 export const TEN_SECONDS = 10000
 export const FIFTEEN_SECONDS = 15000
+export const INDEFINITE = Infinity
 
 export const HOMEPAGE_PATHNAME = 'me'
 

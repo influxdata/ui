bc21b50487c7c113754a3027e65dff92762b97a5
Roman Wehmhőner <r.wehmhoner@gmail.com>
2020-09-16 08:04:57 +0200

    test: Sn tests missing in Cypress added

--- cypress/e2e/login.test.ts
+++ cypress/e2e/login.test.ts
@@ -1,6 +1,16 @@
+interface LoginUser {
+  username: string
+  password: string
+}
+
 describe('The Login Page', () => {
+  let user: LoginUser
   beforeEach(() => {
     cy.flush()
+
+    cy.fixture('user').then(u => {
+      user = u
+    })
 
     cy.setupUser().then(({body}) => {
       cy.wrap(body.org.id).as('orgID')
@@ -9,12 +19,10 @@ describe('The Login Page', () => {
     cy.visit('/')
   })
 
-  // NOTE: we aren't currently loading the login page
-  // for dex
-  it.skip('can login and logout', () => {
-    cy.get('#username').type(Cypress.env('username'))
-    cy.get('#password').type(Cypress.env('password'))
-    cy.get('#submit-login').click()
+  it('can login and logout', () => {
+    cy.getByInputName('username').type(user.username)
+    cy.getByInputName('password').type(user.password)
+    cy.get('button[type=submit]').click()
 
     cy.getByTestID('tree-nav').should('exist')
 
@@ -31,33 +39,49 @@ describe('The Login Page', () => {
     cy.getByTestID('signin-page--content').should('exist')
   })
 
-  // NOTE: we aren't currently loading the login page
-  // for dex
-  describe.skip('login failure', () => {
+  it('can logout from homepage', () => {
+    cy.flush()
+    cy.signin()
+    cy.visit('/')
+
+    cy.getByTestID('logout--button').click()
+
+    cy.getByTestID('signin-page--content').should('exist')
+
+    // try to access a protected route
+    cy.get<string>('@orgID').then(orgID => {
+      cy.visit(`/orgs/${orgID}`)
+    })
+
+    // assert that user is routed to signin
+    cy.getByTestID('signin-page--content').should('exist')
+  })
+
+  describe('login failure', () => {
     it('if username is not present', () => {
-      cy.get('#password').type(Cypress.env('password'))
-      cy.get('#submit-login').click()
+      cy.getByInputName('password').type(user.password)
+      cy.get('button[type=submit]').click()
 
       cy.getByTestID('notification-error').should('exist')
     })
 
     it('if password is not present', () => {
-      cy.get('#username').type(Cypress.env('username'))
-      cy.get('#submit-login').click()
+      cy.getByInputName('username').type(user.username)
+      cy.get('button[type=submit]').click()
 
       cy.getByTestID('notification-error').should('exist')
     })
 
     it('if username is incorrect', () => {
       cy.getByInputName('username').type('not-a-user')
-      cy.getByInputName('password').type(Cypress.env('password'))
+      cy.getByInputName('password').type(user.password)
       cy.get('button[type=submit]').click()
 
       cy.getByTestID('notification-error').should('exist')
     })
 
     it('if password is incorrect', () => {
-      cy.getByInputName('username').type(Cypress.env('username'))
+      cy.getByInputName('username').type(user.username)
       cy.getByInputName('password').type('not-a-password')
       cy.get('button[type=submit]').click()
 

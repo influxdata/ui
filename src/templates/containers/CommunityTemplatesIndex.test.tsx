c979d8fb59ced7da029e2e7b20d8d4c9a3003433
Bucky Schwarz <d.w.schwarz@gmail.com>
2020-09-23 15:58:12 -0700

    try to render a component

--- src/templates/containers/CommunityTemplatesIndex.test.tsx
+++ src/templates/containers/CommunityTemplatesIndex.test.tsx
@@ -3,8 +3,13 @@ import React, {Component} from 'react'
 import {CommunityTemplatesIndex} from 'src/templates/containers/CommunityTemplatesIndex'
 import {renderWithReduxAndRouter} from 'src/mockState'
 
+import {withRouterProps} from 'mocks/dummyData'
+
 describe('the Community Templates integration test', () => {
   it('is super basic', () => {
+    const props = {...withRouterProps}
+    const {getByTitle} = renderWithReduxAndRouter(<CommunityTemplatesIndex {...props} />, () => redux)
+
     expect(true).toBeTruthy()
   })
 })

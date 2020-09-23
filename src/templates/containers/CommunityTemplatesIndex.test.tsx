ae5dd983bb0d353b805c5c2cd82557c6f6e9301f
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2020-09-23 16:54:42 -0600

    reactredux

--- src/templates/containers/CommunityTemplatesIndex.test.tsx
+++ src/templates/containers/CommunityTemplatesIndex.test.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 
 import {CommunityTemplatesIndex} from 'src/templates/containers/CommunityTemplatesIndex'
+import {renderWithReduxAndRouter} from 'src/mockState'
 
 describe('the Community Templates integration test', () => {
   it('is super basic', () => {

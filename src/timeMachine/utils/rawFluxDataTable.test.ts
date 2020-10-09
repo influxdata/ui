9d37ae025ea070887b7f8007b96f48f22fe3fe45
asalem <ariel.salem1989@gmail.com>
2020-10-09 11:12:59 -0700

    feat(object-parsing): added tests for object parsing

--- src/timeMachine/utils/rawFluxDataTable.test.ts
+++ src/timeMachine/utils/rawFluxDataTable.test.ts
@@ -285,6 +285,422 @@ there",5
 
     expect(parseFilesWithFromFlux([CSV])).toEqual(expected)
   })
+
+  test('can parse objects in CSV', () => {
+    const CSV = `#group,false,false,true,true,false,false,true,true,true,true,true,true,true,true,true,true,true
+#datatype,string,long,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,string,string,string,string,string,string,string,string,string,string,string,string
+#default,_result,,,,,,,,,,,,,,,,
+,result,table,_start,_stop,_time,_value,_field,_measurement,env,host,hostname,nodename,orgID,ot_trace_sampled,query-role,role,source
+,,0,2020-06-03T03:17:00Z,2020-06-03T03:18:00Z,2020-06-03T03:17:00.235373882Z,"{""request"":""howdy"",""error"":""something is wrong""}",request,query_log,stag01-us-east-4,queryd-v1-6c46d687dc-2t5r5,queryd-v1-6c46d687dc-2t5r5,gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6,342e059bf55331f9,false,unknown,queryd-v1,tasks`
+
+    const expectedData = [
+      [
+        '#group',
+        'false',
+        'false',
+        'true',
+        'true',
+        'false',
+        'false',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+      ],
+      [
+        '#datatype',
+        'string',
+        'long',
+        'dateTime:RFC3339',
+        'dateTime:RFC3339',
+        'dateTime:RFC3339',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+      ],
+      ['#default', '_result'],
+      [
+        '',
+        'result',
+        'table',
+        '_start',
+        '_stop',
+        '_time',
+        '_value',
+        '_field',
+        '_measurement',
+        'env',
+        'host',
+        'hostname',
+        'nodename',
+        'orgID',
+        'ot_trace_sampled',
+        'query-role',
+        'role',
+        'source',
+      ],
+      [
+        '',
+        '',
+        '0',
+        '2020-06-03T03:17:00.000Z',
+        '2020-06-03T03:18:00.000Z',
+        '2020-06-03T03:17:00.235Z',
+        '{"request":"howdy","error":"something is wrong"}',
+        'request',
+        'query_log',
+        'stag01-us-east-4',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6',
+        '342e059bf55331f9',
+        'false',
+        'unknown',
+        'queryd-v1',
+        'tasks',
+      ],
+    ]
+
+    const expected = {
+      data: expectedData,
+      maxColumnCount: 18,
+    }
+
+    const result = parseFilesWithFromFlux([CSV])
+    expect(result).toEqual(expected)
+
+    expect(() => {
+      JSON.parse(result.data[4][6])
+    }).not.toThrow()
+  })
+
+  test('can parse nested objects in CSV', () => {
+    const CSV = `#group,false,false,true,true,false,false,true,true,true,true,true,true,true,true,true,true,true
+#datatype,string,long,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,string,string,string,string,string,string,string,string,string,string,string,string
+#default,_result,,,,,,,,,,,,,,,,
+,result,table,_start,_stop,_time,_value,_field,_measurement,env,host,hostname,nodename,orgID,ot_trace_sampled,query-role,role,source
+,,0,2020-06-03T03:17:00Z,2020-06-03T03:18:00Z,2020-06-03T03:17:00.235373882Z,"{""request"":{""nested"":""property""},""error"":""file not found""}",request,query_log,stag01-us-east-4,queryd-v1-6c46d687dc-2t5r5,queryd-v1-6c46d687dc-2t5r5,gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6,342e059bf55331f9,false,unknown,queryd-v1,tasks`
+
+    const expectedData = [
+      [
+        '#group',
+        'false',
+        'false',
+        'true',
+        'true',
+        'false',
+        'false',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+      ],
+      [
+        '#datatype',
+        'string',
+        'long',
+        'dateTime:RFC3339',
+        'dateTime:RFC3339',
+        'dateTime:RFC3339',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+      ],
+      ['#default', '_result'],
+      [
+        '',
+        'result',
+        'table',
+        '_start',
+        '_stop',
+        '_time',
+        '_value',
+        '_field',
+        '_measurement',
+        'env',
+        'host',
+        'hostname',
+        'nodename',
+        'orgID',
+        'ot_trace_sampled',
+        'query-role',
+        'role',
+        'source',
+      ],
+      [
+        '',
+        '',
+        '0',
+        '2020-06-03T03:17:00.000Z',
+        '2020-06-03T03:18:00.000Z',
+        '2020-06-03T03:17:00.235Z',
+        '{"request":{"nested":"property"},"error":"file not found"}',
+        'request',
+        'query_log',
+        'stag01-us-east-4',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6',
+        '342e059bf55331f9',
+        'false',
+        'unknown',
+        'queryd-v1',
+        'tasks',
+      ],
+    ]
+
+    const expected = {
+      data: expectedData,
+      maxColumnCount: 18,
+    }
+
+    const result = parseFilesWithFromFlux([CSV])
+    expect(result).toEqual(expected)
+
+    expect(() => {
+      JSON.parse(result.data[4][6])
+    }).not.toThrow()
+  })
+
+  test('can parse objects in multi-csv response', () => {
+    const CSV = `#group,false,false,true,true,false,false,true,true,true,true,true,true,true,true,true,true,true
+#datatype,string,long,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,string,string,string,string,string,string,string,string,string,string,string,string
+#default,_result,,,,,,,,,,,,,,,,
+,result,table,_start,_stop,_time,_value,_field,_measurement,env,host,hostname,nodename,orgID,ot_trace_sampled,query-role,role,source
+,,0,2020-06-03T03:17:00Z,2020-06-03T03:18:00Z,2020-06-03T03:17:00.235373882Z,"{""request"":""howdy"",""error"":""something is wrong""}",request,query_log,stag01-us-east-4,queryd-v1-6c46d687dc-2t5r5,queryd-v1-6c46d687dc-2t5r5,gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6,342e059bf55331f9,false,unknown,queryd-v1,tasks
+
+#group,false,false,true,true,false,false,true,true,true,true,true,true,true,true,true,true,true
+#datatype,string,long,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,string,string,string,string,string,string,string,string,string,string,string,string
+#default,_result,,,,,,,,,,,,,,,,
+,result,table,_start,_stop,_time,_value,_field,_measurement,env,host,hostname,nodename,orgID,ot_trace_sampled,query-role,role,source
+,,0,2020-06-03T03:17:00Z,2020-06-03T03:18:00Z,2020-06-03T03:17:00.235373882Z,"{""request0"":""yo"",""error"":""something is seriously wrong""}",request,query_log,stag01-us-east-4,queryd-v1-6c46d687dc-2t5r5,queryd-v1-6c46d687dc-2t5r5,gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6,342e059bf55331f9,false,unknown,queryd-v1,tasks
+,,0,2020-06-03T03:17:00Z,2020-06-03T03:18:00Z,2020-06-03T03:17:00.235373882Z,"{""request1"":""hi"",""error"":""oops, another error""}",request,query_log,stag01-us-east-4,queryd-v1-6c46d687dc-2t5r5,queryd-v1-6c46d687dc-2t5r5,gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6,342e059bf55331f9,false,unknown,queryd-v1,tasks
+
+#group,false,false,true,true,false,false,true,true,true,true,true,true,true,true,true,true,true
+#datatype,string,long,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,string,string,string,string,string,string,string,string,string,string,string,string
+#default,_result,,,,,,,,,,,,,,,,
+,result,table,_start,_stop,_time,_value,_field,_measurement,env,host,hostname,nodename,orgID,ot_trace_sampled,query-role,role,source
+,,0,2020-06-03T03:17:00Z,2020-06-03T03:18:00Z,2020-06-03T03:17:00.235373882Z,"{""request"":""greetings"",""error"":""file not found""}",request,query_log,stag01-us-east-4,queryd-v1-6c46d687dc-2t5r5,queryd-v1-6c46d687dc-2t5r5,gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6,342e059bf55331f9,false,unknown,queryd-v1,tasks
+
+#group,false,false,true,true,false,false,true,true,true,true,true,true,true,true,true,true,true
+#datatype,string,long,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,string,string,string,string,string,string,string,string,string,string,string,string
+#default,_result,,,,,,,,,,,,,,,,
+,result,table,_start,_stop,_time,_value,_field,_measurement,env,host,hostname,nodename,orgID,ot_trace_sampled,query-role,role,source
+,,0,2020-06-03T03:17:00Z,2020-06-03T03:18:00Z,2020-06-03T03:17:00.235373882Z,"{""request99"":""hello"",""error"":""my name is bob""}",request,query_log,stag01-us-east-4,queryd-v1-6c46d687dc-2t5r5,queryd-v1-6c46d687dc-2t5r5,gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6,342e059bf55331f9,false,unknown,queryd-v1,tasks`
+
+    const expectedData = [
+      [
+        '#group',
+        'false',
+        'false',
+        'true',
+        'true',
+        'false',
+        'false',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+        'true',
+      ],
+      [
+        '#datatype',
+        'string',
+        'long',
+        'dateTime:RFC3339',
+        'dateTime:RFC3339',
+        'dateTime:RFC3339',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+        'string',
+      ],
+      ['#default', '_result'],
+      [
+        '',
+        'result',
+        'table',
+        '_start',
+        '_stop',
+        '_time',
+        '_value',
+        '_field',
+        '_measurement',
+        'env',
+        'host',
+        'hostname',
+        'nodename',
+        'orgID',
+        'ot_trace_sampled',
+        'query-role',
+        'role',
+        'source',
+      ],
+      [
+        '',
+        '',
+        '0',
+        '2020-06-03T03:17:00.000Z',
+        '2020-06-03T03:18:00.000Z',
+        '2020-06-03T03:17:00.235Z',
+        '{"request":"howdy","error":"something is wrong"}',
+        'request',
+        'query_log',
+        'stag01-us-east-4',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6',
+        '342e059bf55331f9',
+        'false',
+        'unknown',
+        'queryd-v1',
+        'tasks',
+      ],
+      [
+        '',
+        '',
+        '0',
+        '2020-06-03T03:17:00.000Z',
+        '2020-06-03T03:18:00.000Z',
+        '2020-06-03T03:17:00.235Z',
+        '{"request0":"yo","error":"something is seriously wrong"}',
+        'request',
+        'query_log',
+        'stag01-us-east-4',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6',
+        '342e059bf55331f9',
+        'false',
+        'unknown',
+        'queryd-v1',
+        'tasks',
+      ],
+      [
+        '',
+        '',
+        '0',
+        '2020-06-03T03:17:00.000Z',
+        '2020-06-03T03:18:00.000Z',
+        '2020-06-03T03:17:00.235Z',
+        '{"request1":"hi","error":"oops, another error"}',
+        'request',
+        'query_log',
+        'stag01-us-east-4',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6',
+        '342e059bf55331f9',
+        'false',
+        'unknown',
+        'queryd-v1',
+        'tasks',
+      ],
+      [
+        '',
+        '',
+        '0',
+        '2020-06-03T03:17:00.000Z',
+        '2020-06-03T03:18:00.000Z',
+        '2020-06-03T03:17:00.235Z',
+        '{"request":"greetings","error":"file not found"}',
+        'request',
+        'query_log',
+        'stag01-us-east-4',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6',
+        '342e059bf55331f9',
+        'false',
+        'unknown',
+        'queryd-v1',
+        'tasks',
+      ],
+      [
+        '',
+        '',
+        '0',
+        '2020-06-03T03:17:00.000Z',
+        '2020-06-03T03:18:00.000Z',
+        '2020-06-03T03:17:00.235Z',
+        '{"request99":"hello","error":"my name is bob"}',
+        'request',
+        'query_log',
+        'stag01-us-east-4',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'queryd-v1-6c46d687dc-2t5r5',
+        'gke-stag01-us-east-4-highmem-preempti-6beb3a50-nsb6',
+        '342e059bf55331f9',
+        'false',
+        'unknown',
+        'queryd-v1',
+        'tasks',
+      ],
+    ]
+
+    const expected = {
+      data: expectedData,
+      maxColumnCount: 18,
+    }
+
+    const result = parseFilesWithFromFlux([CSV])
+
+    expect(result).toEqual(expected)
+
+    expect(() => {
+      JSON.parse(result.data[4][6])
+      JSON.parse(result.data[5][6])
+      JSON.parse(result.data[6][6])
+      JSON.parse(result.data[7][6])
+      JSON.parse(result.data[8][6])
+    }).not.toThrow()
+  })
 })
 
 describe('parseFilesWithObjects', () => {

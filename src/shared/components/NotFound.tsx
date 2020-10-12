8f359a3d05669b7cf58404a2026d403ae23def4b
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2020-10-11 20:31:45 -0600

    fix: the h1 tags

--- src/shared/components/NotFound.tsx
+++ src/shared/components/NotFound.tsx
@@ -4,7 +4,7 @@ const NotFound: FC = () => (
   <div className="container-fluid" data-testid="not-found">
     <div className="panel">
       <div className="panel-heading text-center">
-        <h2 className="deluxe">404</h2>
+        <h1 className="deluxe">404</h1>
         <h4>Bummer! We couldn't find the page you were looking for</h4>
       </div>
     </div>

50deb551003b435f7d26166d965213907dce5d83
Andrew Watkins <andrew.watkinz@gmail.com>
2020-10-12 13:32:28 -0700

    build: disable shared mem space

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,1 +1,9 @@
-module.exports = on => {}
+module.exports = (on, config) => {
+  on('before:browser:launch', (browser = {}, launchOptions) => {
+    if (browser.family === 'chromium' && browser.name !== 'electron') {
+      launchOptions.args.push('--disable-dev-shm-usage')
+    }
+
+    return launchOptions
+  })
+}

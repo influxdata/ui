e86ce2b51aa3067c475e8e8366c5152d481072e5
Bucky Schwarz <hoorayimhelping@users.noreply.github.com>
2020-10-15 15:45:45 -0700

    refactor: remove legacy templates from redux (#177)

--- src/templates/actions/creators.ts
+++ src/templates/actions/creators.ts
@@ -12,11 +12,9 @@ import {InstalledStack} from 'src/types'
 export const ADD_TEMPLATE_SUMMARY = 'ADD_TEMPLATE_SUMMARY'
 export const GET_TEMPLATE_SUMMARIES_FOR_ORG = 'GET_TEMPLATE_SUMMARIES_FOR_ORG'
 export const POPULATE_TEMPLATE_SUMMARIES = 'POPULATE_TEMPLATE_SUMMARIES'
-export const REMOVE_TEMPLATE_SUMMARY = 'REMOVE_TEMPLATE_SUMMARY'
 export const SET_STAGED_TEMPLATE = 'SET_STAGED_TEMPLATE'
 export const SET_STAGED_TEMPLATE_URL = 'SET_STAGED_TEMPLATE_URL'
 export const SET_EXPORT_TEMPLATE = 'SET_EXPORT_TEMPLATE'
-export const SET_TEMPLATE_SUMMARY = 'SET_TEMPLATE_SUMMARY'
 export const SET_TEMPLATES_STATUS = 'SET_TEMPLATES_STATUS'
 export const TOGGLE_TEMPLATE_RESOURCE_INSTALL =
   'TOGGLE_TEMPLATE_RESOURCE_INSTALL'
@@ -31,10 +29,8 @@ export type EnvRefValue = string | number | boolean
 export type Action =
   | ReturnType<typeof addTemplateSummary>
   | ReturnType<typeof populateTemplateSummaries>
-  | ReturnType<typeof removeTemplateSummary>
   | ReturnType<typeof setExportTemplate>
   | ReturnType<typeof setTemplatesStatus>
-  | ReturnType<typeof setTemplateSummary>
   | ReturnType<typeof setStagedCommunityTemplate>
   | ReturnType<typeof setStagedTemplateUrl>
   | ReturnType<typeof toggleTemplateResourceInstall>
@@ -78,24 +74,6 @@ export const setTemplatesStatus = (status: RemoteDataState) =>
   ({
     type: SET_TEMPLATES_STATUS,
     status,
-  } as const)
-
-export const removeTemplateSummary = (id: string) =>
-  ({
-    type: REMOVE_TEMPLATE_SUMMARY,
-    id,
-  } as const)
-
-export const setTemplateSummary = (
-  id: string,
-  status: RemoteDataState,
-  schema?: TemplateSummarySchema<string>
-) =>
-  ({
-    type: SET_TEMPLATE_SUMMARY,
-    id,
-    status,
-    schema,
   } as const)
 
 export const setStagedCommunityTemplate = (template: CommunityTemplate) =>

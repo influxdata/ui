e57f8de83e94a165e4196ff6025813c2c009a07e
asalem <ariel.salem1989@gmail.com>
2020-10-19 14:19:00 -0700

    feat(flows-index): redesigned the flows index page to include helper text

---
+++ src/flows/components/FlowsExplainer.tsx
@@ -0,0 +1,27 @@
+// Libraries
+import React, {FC} from 'react'
+
+// Components
+import {Panel, Gradients} from '@influxdata/clockface'
+
+const FlowsExplainer: FC = () => (
+  <Panel gradient={Gradients.PolarExpress} border={true}>
+    <Panel.Header>
+      <h5>What is Flows?</h5>
+    </Panel.Header>
+    <Panel.Body>
+      <p>
+        Flows is a tool to help you explore, visualize, and process your data.
+        Learn how to downsample data and other handy tutorials in the{' '}
+        <a
+          href="https://v2.docs.influxdata.com/v2.0/write-data/"
+          target="_blank"
+        >
+          Flows Documentation
+        </a>
+      </p>
+    </Panel.Body>
+  </Panel>
+)
+
+export default FlowsExplainer
--- src/flows/components/FlowsIndexEmpty.tsx
+++ src/flows/components/FlowsIndexEmpty.tsx
@@ -1,15 +1,40 @@
 import React from 'react'
-import {ComponentSize, EmptyState} from '@influxdata/clockface'
-import FlowCreateButton from './FlowCreateButton'
+import {Grid, Columns, ComponentSize, EmptyState} from '@influxdata/clockface'
+import FlowsExplainer from 'src/flows/components/FlowsExplainer'
 
 const FlowsIndexEmpty = () => {
   return (
-    <EmptyState size={ComponentSize.Large}>
-      <EmptyState.Text>
-        Looks like there aren't any <b>Flows</b>, why not create one?
-      </EmptyState.Text>
-      <FlowCreateButton />
-    </EmptyState>
+    <Grid>
+      <Grid.Row>
+        <Grid.Column
+          widthXS={Columns.Twelve}
+          widthSM={Columns.Eight}
+          widthMD={Columns.Ten}
+        >
+          <EmptyState size={ComponentSize.Large}>
+            <div className="flow-empty">
+              <div className="flow-empty--graphic" />
+              <EmptyState.Text>
+                You haven't created any Flows yet
+              </EmptyState.Text>
+              <div className="flow-empty--description-text">
+                <p>
+                  Click <strong>Create Flow</strong> in the top right to get
+                  started
+                </p>
+              </div>
+            </div>
+          </EmptyState>
+        </Grid.Column>
+        <Grid.Column
+          widthXS={Columns.Twelve}
+          widthSM={Columns.Four}
+          widthMD={Columns.Two}
+        >
+          <FlowsExplainer />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   )
 }
 

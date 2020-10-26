939c2f942c4f6b397b9e6d16861cfc6e7c897462
alexpaxton <thealexpaxton@gmail.com>
2020-10-26 15:02:07 -0700

    refactor: implement common toolbar in variables control bar

--- src/shared/components/toolbar/Toolbar.tsx
+++ src/shared/components/toolbar/Toolbar.tsx
@@ -13,31 +13,37 @@ import {FlexBox, FlexBoxProps, Omit} from '@influxdata/clockface'
 import 'src/shared/components/toolbar/Toolbar.scss'
 
 interface Props
-  extends Omit<
-    FlexBoxProps,
-    'style' | 'id' | 'stretchToFitWidth' | 'stretchToFitHeight'
-  > {
+  extends Omit<FlexBoxProps, 'stretchToFitWidth' | 'stretchToFitHeight'> {
   children: ReactNode
 }
 
 const Toolbar: FC<Props> = ({
   children,
+  className,
   testID = 'toolbar',
   direction,
   alignItems,
   justifyContent,
   margin,
+  style,
+  id = 'toolbar',
 }) => {
   const isInPresentationMode = useSelector(getPresentationMode)
 
   const toolbarClassName = classnames('toolbar', {
+    [`${className}`]: className,
     'toolbar__presentation-mode': isInPresentationMode,
   })
 
   return (
-    <div className={toolbarClassName} data-testid={testID}>
+    <div
+      className={toolbarClassName}
+      data-testid={testID}
+      style={style}
+      id={id}
+    >
       <FlexBox
-        className="toolbar-contents"
+        className="toolbar--contents"
         direction={direction}
         alignItems={alignItems}
         justifyContent={justifyContent}

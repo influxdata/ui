703f30fdcd3dc2027f671a680fa7c03483d8d6bf
asalem <ariel.salem1989@gmail.com>
2020-10-26 14:27:54 -0700

    chore(test): updated test

--- src/timeMachine/utils/rawFluxDataTable.ts
+++ src/timeMachine/utils/rawFluxDataTable.ts
@@ -2,6 +2,7 @@ import Papa from 'papaparse'
 import HtmlEntities from 'he'
 import unraw from 'unraw'
 import {fromFlux, FromFluxResult} from '@influxdata/giraffe'
+import {reportErrorThroughHoneyBadger} from 'src/shared/utils/errors'
 
 import {parseChunks} from 'src/shared/parsing/flux/response'
 import {
@@ -32,12 +33,11 @@ export const parseFilesWithFromFlux = (
 export const fromFluxTableTransformer = (
   response: string
 ): {tableData: string[][]; max: number} => {
-  let parsedChunk
-  // TODO(ariel): kill this, since it's a stopgap to prevent improper data from
-  // crashing the app when fromFlux can't handle it
-  try {
-    parsedChunk = fromFlux(response)
-  } catch (error) {
+  const parsedChunk = fromFlux(response)
+  if (parsedChunk.error) {
+    reportErrorThroughHoneyBadger(parsedChunk.error, {
+      name: 'fromFluxTableTransformed function',
+    })
     return {
       tableData: [[]],
       max: 0,

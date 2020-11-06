03c8f72a42c8d5a0bcca9cbc1d8bd4f6ba101121
alexpaxton <thealexpaxton@gmail.com>
2020-11-06 10:13:27 -0800

    refactor: convert to function component
    Hooks yo!

--- src/shared/components/notifications/Notifications.tsx
+++ src/shared/components/notifications/Notifications.tsx
@@ -1,16 +1,16 @@
-import React, {PureComponent} from 'react'
-import {connect, ConnectedProps} from 'react-redux'
+import React, {FC} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
 import {get} from 'lodash'
 import {ComponentSize, Gradients, Notification} from '@influxdata/clockface'
 
 // Utils
-import {dismissNotification as dismissNotificationAction} from 'src/shared/actions/notifications'
+import {dismissNotification} from 'src/shared/actions/notifications'
 
-//Types
-import {AppState, NotificationStyle} from 'src/types'
+// Types
+import {NotificationStyle} from 'src/types'
 
-type ReduxProps = ConnectedProps<typeof connector>
-type Props = ReduxProps
+// Selectors
+import {getNotifications} from 'src/shared/selectors/notifications'
 
 const matchGradientToColor = (style: NotificationStyle): Gradients => {
   const converter = {
@@ -23,55 +23,40 @@ const matchGradientToColor = (style: NotificationStyle): Gradients => {
   return get(converter, style, Gradients.DefaultLight)
 }
 
-class Notifications extends PureComponent<Props> {
-  public static defaultProps = {
-    notifications: [],
+const Notifications: FC = () => {
+  const notifications = useSelector(getNotifications)
+  const dispatch = useDispatch()
+
+  const handleDismiss = (id?: string): void => {
+    dispatch(dismissNotification(id))
   }
 
-  public render() {
-    const {notifications, dismissNotification} = this.props
+  return (
+    <>
+      {notifications.map(
+        ({duration, icon, id, message, style, buttonElement}) => {
+          const gradient = matchGradientToColor(style)
 
-    return (
-      <>
-        {notifications.map(
-          ({duration, icon, id, message, style, buttonElement}) => {
-            const gradient = matchGradientToColor(style)
-
-            const handleDismiss = (): void => {
-              dismissNotification(id)
-            }
-
-            return (
-              <Notification
-                key={id}
-                id={id}
-                icon={icon}
-                duration={duration}
-                size={ComponentSize.ExtraSmall}
-                gradient={gradient}
-                onTimeout={dismissNotification}
-                onDismiss={dismissNotification}
-                testID={`notification-${style}`}
-              >
-                <span className="notification--message">{message}</span>
-                {buttonElement(handleDismiss)}
-              </Notification>
-            )
-          }
-        )}
-      </>
-    )
-  }
+          return (
+            <Notification
+              key={id}
+              id={id}
+              icon={icon}
+              duration={duration}
+              size={ComponentSize.ExtraSmall}
+              gradient={gradient}
+              onTimeout={dismissNotification}
+              onDismiss={dismissNotification}
+              testID={`notification-${style}`}
+            >
+              <span className="notification--message">{message}</span>
+              {buttonElement && buttonElement(handleDismiss)}
+            </Notification>
+          )
+        }
+      )}
+    </>
+  )
 }
 
-const mstp = ({notifications}: AppState) => ({
-  notifications,
-})
-
-const mdtp = {
-  dismissNotification: dismissNotificationAction,
-}
-
-const connector = connect(mstp, mdtp)
-
-export default connector(Notifications)
+export default Notifications

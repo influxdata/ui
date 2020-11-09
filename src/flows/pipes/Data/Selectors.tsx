66363698e261dae90b9a8f82b6cf97678ab54b2b
alexpaxton <thealexpaxton@gmail.com>
2020-11-09 10:23:58 -0800

    feat: add empty state when metric selector search yields no results

--- src/flows/pipes/Data/Selectors.tsx
+++ src/flows/pipes/Data/Selectors.tsx
@@ -2,29 +2,40 @@
 import React, {FC, useContext} from 'react'
 
 // Components
-import {InfluxColors, List} from '@influxdata/clockface'
+import {InfluxColors, List, EmptyState} from '@influxdata/clockface'
 import MeasurementSelectors from 'src/flows/pipes/Data/MeasurementSelectors'
 import FieldSelectors from 'src/flows/pipes/Data/FieldSelectors'
 import TagSelectors from 'src/flows/pipes/Data/TagSelectors'
 import {SchemaContext} from 'src/flows/context/schemaProvider'
 
 const Selectors: FC = () => {
-  const {fields, measurements, tags} = useContext(SchemaContext)
+  const {fields, measurements, tags, searchTerm} = useContext(SchemaContext)
 
-  return (
-    <div className="data-source--block-results">
-      <List
-        className="data-source--list"
-        backgroundColor={InfluxColors.Obsidian}
-        maxHeight="304px"
-        style={{height: '304px'}}
-      >
-        <MeasurementSelectors measurements={measurements} />
-        <FieldSelectors fields={fields} />
-        <TagSelectors tags={tags} />
-      </List>
-    </div>
+  const noSearchResults =
+    searchTerm && !fields.length && !measurements.length && !tags.length
+
+  let list = (
+    <List
+      className="data-source--list"
+      backgroundColor={InfluxColors.Obsidian}
+      maxHeight="304px"
+      style={{height: '304px'}}
+    >
+      <MeasurementSelectors measurements={measurements} />
+      <FieldSelectors fields={fields} />
+      <TagSelectors tags={tags} />
+    </List>
   )
+
+  if (noSearchResults) {
+    list = (
+      <EmptyState style={{height: '304px'}}>
+        <p>{`No fields, meausurements, or tags match "${searchTerm}"`}</p>
+      </EmptyState>
+    )
+  }
+
+  return <div className="data-source--block-results">{list}</div>
 }
 
 export default Selectors

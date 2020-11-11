5620f37c5846793b0f80d53f1873b6c8c31acf82
asalem <ariel.salem1989@gmail.com>
2020-11-11 08:29:55 -0800

    chore: WIP

--- cypress/oss/scrapers.test.ts
+++ cypress/oss/scrapers.test.ts
@@ -3,9 +3,7 @@ import {Organization, Bucket} from '../../src/types'
 // a generous commitment to delivering this page in a loaded state
 const PAGE_LOAD_SLA = 10000
 
-// NOTE
-// this isn't supported in cloud mode
-describe.skip('Scrapers', () => {
+describe('Scrapers', () => {
   beforeEach(() => {
     cy.flush()
 

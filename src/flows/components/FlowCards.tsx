d82b0ec5ccde3bd924d6bd80a122b4873f20077c
alexpaxton <thealexpaxton@gmail.com>
2020-11-18 12:05:42 -0800

    refactor: always show explainer widget; show create button in empty state

--- src/flows/components/FlowCards.tsx
+++ src/flows/components/FlowCards.tsx
@@ -3,22 +3,36 @@ import React, {useContext} from 'react'
 import {ResourceList, Grid, Columns} from '@influxdata/clockface'
 import {FlowListContext} from 'src/flows/context/flow.list'
 import FlowsIndexEmpty from 'src/flows/components/FlowsIndexEmpty'
-
+import FlowsExplainer from 'src/flows/components/FlowsExplainer'
 import FlowCard from 'src/flows/components/FlowCard'
 
 const FlowCards = () => {
   const {flows} = useContext(FlowListContext)
+  // const floows = Object.entries(flows)
+
+  const floows = []
   return (
     <Grid>
       <Grid.Row>
-        <Grid.Column widthXS={Columns.Twelve}>
+        <Grid.Column
+          widthXS={Columns.Twelve}
+          widthSM={Columns.Eight}
+          widthMD={Columns.Ten}
+        >
           <ResourceList>
             <ResourceList.Body emptyState={<FlowsIndexEmpty />}>
-              {Object.entries(flows).map(([id, {name}]) => {
+              {floows.map(([id, {name}]) => {
                 return <FlowCard key={id} id={id} name={name} />
               })}
             </ResourceList.Body>
           </ResourceList>
+        </Grid.Column>
+        <Grid.Column
+          widthXS={Columns.Twelve}
+          widthSM={Columns.Four}
+          widthMD={Columns.Two}
+        >
+          <FlowsExplainer />
         </Grid.Column>
       </Grid.Row>
     </Grid>

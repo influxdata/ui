6ca0e7f55be423718f25f50c0e0f2c9a794e784d
alexpaxton <thealexpaxton@gmail.com>
2020-11-18 17:21:31 -0800

    refactor: size height of script preview based on line count
    This implementation feels fragile, there has to be more reliable way to get line height from monaco

--- src/flows/components/QueryTextPreview.tsx
+++ src/flows/components/QueryTextPreview.tsx
@@ -1,24 +1,52 @@
-import React, {FC, useContext} from 'react'
+// Libraries
+import React, {FC, useContext, useRef} from 'react'
+
+// Components
 import {Form} from '@influxdata/clockface'
-import FluxEditor from 'src/shared/components/FluxMonacoEditor'
+import FluxEditorMonaco from 'src/shared/components/FluxMonacoEditor'
+
+// Utils
 import {formatQueryText} from 'src/flows/shared/utils'
+
+// Contexts
 import {PopupContext} from 'src/flows/context/popup'
 
+// Types
+import {EditorType} from 'src/types'
+
+// Constants
+const MONACO_LINE_EL_CLASS = 'view-line'
+const MAX_PREVIEW_HEIGHT = 300
+
 const QueryTextPreview: FC = () => {
+  const editor = useRef<EditorType>(null)
   const {data} = useContext(PopupContext)
 
-  const formattedQueryText = formatQueryText(data.query)
+  const handleSetEditorInstance = (monacoEditor: EditorType): void => {
+    editor.current = monacoEditor
+  }
+
+  const {text, lineCount} = formatQueryText(data.query)
+
+  const {height} = document
+    .getElementsByClassName(MONACO_LINE_EL_CLASS)[0]
+    .getBoundingClientRect()
+
+  const heightPixels = Math.min(height * (lineCount + 1), MAX_PREVIEW_HEIGHT)
 
   return (
-    <Form.Element label="" style={{height: 300, position: 'relative'}}>
-      <div className="flux-editor">
-        <div className="flux-editor--left-panel">
-          <FluxEditor
-            script={formattedQueryText}
-            onChangeScript={() => {}}
-            readOnly={true}
-          />
-        </div>
+    <Form.Element label="">
+      <div
+        className="flow-visualization--script-preview"
+        style={{height: `${heightPixels}px`}}
+      >
+        <FluxEditorMonaco
+          script={text}
+          onChangeScript={() => {}}
+          readOnly={true}
+          autogrow
+          setEditorInstance={handleSetEditorInstance}
+        />
       </div>
     </Form.Element>
   )

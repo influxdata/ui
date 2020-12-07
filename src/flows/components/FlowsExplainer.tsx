fec47149ab3b1581b0dfd48d2d625edbca89783e
alexpaxton <thealexpaxton@gmail.com>
2020-12-07 15:18:59 -0800

    fix: update grammar, replace link with button

--- src/flows/components/FlowsExplainer.tsx
+++ src/flows/components/FlowsExplainer.tsx
@@ -2,28 +2,41 @@
 import React, {FC} from 'react'
 
 // Components
-import {Panel, Gradients} from '@influxdata/clockface'
-import {PROJECT_NAME_PLURAL} from 'src/flows'
+import {
+  Panel,
+  Gradients,
+  Button,
+  ComponentColor,
+  AlignItems,
+  IconFont,
+} from '@influxdata/clockface'
+import {PROJECT_NAME_PLURAL, NOTEBOOKS_DOCUMENTATION_LINK} from 'src/flows'
 
-const FlowsExplainer: FC = () => (
-  <Panel gradient={Gradients.PolarExpress} border={true}>
-    <Panel.Header>
-      <h5>What is {PROJECT_NAME_PLURAL}?</h5>
-    </Panel.Header>
-    <Panel.Body>
-      <p>
-        {PROJECT_NAME_PLURAL} are a tool to help you explore, visualize, and
-        process your data. Learn how to downsample data and other handy
-        tutorials in the{' '}
-        <a
-          href="https://docs.influxdata.com/influxdb/cloud/notebooks/create-notebook/"
-          target="_blank"
-        >
-          Documentation
-        </a>
-      </p>
-    </Panel.Body>
-  </Panel>
-)
+const FlowsExplainer: FC = () => {
+  const handleDocumentationClick = (): void => {
+    window.open(NOTEBOOKS_DOCUMENTATION_LINK, '_blank')
+  }
+
+  return (
+    <Panel gradient={Gradients.PolarExpress} border={true}>
+      <Panel.Header>
+        <h5>What are {PROJECT_NAME_PLURAL.toLowerCase()}?</h5>
+      </Panel.Header>
+      <Panel.Body alignItems={AlignItems.FlexStart}>
+        <p>
+          {PROJECT_NAME_PLURAL} are a tool to help you explore, visualize, and
+          process your data. Learn how to downsample data and other handy
+          tutorials:
+        </p>
+        <Button
+          icon={IconFont.TextBlock}
+          onClick={handleDocumentationClick}
+          color={ComponentColor.Secondary}
+          text="Notebooks Docs"
+        />
+      </Panel.Body>
+    </Panel>
+  )
+}
 
 export default FlowsExplainer

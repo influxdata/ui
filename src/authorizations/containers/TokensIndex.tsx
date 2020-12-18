352810def64267589145d25312deb2533664105f
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2020-12-18 16:42:08 -0700

    refactor: turned it into a functional componenet

--- src/authorizations/containers/TokensIndex.tsx
+++ src/authorizations/containers/TokensIndex.tsx
@@ -1,10 +1,9 @@
 // Libraries
-import React, {Component} from 'react'
+import React, {FC} from 'react'
 import {Switch, Route} from 'react-router-dom'
 
 // Components
 import {Page} from '@influxdata/clockface'
-import {ErrorHandling} from 'src/shared/decorators/errors'
 import LoadDataTabbedPage from 'src/settings/components/LoadDataTabbedPage'
 import LoadDataHeader from 'src/settings/components/LoadDataHeader'
 import GetResources from 'src/resources/components/GetResources'
@@ -24,32 +23,27 @@ import {ORGS, ORG_ID, TOKENS} from 'src/shared/constants/routes'
 
 const tokensPath = `/${ORGS}/${ORG_ID}/load-data/${TOKENS}/generate`
 
-@ErrorHandling
-class TokensIndex extends Component {
-  public render() {
-    return (
-      <>
-        <Page titleTag={pageTitleSuffixer(['Tokens', 'Load Data'])}>
-          <LoadDataHeader />
-          <LoadDataTabbedPage activeTab="tokens">
-            <GetResources resources={[ResourceType.Authorizations]}>
-              <TokensTab />
-            </GetResources>
-          </LoadDataTabbedPage>
-        </Page>
-        <Switch>
-          <Route
-            path={`${tokensPath}/all-access`}
-            component={AllAccessTokenOverlay}
-          />
-          <Route
-            path={`${tokensPath}/buckets`}
-            component={BucketsTokenOverlay}
-          />
-        </Switch>
-      </>
-    )
-  }
+export const TokensIndex: FC = () => {
+  return (
+    <>
+    <Page titleTag={pageTitleSuffixer(['Tokens', 'Load Data'])}>
+      <LoadDataHeader />
+      <LoadDataTabbedPage activeTab="tokens">
+        <GetResources resources={[ResourceType.Authorizations]}>
+          <TokensTab />
+        </GetResources>
+      </LoadDataTabbedPage>
+    </Page>
+    <Switch>
+      <Route
+        path={`${tokensPath}/all-access`}
+        component={AllAccessTokenOverlay}
+      />
+      <Route
+        path={`${tokensPath}/buckets`}
+        component={BucketsTokenOverlay}
+      />
+    </Switch>
+  </>
+  )
 }
-
-export default TokensIndex

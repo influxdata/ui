08231f0f889fd6a9f0c7f7ef18041a0e751c6e35
Iris Scholten <ischolten.is@gmail.com>
2021-01-05 15:53:31 -0800

    refactor(fitbit): extract out fitbit graph for cleanup

---
+++ src/writeData/components/Integrations/FitbitGraph.tsx
@@ -0,0 +1,87 @@
+// Libraries
+import React, {useEffect, FC, useState} from 'react'
+
+// Components
+import {get} from 'lodash'
+import {Plot, newTable} from '@influxdata/giraffe'
+import {AppState} from 'src/types'
+import {connect} from 'react-redux'
+
+const getData = async (resourceInfo, accessToken: string) => {
+  const URL = `https://api.fitbit.com/1/user/-/${resourceInfo.path}/date/today/1m.json`
+
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization: `Bearer ${accessToken}`,
+  }
+  const resp = await fetch(URL, {method: 'GET', headers})
+  const respBody = await resp.json()
+  return get(respBody, resourceInfo.key, [])
+}
+
+interface StateProps {
+  accessToken: string
+}
+
+interface Resource {
+  path: string
+  key: string
+  title: string
+}
+
+interface Props extends StateProps {
+  resource: Resource
+}
+
+const toDate = stringDate => {
+  return Date.parse(stringDate)
+}
+
+const buildTable = data => {
+  const timeValues = data.map(({dateTime}) => toDate(dateTime))
+  const values = data.map(({value}) => parseInt(value))
+  const table = newTable(data.length)
+    .addColumn('_time', 'dateTime:RFC3339', 'time', timeValues)
+    .addColumn('_value', 'double', 'number', values)
+
+  return table
+}
+
+const FitbitGraph: FC<Props> = ({accessToken, resource}) => {
+  const [data, setData] = useState([])
+
+  const lineLayer = {
+    type: 'line',
+    x: '_time',
+    y: '_value',
+  }
+
+  const config = {
+    table: buildTable(data),
+    layers: [lineLayer],
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getData(resource, accessToken)
+      setData(data)
+    }
+    fetchData()
+  }, [])
+
+  const style = {height: '300px', padding: '32px 0'}
+
+  return (
+    <div style={style}>
+      <h2>{resource.title}</h2>
+      <Plot config={config} style={style} />
+    </div>
+  )
+}
+const mstp = (state: AppState) => {
+  const accessToken = get(state, 'integrations.fitbit', '')
+
+  return {accessToken}
+}
+
+export default connect<StateProps>(mstp)(FitbitGraph)
--- src/writeData/components/Integrations/FitbitViewer.tsx
+++ src/writeData/components/Integrations/FitbitViewer.tsx
@@ -1,151 +1,56 @@
 // Libraries
-import React, {useEffect, FC, useState} from 'react'
+import React, {FC} from 'react'
 
 // Components
 import {Page} from '@influxdata/clockface'
-import {get} from 'lodash'
 import {pageTitleSuffixer} from 'src/shared/utils/pageTitles'
-import {Plot, newTable} from '@influxdata/giraffe'
-import {AppState} from 'src/types'
-import {connect} from 'react-redux'
+import FitbitGraph from './FitbitGraph'
 
 const resources = [
-  {path: 'activities/calories', key: 'activities-calories'},
-  {path: 'activities/caloriesBMR', key: 'activities-caloriesBMR'},
-  {path: 'activities/steps', key: 'activities-steps'},
-  {path: 'activities/distance', key: 'activities-distance'},
-  {path: 'activities/minutesSedentary', key: 'activities-minutesSedentary'},
+  {
+    path: 'activities/calories',
+    key: 'activities-calories',
+    title: 'Daily Calories Past 30 days',
+  },
+  {
+    path: 'activities/caloriesBMR',
+    key: 'activities-caloriesBMR',
+    title: 'Daily Calories BMR Past 30 days',
+  },
+  {
+    path: 'activities/steps',
+    key: 'activities-steps',
+    title: 'Daily Steps Past 30 days',
+  },
+  {
+    path: 'activities/distance',
+    key: 'activities-distance',
+    title: 'Daily Distance Past 30 days',
+  },
+  {
+    path: 'activities/minutesSedentary',
+    key: 'activities-minutesSedentary',
+    title: 'Daily Minutes Past 30 days',
+  },
 ]
-
-const getData = async (resourceInfo, accessToken: string) => {
-  const URL = `https://api.fitbit.com/1/user/-/${resourceInfo.path}/date/today/1m.json`
-
-  const headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    Authorization: `Bearer ${accessToken}`,
-  }
-  const resp = await fetch(URL, {method: 'GET', headers})
-  const respBody = await resp.json()
-  console.log(respBody)
-  return get(respBody, resourceInfo.key, [])
-}
 
 interface StateProps {
   accessToken: string
 }
 
-interface StepLog {
-  dateTime: string
-  value: number
-}
-
-interface State {
-  stepsData: StepLog[]
-}
-
-const toDate = stringDate => {
-  return Date.parse(stringDate)
-}
-
-const buildTable = data => {
-  const timeValues = data.map(({dateTime}) => toDate(dateTime))
-  const values = data.map(({value}) => parseInt(value))
-  const table = newTable(data.length)
-    .addColumn('_time', 'dateTime:RFC3339', 'time', timeValues)
-    .addColumn('_value', 'double', 'number', values)
-
-  return table
-}
-
-const FitbitViewer: FC<StateProps> = ({accessToken}) => {
-  const [caloriesData, setCaloriesData] = useState([])
-  const [caloriesBMRData, setCaloriesBMRData] = useState([])
-  const [stepsData, setStepsData] = useState([])
-  const [distanceData, setDistanceData] = useState([])
-  const [sedentaryData, setSedentaryData] = useState([])
-
-  const lineLayer = {
-    type: 'line',
-    x: '_time',
-    y: '_value',
-  }
-
-  const stepsConfig = {
-    table: buildTable(stepsData),
-    layers: [lineLayer],
-  }
-
-  const distanceConfig = {
-    table: buildTable(distanceData),
-    layers: [lineLayer],
-  }
-
-  const caloriesConfig = {
-    table: buildTable(caloriesData),
-    layers: [lineLayer],
-  }
-
-  const caloriesBMRConfig = {
-    table: buildTable(caloriesBMRData),
-    layers: [lineLayer],
-  }
-
-  const sedentaryConfig = {
-    table: buildTable(sedentaryData),
-    layers: [lineLayer],
-  }
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await Promise.all(
-        resources.map(r => getData(r, accessToken))
-      )
-      const [calories, caloriesBMR, steps, distance, sedentary] = data
-      setCaloriesData(calories)
-      setCaloriesBMRData(caloriesBMR)
-      setStepsData(steps)
-      setDistanceData(distance)
-      setSedentaryData(sedentary)
-    }
-    fetchData()
-  }, [])
-
-  const style = {height: '300px', width: '500px', paddingBottom: '32px'}
-
+const FitbitViewer: FC<StateProps> = () => {
   return (
     <Page titleTag={pageTitleSuffixer(['Fitbit', 'Load Data'])}>
       <Page.Header fullWidth={false}>
         <Page.Title title="Fitbit" />
       </Page.Header>
       <Page.Contents fullWidth={false} scrollable={true}>
-        <div style={style}>
-          <h2>Daily Steps past 30 days</h2>
-          <Plot config={stepsConfig} style={style} />
-        </div>
-        <div style={style}>
-          <h2>Daily Distance past 30 days</h2>
-          <Plot config={distanceConfig} style={style} />
-        </div>
-        <div style={style}>
-          <h2>Daily Calories past 30 days</h2>
-          <Plot config={caloriesConfig} style={style} />
-        </div>
-        <div style={style}>
-          <h2>Daily Calories BMR past 30 days</h2>
-          <Plot config={caloriesBMRConfig} style={style} />
-        </div>
-        <div style={style}>
-          <h2>Daily Sedentary past 30 days</h2>
-          <Plot config={sedentaryConfig} style={style} />
-        </div>
+        {resources.map(resource => (
+          <FitbitGraph resource={resource} key={resource.key} />
+        ))}
       </Page.Contents>
     </Page>
   )
 }
-const mstp = (state: AppState) => {
-  const accessToken = get(state, 'integrations.fitbit', '')
 
-  return {accessToken}
-}
-
-export default connect<StateProps>(mstp)(FitbitViewer)
+export default FitbitViewer

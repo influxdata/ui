929b52cbf1b666ea40ae240949d76b4f24ed872d
Deniz Kusefoglu <deniz@influxdata.com>
2021-01-12 09:14:54 -0700

    feat(e2e-in-KinD): Add cypress-log-to-output plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,9 +1,24 @@
 module.exports = (on, config) => {
-  on('before:browser:launch', (browser = {}, launchOptions) => {
-    if (browser.family === 'chromium' && browser.name !== 'electron') {
-      launchOptions.args.push('--disable-dev-shm-usage')
+  // on('before:browser:launch', (browser = {}, launchOptions) => {
+  //   if (browser.family === 'chromium' && browser.name !== 'electron') {
+  //     launchOptions.args.push('--disable-dev-shm-usage')
+  //   }
+
+  //   return launchOptions
+  // })
+  require('cypress-log-to-output').install(on, (_, event) => {
+    if (
+      event.url &&
+      (event.url.includes('www.googletagmanager.com') ||
+        event.url.includes('api/v2/me'))
+    ) {
+      return false
     }
 
-    return launchOptions
+    if (event.level === 'error') {
+      return true
+    }
+
+    return false
   })
 }

355f3b358f9996c99675544cc7b2d6b15343a8b5
Deniz Kusefoglu <deniz@influxdata.com>
2021-01-12 15:30:28 -0700

    feat(e2e-in-KinD): Add back --disable-dev-shm-usage flag to chrome launch options

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,24 +1,28 @@
 module.exports = (on, config) => {
-  // on('before:browser:launch', (browser = {}, launchOptions) => {
-  //   if (browser.family === 'chromium' && browser.name !== 'electron') {
-  //     launchOptions.args.push('--disable-dev-shm-usage')
-  //   }
+  on('before:browser:launch', (browser = {}, launchOptions) => {
+    // see https://github.com/flotwig/cypress-log-to-output/issues/5
+    launchOptions = require('cypress-log-to-output')
+      .install(on, (_, event) => {
+        if (
+          event.url &&
+          (event.url.includes('www.googletagmanager.com') ||
+            event.url.includes('api/v2/me'))
+        ) {
+          return false
+        }
 
-  //   return launchOptions
-  // })
-  require('cypress-log-to-output').install(on, (_, event) => {
-    if (
-      event.url &&
-      (event.url.includes('www.googletagmanager.com') ||
-        event.url.includes('api/v2/me'))
-    ) {
-      return false
+        if (event.level === 'error') {
+          return true
+        }
+
+        return false
+      })
+      .browserLaunchHandler(browser, args)
+
+    if (browser.family === 'chromium' && browser.name !== 'electron') {
+      launchOptions.args.push('--disable-dev-shm-usage')
     }
 
-    if (event.level === 'error') {
-      return true
-    }
-
-    return false
+    return launchOptions
   })
 }

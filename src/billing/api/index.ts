071c735befc12bbf41f3993918817e6004e87c98
asalem <ariel.salem1989@gmail.com>
2021-01-12 10:55:05 -0800

    chore: removed unused files

--- src/billing/api/index.ts
+++ src/billing/api/index.ts
@@ -50,7 +50,7 @@ export const getBillingAccount = (): ReturnType<typeof getBillingAccountGenerate
       subscriberId: 'id123',
       status: 'paid',
     },
-    type: 'cancelled',
+    type: 'pay_as_you_go',
     updatedAt: new Date().toString(),
     users: [{}],
     zuoraAccountId: 'zID123',

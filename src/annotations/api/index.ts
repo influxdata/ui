0658f90a2d01e37477c5ddc089f6b142b6df3625
ShmuelLotman <shmuel.lotman@gmail.com>
2021-01-12 16:16:08 -0600

    feat: initial work on annotations RUD interface

--- src/annotations/api/index.ts
+++ src/annotations/api/index.ts
@@ -1,5 +1,10 @@
 // Types
-import {PostAnnotationResponse, PostAnnotationPayload} from 'src/types'
+import {
+  PostAnnotationResponse,
+  PostAnnotationPayload,
+  GetAnnotationResponse,
+  GetAnnotationPayload,
+} from 'src/types'
 
 // Libraries
 import axios, {AxiosResponse} from 'axios'
@@ -21,14 +26,74 @@ export const writeAnnotation = async (
 
   const transformedResponse = res.data.map(d => {
     const newData = {
-      startTime: d['start-time'],
-      endTime: d['end-time'],
+      startTime: d['start'],
+      endTime: d['end'],
       stream: d.stream,
       summary: d.summary,
       stickers: d.stickers,
     }
 
     return newData
+  })
+
+  return transformedResponse
+}
+
+const formatAnnotationQueryString = (data: GetAnnotationPayload): string => {
+  let qs: string = ''
+
+  const {stream, startTime, endTime, summary, sticker} = data
+
+  if (stream) {
+    qs += `${qs.length ? '&' : '?'}stream=${stream}`
+  }
+
+  if (startTime) {
+    qs += `${qs.length ? '&' : '?'}start=${startTime}`
+  }
+
+  if (endTime) {
+    qs += `${qs.length ? '&' : '?'}end=${endTime}`
+  }
+
+  if (summary) {
+    qs += `${qs.length ? '&' : '?'}summary=${encodeURI(summary)}`
+  }
+
+  if (sticker) {
+    Object.keys(sticker).forEach((s: string) => {
+      qs += `${qs.length ? '&' : '?'}sticker-includes[${s}]=${encodeURI(
+        sticker[s]
+      )}`
+    })
+  }
+
+  return qs
+}
+
+export const getAnnotation = async (
+  data: GetAnnotationPayload
+): Promise<AxiosResponse<GetAnnotationResponse[]>> => {
+  const formattedQueryString = formatAnnotationQueryString(data)
+  const appendedURL = url + formattedQueryString
+  console.log(appendedURL, 'THIS IS THE URL')
+  const res = await axios.get(appendedURL)
+
+  if (res.status >= 300) {
+    throw new Error(res.data?.message)
+  }
+
+  const transformedResponse = res.data.map(d => {
+    const newDataBlock = {
+      stream: d?.stream,
+      annotations: d.annotations.map(a => ({
+        startTime: a['start'],
+        endTime: a['end'],
+        summary: a.summary,
+        stickers: a.stickers,
+      })),
+    }
+    return newDataBlock
   })
 
   return transformedResponse
--- src/types/annotation.ts
+++ src/types/annotation.ts
@@ -9,3 +9,22 @@ export interface PostAnnotationResponse {
 export interface PostAnnotationPayload {
   summary: string
 }
+
+type Annotation = {
+  endTime: string
+  startTime: string
+  summary: string
+  stickers?: any
+}
+export interface GetAnnotationResponse {
+  stream: string
+  annotations: Annotation[]
+}
+
+export interface GetAnnotationPayload {
+  stream: string
+  startTime: string
+  endTime: string
+  summary: string
+  sticker: any
+}

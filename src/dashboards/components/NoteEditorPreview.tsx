ab0d7abea6c3814c52fceef4de1ab53b4cd8675e
drdelambre <drdelambre>
2021-01-12 13:43:34 -0800

    feat: remove one instance of codemirror

--- src/dashboards/components/NoteEditorPreview.tsx
+++ src/dashboards/components/NoteEditorPreview.tsx
@@ -1,22 +1,16 @@
-import React, {SFC, MouseEvent} from 'react'
+import React, {SFC} from 'react'
 
 import {MarkdownRenderer} from 'src/shared/components/views/MarkdownRenderer'
 import {DapperScrollbars} from '@influxdata/clockface'
 
 interface Props {
   note: string
-  scrollTop: number
-  onScroll: (e: MouseEvent) => void
 }
 
 const NoteEditorPreview: SFC<Props> = props => {
   return (
     <div className="note-editor--preview" data-testid="note-editor--preview">
-      <DapperScrollbars
-        className="note-editor--preview-scroll"
-        scrollTop={props.scrollTop}
-        onScroll={props.onScroll}
-      >
+      <DapperScrollbars className="note-editor--preview-scroll">
         <div className="note-editor--markdown-container">
           <MarkdownRenderer text={props.note} className="markdown-format" />
         </div>

3847a3cf058effafe5622955db5281d07c4d3f52
Gene Hynson <ghynson@influxdata.com>
2021-01-13 13:50:03 -0800

    chore: upgrades cypress to v6.2 (#515)
    * chore: upgrades cypress to v6.2, fixes checks test
    
    * test: fix buckets delete
    
    * test: fix labels test
    
    * test: fix tokens test
    
    * test: fix dashboard and notifications test
    
    * test: fix explorer failures
    
    * test: fix checks test
    
    Co-authored-by: Andrew Watkins <andrew.watkinz@gmail.com>

--- cypress/e2e/shared/explorer.test.ts
+++ cypress/e2e/shared/explorer.test.ts
@@ -1087,7 +1087,7 @@ describe('DataExplorer', () => {
 
     it('can open/close save as dialog and navigate inside', () => {
       // open save as
-      cy.getByTestID('overlay--container').should('not.be.visible')
+      cy.getByTestID('overlay--container').should('not.exist')
       cy.getByTestID('save-query-as').click()
       cy.getByTestID('overlay--container').should('be.visible')
 
@@ -1103,7 +1103,7 @@ describe('DataExplorer', () => {
       cy.getByTestID('save-as-overlay--header').within(() => {
         cy.get('button').click()
       })
-      cy.getByTestID('overlay--container').should('not.be.visible')
+      cy.getByTestID('overlay--container').should('not.exist')
     })
 
     describe('as dashboard cell', () => {

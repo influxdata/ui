01ed8f27d6645385b03f208f16970d8fc13f9ff4
ShmuelLotman <shmuel.lotman@gmail.com>
2021-01-14 18:59:51 -0600

    chore: cleanup and some clarification

--- src/annotations/api/index.ts
+++ src/annotations/api/index.ts
@@ -7,7 +7,7 @@ import {
 } from 'src/types'
 
 // Libraries
-import axios, {AxiosResponse} from 'axios'
+import axios from 'axios'
 
 // Constants
 import {API_BASE_PATH} from 'src/shared/constants'
@@ -18,53 +18,63 @@ const url = `${API_BASE_PATH}api/v2private/annotations`
 
 export const writeAnnotation = async (
   annotations: Annotation[]
-): Promise<AxiosResponse<Annotation[]>> => {
+): Promise<Annotation[]> => {
   const res = await axios.post(url, annotations)
 
   if (res.status >= 300) {
     throw new Error(res.data?.message)
   }
 
-  return res.data
+  const [{start, end, summary, message, stickers, stream}] = res.data
+  return [
+    {
+      start,
+      end,
+      summary,
+      message,
+      stickers,
+      stream,
+    },
+  ]
 }
 
 export const formatAnnotationQueryString = (
-  data: GetAnnotationPayload,
+  annotation: GetAnnotationPayload,
   requestType?: string
 ): string => {
-  let qs: string = ''
+  let queryString: string = ''
 
-  const {stream, start, end, stickers} = data
+  const {stream, start, end, stickers} = annotation
 
   if (stream) {
-    qs += `${qs.length ? '&' : '?'}${
+    queryString += `${queryString.length ? '&' : '?'}${
       requestType === 'delete' ? 'stream' : 'streamIncludes'
     }=${encodeURI(stream)}`
   }
 
   if (start) {
-    qs += `${qs.length ? '&' : '?'}start=${start}`
+    queryString += `${queryString.length ? '&' : '?'}start=${start}`
   }
 
   if (end) {
-    qs += `${qs.length ? '&' : '?'}end=${end}`
+    queryString += `${queryString.length ? '&' : '?'}end=${end}`
   }
 
   if (stickers) {
     Object.keys(stickers).forEach((s: string) => {
-      qs += `${qs.length ? '&' : '?'}stickerIncludes[${s}]=${encodeURI(
-        stickers[s]
-      )}`
+      queryString += `${
+        queryString.length ? '&' : '?'
+      }stickerIncludes[${s}]=${encodeURI(stickers[s])}`
     })
   }
 
-  return qs
+  return queryString
 }
 
 export const getAnnotation = async (
-  data: GetAnnotationPayload
-): Promise<AxiosResponse<GetAnnotationResponse[]>> => {
-  const formattedQueryString = formatAnnotationQueryString(data)
+  annotation: GetAnnotationPayload
+): Promise<GetAnnotationResponse[]> => {
+  const formattedQueryString = formatAnnotationQueryString(annotation)
   const appendedURL = url + formattedQueryString
 
   const res = await axios.get(appendedURL)
@@ -73,20 +83,31 @@ export const getAnnotation = async (
     throw new Error(res.data?.message)
   }
 
-  return res.data
+  return res.data.map((anno: GetAnnotationResponse) => ({
+    stream: anno.stream,
+    annotations: anno.annotations,
+  }))
 }
 
 export const updateAnnotation = async (
   oldAnnotation: UpdateAnnotationPayload['old'],
   newAnnotation: UpdateAnnotationPayload['new']
-): Promise<AxiosResponse<Annotation>> => {
+): Promise<Annotation> => {
   const res = await axios.put(url, {old: oldAnnotation, new: newAnnotation})
 
   if (res.status >= 300) {
     throw new Error(res.data?.message)
   }
 
-  return res.data
+  const {start, end, summary, message, stickers, stream} = res.data
+  return {
+    start,
+    end,
+    summary,
+    message,
+    stickers,
+    stream,
+  }
 }
 
 export const deleteAnnotation = async (

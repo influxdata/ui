1fdf5df7f7c76df54c7a1d38551a6192286b6356
drdelambre <drdelambre>
2021-01-19 12:15:20 -0800

    fix: adding back the view

---
+++ src/visualization/types/Map/view.tsx
@@ -0,0 +1,54 @@
+// Libraries
+import React, {FC} from 'react'
+import {
+  Plot
+} from '@influxdata/giraffe'
+
+// Types
+import {GeoViewProperties} from 'src/types'
+import {VisProps} from 'src/visualization'
+
+interface Props extends VisProps {
+  properties: GaugeViewProperties
+}
+
+const GeoPlot: FC<Props> = ({
+    result,
+    properties
+}) => {
+  const {
+    layers,
+    zoom,
+    allowPanAndZoom,
+    detectCoordinateFields,
+    mapStyle,
+  } = properties
+  const tileServerConfiguration = {
+    tileServerUrl: '',
+    bingKey: '',
+  }
+
+  const {lat, lon} = properties.center
+
+  return (
+      <Plot config={{
+      table: result.table,
+    showAxes: false,
+    layers: [
+      {
+        type: 'geo',
+        lat,
+        lon,
+        zoom,
+        allowPanAndZoom,
+        detectCoordinateFields,
+        mapStyle,
+        layers,
+        tileServerConfiguration: tileServerConfiguration,
+      },
+    ],
+      }}/>
+  )
+}
+
+export default GeoPlot

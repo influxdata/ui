7e9fdddb5bc403f8b2c016727d8bd4752a0aa7d7
Jill R Pelavin <jpelavin@influxdata.com>
2021-01-28 17:05:00 -0500

    chore: replacing orientation threshold with toggles.        -not changing giraffe backend at all; just using a very high threshold to turn things to horizontal,        or a very low threshold (0) to turn things to vertical

--- src/timeMachine/components/view_options/LegendOrientation.tsx
+++ src/timeMachine/components/view_options/LegendOrientation.tsx
@@ -10,18 +10,19 @@ import {getActiveTimeMachine} from 'src/timeMachine/selectors'
 
 // Components
 import {
+  AlignItems,
+  Appearance,
+  ComponentColor,
   ComponentSize,
-  ComponentStatus,
-  Form,
-  Grid,
-  Input,
-  InputLabel,
-  InputType,
-  RangeSlider,
-  SlideToggle,
   FlexBox,
   FlexDirection,
-  AlignItems,
+  Form,
+  Grid,
+  InputLabel,
+  InputToggleType,
+  RangeSlider,
+  SlideToggle,
+  Toggle,
 } from '@influxdata/clockface'
 
 // Types
@@ -34,6 +35,9 @@ import {
   LEGEND_OPACITY_MINIMUM,
   LEGEND_OPACITY_STEP,
   LEGEND_COLORIZE_ROWS_DEFAULT,
+  LEGEND_ORIENTATION_THRESHOLD_DEFAULT,
+  LEGEND_ORIENTATION_THRESHOLD_HORIZONTAL,
+  LEGEND_ORIENTATION_THRESHOLD_VERTICAL,
 } from 'src/shared/constants'
 
 interface OwnProps {
@@ -45,6 +49,32 @@ interface OwnProps {
 type ReduxProps = ConnectedProps<typeof connector>
 type Props = OwnProps & ReduxProps
 
+const HORIZONTAL = 'horizontal'
+const VERTICAL = 'vertical'
+
+/**
+ *  The LegendOrientation Component consists of three properties:
+ *  1.  The Orientation itself (horizontal vs. vertical); horizontal is the default
+ *  2.  opacity of the legend (ranges from 20% to 100%); default is 100%
+ *  3.  row colorization:  does each row have colored text (the default), or does each row have a dot of color prefacing it, with
+ *  text all the same default (foreground) color
+ *
+ *  The above is what the user sees.
+ *
+ *  what is actually happening:
+ *
+ *    1.  Legend Orientation.  Default is horizontal
+ *          behind the scenes it is a 'rotation threshold'.  if the number of items is above the threshold, then
+ *          the legend is displayed vertically (each line is a column); if the number of items is below the threshold, then the legend is
+ *          displayed horizontally (each line is a row).  therefore, since the default is horizontal, the numbers being set are very high
+ *          for horizontal (high enough that the database would thrash out/throttle before it got that high), or low (0 for vertical, since
+ *          to display something it has to be higher than zero).  The default number itself has changed, so some users may see their graphs flip
+ *          (before; without the feature flag enabled, the default was hard-coded to 10; so graphs with more than 10 lines of  data in the tooltip
+ *          were vertical)
+ *
+ *    2.  Legend Opacity.  The actual number is between the minimum .2 and the maximum 1.0; but we are displaying it as a matter of percentage
+ *    3.  Row Colorization.  This is the simplest.  just a true/false flag, it is true by default.
+ */
 const LegendOrientation: FC<Props> = props => {
   const {
     legendOpacity,
@@ -55,11 +85,19 @@ const LegendOrientation: FC<Props> = props => {
     onLegendColorizeRowsChange,
   } = props
 
-  const [thresholdInputStatus, setThresholdInputStatus] = useState(
-    ComponentStatus.Default
-  )
-  const [thresholdInput, setThresholdInput] = useState(
-    legendOrientationThreshold
+  const getOrientation = threshold => {
+    switch (threshold) {
+      case LEGEND_ORIENTATION_THRESHOLD_HORIZONTAL:
+        return HORIZONTAL
+      case LEGEND_ORIENTATION_THRESHOLD_VERTICAL:
+        return VERTICAL
+      default:
+        return LEGEND_ORIENTATION_THRESHOLD_DEFAULT
+    }
+  }
+
+  const [legendOrientation, setLegendOrientation] = useState(
+    getOrientation(legendOrientationThreshold)
   )
   const [colorizeRowsInput, setColorizeRowsInput] = useState(legendColorizeRows)
 
@@ -67,16 +105,13 @@ const LegendOrientation: FC<Props> = props => {
     return null
   }
 
-  const handleSetThreshold = (event: ChangeEvent<HTMLInputElement>): void => {
-    const value = convertUserInputToNumOrNaN(event)
-
-    setThresholdInput(value)
-    if (isNaN(value)) {
-      setThresholdInputStatus(ComponentStatus.Error)
-    } else {
-      setThresholdInputStatus(ComponentStatus.Default)
-      onLegendOrientationThresholdChange(value)
+  const handleSetOrientation = (value: string): void => {
+    let threshold = LEGEND_ORIENTATION_THRESHOLD_HORIZONTAL
+    if (value === VERTICAL) {
+      threshold = LEGEND_ORIENTATION_THRESHOLD_VERTICAL
     }
+    setLegendOrientation(value)
+    onLegendOrientationThresholdChange(threshold)
   }
 
   const handleSetOpacity = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -103,23 +138,59 @@ const LegendOrientation: FC<Props> = props => {
   // can get numbers like 45.000009% which we want to avoid
   const percentLegendOpacity = (legendOpacity * 100).toFixed(0)
 
+  const orientationToggle = (
+    <FlexBox
+      direction={FlexDirection.Column}
+      margin={ComponentSize.Large}
+      alignItems={AlignItems.FlexStart}
+      style={{marginBottom: 12}}
+    >
+      <p>Legend Orientation</p>
+      <Toggle
+        tabIndex={1}
+        value="horizontal"
+        id="horizontal-legend-orientation"
+        name="horizontal"
+        checked={legendOrientation === HORIZONTAL}
+        onChange={() => handleSetOrientation(HORIZONTAL)}
+        type={InputToggleType.Radio}
+        size={ComponentSize.ExtraSmall}
+        color={ComponentColor['Primary']}
+        appearance={Appearance['Outline']}
+      >
+        <InputLabel
+          active={legendOrientation === 'horizontal'}
+          htmlFor="horizontal-legend-orientation"
+        >
+          Horizontal
+        </InputLabel>
+      </Toggle>
+      <Toggle
+        tabIndex={2}
+        value="vertical"
+        id="vertical-legend-orientation"
+        name="vertical"
+        checked={legendOrientation === VERTICAL}
+        onChange={() => handleSetOrientation(VERTICAL)}
+        type={InputToggleType.Radio}
+        size={ComponentSize.ExtraSmall}
+        color={ComponentColor['Primary']}
+        appearance={Appearance['Outline']}
+      >
+        <InputLabel
+          active={legendOrientation === 'vertical'}
+          htmlFor="vertical-legend-orientation"
+        >
+          Vertical
+        </InputLabel>
+      </Toggle>
+    </FlexBox>
+  )
+
   return (
     <Grid.Column>
       <h5 className="view-options--header">Legend</h5>
-      <Form.Element
-        label="Rotation Threshold"
-        helpText="Legends with column count exceeding this threshold appear vertically instead of horizontally"
-      >
-        <Input
-          name="legend-columns"
-          onChange={handleSetThreshold}
-          onFocus={handleSetThreshold}
-          placeholder="Enter a number"
-          status={thresholdInputStatus}
-          type={InputType.Number}
-          value={thresholdInput}
-        />
-      </Form.Element>
+      {orientationToggle}
       <Form.Element label={`Opacity: ${percentLegendOpacity}%`}>
         <RangeSlider
           max={LEGEND_OPACITY_MAXIMUM}

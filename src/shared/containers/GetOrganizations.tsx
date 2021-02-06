00b3a27e6d10dd6325935f712f08c49496152b3b
Gene Hynson <ghynson@influxdata.com>
2021-02-05 16:36:02 -0800

    fix: wasm download failures due to lazy loading signin (#615)

--- src/shared/containers/GetOrganizations.tsx
+++ src/shared/containers/GetOrganizations.tsx
@@ -1,13 +1,13 @@
 // Libraries
-import React, {useEffect, FunctionComponent} from 'react'
+import React, {useEffect, FunctionComponent, lazy, Suspense} from 'react'
 import {connect, ConnectedProps, useDispatch} from 'react-redux'
 import {Route, Switch} from 'react-router-dom'
 
 // Components
-import {SpinnerContainer, TechnoSpinner} from '@influxdata/clockface'
-import NoOrgsPage from 'src/organizations/containers/NoOrgsPage'
-import App from 'src/App'
-import NotFound from 'src/shared/components/NotFound'
+import PageSpinner from 'src/perf/components/PageSpinner'
+const NoOrgsPage = lazy(() => import('src/organizations/containers/NoOrgsPage'))
+const App = lazy(() => import('src/App'))
+const NotFound = lazy(() => import('src/shared/components/NotFound'))
 
 // Types
 import {RemoteDataState, AppState} from 'src/types'
@@ -28,14 +28,16 @@ const GetOrganizations: FunctionComponent<Props> = ({status}) => {
   }, [dispatch, status])
 
   return (
-    <SpinnerContainer loading={status} spinnerComponent={<TechnoSpinner />}>
-      <Switch>
-        <Route path="/no-orgs" component={NoOrgsPage} />
-        <Route path="/orgs" component={App} />
-        <Route exact path="/" component={RouteToOrg} />
-        <Route component={NotFound} />
-      </Switch>
-    </SpinnerContainer>
+    <PageSpinner loading={status}>
+      <Suspense fallback={<PageSpinner />}>
+        <Switch>
+          <Route path="/no-orgs" component={NoOrgsPage} />
+          <Route path="/orgs" component={App} />
+          <Route exact path="/" component={RouteToOrg} />
+          <Route component={NotFound} />
+        </Switch>
+      </Suspense>
+    </PageSpinner>
   )
 }
 

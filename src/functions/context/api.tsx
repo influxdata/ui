5dda02d20c9732d5f8206d827046be31854a0eaf
Deniz Kusefoglu <deniz@influxdata.com>
2021-02-17 19:00:12 -0800

    feat(managed-functions): Remove flag check at API level since Alex made fun of them

--- src/functions/context/api.tsx
+++ src/functions/context/api.tsx
@@ -6,68 +6,58 @@ import {
   postApiV2FunctionsTrigger,
   FunctionTriggerRequest,
 } from 'src/client/managedFunctionsRoutes'
-import {isFlagEnabled} from 'src/shared/utils/featureFlag'
 
 export const createAPI = async (functionCreate: FunctionCreateRequest) => {
-  if (isFlagEnabled('managed-functions')) {
-    const res = await postApiV2Function({data: functionCreate})
+  const res = await postApiV2Function({data: functionCreate})
 
-    if (res.status != 201) {
-      throw new Error(res.data.message)
-    }
-    return res.data
+  if (res.status != 201) {
+    throw new Error(res.data.message)
   }
+  return res.data
 }
 
 export const deleteAPI = async (id: string) => {
-  if (isFlagEnabled('managed-functions')) {
-    const res = await deleteApiV2Function({functionID: id})
+  const res = await deleteApiV2Function({functionID: id})
 
-    if (res.status != 204) {
-      throw new Error(res.data.message)
-    }
+  if (res.status != 204) {
+    throw new Error(res.data.message)
   }
 }
 
 export const getAllAPI = async (orgID: string) => {
-  if (isFlagEnabled('managed-functions')) {
-    const res = await getApiV2Functions({query: {orgID}})
-    if (res.status != 200) {
-      //  throw new Error(res.data.message)
-      return {
-        functions: [
-          {
-            name: 'functionb',
-            id: '1',
-            orgID: '0',
-            script: 'this is a script',
-            url: 'www.url.com',
-            description: 'best function ever',
-          },
-          {
-            name: 'functiona',
-            id: '2',
-            orgID: '0',
-            script: 'this is another script',
-            url: 'www.url.com',
-            description: 'second best function ever',
-          },
-        ],
-      }
-    } else {
-      return res.data
+  const res = await getApiV2Functions({query: {orgID}})
+  if (res.status != 200) {
+    //  throw new Error(res.data.message)
+    return {
+      functions: [
+        {
+          name: 'functionb',
+          id: '1',
+          orgID: '0',
+          script: 'this is a script',
+          url: 'www.url.com',
+          description: 'best function ever',
+        },
+        {
+          name: 'functiona',
+          id: '2',
+          orgID: '0',
+          script: 'this is another script',
+          url: 'www.url.com',
+          description: 'second best function ever',
+        },
+      ],
     }
+  } else {
+    return res.data
   }
-  return {}
 }
 
 export const triggerAPI = async (triggerRequest: FunctionTriggerRequest) => {
-  if (isFlagEnabled('managed-functions')) {
-    const res = await postApiV2FunctionsTrigger({data: triggerRequest})
+  const res = await postApiV2FunctionsTrigger({data: triggerRequest})
 
-    if (res.status != 200) {
-      throw new Error(res.data.message)
-    }
-    return res.data
+  if (res.status != 200) {
+    throw new Error(res.data.message)
   }
+  return res.data
 }

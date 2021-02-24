77f96a1da0a5e5c0088d683f9deebeaa39403a37
Gene Hynson <ghynson@influxdata.com>
2021-02-24 08:32:20 -0800

    test: use global variables for bucket names

--- cypress/e2e/cloud/buckets.test.ts
+++ cypress/e2e/cloud/buckets.test.ts
@@ -16,85 +16,105 @@ describe('Buckets', () => {
 
   // TODO: Skipping this until we can sort out the differences between OSS and Cloud
   it('can sort by name and retention', () => {
-    const demoDataBucket = 'Website Monitoring Bucket'
-    const buckets = [demoDataBucket, 'defbuck', '_tasks', '_monitoring']
-    const retentionDesc = ['_monitoring', demoDataBucket, '_tasks', 'defbuck']
-    const retentionAsc = ['defbuck', '_tasks', '_monitoring', demoDataBucket]
-
-    // if demo data bucket doesn't exist, create a bucket with the same name
-    cy.getByTestID('resource-list').then($body => {
-      if (
-        $body.find(`[data-testid="bucket-card ${demoDataBucket}"]`).length === 0
-      ) {
-        cy.getByTestID(`bucket-card ${demoDataBucket}`).should('not.exist')
-        cy.getByTestID('Create Bucket').click()
-        cy.getByTestID('overlay--container').within(() => {
-          cy.getByInputName('name').type(demoDataBucket)
-          cy.getByTestID('retention-intervals--button').click()
-          cy.getByTestID('duration-selector--button').click()
-          cy.getByTestID('duration-selector--7d')
-            .click()
-            .then(() => {
-              cy.getByTestID('bucket-form-submit').click()
-            })
-        })
-      }
-    })
-
-    cy.getByTestID('resource-sorter--button')
-      .click()
-      .then(() => {
-        cy.getByTestID('resource-sorter--name-desc').click()
-      })
-      .then(() => {
-        cy.get('[data-testid*="bucket-card"]').each((val, index) => {
-          const testID = val.attr('data-testid')
-          expect(testID).to.include(buckets[index])
-        })
-      })
-
-    cy.getByTestID('resource-sorter--button')
-      .click()
-      .then(() => {
-        cy.getByTestID(
-          'resource-sorter--retentionRules[0].everySeconds-desc'
-        ).click()
-      })
-      .then(() => {
-        cy.get('[data-testid*="bucket-card"]').each((val, index) => {
-          const testID = val.attr('data-testid')
-          expect(testID).to.include(retentionDesc[index])
-        })
-      })
-
-    cy.getByTestID('resource-sorter--button')
-      .click()
-      .then(() => {
-        cy.getByTestID(
-          'resource-sorter--retentionRules[0].everySeconds-asc'
-        ).click()
-      })
-      .then(() => {
-        cy.get('[data-testid*="bucket-card"]').each((val, index) => {
-          const testID = val.attr('data-testid')
-          expect(testID).to.include(retentionAsc[index])
-        })
-      })
-
-    // assert buckets amount
-    cy.get('.cf-resource-card').should('have.length', 4)
-
-    // filter a bucket
-    cy.getByTestID('search-widget').type('def')
-
     cy.get<string>('@defaultBucket').then((defaultBucket: string) => {
-      cy.get('.cf-resource-card')
-        .should('have.length', 1)
-        .should('contain', defaultBucket)
-    })
+      const demoDataBucket = 'Website Monitoring Bucket'
+      const tasksBucket = '_tasks'
+      const monitoringBucket = '_monitoring'
+      const buckets = [
+        demoDataBucket,
+        defaultBucket,
+        tasksBucket,
+        monitoringBucket,
+      ]
+      const retentionDesc = [
+        monitoringBucket,
+        demoDataBucket,
+        tasksBucket,
+        defaultBucket,
+      ]
+      const retentionAsc = [
+        defaultBucket,
+        tasksBucket,
+        monitoringBucket,
+        demoDataBucket,
+      ]
 
-    // clear filter and assert all buckets are visible
-    cy.getByTestID('search-widget').clear()
-    cy.get('.cf-resource-card').should('have.length', 4)
+      // if demo data bucket doesn't exist, create a bucket with the same name
+      cy.getByTestID('resource-list').then($body => {
+        if (
+          $body.find(`[data-testid="bucket-card ${demoDataBucket}"]`).length ===
+          0
+        ) {
+          cy.getByTestID(`bucket-card ${demoDataBucket}`).should('not.exist')
+          cy.getByTestID('Create Bucket').click()
+          cy.getByTestID('overlay--container').within(() => {
+            cy.getByInputName('name').type(demoDataBucket)
+            cy.getByTestID('retention-intervals--button').click()
+            cy.getByTestID('duration-selector--button').click()
+            cy.getByTestID('duration-selector--7d')
+              .click()
+              .then(() => {
+                cy.getByTestID('bucket-form-submit').click()
+              })
+          })
+        }
+      })
+
+      cy.getByTestID('resource-sorter--button')
+        .click()
+        .then(() => {
+          cy.getByTestID('resource-sorter--name-desc').click()
+        })
+        .then(() => {
+          cy.get('[data-testid*="bucket-card"]').each((val, index) => {
+            const testID = val.attr('data-testid')
+            expect(testID).to.include(buckets[index])
+          })
+        })
+
+      cy.getByTestID('resource-sorter--button')
+        .click()
+        .then(() => {
+          cy.getByTestID(
+            'resource-sorter--retentionRules[0].everySeconds-desc'
+          ).click()
+        })
+        .then(() => {
+          cy.get('[data-testid*="bucket-card"]').each((val, index) => {
+            const testID = val.attr('data-testid')
+            expect(testID).to.include(retentionDesc[index])
+          })
+        })
+
+      cy.getByTestID('resource-sorter--button')
+        .click()
+        .then(() => {
+          cy.getByTestID(
+            'resource-sorter--retentionRules[0].everySeconds-asc'
+          ).click()
+        })
+        .then(() => {
+          cy.get('[data-testid*="bucket-card"]').each((val, index) => {
+            const testID = val.attr('data-testid')
+            expect(testID).to.include(retentionAsc[index])
+          })
+        })
+
+      // assert buckets amount
+      cy.get('.cf-resource-card').should('have.length', 4)
+
+      // filter a bucket
+      cy.getByTestID('search-widget').type('def')
+
+      cy.get<string>('@defaultBucket').then((defaultBucket: string) => {
+        cy.get('.cf-resource-card')
+          .should('have.length', 1)
+          .should('contain', defaultBucket)
+      })
+
+      // clear filter and assert all buckets are visible
+      cy.getByTestID('search-widget').clear()
+      cy.get('.cf-resource-card').should('have.length', 4)
+    })
   })
 })

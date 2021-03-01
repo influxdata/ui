127344770cd81b463bd4164e3bfd52066d898f4b
Alex Boatwright <drdelambre@gmail.com>
2021-03-01 15:19:07 -0800

    feat: remove the feedback cta (#723)

--- src/flows/components/FlowsIndex.tsx
+++ src/flows/components/FlowsIndex.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 
 // Components
-import {Page, PageHeader, Button, IconFont} from '@influxdata/clockface'
+import {Page, PageHeader} from '@influxdata/clockface'
 import FlowCreateButton from 'src/flows/components/FlowCreateButton'
 import FlowListProvider from 'src/flows/context/flow.list'
 import FlowCards from 'src/flows/components/FlowCards'
@@ -12,13 +12,6 @@ import {pageTitleSuffixer} from 'src/shared/utils/pageTitles'
 import {PROJECT_NAME_PLURAL} from 'src/flows'
 
 const FlowsIndex = () => {
-  const handleFeedbackClick = (): void => {
-    window.open(
-      'https://w2.influxdata.com/feedback-notebooks-early-access/',
-      '_blank'
-    )
-  }
-
   return (
     <FlowListProvider>
       <Page
@@ -29,14 +22,6 @@ const FlowsIndex = () => {
           <Page.Title title={PROJECT_NAME_PLURAL} />
         </PageHeader>
         <Page.ControlBar fullWidth={false}>
-          <Page.ControlBarLeft>
-            <Button
-              onClick={handleFeedbackClick}
-              text="Tell us your thoughts"
-              icon={IconFont.Chat}
-            />
-            <p className="flows-early-access-feedback">{`You've been given early access to ${PROJECT_NAME_PLURAL}, your feedback is a gift`}</p>
-          </Page.ControlBarLeft>
           <Page.ControlBarRight>
             <FlowCreateButton />
           </Page.ControlBarRight>

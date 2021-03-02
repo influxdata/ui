9fc84893d947303df06807459849c8468e497a3c
Jill R Pelavin <jpelavin@influxdata.com>
2021-03-02 14:50:05 -0500

    feat: add toggle/checkbox to enable 1-click annotations   -added toggle; not connected yet tho

--- src/annotations/components/controlBar/AnnotationsControlBar.tsx
+++ src/annotations/components/controlBar/AnnotationsControlBar.tsx
@@ -1,16 +1,20 @@
 // Libraries
-import React, {FC} from 'react'
+import React, {useState, FC} from 'react'
 import {useSelector} from 'react-redux'
 import {useHistory, useParams} from 'react-router-dom'
 
 // Components
 import {
+  ComponentColor,
   ComponentSize,
   FlexBox,
   FlexBoxChild,
   IconFont,
+  InputLabel,
+  InputToggleType,
   JustifyContent,
   SquareButton,
+  Toggle,
 } from '@influxdata/clockface'
 import ErrorBoundary from 'src/shared/components/ErrorBoundary'
 import Toolbar from 'src/shared/components/toolbar/Toolbar'
@@ -28,8 +32,15 @@ export const AnnotationsControlBar: FC = () => {
   const isVisible = useSelector(getAnnotationControlsVisibility)
   const {orgID} = useParams<{orgID: string; dashboardID: string}>()
 
+  const [annotationWriteMode, setAnnotationWriteMode] = useState(false)
+
   if (!isVisible) {
     return null
+  }
+
+  const onModeChange = () => {
+    //todo:  call prop method to change the state
+    setAnnotationWriteMode(!annotationWriteMode)
   }
 
   const handleSettingsClick = (): void => {
@@ -50,6 +61,18 @@ export const AnnotationsControlBar: FC = () => {
           <AnnotationPills />
         </FlexBoxChild>
         <FlexBox margin={ComponentSize.Small}>
+          <Toggle
+            style={{marginRight: 20}}
+            id="enableAnnotationMode"
+            type={InputToggleType.Checkbox}
+            checked={annotationWriteMode}
+            onChange={onModeChange}
+            color={ComponentColor.Primary}
+            size={ComponentSize.ExtraSmall}
+          >
+            {' '}
+            <InputLabel>Enable 1-Click Annotations</InputLabel>
+          </Toggle>
           <SquareButton
             testID="annotations-control-bar--settings"
             icon={IconFont.CogThick}

5ef61c32e88488f712c3f11519bba2a1cb55961c
Chitlange Sahas <chitlangesahas@gmail.com>
2021-03-08 10:28:30 -0700

    feat: graph tips shows annotation tip if feature flag enabled (#792)

--- src/shared/components/graph_tips/GraphTips.tsx
+++ src/shared/components/graph_tips/GraphTips.tsx
@@ -3,6 +3,7 @@ import React, {FC} from 'react'
 
 // Components
 import {ComponentColor, QuestionMarkTooltip} from '@influxdata/clockface'
+import {FeatureFlag} from '../../utils/featureFlag'
 
 const GraphTips: FC = () => (
   <QuestionMarkTooltip
@@ -16,6 +17,10 @@ const GraphTips: FC = () => (
           <code>Click + Drag</code> Zoom in (X or Y)
           <br />
           <code>Double Click</code> Reset Graph Window
+          <br />
+          <FeatureFlag name="annotations">
+            <code>Single Click</code> Annotate the Graph
+          </FeatureFlag>
         </p>
       </>
     }

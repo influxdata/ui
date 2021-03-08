e3f11b23e1e89255c1e434ec02bf8566692ab93f
ChitlangeSahas <chitlangesahas@gmail.com>
2021-03-08 06:32:06 -0700

    feat: default streams are now visible in Annotation bar on page load

--- src/annotations/constants/mocks.ts
+++ src/annotations/constants/mocks.ts
@@ -26,6 +26,29 @@ export interface AnnotationStream {
 
 export const MOCK_ANNOTATION_STREAMS: AnnotationStream[] = [
   {
+    id: 'default',
+    name: 'Default',
+    description: 'Default Annotation stream',
+    meta: {
+      createdAt: '2d ago',
+      updatedAt: '6h ago',
+    },
+    display: {
+      color: '#8e1fc3',
+      summaryCol: 'summary',
+      messageCol: 'message',
+      timeStartCol: '_timeStart',
+      timeStopCol: '_timeStop',
+    },
+    query: {
+      tags: {
+        _field: 'deployments',
+      },
+      measurement: 'annotations',
+      bucketName: 'defBuck',
+    },
+  },
+  {
     id: 'anno1',
     name: 'Deployments',
     description: 'Event data for deployments',

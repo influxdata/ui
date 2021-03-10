7e7d768b39120ddec5d9def0ea512bb516fffe4e
Gene Hynson <ghynson@influxdata.com>
2021-03-09 17:26:51 -0800

    test: fix broken querybuilder test for toggle switch

--- cypress/e2e/shared/queryBuilder.test.ts
+++ cypress/e2e/shared/queryBuilder.test.ts
@@ -125,8 +125,9 @@ describe('The Query Builder', () => {
       cy.getByTestID('auto-window-period').click()
 
       cy.getByTestID('duration-input--error').should('not.exist')
-
-      cy.getByTestID('toggle').click()
+      cy.getByTestID('aggregation-selector').within(() => {
+        cy.getByTestID('toggle').click()
+      })
       cy.getByTestID('switch-to-script-editor').click()
       cy.contains(
         '|> aggregateWindow(every: v.windowPeriod, fn: mean, createEmpty: true)'

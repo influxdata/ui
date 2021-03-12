4acd6bfb7384e9c7293d02f3625215a254bebadd
ChitlangeSahas <chitlangesahas@gmail.com>
2021-03-11 21:26:35 -0700

    refactor: update single stat in UI to use giraffe's single stat

--- src/visualization/types/SingleStat/view.tsx
+++ src/visualization/types/SingleStat/view.tsx
@@ -2,15 +2,14 @@
 import React, {FC} from 'react'
 
 // Utils
-import LatestValueTransform from 'src/visualization/components/LatestValueTransform'
 import {generateThresholdsListHexs} from 'src/shared/constants/colorOperations'
-import {formatStatValue} from 'src/visualization/utils/formatStatValue'
 
 // Types
 import {SingleStatViewProperties} from 'src/types/dashboards'
 import {VisualizationProps} from 'src/visualization'
 
-import './style.scss'
+import {Config, Plot} from '@influxdata/giraffe'
+import {latestValues as getLatestValues} from '../../../shared/utils/latestValues'
 
 interface Props extends VisualizationProps {
   properties: SingleStatViewProperties
@@ -19,54 +18,38 @@ interface Props extends VisualizationProps {
 const SingleStat: FC<Props> = ({properties, result}) => {
   const {prefix, suffix, colors, decimalPlaces} = properties
 
-  return (
-    <LatestValueTransform table={result.table} allowString={true}>
-      {latestValue => {
-        const {
-          bgColor: backgroundColor,
-          textColor,
-        } = generateThresholdsListHexs({
-          colors,
-          lastValue: latestValue,
-          cellType: 'single-stat',
-        })
+  const latestValues = getLatestValues(result.table)
+  const latestValue = latestValues[0]
 
-        const formattedValue = formatStatValue(latestValue, {
-          decimalPlaces,
-          prefix,
-          suffix,
-        })
-        return (
-          <div
-            className="single-stat"
-            style={{backgroundColor}}
-            data-testid="single-stat"
-          >
-            <div className="single-stat--resizer">
-              <svg
-                width="100%"
-                height="100%"
-                viewBox={`0 0 ${formattedValue.length * 55} 100`}
-              >
-                <text
-                  className="single-stat--text"
-                  data-testid="single-stat--text"
-                  fontSize="100"
-                  y="59%"
-                  x="50%"
-                  dominantBaseline="middle"
-                  textAnchor="middle"
-                  style={{fill: textColor}}
-                >
-                  {formattedValue}
-                </text>
-              </svg>
-            </div>
-          </div>
-        )
-      }}
-    </LatestValueTransform>
-  )
+  const {bgColor: backgroundColor, textColor} = generateThresholdsListHexs({
+    colors,
+    lastValue: latestValue,
+    cellType: 'single-stat',
+  })
+
+  const config: Config = {
+    table: result.table,
+    showAxes: false,
+    layers: [
+      {
+        type: 'single stat',
+        prefix,
+        suffix,
+        decimalPlaces: decimalPlaces,
+        textColor: textColor,
+        textOpacity: 100,
+        backgroundColor: backgroundColor? backgroundColor : '',
+        svgTextStyle: {
+          fontSize: '100',
+          fontWeight: 'lighter',
+          dominantBaseline: 'middle',
+          textAnchor: 'middle',
+          letterSpacing: '-0.05em',
+        },
+      },
+    ],
+  }
+  return <Plot config={config} />
 }
 
 export default SingleStat

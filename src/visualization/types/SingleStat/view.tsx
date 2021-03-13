ffc0bd7252468fff8eb70e90eb85fe3b4c27f8ca
ChitlangeSahas <chitlangesahas@gmail.com>
2021-03-13 10:29:20 -0700

    feat: use single stat from giraffe in singleStatWithLine

--- src/visualization/types/SingleStat/view.tsx
+++ src/visualization/types/SingleStat/view.tsx
@@ -11,62 +11,105 @@ import {SingleStatViewProperties} from 'src/types/dashboards'
 import {VisualizationProps} from 'src/visualization'
 
 import './style.scss'
+import {latestValues as getLatestValues} from '../../../shared/utils/latestValues'
+import {Config, Plot} from '@influxdata/giraffe'
 
 interface Props extends VisualizationProps {
   properties: SingleStatViewProperties
 }
 
+// Temporary feature flag, replace with actual feature flag logic
+const FEATURE_FLAG_ENABLED = true
+
 const SingleStat: FC<Props> = ({properties, result}) => {
   const {prefix, suffix, colors, decimalPlaces} = properties
 
-  return (
-    <LatestValueTransform table={result.table} allowString={true}>
-      {latestValue => {
-        const {
-          bgColor: backgroundColor,
-          textColor,
-        } = generateThresholdsListHexs({
-          colors,
-          lastValue: latestValue,
-          cellType: 'single-stat',
-        })
+  if (FEATURE_FLAG_ENABLED) {
+    const latestValues = getLatestValues(result.table)
+    const latestValue = latestValues[0]
 
-        const formattedValue = formatStatValue(latestValue, {
-          decimalPlaces,
+    const {bgColor: backgroundColor, textColor} = generateThresholdsListHexs({
+      colors,
+      lastValue: latestValue,
+      cellType: 'single-stat',
+    })
+    const config: Config = {
+      table: result.table,
+      showAxes: false,
+      layers: [
+        {
+          type: 'single stat',
           prefix,
           suffix,
-        })
-        return (
-          <div
-            className="single-stat"
-            style={{backgroundColor}}
-            data-testid="single-stat"
-          >
-            <div className="single-stat--resizer">
-              <svg
-                width="100%"
-                height="100%"
-                viewBox={`0 0 ${formattedValue.length * 55} 100`}
-              >
-                <text
-                  className="single-stat--text"
-                  data-testid="single-stat--text"
-                  fontSize="100"
-                  y="59%"
-                  x="50%"
-                  dominantBaseline="middle"
-                  textAnchor="middle"
-                  style={{fill: textColor}}
+          decimalPlaces: decimalPlaces,
+          textColor: textColor,
+          textOpacity: 100,
+          backgroundColor: backgroundColor ? backgroundColor : '',
+          svgTextStyle: {
+            fontSize: '100',
+            fontWeight: 'lighter',
+            dominantBaseline: 'middle',
+            textAnchor: 'middle',
+            letterSpacing: '-0.05em',
+          },
+          svgTextAttributes: {
+            'data-testid': 'single-stat--text',
+          },
+        },
+      ],
+    }
+    return <Plot config={config} />
+  }
+  else {
+    return (
+      <LatestValueTransform table={result.table} allowString={true}>
+        {latestValue => {
+          const {
+            bgColor: backgroundColor,
+            textColor,
+          } = generateThresholdsListHexs({
+            colors,
+            lastValue: latestValue,
+            cellType: 'single-stat',
+          })
+
+          const formattedValue = formatStatValue(latestValue, {
+            decimalPlaces,
+            prefix,
+            suffix,
+          })
+          return (
+            <div
+              className="single-stat"
+              style={{backgroundColor}}
+              data-testid="single-stat"
+            >
+              <div className="single-stat--resizer">
+                <svg
+                  width="100%"
+                  height="100%"
+                  viewBox={`0 0 ${formattedValue.length * 55} 100`}
                 >
-                  {formattedValue}
-                </text>
-              </svg>
+                  <text
+                    className="single-stat--text"
+                    data-testid="single-stat--text"
+                    fontSize="100"
+                    y="59%"
+                    x="50%"
+                    dominantBaseline="middle"
+                    textAnchor="middle"
+                    style={{fill: textColor}}
+                  >
+                    {formattedValue}
+                  </text>
+                </svg>
+              </div>
             </div>
-          </div>
-        )
-      }}
-    </LatestValueTransform>
-  )
+          )
+        }}
+      </LatestValueTransform>
+    )
+  }
 }
 
 export default SingleStat

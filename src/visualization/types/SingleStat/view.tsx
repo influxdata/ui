d79a8176e0537cfe188f89caee2fef0ffde92fd5
ChitlangeSahas <chitlangesahas@gmail.com>
2021-03-12 18:52:54 -0700

    fix: added feature flag code.

--- src/visualization/types/SingleStat/view.tsx
+++ src/visualization/types/SingleStat/view.tsx
@@ -11,49 +11,104 @@ import {VisualizationProps} from 'src/visualization'
 import {Config, Plot} from '@influxdata/giraffe'
 
 import {latestValues as getLatestValues} from '../../../shared/utils/latestValues'
+import LatestValueTransform from '../../components/LatestValueTransform'
+import {formatStatValue} from '../../utils/formatStatValue'
 
 interface Props extends VisualizationProps {
   properties: SingleStatViewProperties
 }
 
+// TODO: temporary feature flag variable, replace later.
+const FEATURE_FLAG_ENABLED = true
+
 const SingleStat: FC<Props> = ({properties, result}) => {
   const {prefix, suffix, colors, decimalPlaces} = properties
 
-  const latestValues = getLatestValues(result.table)
-  const latestValue = latestValues[0]
+  if (FEATURE_FLAG_ENABLED) {
+    const latestValues = getLatestValues(result.table)
+    const latestValue = latestValues[0]
 
-  const {bgColor: backgroundColor, textColor} = generateThresholdsListHexs({
-    colors,
-    lastValue: latestValue,
-    cellType: 'single-stat',
-  })
+    const {bgColor: backgroundColor, textColor} = generateThresholdsListHexs({
+      colors,
+      lastValue: latestValue,
+      cellType: 'single-stat',
+    })
+    const config: Config = {
+      table: result.table,
+      showAxes: false,
+      layers: [
+        {
+          type: 'single stat',
+          prefix,
+          suffix,
+          decimalPlaces: decimalPlaces,
+          textColor: textColor,
+          textOpacity: 100,
+          backgroundColor: backgroundColor ? backgroundColor : '',
+          svgTextStyle: {
+            fontSize: '100',
+            fontWeight: 'lighter',
+            dominantBaseline: 'middle',
+            textAnchor: 'middle',
+            letterSpacing: '-0.05em',
+          },
+          svgTextAttributes: {
+            'data-testid': 'single-stat--text',
+          },
+        },
+      ],
+    }
+    return <Plot config={config} />
+  } else {
+    return (
+      <LatestValueTransform table={result.table} allowString={true}>
+        {latestValue => {
+          const {
+            bgColor: backgroundColor,
+            textColor,
+          } = generateThresholdsListHexs({
+            colors,
+            lastValue: latestValue,
+            cellType: 'single-stat',
+          })
 
-  const config: Config = {
-    table: result.table,
-    showAxes: false,
-    layers: [
-      {
-        type: 'single stat',
-        prefix,
-        suffix,
-        decimalPlaces: decimalPlaces,
-        textColor: textColor,
-        textOpacity: 100,
-        backgroundColor: backgroundColor ? backgroundColor : '',
-        svgTextStyle: {
-          fontSize: '100',
-          fontWeight: 'lighter',
-          dominantBaseline: 'middle',
-          textAnchor: 'middle',
-          letterSpacing: '-0.05em',
-        },
-        svgTextAttributes: {
-          'data-testid': 'single-stat--text',
-        },
-      },
-    ],
+          const formattedValue = formatStatValue(latestValue, {
+            decimalPlaces,
+            prefix,
+            suffix,
+          })
+          return (
+            <div
+              className="single-stat"
+              style={{backgroundColor}}
+              data-testid="single-stat"
+            >
+              <div className="single-stat--resizer">
+                <svg
+                  width="100%"
+                  height="100%"
+                  viewBox={`0 0 ${formattedValue.length * 55} 100`}
+                >
+                  <text
+                    className="single-stat--text"
+                    data-testid="single-stat--text"
+                    fontSize="100"
+                    y="59%"
+                    x="50%"
+                    dominantBaseline="middle"
+                    textAnchor="middle"
+                    style={{fill: textColor}}
+                  >
+                    {formattedValue}
+                  </text>
+                </svg>
+              </div>
+            </div>
+          )
+        }}
+      </LatestValueTransform>
+    )
   }
-  return <Plot config={config} />
 }
 
 export default SingleStat

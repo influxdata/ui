209bd7b94f857505cea415cb135d22fda0b0a295
Palak Bhojani <palak@palaks-mbp-2.lan>
2021-03-16 16:13:02 -0700

    feat: update ui to request map token from map service

---
+++ src/visualization/types/Map/api.ts
@@ -0,0 +1,13 @@
+// Libraries
+import axios from 'axios'
+
+// Constants
+import { API_BASE_PATH } from 'src/shared/constants'
+
+// URL
+const url = `${API_BASE_PATH}api/v2/maps/mapToken`
+
+export const getMapToken = async () => {
+  const res = await axios.get(url)
+  return res.data ?? {}
+}
--- src/visualization/types/Map/view.tsx
+++ src/visualization/types/Map/view.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import React, {FC} from 'react'
+import React, {FC, useEffect, useState} from 'react'
 import {Plot} from '@influxdata/giraffe'
 
 // Types
@@ -8,9 +8,15 @@ import {VisualizationProps} from 'src/visualization'
 
 // Utils
 import {getGeoCoordinates} from 'src/shared/utils/vis'
+import {getMapToken} from './api'
 
 interface Props extends VisualizationProps {
   properties: GeoViewProperties
+}
+
+type Coordinates = {
+  lat: number
+  lon: number
 }
 
 const GeoPlot: FC<Props> = ({result, properties}) => {
@@ -21,51 +27,97 @@ const GeoPlot: FC<Props> = ({result, properties}) => {
     detectCoordinateFields,
     mapStyle,
   } = properties
-  const tileServerConfiguration = {
-    tileServerUrl: '',
-    bingKey: '',
-  }
 
   const {lat, lon} = properties.center
 
-  let calculatedGeoCoordinates = {
+  const [errorCode, setErrorCode] = useState<string>('')
+  const [mapToken, setMapToken] = useState<string>('')
+  const [geoCoordinates, setGeoCoordinates] = useState<Coordinates>({
     lat,
     lon,
-  }
-  try {
-    calculatedGeoCoordinates = getGeoCoordinates(result.table, 0)
-  } catch (err) {
-    const error =
-      'Map type is not supported with the data provided: Missing latitude/longitude values'
+  })
 
-    return (
-      <div className="panel-resizer--error" data-testid="geoplot-error">
-        {error}
-      </div>
-    )
+  useEffect(() => {
+    const getToken = async () => {
+      try {
+        const {token} = await getMapToken()
+        setMapToken(token)
+      } catch (err) {
+        setErrorCode('MapServiceUnavailable')
+      }
+    }
+    const getCoordinates = () => {
+      let calculatedGeoCoordinates = {
+        lat,
+        lon,
+      }
+      try {
+        calculatedGeoCoordinates = getGeoCoordinates(result.table, 0)
+        setGeoCoordinates(calculatedGeoCoordinates)
+      } catch (err) {
+        setErrorCode('InvalidCoordinates')
+      }
+    }
+    getCoordinates()
+    getToken()
+  }, [])
+
+  let error = ''
+
+  const getMapboxUrl = () => {
+    if (mapToken) {
+      return `https://api.mapbox.com/styles/v1/influxdata/cklspghhd1opp17mmz0yk7rcy/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaW5mbHV4ZGF0YSIsImEiOiJja2hndnM0eWQwNTVtMzRwZnd5cmQxNHhoIn0.IEAjIguiM67ql4mhiD88rw`
+    }
+    return ''
   }
 
-  return (
-    <Plot
-      config={{
-        table: result.table,
-        showAxes: false,
-        layers: [
-          {
-            type: 'geo',
-            lat: calculatedGeoCoordinates.lat,
-            lon: calculatedGeoCoordinates.lon,
-            zoom,
-            allowPanAndZoom,
-            detectCoordinateFields,
-            mapStyle,
-            layers,
-            tileServerConfiguration: tileServerConfiguration,
-          },
-        ],
-      }}
-    />
-  )
+  switch (errorCode) {
+    case 'MapServiceUnavailable':
+      error =
+        'Map type is having issues connecting to the server. Please try again later.'
+
+      return (
+        <div className="panel-resizer--error" data-testid="geoplot-error">
+          {error}
+        </div>
+      )
+    case 'InvalidCoordinates':
+      error =
+        'Map type is not supported with the data provided: Missing latitude/longitude values'
+
+      return (
+        <div className="panel-resizer--error" data-testid="geoplot-error">
+          {error}
+        </div>
+      )
+    default:
+      const tileServerConfiguration = {
+        tileServerUrl: getMapboxUrl(),
+        bingKey: '',
+      }
+
+      return (
+        <Plot
+          config={{
+            table: result.table,
+            showAxes: false,
+            layers: [
+              {
+                type: 'geo',
+                lat: geoCoordinates.lat,
+                lon: geoCoordinates.lon,
+                zoom,
+                allowPanAndZoom,
+                detectCoordinateFields,
+                mapStyle,
+                layers,
+                tileServerConfiguration: tileServerConfiguration,
+              },
+            ],
+          }}
+        />
+      )
+  }
 }
 
 export default GeoPlot

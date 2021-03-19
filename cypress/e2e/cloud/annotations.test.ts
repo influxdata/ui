221a61b1784a9b99c50ecfd69c394923c83d5339
ChitlangeSahas <chitlangesahas@gmail.com>
2021-03-19 09:20:02 -0700

    test: rename tests

--- cypress/e2e/cloud/annotations.test.ts
+++ cypress/e2e/cloud/annotations.test.ts
@@ -106,36 +106,36 @@ describe('The Annotations UI functionality', () => {
     // the pill should be back
     cy.getByTestID('annotation-pill default').should('exist')
   })
-  it('Clicking the graph with the control bar open creates an annotation', () => {
+  it('can create an annotation when graph is clicked and the control bar is open', () => {
     cy.getByTestID('cell blah').within(() => {
-        cy.getByTestID('giraffe-inner-plot').click()
-      })
-      cy.getByTestID('overlay--container').within(() => {
-        cy.getByTestID('textarea')
-          .should('be.visible')
-          .click()
-          .focused()
-          .type('yoho')
-        cy.getByTestID('add-annotation-submit').click()
-      })
-      // we need to see if the annotations got created and that the tooltip says "yoho"
+      cy.getByTestID('giraffe-inner-plot').click()
+    })
+    cy.getByTestID('overlay--container').within(() => {
+      cy.getByTestID('textarea')
+        .should('be.visible')
+        .click()
+        .focused()
+        .type('yoho')
+      cy.getByTestID('add-annotation-submit').click()
+    })
+    // we need to see if the annotations got created and that the tooltip says "yoho"
   })
 
-  it('Clicking the graph with the control bar closed creates an annotation', () => {
+  it('can create an annotation when graph is clicked and the control bar is closed', () => {
     cy.getByTestID('toggle-annotations-controls').click()
-    
+
     cy.getByTestID('cell blah').within(() => {
-        cy.getByTestID('giraffe-inner-plot').click()
-      })
-      cy.getByTestID('overlay--container').within(() => {
-        cy.getByTestID('textarea')
-          .should('be.visible')
-          .click()
-          .focused()
-          .type('yoho')
-        cy.getByTestID('add-annotation-submit').click()
-      })
-      // we need to see if the annotations got created and that the tooltip says "yoho"
+      cy.getByTestID('giraffe-inner-plot').click()
+    })
+    cy.getByTestID('overlay--container').within(() => {
+      cy.getByTestID('textarea')
+        .should('be.visible')
+        .click()
+        .focused()
+        .type('yoho')
+      cy.getByTestID('add-annotation-submit').click()
+    })
+    // we need to see if the annotations got created and that the tooltip says "yoho"
   })
   it('can hide the Annotations Control bar after clicking on the Annotations Toggle Button', () => {
     cy.getByTestID('toggle-annotations-controls').click()

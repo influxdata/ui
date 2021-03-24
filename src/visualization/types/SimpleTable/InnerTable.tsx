22d1b0b5d24901583e6e9f850979fd44f28c0791
drdelambre <drdelambre>
2021-03-24 13:28:12 -0700

    fix: format times as times for humans

--- src/visualization/types/SimpleTable/InnerTable.tsx
+++ src/visualization/types/SimpleTable/InnerTable.tsx
@@ -17,14 +17,22 @@ const InnerTable: FC<InnerProps> = ({table}) => {
   const rows = Array(table.end - table.start)
     .fill(null)
     .map((_, idx) => {
-      const cells = Object.values(table.cols).map(c => (
-        <Table.Cell
-          key={`h${c.name}:r${idx}`}
-          testID={`table-cell ${c.data[idx]}`}
-        >
-          {c.data[idx]}
-        </Table.Cell>
-      ))
+      const cells = Object.values(table.cols).map(c => {
+        let val = c.data[idx]
+
+        if (c.type === 'time') {
+          val = new Date(val).toISOString()
+        }
+
+        return (
+          <Table.Cell
+            key={`h${c.name}:r${idx}`}
+            testID={`table-cell ${c.data[idx]}`}
+          >
+            {val}
+          </Table.Cell>
+        )
+      })
 
       return <Table.Row key={`r${idx}`}>{cells}</Table.Row>
     })

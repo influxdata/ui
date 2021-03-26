1198f6e123575fc8e93c02fd7b62d9dc92207d54
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2021-03-25 23:41:39 -0600

    fix: add in the old code and flag

--- src/visualization/types/Gauge/view.tsx
+++ src/visualization/types/Gauge/view.tsx
@@ -1,11 +1,19 @@
 // Libraries
-import React, {FC} from 'react'
+import React, {FC, useContext} from 'react'
 
 // Components
 import {Config, Plot} from '@influxdata/giraffe'
 // Types
 import {GaugeViewProperties} from 'src/types/dashboards'
 import {VisualizationProps} from 'src/visualization'
+import {isFlagEnabled} from '../../../shared/utils/featureFlag'
+
+// delete after testing
+import {AutoSizer} from 'react-virtualized'
+import {GAUGE_THEME_LIGHT, GAUGE_THEME_DARK} from './constants'
+import {AppSettingContext} from 'src/shared/contexts/app'
+import Gauge from './Gauge'
+import LatestValueTransform from 'src/visualization/components/LatestValueTransform'
 
 interface Props extends VisualizationProps {
   properties: GaugeViewProperties
@@ -21,21 +29,50 @@ const GaugeChart: FC<Props> = ({result, properties}) => {
     decimalPlaces,
   } = properties
 
-  const config: Config = {
-    table: result.table,
-    layers: [
-      {
-        type: 'gauge',
-        prefix: prefix,
-        suffix: suffix,
-        tickPrefix: tickPrefix,
-        tickSuffix: tickSuffix,
-        decimalPlaces: decimalPlaces,
-        gaugeColors: colors,
-      },
-    ],
+  // delete after testing code
+  const {theme} = useContext(AppSettingContext)
+  const currentTheme = theme === 'light' ? GAUGE_THEME_LIGHT : GAUGE_THEME_DARK
+
+  if (isFlagEnabled('useGiraffeGraphs')) {
+    const config: Config = {
+      table: result.table,
+      layers: [
+        {
+          type: 'gauge',
+          prefix: prefix,
+          suffix: suffix,
+          tickPrefix: tickPrefix,
+          tickSuffix: tickSuffix,
+          decimalPlaces: decimalPlaces,
+          gaugeColors: colors,
+        },
+      ],
+    }
+    return <Plot config={config} />
+  } else {
+    ;<LatestValueTransform table={result.table} allowString={false}>
+      {latestValue => (
+        <AutoSizer>
+          {({width, height}) => (
+            <div className="gauge">
+              <Gauge
+                width={width}
+                height={height}
+                colors={colors}
+                prefix={prefix}
+                tickPrefix={tickPrefix}
+                suffix={suffix}
+                tickSuffix={tickSuffix}
+                gaugePosition={latestValue}
+                decimalPlaces={decimalPlaces}
+                theme={currentTheme}
+              />
+            </div>
+          )}
+        </AutoSizer>
+      )}
+    </LatestValueTransform>
   }
-  return <Plot config={config} />
 }
 
 export default GaugeChart

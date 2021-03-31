b92de819314f05738e5d152ddd6534cf24bcd7a5
Alex Boatwright <drdelambre@gmail.com>
2021-03-31 12:21:40 -0700

    feat: better pagination component for simple table (#1056)

--- src/visualization/types/SimpleTable/PageControl.tsx
+++ src/visualization/types/SimpleTable/PageControl.tsx
@@ -1,57 +1,26 @@
 // Libraries
 import React, {FC, useContext} from 'react'
 import {PaginationContext} from 'src/visualization/context/pagination'
-import {event} from 'src/cloud/utils/reporting'
 
 // Components
-import {
-  Button,
-  ComponentStatus,
-  IconFont,
-  ComponentSize,
-} from '@influxdata/clockface'
+import {PaginationNav} from '@influxdata/clockface'
 
 const PageControl: FC = () => {
-  const {offset, size, total, previous, next} = useContext(PaginationContext)
-
-  const _previous = () => {
-    event('Previous Page Clicked')
-
-    previous()
-  }
-
-  const _next = () => {
-    event('Next Page Clicked')
-
-    next()
-  }
-
-  const prevButtonStatus =
-    offset === 0 ? ComponentStatus.Disabled : ComponentStatus.Default
-
-  const nextButtonStatus =
-    offset >= total - size ? ComponentStatus.Disabled : ComponentStatus.Default
+  const {offset, size, total, setPage} = useContext(PaginationContext)
 
   return (
     <div className="visualization--simple-table--paging">
-      <span className="visualization--simple-table--paging-label">{`Showing most recent 100 results per series ${offset} - ${offset +
-        size}`}</span>
-      <Button
-        className="visualization--simple-table--paging-button"
-        text="Previous"
-        status={prevButtonStatus}
-        icon={IconFont.CaretLeft}
-        onClick={_previous}
-        size={ComponentSize.ExtraSmall}
-      />
-      <Button
-        className="visualization--simple-table--paging-button"
-        text="Next"
-        status={nextButtonStatus}
-        icon={IconFont.CaretRight}
-        onClick={_next}
-        size={ComponentSize.ExtraSmall}
-      />
+      <span className="visualization--simple-table--paging-label">
+        Showing most recent 100 results per series
+      </span>
+      {total && size && (
+        <PaginationNav.PaginationNav
+          totalPages={Math.ceil(total / size)}
+          currentPage={Math.floor(offset / size) + 1}
+          pageRangeOffset={1}
+          onChange={setPage}
+        />
+      )}
     </div>
   )
 }

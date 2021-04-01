62897785f07c651a52447cc0f4035d969c161cc5
Gene Hynson <ghynson@influxdata.com>
2021-04-01 11:25:48 -0700

    chore: log json body

--- scripts/run-monitor-ci-tests.bash
+++ scripts/run-monitor-ci-tests.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo "{\"branch\":\"${BRANCH}\", \"sha\":\"${SHA}\"}"
+
 curl --request POST \
   --url https://circleci.com/api/v2/project/gh/influxdata/monitor-ci/pipeline \
   --header "Circle-Token: ${API_KEY}" \
   --header 'content-type: application/json' \
 	--header 'Accept: application/json'    \
-  --data "{'branch':'${BRANCH}', 'sha':'${SHA}'}"
+  --data "{\"branch\":\"${BRANCH}\", \"sha\":\"${SHA}\"}"

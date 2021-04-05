5ef56edc01b3f20e57acef774662ec55fb4fabc5
ChitlangeSahas <chitlangesahas@gmail.com>
2021-04-04 21:19:17 -0700

    Revert "fix: show error message when value for a variable name does not exist"
    This reverts commit c64e6ace

--- src/shared/copy/notifications.ts
+++ src/shared/copy/notifications.ts
@@ -329,15 +329,6 @@ export const invalidTimeRangeValueInURLQuery = (): Notification => ({
   message: `Invalid URL query value supplied for lower or upper time range.`,
 })
 
-export const invalidVariableNameValuePairInURLQuery = (
-  name: string,
-  value: string
-): Notification => ({
-  ...defaultErrorNotification,
-  icon: IconFont.Cube,
-  message: `Invalid URL query value "${value}" supplied for variable name "${name}".`,
-})
-
 export const getVariablesFailed = (): Notification => ({
   ...defaultErrorNotification,
   message: 'Failed to fetch variables',

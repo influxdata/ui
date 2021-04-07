d7360088d163e655ab4a11ba4a2ad2051546709c
Deniz Kusefoglu <deniz@influxdata.com>
2021-04-07 11:56:06 -0700

    feat(managed-functions): Add dapper scrollbars

--- src/functions/components/FunctionResponse.tsx
+++ src/functions/components/FunctionResponse.tsx
@@ -8,6 +8,7 @@ import {
   Panel,
   Gradients,
   AlignItems,
+  DapperScrollbars,
 } from '@influxdata/clockface'
 
 // Types
@@ -56,19 +57,25 @@ const FunctionResponse: FC<Props> = ({
           border={true}
           style={{overflow: 'scroll'}}
         >
-          <Panel.Header>
-            <h5>{statusText}</h5>
-          </Panel.Header>
-          <Panel.Body alignItems={AlignItems.FlexStart}>
-            {status == 'ok' ? (
-              <>
-                {dataOutput()}
-                {logsOutput()}
-              </>
-            ) : (
-              <div>{JSON.stringify(error)}</div>
-            )}
-          </Panel.Body>
+          <DapperScrollbars
+            className="function-form--response-scroll-bar"
+            autoHide={true}
+            autoSizeHeight={true}
+          >
+            <Panel.Header>
+              <h5>{statusText}</h5>
+            </Panel.Header>
+            <Panel.Body alignItems={AlignItems.FlexStart}>
+              {status == 'ok' ? (
+                <>
+                  {dataOutput()}
+                  {logsOutput()}
+                </>
+              ) : (
+                <div>{JSON.stringify(error, null, '\t')}</div>
+              )}
+            </Panel.Body>
+          </DapperScrollbars>
         </Panel>
       </Grid.Column>
     </Grid.Row>

2338d2cf5962e8461aa432a8541789f17295b8d4
asalem <ariel.salem1989@gmail.com>
2021-04-12 14:28:42 -0700

    feat: log the user out of auth0 and redirect to IDPE after

--- src/Logout.tsx
+++ src/Logout.tsx
@@ -2,12 +2,19 @@
 import {FC, useEffect} from 'react'
 import {useDispatch} from 'react-redux'
 import {RouteComponentProps} from 'react-router-dom'
+import auth0js from 'auth0-js'
 
 // APIs
 import {postSignout} from 'src/client'
 
 // Constants
-import {CLOUD, CLOUD_URL, CLOUD_LOGOUT_PATH} from 'src/shared/constants'
+import {
+  CLOUD,
+  CLOUD_URL,
+  CLOUD_LOGOUT_PATH,
+  CLOUD_SIGNOUT_PATHNAME,
+} from 'src/shared/constants'
+import {isFlagEnabled} from 'src/shared/utils/featureFlag'
 
 // Components
 import {reset} from 'src/shared/actions/flags'
@@ -19,6 +26,18 @@ const Logout: FC<Props> = ({history}) => {
 
   useEffect(() => {
     const handleSignOut = async () => {
+      if (CLOUD && isFlagEnabled('authSessionCookieOn')) {
+        const signoutUrl = new URL(
+          `${window.location.origin}${CLOUD_SIGNOUT_PATHNAME}`
+        )
+        const auth0 = new auth0js.WebAuth({
+          returnTo: signoutUrl.href,
+        })
+        // Log the user out of Auth0, then destroy their API session
+        auth0.logout()
+        return
+      }
+
       if (CLOUD) {
         window.location.href = `${CLOUD_URL}${CLOUD_LOGOUT_PATH}`
         return

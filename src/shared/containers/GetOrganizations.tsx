372d66b2be11fdf503dc6097748e565ff8bd2e84
asalem <ariel.salem1989@gmail.com>
2021-04-20 14:52:04 -0700

    chore: updated route and feature flag names

--- src/shared/containers/GetOrganizations.tsx
+++ src/shared/containers/GetOrganizations.tsx
@@ -56,18 +56,18 @@ const GetOrganizations: FunctionComponent = () => {
   return (
     <PageSpinner loading={status}>
       <Suspense fallback={<PageSpinner />}>
-        {isFlagEnabled('unity-me-api') ? (
+        {isFlagEnabled('unityMeApi') ? (
           <PageSpinner loading={quartzMeStatus}>
             <Switch>
               <Route path="/no-orgs" component={NoOrgsPage} />
               <Route path="/orgs" component={App} />
               <Route exact path="/" component={RouteToOrg} />
               {CLOUD &&
-                isFlagEnabled('unity-checkout') &&
+                isFlagEnabled('unityCheckout') &&
                 canAccessCheckout(me) && (
                   <Route path="/checkout" component={CheckoutPage} />
                 )}
-              {CLOUD && isFlagEnabled('unity-operator') && me?.isOperator && (
+              {CLOUD && isFlagEnabled('unityOperator') && me?.isOperator && (
                 <Route path="/operator" component={OperatorPage} />
               )}
               <Route component={NotFound} />

4b5299ac4165d0936c7eef01c9ed0197033c8dac
greg linton <greg@influxdata.com>
2021-04-21 16:49:19 -0600

    chore: update function names to match swagger

--- src/functions/context/api.tsx
+++ src/functions/context/api.tsx
@@ -1,17 +1,17 @@
 import {
-  getApiV2Functions,
-  deleteApiV2Function,
-  postApiV2Function,
+  getFunctions,
+  deleteFunction,
+  postFunction,
   FunctionCreateRequest,
-  postApiV2FunctionsTrigger,
+  postFunctionsTrigger,
   FunctionTriggerRequest,
-  patchApiV2Function,
+  patchFunction,
   FunctionUpdateRequest,
-  getApiV2FunctionsRuns,
+  getFunctionsRuns,
 } from 'src/client/managedFunctionsRoutes'
 
 export const createAPI = async (functionCreate: FunctionCreateRequest) => {
-  const res = await postApiV2Function({data: functionCreate})
+  const res = await postFunction({data: functionCreate})
 
   if (res.status != 201) {
     throw new Error(res.data.message)
@@ -20,7 +20,7 @@ export const createAPI = async (functionCreate: FunctionCreateRequest) => {
 }
 
 export const deleteAPI = async (id: string) => {
-  const res = await deleteApiV2Function({functionID: id})
+  const res = await deleteFunction({functionID: id})
 
   if (res.status != 204) {
     throw new Error(res.data.message)
@@ -28,7 +28,7 @@ export const deleteAPI = async (id: string) => {
 }
 
 export const getAllAPI = async (orgID: string) => {
-  const res = await getApiV2Functions({query: {orgID}})
+  const res = await getFunctions({query: {orgID}})
   if (res.status != 200) {
     throw new Error(res.data.message)
   }
@@ -36,7 +36,7 @@ export const getAllAPI = async (orgID: string) => {
 }
 
 export const triggerAPI = async (triggerRequest: FunctionTriggerRequest) => {
-  const res = await postApiV2FunctionsTrigger({
+  const res = await postFunctionsTrigger({
     data: triggerRequest,
   })
 
@@ -50,7 +50,7 @@ export const updateAPI = async (
   functionID: string,
   updateRequest: FunctionUpdateRequest
 ) => {
-  const res = await patchApiV2Function({functionID, data: updateRequest})
+  const res = await patchFunction({functionID, data: updateRequest})
 
   if (res.status != 200) {
     throw new Error(res.data.message)
@@ -59,7 +59,7 @@ export const updateAPI = async (
 }
 
 export const getRunsAPI = async (functionID: string) => {
-  const res = await getApiV2FunctionsRuns({functionID})
+  const res = await getFunctionsRuns({functionID})
 
   if (res.status != 200) {
     throw new Error(res.data.message)

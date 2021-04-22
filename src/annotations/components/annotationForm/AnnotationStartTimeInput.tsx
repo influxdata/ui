38013a5f85e86b8a91e2b4f0f08270e14b4807d3
ChitlangeSahas <chitlangesahas@gmail.com>
2021-04-22 13:39:32 -0700

    fix: match annotation start time input behaviour similar to Date time picker

--- src/annotations/components/annotationForm/AnnotationStartTimeInput.tsx
+++ src/annotations/components/annotationForm/AnnotationStartTimeInput.tsx
@@ -1,5 +1,6 @@
 // Libraries
-import React, {FC, ChangeEvent, KeyboardEvent} from 'react'
+import React, {FC, ChangeEvent, KeyboardEvent, useState} from 'react'
+import moment from 'moment'
 
 // Components
 import {
@@ -17,10 +18,26 @@ interface Props {
 }
 
 export const AnnotationStartTimeInput: FC<Props> = (props: Props) => {
-  const validationMessage = props.startTime ? '' : 'This field is required'
+
+  // initial input value is the startTime passed
+  const [inputValue, setInputValue] = useState<string>(
+    moment(props.startTime).format('YYYY-MM-DD HH:mm:ss.SSS')
+  )
+
+  const isValidTimeFormat = (d: string): boolean => {
+    return moment(d, 'YYYY-MM-DD HH:mm:ss.SSS', true).isValid()
+  }
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    props.onChange(event.target.value)
+    setInputValue(event.target.value)
+
+    if (isValidTimeFormat(event.target.value)) {
+      props.onChange(
+        moment(event.target.value)
+          .toDate()
+          .toISOString()
+      )
+    }
   }
 
   const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
@@ -29,6 +46,24 @@ export const AnnotationStartTimeInput: FC<Props> = (props: Props) => {
       return
     }
   }
+
+  const isInputValueInvalid = (): boolean => {
+    if (inputValue === null) {
+      return false
+    }
+
+    return !isValidTimeFormat(inputValue)
+  }
+
+  const inputErrorMessage = (): string | undefined => {
+    if (isInputValueInvalid()) {
+      return 'Format must be YYYY-MM-DD [HH:mm:ss.SSS]'
+    }
+
+    return '\u00a0\u00a0'
+  }
+
+  const validationMessage = inputErrorMessage()
 
   return (
     <Grid.Column widthXS={Columns.Twelve}>
@@ -39,7 +74,7 @@ export const AnnotationStartTimeInput: FC<Props> = (props: Props) => {
       >
         <Input
           name="startTime"
-          value={new Date(props.startTime).toString()}
+          value={inputValue}
           onChange={handleChange}
           onKeyPress={handleKeyPress}
           status={ComponentStatus.Default}

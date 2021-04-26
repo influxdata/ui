28f9d133a73fbe8f0ce9788697f9d62a05cdcc32
Chitlange Sahas <chitlangesahas@gmail.com>
2021-04-26 13:12:48 -0700

    fix: make the dispatch async; add await for create and edit annotation forms (#1293)
    Co-authored by: @zoesteinkamp

--- src/visualization/types/Graph/view.tsx
+++ src/visualization/types/Graph/view.tsx
@@ -58,6 +58,7 @@ import {
 } from 'src/shared/utils/vis'
 import {isFlagEnabled} from 'src/shared/utils/featureFlag'
 import {event} from 'src/cloud/utils/reporting'
+import {getErrorMessage} from 'src/utils/api'
 
 // Notifications
 import {createAnnotationFailed} from 'src/shared/copy/notifications'
@@ -181,10 +182,10 @@ const XYPlot: FC<Props> = ({
   }
 
   const makeSingleClickHandler = () => {
-    const createAnnotation = userModifiedAnnotation => {
+    const createAnnotation = async userModifiedAnnotation => {
       const {message, startTime} = userModifiedAnnotation
       try {
-        dispatch(
+        await dispatch(
           writeThenFetchAndSetAnnotations([
             {
               summary: message,
@@ -196,7 +197,7 @@ const XYPlot: FC<Props> = ({
         )
         event('xyplot.annotations.create_annotation.create')
       } catch (err) {
-        dispatch(notify(createAnnotationFailed(err)))
+        dispatch(notify(createAnnotationFailed(getErrorMessage(err))))
         event('xyplot.annotations.create_annotation.failure')
       }
     }

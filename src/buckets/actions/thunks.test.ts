76b42b24bced4c7150f1c32d179e8300950d668b
TheRWe <r.wehmhoner@gmail.com>
2021-04-26 12:03:43 +0200

    test: even better thunks mock, clean code

--- src/buckets/actions/thunks.test.ts
+++ src/buckets/actions/thunks.test.ts
@@ -1,72 +1,88 @@
+import {fetchAllBuckets, getBuckets} from './thunks'
+import * as api from 'src/client'
+import {fetchDemoDataBuckets} from 'src/cloud/apis/demodata'
+import {getMockAppState} from 'src/mockAppState'
+import {RemoteDataState} from '@influxdata/clockface'
 
-import { fetchAllBuckets, getBuckets } from "./thunks";
-
-const createMockGetBuckets = (shouldSucess: boolean): (typeof api.getBuckets) => () => {
-  if (shouldSucess) {
-    const res: ReturnType<(typeof api.getBuckets)> = Promise.resolve(
-      { data: { buckets: [{ name: "buck1", retentionRules: [], id: "custom-id" }] }, headers: {} as any, status: 200 }
-    );
-    return res;
-  } else {
-    const res: ReturnType<(typeof api.getBuckets)> = Promise.resolve(
-      { data: { message: "simulated error", code: "not found" }, headers: {} as any, status: 500 }
-    );
-    return res;
-  }
-};
-
-
-jest.mock("src/client", () => ({
+jest.mock('src/client', () => ({
   getBuckets: jest.fn(),
 }))
-import * as api from 'src/client';
 
-import { fetchDemoDataBuckets } from 'src/cloud/apis/demodata';
-import { getMockAppState } from "src/mockAppState";
-import { RemoteDataState } from "@influxdata/clockface";
-jest.mock("src/cloud/apis/demodata", () => ({
+jest.mock('src/cloud/apis/demodata', () => ({
   fetchDemoDataBuckets: jest.fn(() => {
-    const res: ReturnType<(typeof fetchDemoDataBuckets)> = Promise.resolve(
-      [{ id: "demo-bucket", retentionRules: [], name: "demo-buck", readableRetention: "retee", type: "demodata" }]
-    );
-    return res;
+    const res: ReturnType<typeof fetchDemoDataBuckets> = Promise.resolve([
+      {
+        id: 'demo-bucket',
+        retentionRules: [],
+        name: 'demo-buck',
+        readableRetention: 'retee',
+        type: 'demodata',
+      },
+    ])
+    return res
   }),
 }))
 
+const mockGetBuckets = (shouldSucess: boolean) => {
+  const headers = {} as any
 
-describe("buckets thunks", () => {
-  describe("fetchAllBuckets", () => {
-    it("success", async () => {
-      (api.getBuckets as any).mockImplementationOnce(createMockGetBuckets(true))
+  let mock: typeof api.getBuckets = () => {
+    const res: ReturnType<typeof api.getBuckets> = Promise.resolve(
+      shouldSucess
+        ? {
+            data: {
+              buckets: [{name: 'buck1', retentionRules: [], id: 'custom-id'}],
+            },
+            headers,
+            status: 200,
+          }
+        : {
+            data: {message: 'simulated error', code: 'not found'},
+            headers,
+            status: 500,
+          }
+    )
+    return res
+  }
 
-      const bucks = await fetchAllBuckets("ord01")
-      expect(bucks.result).toContain("custom-id")
-      expect(bucks.result).toContain("demo-bucket")
+  ;(api.getBuckets as any).mockImplementationOnce(mock)
+}
+
+
+describe('buckets thunks', () => {
+  describe('fetchAllBuckets', () => {
+    it('success', async () => {
+      mockGetBuckets(true)
+
+      const bucks = await fetchAllBuckets('ord01')
+      expect(bucks.result).toContain('custom-id')
+      expect(bucks.result).toContain('demo-bucket')
     })
 
-    it("fails", async () => {
-      (api.getBuckets as any).mockImplementationOnce(createMockGetBuckets(false))
+    it('fails', async () => {
+      mockGetBuckets(false)
 
-      await expect(fetchAllBuckets("ord01"))
-        .rejects
-        .toThrowError("simulated error")
+      await expect(fetchAllBuckets('ord01')).rejects.toThrowError(
+        'simulated error'
+      )
     })
   })
 
-  describe("getBuckets", () => {
-    it("starts", async () => {
-      (api.getBuckets as any).mockImplementationOnce(createMockGetBuckets(true));
-      const dispatched = [];
-      const dispatch = jest.fn(Array.prototype.push.bind(dispatched));
-      const getState = jest.fn(getMockAppState) as any;
+  describe('getBuckets', () => {
+    it('success', async () => {
+      mockGetBuckets(true)
+      const dispatch = jest.fn()
+      const getState = jest.fn(getMockAppState) as any
 
       await getBuckets()(dispatch, getState)
+
+      const dispatched = dispatch.mock.calls.map(([x]) => x)
+
       expect(dispatched.length).toBe(2)
       expect(dispatched[0].status).toBe(RemoteDataState.Loading)
       expect(dispatched[1].status).toBe(RemoteDataState.Done)
-      expect(dispatched[1].schema.result).toContain("custom-id")
-      expect(dispatched[1].schema.result).toContain("demo-bucket")
+      expect(dispatched[1].schema.result).toContain('custom-id')
+      expect(dispatched[1].schema.result).toContain('demo-bucket')
     })
   })
-
 })

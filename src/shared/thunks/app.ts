48f32437e7897c688459860e658fb269bd7141f2
Will Baker <wbaker@gmail.com>
2021-05-14 15:46:43 -0400

    chore: added comment about using fetch

--- src/shared/thunks/app.ts
+++ src/shared/thunks/app.ts
@@ -8,6 +8,9 @@ export const fetchVersionInfo = () => async (
 ): Promise<VersionInfo> => {
   try {
     const url = `${getAPIBasepath()}/health`
+    // Have to use fetch here since the oats client doesn't work with the servers option in the oss swagger
+    // Ex: https://github.com/influxdata/openapi/blob/35d734671d05ba9337dd6fbd8bb5c6085482011b/contracts/oss.yml#L5887
+    // The "/health" endpoint is actually not prefixed by "api/v2", but the oats client has the prefix
     const response = await fetch(url)
     const info = await response.json()
 

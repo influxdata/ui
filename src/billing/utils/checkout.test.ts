194357fca44d98a367757971b5ab44ecb07b2e20
TheRWe <r.wehmhoner@gmail.com>
2021-05-31 13:40:57 +0200

    test: checkout util convertKeys

---
+++ src/billing/utils/checkout.test.ts
@@ -0,0 +1,27 @@
+import { convertKeysToCamelCase, convertKeysToSnakecase } from "./checkout";
+
+type stringDict = ({ [key: string]: string });
+const testCases: ({ snakeCase: stringDict, camelCase: stringDict })[] = [
+  { camelCase: { keyOne: "some string" }, snakeCase: { key_one: "some string" } },
+  { camelCase: { testing: "some string" }, snakeCase: { testing: "some string" } },
+  {
+    camelCase: { numbers01Test: "some string", numBer1Test2Blah: "other string" },
+    snakeCase: { numbers_01_test: "some string", num_ber_1_test_2_blah: "other string" }
+  },
+]
+
+
+describe("checkout", () => {
+  testCases.forEach(({ camelCase, snakeCase }, i) => it(`converts object both ways ${i}`, () => {
+    expect(convertKeysToSnakecase(camelCase)).toMatchObject(snakeCase)
+    expect(convertKeysToCamelCase(snakeCase)).toMatchObject(camelCase)
+  }))
+
+  it(`converts abbreviations`, () => {
+    expect(convertKeysToSnakecase({ UPPERCASEABBREVIATIONS: "some string" }))
+      .toMatchObject({ uppercaseabbreviations: "some string" })
+    expect(convertKeysToCamelCase({ UPPERCASEABBREVIATIONS: "some string" }))
+      .toMatchObject({ uppercaseabbreviations: "some string" })
+  })
+})
+

dcbcaafe81169e6d49d7c4ce5de8c131f6fab0de
asalem <ariel.salem1989@gmail.com>
2021-06-01 06:35:51 -0700

    chore: creating a custom query for the usage stats to see if the results populate correctly when the results are received and handled correctly

--- src/usage/context/usage.tsx
+++ src/usage/context/usage.tsx
@@ -5,17 +5,18 @@ import React, {FC, useCallback, useEffect, useState} from 'react'
 import {
   getBillingStartDate,
   getUsage,
-  getUsageBillingStats,
+  // getUsageBillingStats,
   getUsageVectors,
   getUsageRateLimits,
 } from 'src/client/unityRoutes'
+import {processResponse} from 'src/shared/apis/query'
 
 // Constants
 import {DEFAULT_USAGE_TIME_RANGE} from 'src/shared/constants/timeRanges'
+import {API_BASE_PATH} from 'src/shared/constants'
 
 // Types
-import {SelectableDurationTimeRange} from 'src/types'
-import {UsageVector} from 'src/types/billing'
+import {SelectableDurationTimeRange, UsageVector} from 'src/types'
 
 export type Props = {
   children: JSX.Element
@@ -114,22 +115,44 @@ export const UsageProvider: FC<Props> = React.memo(({children}) => {
 
   const handleGetBillingStats = useCallback(async () => {
     try {
-      const resp = await getUsageBillingStats({})
+      // TODO(ariel): making a custom function here because oats can't handle CSV responses?
+      const request = fetch(
+        `${API_BASE_PATH}api/v2/quartz/usage/billing_stats`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept-Encoding': 'gzip',
+          },
+        }
+      )
 
-      if (resp.status !== 200) {
-        throw new Error(resp.data.message)
-      }
+      const result = await request.then(processResponse)
 
       // TODO(ariel): keeping this in for testing purposes in staging
       // This will need to be removed for flipping the feature flag on
-      console.warn({resp})
+      console.warn({result})
 
-      const csvs = resp.data?.split('\n\n')
+      if (result.type !== 'SUCCESS') {
+        throw new Error(result.message)
+      }
 
+      const csvs = result?.csv?.split('\n\n')
       // TODO(ariel): keeping this in for testing purposes in staging
       // This will need to be removed for flipping the feature flag on
       console.warn({csvs})
-      setBillingStats(csvs)
+
+      setBillingStats(csvs ?? [''])
+
+      // const resp = await getUsageBillingStats({})
+
+      // if (resp.status !== 200) {
+      //   throw new Error(resp.data.message)
+      // }
+
+      // const csvs = result?.csv?.split('\n\n')
+
+      // setBillingStats(csvs)
     } catch (error) {
       console.error('getBillingStats: ', error)
     }

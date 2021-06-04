cb35a2940892cd6491c44fd19b943a275a6a10b6
TheRWe <r.wehmhoner@gmail.com>
2021-06-04 11:41:43 +0200

    test: processStatusesResponse refactor, new test

--- src/alerting/utils/statusEvents.test.ts
+++ src/alerting/utils/statusEvents.test.ts
@@ -1,7 +1,7 @@
-import { processStatusesResponse } from 'src/alerting/utils/statusEvents'
-import { fromFlux, Table } from '@influxdata/giraffe'
-import { range } from "lodash"
-import { mocked } from "ts-jest/utils"
+import {processStatusesResponse} from 'src/alerting/utils/statusEvents'
+import {fromFlux, Table} from '@influxdata/giraffe'
+import {range} from 'lodash'
+import {mocked} from 'ts-jest/utils'
 
 jest.mock('@influxdata/giraffe', () => ({
   fromFlux: jest.fn(),
@@ -11,65 +11,85 @@ jest.mock('src/cloud/utils/reporting', () => ({
   event: jest.fn(),
 }))
 
-const mockGetBuckets = (fnc: typeof fromFlux) => {
-  ; (fromFlux as any).mockImplementationOnce(fnc)
-}
+const alwaysZero = new Proxy([], {
+  get: (target, prop) =>
+    typeof prop === 'string' && Number.isInteger(+prop) ? 0 : target[prop],
+})
 
-const alwaysZero = new Proxy([], { get: (_, key: any) => Number.isInteger(+key) ? 0 : [][key] })
+const csv = 'some csv'
+const cancel = jest.fn()
 
 describe('process statuses response', () => {
   beforeEach(jest.clearAllMocks)
 
   it('process empty table', async () => {
-    mockGetBuckets(() => ({ table: { length: 0 } } as any));
+    mocked(fromFlux).mockImplementationOnce(() => ({table: {length: 0}}))
 
-    const { promise } = processStatusesResponse({
+    const actual = await processStatusesResponse({
       promise: Promise.resolve({
-        type: "SUCCESS",
-        csv: "some csv",
+        type: 'SUCCESS',
+        csv,
         bytesRead: 0,
         didTruncate: false,
-      }), cancel: jest.fn(() => { })
-    })
-    const actual = await promise;
+      }),
+      cancel,
+    }).promise
 
-    expect(actual.length).toBe(1);
-    expect(actual[0].length).toBe(0);
+    expect(actual.length).toBe(1)
+    expect(actual[0].length).toBe(0)
   })
 
   it('process single table', async () => {
-    const csv = "some csv";
-    const length = 100;
-    const cols = 20;
+    const length = 100
+    const cols = 20
     const table: Table = {
       columnKeys: ['table', ...range(cols).map(x => x.toString(10))],
       getColumn: (col: string) =>
         col === 'table'
           ? alwaysZero
-          : range(length).map(x => x * +col) as any,
+          : (range(length).map(x => x * +col) as any),
       length,
       getColumnName: jest.fn(),
       getColumnType: jest.fn(),
       getOriginalColumnType: jest.fn(),
       addColumn: jest.fn(),
-    };
-    mockGetBuckets(() => ({ table }) as any);
+    }
+    mocked(fromFlux).mockImplementationOnce(() => ({table}))
     const expected = range(length).map(i =>
-      Object.fromEntries([['table', 0], ...range(cols).map(col => [col, i * col])])
+      Object.fromEntries([
+        ['table', 0],
+        ...range(cols).map(col => [col, i * col]),
+      ])
     )
 
-    const { promise } = processStatusesResponse({
+    const actual = await processStatusesResponse({
       promise: Promise.resolve({
-        type: "SUCCESS",
+        type: 'SUCCESS',
         csv,
         bytesRead: 0,
         didTruncate: false,
-      }), cancel: jest.fn(() => { })
-    })
-    const actual = await promise;
+      }),
+      cancel,
+    }).promise
 
-    expect(mocked(fromFlux).mock.calls[0][0]).toBe(csv);
-    expect(actual.length).toBe(1);
-    expect(actual[0]).toMatchObject(expected);
+    expect(mocked(fromFlux).mock.calls[0][0]).toBe(csv)
+    expect(actual.length).toBe(1)
+    expect(actual[0]).toMatchObject(expected)
+  })
+
+  it('rejects on non-success response', async () => {
+    const message = 'resulted with error'
+    const {promise} = processStatusesResponse({
+      promise: Promise.resolve({
+        type: 'UNKNOWN_ERROR',
+        message,
+        csv: '',
+        bytesRead: 0,
+        didTruncate: false,
+      }),
+      cancel,
+    })
+
+    await expect(promise).rejects.toEqual(new Error(message))
   })
 })

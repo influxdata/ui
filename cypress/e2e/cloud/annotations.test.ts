4126a99a6399877cf7a13ecfa53b88c806c35d21
Jill R Pelavin <jpelavin@influxdata.com>
2021-06-07 17:12:49 -0400

    chore: adding more e2e tests for annotation point->range switching

--- cypress/e2e/cloud/annotations.test.ts
+++ cypress/e2e/cloud/annotations.test.ts
@@ -314,7 +314,74 @@ describe('The Annotations UI functionality', () => {
       actuallyDeleteAnnotation(cy)
     })
 
-    it.only('can add an annotation; that is originally a point and then switch to a range', () => {
+    it('can add a range annotation, then edit it and switch back and forth from point->range and the endtime stays the same', () => {
+      addRangeAnnotation(cy)
+      startEditingAnnotation(cy)
+
+      // verify there is an end time:
+      cy.getByTestID('endTime-testID').should('be.visible')
+
+      // verify that it is range annotation (the range selector option is selected)
+      cy.getByTestID('annotation-form-range-type-option--input').should(
+        'be.checked'
+      )
+
+      // get the 'end time' value:
+      cy.getByTestID('endTime-testID')
+        .invoke('val')
+        .then(endTimeValue => {
+          // switch it to a point annotation
+          cy.getByTestID('annotation-form-point-type-option').click()
+
+          // the endTime input should disappear
+          cy.getByTestID('endTime-testID').should('not.exist')
+
+          // switch back to range:
+          cy.getByTestID('annotation-form-range-type-option').click()
+
+          cy.getByTestID('endTime-testID')
+            .should('be.visible')
+            .invoke('val')
+            .then(endTimeValue2 => {
+              expect(endTimeValue).to.equal(endTimeValue2)
+            })
+        })
+    })
+    it('can add a range annotation, then edit it and change to a point annotation', () => {
+      addRangeAnnotation(cy)
+      cy.reload()
+      startEditingAnnotation(cy)
+
+      // verify that it is range annotation (the range selector option is selected)
+      cy.getByTestID('annotation-form-range-type-option--input').should(
+        'be.checked'
+      )
+
+      // switch it to a point annotation
+      cy.getByTestID('annotation-form-point-type-option').click()
+
+      // verify that it is point  annotation now
+      cy.getByTestID('annotation-form-point-type-option--input').should(
+        'be.checked'
+      )
+
+      // the endTime input should disappear
+      cy.getByTestID('endTime-testID').should('not.exist')
+
+      // save it
+      cy.getByTestID('annotation-submit-button').click()
+
+      // reload to make sure it gets to the backend
+      cy.reload()
+      startEditingAnnotation(cy)
+
+      // make sure it is (still) a point annotation:
+      cy.getByTestID('endTime-testID').should('not.exist')
+      cy.getByTestID('annotation-form-point-type-option--input').should(
+        'be.checked'
+      )
+    })
+    it('can add an annotation; that is originally a point and then switch to a range', () => {
       cy.getByTestID('cell blah').within(() => {
         cy.getByTestID('giraffe-inner-plot').click()
       })

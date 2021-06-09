5cbd9e1fa607736660acc6e5c2a31a883a33b7f9
Joel Fried <jfried@influxdata.com>
2021-06-09 10:36:38 -0400

    fix: move from class component to function component.

--- src/secrets/containers/SecretsIndex.tsx
+++ src/secrets/containers/SecretsIndex.tsx
@@ -1,9 +1,8 @@
 // Libraries
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
+import React, {FC} from 'react'
+import {useSelector, connect} from 'react-redux'
 
 // Components
-import {ErrorHandling} from 'src/shared/decorators/errors'
 import SettingsTabbedPage from 'src/settings/components/SettingsTabbedPage'
 import SettingsHeader from 'src/settings/components/SettingsHeader'
 import {Page} from '@influxdata/clockface'
@@ -11,17 +10,11 @@ import {Page} from '@influxdata/clockface'
 // Utils
 import {pageTitleSuffixer} from 'src/shared/utils/pageTitles'
 
-// Types
-import {AppState, Organization} from 'src/types'
+// Selectors
+import {getOrg} from 'src/organizations/selectors'
 
-interface StateProps {
-    org: Organization
-}
-
-@ErrorHandling
-class SecretsIndex extends Component<StateProps> {
-    public render() {
-        const {org} = this.props
+const SecretsIndex: FC = () => {
+        const org = useSelector(getOrg)
 
         return (
             <>
@@ -33,11 +26,6 @@ class SecretsIndex extends Component<StateProps> {
                 </Page>
             </>
         )
-    }
 }
 
-const mstp = (state: AppState) => {
-    return {org: state.resources.orgs.org}
-}
-
-export default connect<StateProps>(mstp)(SecretsIndex)
+export default SecretsIndex

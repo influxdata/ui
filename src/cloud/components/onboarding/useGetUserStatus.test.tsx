d3284d4634d85e6caf8948b62312fde762d8214f
ShmuelLotman <shmuel.lotman@gmail.com>
2021-06-15 10:38:14 -0500

    chore: try to skip if not cloud

--- src/cloud/components/onboarding/useGetUserStatus.test.tsx
+++ src/cloud/components/onboarding/useGetUserStatus.test.tsx
@@ -1,19 +1,39 @@
 import {getUserStatus, USER_PILOT_USER_STATUS} from './useGetUserStatus'
 import {usageStatsCsv} from 'src/shared/utils/mocks/usagestats'
 import {fromFlux} from '@influxdata/giraffe'
+import {CLOUD} from 'src/shared/constants'
 
-jest.mock('src/client')
+export const itif = (
+  name: string,
+  condition: () => boolean | Promise<boolean>,
+  cb
+) => {
+  it(name, done => {
+    if (condition()) {
+      cb(done)
+    } else {
+      console.warn(`[skipped]: ${name}`)
+      done()
+    }
+  })
+}
+
 describe('useGetUserStatus helper', () => {
   const parsedTable = fromFlux(usageStatsCsv)
 
-  it('gets user status based on passed usage data', () => {
-    const status = getUserStatus(parsedTable.table)
+  itif(
+    'gets user status based on passed usage data',
+    () => !!CLOUD,
+    done => {
+      const status = getUserStatus(parsedTable.table)
 
-    expect(status).toEqual([
-      USER_PILOT_USER_STATUS.ACTIVE_WRITING_USER,
-      USER_PILOT_USER_STATUS.ACTIVE_USER,
-    ])
-  })
+      expect(status).toEqual([
+        USER_PILOT_USER_STATUS.ACTIVE_WRITING_USER,
+        USER_PILOT_USER_STATUS.ACTIVE_USER,
+      ])
+      done()
+    }
+  )
 
   it('returns a NEW_USER status if no states are ascertained', () => {
     const status = getUserStatus({getColumn: () => []})

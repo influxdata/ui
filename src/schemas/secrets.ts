e1265d51d7d12b0457fde517fd49c1a310d63aba
Joel Fried <jfried@influxdata.com>
2021-06-17 14:31:49 -0400

    chore: fix comment

--- src/schemas/secrets.ts
+++ src/schemas/secrets.ts
@@ -4,7 +4,7 @@ import {schema} from 'normalizr'
 // Types
 import {ResourceType} from 'src/types'
 
-// Defines the schema for the tasks resource
+// Defines the schema for the secrets resource
 export const secretsSchema = new schema.Entity(ResourceType.Secrets)
 
 export const arrayOfSecrets = [secretsSchema]

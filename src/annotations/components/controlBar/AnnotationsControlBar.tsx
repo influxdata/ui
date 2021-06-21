e11e2c0d7059db350f9802bfdc2e4cc65e610b8a
Jill R Pelavin <jpelavin@influxdata.com>
2021-06-21 16:35:06 -0400

    chore: removing comments for another issue

--- src/annotations/components/controlBar/AnnotationsControlBar.tsx
+++ src/annotations/components/controlBar/AnnotationsControlBar.tsx
@@ -2,11 +2,7 @@
 import React, {FC} from 'react'
 
 // Components
-import {
-  ComponentSize,
-  InfluxColors,
-  TextBlock,
-} from '@influxdata/clockface'
+import {ComponentSize, InfluxColors, TextBlock} from '@influxdata/clockface'
 
 import {useSelector} from 'react-redux'
 import {isAnnotationsModeEnabled} from 'src/annotations/selectors'

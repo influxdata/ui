83d58bfe852451cf72ad4f233b70564949f14942
Subir Jolly <subirjolly@gmail.com>
2021-06-30 16:56:18 -0400

    chore: add notebook copy  to clipboard tests (#1863)
    * chore: add tests for export to clipboard feat
    
    * chore: ignore unauthorized access error for cypress
    
    * Update src/writeData/components/ClientCodeQueryHelper.tsx

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -11,6 +11,7 @@ Cypress.on('uncaught:exception', (err, _) => {
   // we expect a 401 error when logging in with DEX's APIs, so ignore that but fail for all other console errors.
   // we also can ignore AbortErrors for when network requests are terminated early.
   return !(
+    err.message.includes('unauthorized access') ||
     err.message.includes('Request failed with status code 401') ||
     err.message.includes('The operation was aborted') ||
     err.message.includes('NetworkError') ||

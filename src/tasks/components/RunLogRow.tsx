89988265cf1a37ad12b080ad00cce16a74b7ea42
Bethelhem Legesse <legessebl@gmail.com>
2021-06-30 16:00:06 -0500

    feat: changed moment with date formatter in Run log row  (#1836)
    * fix: moment with dateFormatter switch
    
    * fix: prettier
    
    * chore: removed resolveTimeFormat

--- src/tasks/components/RunLogRow.tsx
+++ src/tasks/components/RunLogRow.tsx
@@ -1,6 +1,5 @@
 // Libraries
 import React, {PureComponent} from 'react'
-import moment from 'moment'
 
 // Components
 import {IndexList} from '@influxdata/clockface'
@@ -8,6 +7,7 @@ import {IndexList} from '@influxdata/clockface'
 // Types
 import {LogEvent} from 'src/types'
 import {DEFAULT_TIME_FORMAT} from 'src/shared/constants'
+import {createDateTimeFormatter} from 'src/utils/datetime/formatters'
 
 interface Props {
   log: LogEvent
@@ -41,7 +41,9 @@ class RunLogRow extends PureComponent<Props> {
     }
 
     const newdate = new Date(dt)
-    const formatted = moment(newdate).format(DEFAULT_TIME_FORMAT)
+    const formatted = createDateTimeFormatter(DEFAULT_TIME_FORMAT).format(
+      newdate
+    )
 
     return formatted
   }

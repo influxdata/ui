95319d98c1914d4f77545355cd48ee8ea5e7316a
asalem <ariel.salem1989@gmail.com>
2021-07-06 13:59:42 -0700

    chore: WIP

--- src/organizations/components/OrgNavigation.tsx
+++ src/organizations/components/OrgNavigation.tsx
@@ -1,128 +1,80 @@
 // Libraries
-import React, {PureComponent} from 'react'
+import React, {FC} from 'react'
+import {useSelector} from 'react-redux'
 import {Link} from 'react-router-dom'
 
 // Components
 import {Tabs, Orientation, ComponentSize} from '@influxdata/clockface'
-import CloudExclude from 'src/shared/components/cloud/CloudExclude'
-import CloudOnly from 'src/shared/components/cloud/CloudOnly'
-import {FeatureFlag} from 'src/shared/utils/featureFlag'
 
 // Constants
-import {CLOUD_USERS_PATH, CLOUD_URL} from 'src/shared/constants'
-import {isFlagEnabled} from 'src/shared/utils/featureFlag'
+import {CLOUD} from 'src/shared/constants'
 
-// Decorators
-import {ErrorHandling} from 'src/shared/decorators/errors'
+// Utils
+import {getOrg} from 'src/organizations/selectors'
 
 interface Props {
   activeTab: string
-  orgID: string
 }
 
 interface OrgPageTab {
   text: string
   id: string
-  cloudExclude?: boolean
-  cloudOnly?: boolean
-  href?: string
-  link?: string
-  featureFlag?: string
-  featureFlagValue?: boolean
+  enabled?: () => boolean
+  link: string
 }
 
-@ErrorHandling
-class OrgNavigation extends PureComponent<Props> {
-  public render() {
-    const {activeTab, orgID} = this.props
+const OrgNavigation: FC<Props> = ({activeTab}) => {
+  const orgID = useSelector(getOrg)?.id
 
-    // TODO(ariel): once the `unityUsers` flag goes live we won't need any of the href logic:
-    // https://github.com/influxdata/ui/issues/1405
-    const tabs: OrgPageTab[] = [
-      {
-        text: 'Members',
-        id: 'members-oss',
-        cloudExclude: true,
-        link: `/orgs/${orgID}/members`,
-      },
-      {
-        text: 'Users',
-        id: 'members-quartz',
-        cloudOnly: true,
-        link: isFlagEnabled('unityUsers') ? `/orgs/${orgID}/users` : null,
-        href: isFlagEnabled('unityUsers')
-          ? null
-          : `${CLOUD_URL}/organizations/${orgID}${CLOUD_USERS_PATH}`,
-      },
-      {
-        text: 'About',
-        id: 'about',
-        link: `/orgs/${orgID}/about`,
-      },
-    ]
+  const tabs: OrgPageTab[] = [
+    {
+      text: 'Members',
+      id: 'members-oss',
+      enabled: () => !CLOUD,
+      link: `/orgs/${orgID}/members`,
+    },
+    {
+      text: 'Users',
+      id: 'users',
+      enabled: () => CLOUD,
+      link: `/orgs/${orgID}/users`,
+    },
+    {
+      text: 'About',
+      id: 'about',
+      link: `/orgs/${orgID}/about`,
+    },
+  ]
 
-    return (
-      <Tabs orientation={Orientation.Horizontal} size={ComponentSize.Large}>
-        {tabs.map(t => {
+  return (
+    <Tabs orientation={Orientation.Horizontal} size={ComponentSize.Large}>
+      {tabs
+        .filter(t => {
+          if (t?.enabled) {
+            return t.enabled()
+          }
+          return true
+        })
+        .map(t => {
           let isActive = t.id === activeTab
-          let tab = <></>
-          if (t.id === 'members-oss' || t.id === 'members-cloud') {
-            if (activeTab === 'members') {
-              isActive = true
-            }
+          if (t.id === 'members-oss' && activeTab === 'members') {
+            isActive = true
           }
 
-          if (t.href) {
-            tab = (
-              <Tabs.Tab
-                key={t.id}
-                text={t.text}
-                id={t.id}
-                linkElement={className => (
-                  <a href={t.href} className={className} />
-                )}
-                active={isActive}
-              />
-            )
-          } else if (t.link) {
-            tab = (
-              <Tabs.Tab
-                key={t.id}
-                text={t.text}
-                id={t.id}
-                linkElement={className => (
-                  <Link to={t.link} className={className} />
-                )}
-                active={isActive}
-              />
-            )
-          }
-
-          if (t.cloudExclude) {
-            tab = <CloudExclude key={t.id}>{tab}</CloudExclude>
-          }
-
-          if (t.cloudOnly) {
-            tab = <CloudOnly key={t.id}>{tab}</CloudOnly>
-          }
-
-          if (t.featureFlag) {
-            tab = (
-              <FeatureFlag
-                key={t.id}
-                name={t.featureFlag}
-                equals={t.featureFlagValue}
-              >
-                {tab}
-              </FeatureFlag>
-            )
-          }
-
-          return tab
+          return (
+            <Tabs.Tab
+              key={t.id}
+              text={t.text}
+              id={t.id}
+              linkElement={className => (
+                <Link to={t.link} className={className} />
+              )}
+              active={isActive}
+            />
+          )
         })}
-      </Tabs>
-    )
-  }
+    </Tabs>
+  )
 }
 
 export default OrgNavigation
--- src/organizations/containers/OrgProfilePage.tsx
+++ src/organizations/containers/OrgProfilePage.tsx
@@ -1,55 +1,32 @@
 // Libraries
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
+import React, {FC} from 'react'
 import {Switch, Route} from 'react-router-dom'
 
 // Components
-import {ErrorHandling} from 'src/shared/decorators/errors'
 import OrgTabbedPage from 'src/organizations/components/OrgTabbedPage'
 import OrgHeader from 'src/organizations/components/OrgHeader'
-import {Grid, Page} from '@influxdata/clockface'
+import {Page} from '@influxdata/clockface'
 import OrgProfileTab from 'src/organizations/components/OrgProfileTab'
 import RenameOrgOverlay from 'src/organizations/components/RenameOrgOverlay'
+import DeleteOrgOverlay from 'src/organizations/components/DeleteOrgOverlay'
 
 // Utils
 import {pageTitleSuffixer} from 'src/shared/utils/pageTitles'
-import {getOrg} from 'src/organizations/selectors'
 
-// Types
-import {AppState, Organization} from 'src/types'
+const OrgProfilePage: FC = () => (
+  <>
+    <Page titleTag={pageTitleSuffixer(['About', 'Organization'])}>
+      <OrgHeader />
+      <OrgTabbedPage activeTab="about">
+        <OrgProfileTab />
+      </OrgTabbedPage>
+    </Page>
+    <Switch>
+      <Route path="/orgs/:orgID/about/rename" component={RenameOrgOverlay} />
+      {/* TODO(ariel): guard against this route for only free users */}
+      <Route path="/orgs/:orgID/about/delete" component={DeleteOrgOverlay} />
+    </Switch>
+  </>
+)
 
-interface StateProps {
-  org: Organization
-}
-
-@ErrorHandling
-class OrgProfilePage extends Component<StateProps> {
-  public render() {
-    const {org} = this.props
-
-    return (
-      <>
-        <Page titleTag={pageTitleSuffixer(['About', 'Organization'])}>
-          <OrgHeader />
-          <OrgTabbedPage activeTab="about" orgID={org.id}>
-            <Grid>
-              <Grid.Row>
-                <OrgProfileTab />
-              </Grid.Row>
-            </Grid>
-          </OrgTabbedPage>
-        </Page>
-        <Switch>
-          <Route
-            path="/orgs/:orgID/about/rename"
-            component={RenameOrgOverlay}
-          />
-        </Switch>
-      </>
-    )
-  }
-}
-
-const mstp = (state: AppState) => ({org: getOrg(state)})
-
-export default connect<StateProps>(mstp)(OrgProfilePage)
+export default OrgProfilePage

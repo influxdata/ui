101ad9ea2caeb0ada0591cb510bffc37619559ac
ChitlangeSahas <chitlangesahas@gmail.com>
2021-07-07 13:29:13 -0700

    refactor: use the new date time formatter instead of moment, in FunctionRunsRow

--- src/functions/components/FunctionRunsRow.tsx
+++ src/functions/components/FunctionRunsRow.tsx
@@ -14,9 +14,9 @@ import FunctionRunLogsOverlay from 'src/functions/components/FunctionRunLogsOver
 // Types
 import {FunctionRun} from 'src/client/managedFunctionsRoutes'
 import {DEFAULT_TIME_FORMAT} from 'src/shared/constants'
+import {FormattedDateTime} from '../../utils/datetime/FormattedDateTime'
 
 // Utils
-import {createDateTimeFormatter} from 'src/utils/datetime/formatters'
 
 interface Props {
   run: FunctionRun
@@ -24,22 +24,17 @@ interface Props {
 
 const FunctionRunsRow: FC<Props> = ({run}) => {
   const [overlayVisible, setOverlayVisibility] = useState(false)
-  const dateTimeIfy = (dt: string): string => {
-    if (!dt) {
-      return ''
-    }
-    const newdate = new Date(dt)
-    const formatter = createDateTimeFormatter(DEFAULT_TIME_FORMAT)
-    const formatted = formatter.format(newdate)
-
-    return formatted
-  }
 
   return (
     <IndexList.Row>
       <IndexList.Cell>{run.status}</IndexList.Cell>
       <IndexList.Cell>{run.id}</IndexList.Cell>
-      <IndexList.Cell>{dateTimeIfy(run.startedAt)}</IndexList.Cell>
+      <IndexList.Cell>
+        <FormattedDateTime
+          format={DEFAULT_TIME_FORMAT}
+          date={new Date(run.startedAt)}
+        />
+      </IndexList.Cell>
       <IndexList.Cell></IndexList.Cell>
       <IndexList.Cell>
         <Button

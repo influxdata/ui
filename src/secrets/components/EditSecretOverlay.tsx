1d6111771e5fec9e685191f3faf1f4a442410b3c
asalem <ariel.salem1989@gmail.com>
2021-07-09 15:05:04 -0700

    chore: this is almost done

--- src/secrets/components/EditSecretOverlay.tsx
+++ src/secrets/components/EditSecretOverlay.tsx
@@ -1,6 +1,7 @@
 // Libraries
 import React, {FC} from 'react'
 import {useHistory} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 // Components
 import {Overlay} from '@influxdata/clockface'
@@ -10,16 +11,18 @@ import EditSecretForm from 'src/secrets/components/EditSecretForm'
 import ErrorBoundary from 'src/shared/components/ErrorBoundary'
 import GetResources from 'src/resources/components/GetResources'
 import {ResourceType} from 'src/types'
+import {getOrg} from 'src/organizations/selectors'
 
 const EditSecretOverlay: FC = () => {
   const history = useHistory()
+  const orgId = useSelector(getOrg)
 
   return (
     <Overlay visible={true}>
       <Overlay.Container maxWidth={750}>
         <Overlay.Header
           title="Edit Secret"
-          onDismiss={() => history.goBack()}
+          onDismiss={() => history.push(`/orgs/${orgId}/settings/secrets`)}
           testID="modify-secret-overlay-header"
         />
         <Overlay.Body>

a8d38a455afa63989e847f2e598252208a1f7f8b
Jill R Pelavin <jpelavin@influxdata.com>
2021-07-12 17:05:02 -0400

    working on test

--- src/annotations/components/annotationForm/AnnotationForm.test.tsx
+++ src/annotations/components/annotationForm/AnnotationForm.test.tsx
@@ -3,6 +3,7 @@ import {renderWithRedux} from 'src/mockState'
 import {AnnotationForm} from 'src/annotations/components/annotationForm/AnnotationForm'
 import {fireEvent} from '@testing-library/react'
 
+
 //jest.mock('src/resources/components/GetResources')
 //jest.mock('src/annotations/actions/thunks')
 const setup = (annoType, startTime) => {
@@ -38,9 +39,9 @@ describe('Annotation Form Component tests', () => {
   })
 
   it('should disallow a start time in the future', () => {
-    const oneHourInFuture = startTime + 60 * 60 * 1000
+    const oneHourInFuture = startTime + (60 * 60 * 1000)
 
-    const {getByTestId} = setup('point', oneHourInFuture)
+    const {queryByTestId, getByTestId} = setup('point', oneHourInFuture)
 
     fireEvent.change(getByTestId('edit-annotation-message'), {
       target: {value: 'abcde'},
@@ -48,6 +49,13 @@ describe('Annotation Form Component tests', () => {
 
     expect(getByTestId('form--element-error')).toHaveTextContent('Start Time cannot be in the future')
 
+    // fix it; error should go away:
+    fireEvent.change(getByTestId('startTime-testID'), {
+      target: {value: startTime},
+    })
+
+    const ack= queryByTestId('form--element-error')
+    console.log(ack)
 
 
   })

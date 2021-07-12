035cb3afff3d372dddaf6cc269a2ad6b33d516cb
Jill R Pelavin <jpelavin@influxdata.com>
2021-07-12 12:29:02 -0400

    chore: starting to convert from e2e->unit test for annotation form (2: able to input text now)

--- src/annotations/components/annotationForm/AnnotationForm.test.tsx
+++ src/annotations/components/annotationForm/AnnotationForm.test.tsx
@@ -1,39 +1,47 @@
 import React from 'react'
 import {renderWithRedux} from 'src/mockState'
 import {AnnotationForm} from 'src/annotations/components/annotationForm/AnnotationForm'
+import {fireEvent} from '@testing-library/react'
 
-jest.mock('src/resources/components/GetResources')
-jest.mock('src/annotations/actions/thunks')
+//jest.mock('src/resources/components/GetResources')
+//jest.mock('src/annotations/actions/thunks')
 const setup = (annoType, startTime) => {
-    const doNothing = () => {}
+  const doNothing = () => {}
 
-    return renderWithRedux(<AnnotationForm
-     title='Add'
-     type={annoType}
-     startTime={startTime}
-     eventPrefix='testing'
-     onClose={doNothing}
-     onSubmit={doNothing}
-    />)
+  return renderWithRedux(
+    <AnnotationForm
+      title="Add"
+      type={annoType}
+      startTime={startTime}
+      eventPrefix="testing"
+      onClose={doNothing}
+      onSubmit={doNothing}
+    />
+  )
 }
-// <AnnotationForm
-//     title="Add"
-//     type={annoType}
-//     onClose={onClose}
-//     onSubmit={handleSubmit}
-//     startTime={startTime}
-//     endTime={endTime}
-//     eventPrefix={eventPrefix}
-// />
 
 describe('Annotation Form Component tests', () => {
-    const startTime = new Date().getTime()
+  const startTime = new Date().getTime()
 
+  it('should disallow an annotation without a message', () => {
+    const {queryByTestId, getByTestId} = setup('point', startTime)
 
+    expect(getByTestId('form--element-error')).toHaveTextContent(
+      'This field is required'
+    )
 
-    it('should disallow an annotation without a message', () => {
-        const {getByTestId} = setup('point', startTime)
-        
-        expect(getByTestId('form--element-error')).toHaveTextContent("This field is required")
+    fireEvent.change(getByTestId('edit-annotation-message'), {
+      target: {value: 'abcde'},
     })
+
+    expect(queryByTestId('form--element-error')).toBeNull()
+  })
+
+  it.skip('should disallow a start time in the future', () => {
+    const oneHourInFuture = startTime + 60 * 60 * 1000
+
+    const {getByTestId} = setup('point', oneHourInFuture)
+
+    //getByTestId('edit-annotation-message--default').set
+  })
 })

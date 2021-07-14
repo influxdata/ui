a42e3126b0c3717bf63b5bccac795d90614f8187
drdelambre <drdelambre@gmail.com>
2021-07-14 10:26:30 -0700

    feat: add events to data explorer export

--- src/dataExplorer/components/SaveAsOverlay.tsx
+++ src/dataExplorer/components/SaveAsOverlay.tsx
@@ -1,5 +1,5 @@
-import React, {PureComponent} from 'react'
-import {withRouter, RouteComponentProps} from 'react-router-dom'
+import React, {FC, useEffect, useState, useCallback} from 'react'
+import {useHistory} from 'react-router-dom'
 
 // Components
 import SaveAsCellForm from 'src/dataExplorer/components/SaveAsCellForm'
@@ -13,84 +13,73 @@ import {
   Orientation,
 } from '@influxdata/clockface'
 
+// Utils
+import {event} from 'src/cloud/utils/reporting'
+
 enum SaveAsOption {
   Dashboard = 'dashboard',
   Task = 'task',
   Variable = 'variable',
 }
 
-interface State {
-  saveAsOption: SaveAsOption
+const SaveAsOverlay: FC = () => {
+  const history = useHistory()
+  const [saveAsOption, setSaveAsOption] = useState(SaveAsOption.Dashboard)
+  const hide = useCallback(() => {
+    history.goBack()
+  }, [history])
+
+  useEffect(() => {
+    event('Data Explorer Save as Menu Changed', {menu: saveAsOption})
+  }, [saveAsOption])
+
+  let saveAsForm = <SaveAsCellForm dismiss={hide} />
+
+  if (saveAsOption === SaveAsOption.Task) {
+    saveAsForm = <SaveAsTaskForm dismiss={hide} />
+  } else if (saveAsOption === SaveAsOption.Variable) {
+    saveAsForm = <SaveAsVariable onHideOverlay={hide} />
+  }
+
+  return (
+    <Overlay visible={true}>
+      <Overlay.Container maxWidth={600}>
+        <Overlay.Header
+          title="Save As"
+          onDismiss={hide}
+          testID="save-as-overlay--header"
+        />
+        <Overlay.Body>
+          <Tabs.Container orientation={Orientation.Horizontal}>
+            <Tabs alignment={Alignment.Center} size={ComponentSize.Medium}>
+              <Tabs.Tab
+                id={SaveAsOption.Dashboard}
+                text="Dashboard Cell"
+                testID="cell--radio-button"
+                onClick={() => setSaveAsOption(SaveAsOption.Dashboard)}
+                active={saveAsOption === SaveAsOption.Dashboard}
+              />
+              <Tabs.Tab
+                id={SaveAsOption.Task}
+                text="Task"
+                testID="task--radio-button"
+                onClick={() => setSaveAsOption(SaveAsOption.Task)}
+                active={saveAsOption === SaveAsOption.Task}
+              />
+              <Tabs.Tab
+                id={SaveAsOption.Variable}
+                text="Variable"
+                testID="variable--radio-button"
+                onClick={() => setSaveAsOption(SaveAsOption.Variable)}
+                active={saveAsOption === SaveAsOption.Variable}
+              />
+            </Tabs>
+            <Tabs.TabContents>{saveAsForm}</Tabs.TabContents>
+          </Tabs.Container>
+        </Overlay.Body>
+      </Overlay.Container>
+    </Overlay>
+  )
 }
 
-class SaveAsOverlay extends PureComponent<RouteComponentProps, State> {
-  public state: State = {
-    saveAsOption: SaveAsOption.Dashboard,
-  }
-
-  render() {
-    const {saveAsOption} = this.state
-
-    return (
-      <Overlay visible={true}>
-        <Overlay.Container maxWidth={600}>
-          <Overlay.Header
-            title="Save As"
-            onDismiss={this.handleHideOverlay}
-            testID="save-as-overlay--header"
-          />
-          <Overlay.Body>
-            <Tabs.Container orientation={Orientation.Horizontal}>
-              <Tabs alignment={Alignment.Center} size={ComponentSize.Medium}>
-                <Tabs.Tab
-                  id={SaveAsOption.Dashboard}
-                  text="Dashboard Cell"
-                  testID="cell--radio-button"
-                  onClick={this.handleSetSaveAsOption}
-                  active={saveAsOption === SaveAsOption.Dashboard}
-                />
-                <Tabs.Tab
-                  id={SaveAsOption.Task}
-                  text="Task"
-                  testID="task--radio-button"
-                  onClick={this.handleSetSaveAsOption}
-                  active={saveAsOption === SaveAsOption.Task}
-                />
-                <Tabs.Tab
-                  id={SaveAsOption.Variable}
-                  text="Variable"
-                  testID="variable--radio-button"
-                  onClick={this.handleSetSaveAsOption}
-                  active={saveAsOption === SaveAsOption.Variable}
-                />
-              </Tabs>
-              <Tabs.TabContents>{this.saveAsForm}</Tabs.TabContents>
-            </Tabs.Container>
-          </Overlay.Body>
-        </Overlay.Container>
-      </Overlay>
-    )
-  }
-
-  private get saveAsForm(): JSX.Element {
-    const {saveAsOption} = this.state
-
-    if (saveAsOption === SaveAsOption.Dashboard) {
-      return <SaveAsCellForm dismiss={this.handleHideOverlay} />
-    } else if (saveAsOption === SaveAsOption.Task) {
-      return <SaveAsTaskForm dismiss={this.handleHideOverlay} />
-    } else if (saveAsOption === SaveAsOption.Variable) {
-      return <SaveAsVariable onHideOverlay={this.handleHideOverlay} />
-    }
-  }
-
-  private handleHideOverlay = () => {
-    this.props.history.goBack()
-  }
-
-  private handleSetSaveAsOption = (saveAsOption: SaveAsOption) => {
-    this.setState({saveAsOption})
-  }
-}
-
-export default withRouter(SaveAsOverlay)
+export default SaveAsOverlay

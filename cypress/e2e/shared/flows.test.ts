e20e6aa2bbb3f392ac4b966c7d85c64e38ed2e29
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2021-07-16 00:33:39 -0600

    test: start of e2e

--- cypress/e2e/shared/flows.test.ts
+++ cypress/e2e/shared/flows.test.ts
@@ -370,4 +370,135 @@ describe('Flows', () => {
     // visualizations should exist
     cy.getByTestID('giraffe-inner-plot').should('be.visible')
   })
+
+  it.only('Make sure map displays properly', () => {
+    const now = Date.now()
+    cy.writeData(
+      [
+        `test,container_name=cool dopeness=12 ${now - 1000}000000`,
+        `test,container_name=beans dopeness=18 ${now - 1200}000000`,
+        `test,container_name=cool dopeness=14 ${now - 1400}000000`,
+        `test,container_name=beans dopeness=10 ${now - 1600}000000`,
+      ],
+      'defbuck'
+    )
+    cy.getByTestID('create-flow--button')
+      .first()
+      .click()
+
+    cy.getByTestID('time-machine-submit-button').should('be.visible')
+
+    cy.getByTestID('panel-add-btn-0').click()
+
+    cy.getByTestID('add-flow-btn--rawFluxEditor').click()
+
+    cy.getByTestID('flows-delete-cell')
+      .eq(1)
+      .click()
+
+    cy.getByTestID('flow-bucket-selector').click()
+    cy.getByTestID('flow-bucket-selector--defbuck').click()
+    cy.getByTestID('measurement-selector test').click()
+
+    cy.getByTestID('time-machine-submit-button').click()
+
+    cy.getByTestID('panel-add-btn-0').click()
+
+    cy.getByTestID('add-flow-btn--visualization').click()
+
+    cy.getByTestID('slide-toggle').click()
+
+    cy.get('.flow-panel--header')
+      .eq(0)
+      .click()
+
+    // test for presentation mode state
+
+    cy.getByTestID('slide-toggle').click()
+  })
+
+  it('can create a bucket from the metric selector and verify it is selected', () => {
+    const newBucketName = 'IDontGiveABuck'
+    cy.getByTestID('create-flow--button')
+      .first()
+      .click()
+
+    cy.getByTestID('time-machine-submit-button').should('be.visible')
+    cy.getByTestID('page-title').click()
+    cy.getByTestID('renamable-page-title--input').type('My Flow {enter}')
+
+    cy.getByTestID('flow-bucket-selector')
+      .click()
+      .then(() => {
+        cy.getByTestID('flow-bucket-selector--create').click()
+      })
+
+    cy.getByTestID('overlay').should('exist')
+
+    cy.getByTestID('bucket-form-name').type(newBucketName)
+    cy.getByTestID('bucket-form-submit')
+      .click()
+      .then(() => {
+        cy.getByTestID('flow-bucket-selector').within(() => {
+          cy.contains(newBucketName).should('exist')
+        })
+      })
+  })
+
+  it('can execute preview, see results, change tags, execute preview, see different results', () => {
+    const newBucketName = 'lets goooo'
+    const now = Date.now()
+    cy.get<Organization>('@org').then(({id, name}: Organization) => {
+      cy.createBucket(id, name, newBucketName)
+    })
+    cy.writeData(
+      [
+        `test,container_name=cool dopeness=12 ${now - 1000}000000`,
+        `test,container_name=beans dopeness=18 ${now - 1200}000000`,
+        `test,container_name=cool dopeness=14 ${now - 1400}000000`,
+        `test,container_name=beans dopeness=10 ${now - 1600}000000`,
+      ],
+      newBucketName
+    )
+
+    cy.getByTestID('create-flow--button')
+      .first()
+      .click()
+    cy.getByTestID('time-machine-submit-button').should('be.visible')
+
+    cy.getByTestID('page-title').click()
+    cy.getByTestID('renamable-page-title--input').type('My Flow {enter}')
+
+    // select our bucket
+    cy.getByTestID('flow-bucket-selector').click()
+
+    cy.getByTestID(`flow-bucket-selector--${newBucketName}`).click()
+
+    // select measurement and field
+    cy.getByTestID('measurement-selector test').click()
+    cy.getByTestID('field-selector dopeness').click()
+
+    // select beans tag and click preview
+    cy.getByTestID('tag-selector beans').click()
+    cy.getByTestID('time-machine-submit-button').click()
+
+    // we should only see beans in the table
+    cy.getByTestID('simple-table').should('be.visible')
+    cy.getByTestID('table-cell beans')
+      .first()
+      .should('be.visible')
+    cy.getByTestID('table-cell cool').should('not.exist')
+
+    // change tag to cool and click preview
+    cy.getByTestID('tag-selector cool').click()
+    cy.getByTestID('time-machine-submit-button').click()
+
+    // we should only see cool in the table
+    cy.getByTestID('simple-table').should('be.visible')
+    cy.getByTestID('table-cell cool')
+      .first()
+      .should('be.visible')
+    cy.getByTestID('table-cell beans').should('not.exist')
+  })
+
 })

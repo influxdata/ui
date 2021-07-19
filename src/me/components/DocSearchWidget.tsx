b3f9b2a1a1cbabe087cc44c063eacb5729d0209e
Shmuel Lotman <shmuel.lotman@gmail.com>
2021-07-19 12:53:26 -0500

    feat: docs panel (#2043)
    * feat: docs panel
    
    * chore: remove unused vars
    
    * chore: update css
    
    * chore: put behind feature flag docSearchWidget
    
    * chore: remove unused test
    
    * chore: add default fallback

---
+++ src/me/components/DocSearchWidget.tsx
@@ -0,0 +1,18 @@
+// Libraries
+import React, {FC} from 'react'
+
+// Components
+import DocSearch from 'src/shared/search/DocSearch'
+
+import './DocSearchWidget.scss'
+
+const DocSearchWidget: FC = () => {
+  return (
+    <div className="WidgetSearch">
+      <DocSearch />
+      <p className="WidgetHelperText">Press CTRL + h on any page to search</p>
+    </div>
+  )
+}
+
+export default DocSearchWidget

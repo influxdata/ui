0f68f597764c4aa387a5927dd649acb7548dd45e
Beth Legesse <legessebl@gmail.com>
2021-07-20 10:58:49 -0500

    feat: description box

--- src/authorizations/components/CustomApiTokenOverlay.tsx
+++ src/authorizations/components/CustomApiTokenOverlay.tsx
@@ -1,7 +1,15 @@
-import React, {FC} from 'react'
+import React, {FC, useState} from 'react'
 
 // Components
-import {Overlay} from '@influxdata/clockface'
+import {
+  Overlay,
+  Form,
+  Input,
+  FlexBox,
+  AlignItems,
+  FlexDirection,
+  ComponentSize,
+} from '@influxdata/clockface'
 
 interface OwnProps {
   onClose: () => void
@@ -11,12 +19,36 @@ const CustomApiTokenOverlay: FC<OwnProps> = props => {
   const handleDismiss = () => {
     props.onClose()
   }
+  const [description, setDescription] = useState("")
+
+  const handleInputChange = (event) => {
+    setDescription(event.target.value)
+  }
+  
   return (
     <Overlay.Container maxWidth={500}>
       <Overlay.Header
         title="Generate a Personal Api Token"
         onDismiss={handleDismiss}
       />
+      <Overlay.Body>
+          <Form>
+            <FlexBox
+              alignItems={AlignItems.Center}
+              direction={FlexDirection.Column}
+              margin={ComponentSize.Large}
+            >
+              <Form.Element label="Description">
+                <Input
+                  placeholder="your_token_description"
+                  value={description}
+                  onChange={handleInputChange}
+                  testID="custome-api-token-input"
+                />
+              </Form.Element>
+            </FlexBox>
+          </Form>
+      </Overlay.Body>
     </Overlay.Container>
   )
 }

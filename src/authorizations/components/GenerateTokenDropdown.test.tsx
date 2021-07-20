4470baabea539834405770b3bc96347030a8f31a
Palak Bhojani <palak@influxdata.com>
2021-07-20 12:23:05 -0700

    fix: dropdown test

--- src/authorizations/components/GenerateTokenDropdown.test.tsx
+++ src/authorizations/components/GenerateTokenDropdown.test.tsx
@@ -4,13 +4,10 @@ import {fireEvent, screen} from '@testing-library/react'
 
 // Fixtures
 import {renderWithReduxAndRouter} from 'src/mockState'
-import {mockAppState} from 'src/mockAppState'
-import {RemoteDataState} from '@influxdata/clockface'
 
 jest.mock('src/shared/constants', () => ({
   CLOUD: true,
 }))
-
 
 const isFlagEnabledMock = jest.fn(() => {
   return false
@@ -43,7 +40,6 @@ describe('GenerateTokenDropdown', () => {
     expect(queryByText('All Access Token')).toBeVisible()
     expect(queryByText('Custom API Token')).toBeNull()
   })
-
   it('renders the new dropdown options when the flag is on', () => {
     isFlagEnabledMock.mockImplementationOnce(() => {
       return true
@@ -54,6 +50,6 @@ describe('GenerateTokenDropdown', () => {
 
     expect(queryByText('Read/Write Token')).toBeNull()
     expect(queryByText('All Access Token')).toBeVisible()
-    expect(queryByText('Custom API Token')).toBeNull()
+    expect(queryByText('Custom API Token')).toBeVisible()
   })
 })

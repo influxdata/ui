e95e73710ea1b682d4842e23947e686b084536ba
asalem <ariel.salem1989@gmail.com>
2021-07-20 10:26:11 -0700

    chore: fix test

--- src/buckets/actions/thunks.test.ts
+++ src/buckets/actions/thunks.test.ts
@@ -10,6 +10,10 @@ jest.mock('src/client', () => ({
   getBuckets: jest.fn(),
 }))
 
+jest.mock('src/shared/constants/index', () => ({
+  CLOUD: true,
+}))
+
 jest.mock('src/cloud/apis/demodata', () => ({
   fetchDemoDataBuckets: jest.fn(() => {
     const res: ReturnType<typeof fetchDemoDataBuckets> = Promise.resolve([
@@ -52,6 +56,8 @@ describe('buckets thunks', () => {
       mockGetBuckets(true)
 
       const bucks = await fetchAllBuckets('ord01')
+
+      console.log({bucks})
       expect(bucks.result).toContain('custom-id')
       expect(bucks.result).toContain('demo-bucket')
     })

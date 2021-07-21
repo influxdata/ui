e95584b5197531b688ceb54090bf401938bc919d
Jill R Pelavin <jpelavin@influxdata.com>
2021-07-21 17:41:28 -0400

    fix: code remediation  * renamed an argument for clarity (from str->formattedTimeStr)  * adding unit test;  for utc custom times NOT changing  * moved date/time functions to new utility class

--- src/dashboards/selectors/index.ts
+++ src/dashboards/selectors/index.ts
@@ -18,6 +18,7 @@ import {getTimezoneOffset} from 'src/dashboards/utils/getTimezoneOffset'
 
 // Constants
 import {DEFAULT_TIME_RANGE} from 'src/shared/constants/timeRanges'
+import {timesNeedConverting} from 'src/shared/utils/dateTimeUtils'
 
 export const getTimeRange = (state: AppState): TimeRange => {
   const contextID = currentContext(state)
@@ -26,25 +27,6 @@ export const getTimeRange = (state: AppState): TimeRange => {
   }
 
   return state.ranges[contextID] || DEFAULT_TIME_RANGE
-}
-
-/**
- *  is the string a valid UTC time without any time zone information?
- *  ie:  "2021-07-17T14:00:00.000Z" is valid
- *  if it has any timezone offset, then it is not a valid UTC time string
- *
- * @param str
- */
-function isUtcTime(str) {
-  const regex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/
-  return regex.test(str)
-}
-
-/** is the time already in utc format?  if so it does not need converting. */
-const timesNeedConverting = newTimeRange => {
-  // if the time range is *already* utc, does not need converting.  check just the first part, as
-  // they will be formatted the same
-  return !isUtcTime(newTimeRange.lower)
 }
 
 /**

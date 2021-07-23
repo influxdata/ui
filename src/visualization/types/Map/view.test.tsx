cec443cebc48637be535ec9722970c5c457c5db5
asalem <ariel.salem1989@gmail.com>
2021-07-23 14:12:56 -0700

    chore: mock the right path

--- src/visualization/types/Map/view.test.tsx
+++ src/visualization/types/Map/view.test.tsx
@@ -5,7 +5,7 @@ import properties from './properties'
 import {DEFAULT_THRESHOLDS_GEO_COLORS} from 'src/shared/constants/thresholds'
 import {CLOUD} from 'src/shared/constants'
 
-jest.mock('src/visualization/types/Map/api')
+jest.mock('src/client/mapsdRoutes')
 
 const table = {
   getColumn: jest.fn(),

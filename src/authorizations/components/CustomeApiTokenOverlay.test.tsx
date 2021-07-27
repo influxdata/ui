d74860fc3c89789f5b737a343dc0dade6df1d502
Beth Legesse <legessebl@gmail.com>
2021-07-27 09:53:27 -0500

    fix: prettier

--- src/authorizations/components/CustomeApiTokenOverlay.test.tsx
+++ src/authorizations/components/CustomeApiTokenOverlay.test.tsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import {render, fireEvent} from '@testing-library/react'
-import CustomApiTokenOverlay from './CustomApiTokenOverlay';
-
+import CustomApiTokenOverlay from './CustomApiTokenOverlay'
 
 describe('CustomeApitokenDescription', () => {
-    const props = {
-        onClose: () => true,
-    };
+  const props = {
+    onClose: () => true,
+  }
 
-    it('displays description box', () => {
-        const { getByText } = render(<CustomApiTokenOverlay {...props} />);
+  it('displays description box', () => {
+    const {getByText} = render(<CustomApiTokenOverlay {...props} />)
 
-        expect(getByText("Description")).toBeDefined();
-        expect(getByText("Generate a Personal Api Token")).toBeDefined();
-    });
+    expect(getByText('Description')).toBeDefined()
+    expect(getByText('Generate a Personal Api Token')).toBeDefined()
+  })
 
-    describe('when user inputs something into the description box', () => {
-        it('should update the component\'s state', () => {
-            const { getByTestId } = render(<CustomApiTokenOverlay {...props} />);
+  describe('when user inputs something into the description box', () => {
+    it("should update the component's state", () => {
+      const {getByTestId} = render(<CustomApiTokenOverlay {...props} />)
 
-            fireEvent.change(getByTestId("custome-api-token-input"), { target: { value: 'chocolate' } });
+      fireEvent.change(getByTestId('custome-api-token-input'), {
+        target: {value: 'chocolate'},
+      })
 
-            const element = getByTestId("custome-api-token-input");
-            expect(element['value']).toEqual('chocolate');
-        });
-    });
+      const element = getByTestId('custome-api-token-input')
+      expect(element['value']).toEqual('chocolate')
+    })
+  })
 })

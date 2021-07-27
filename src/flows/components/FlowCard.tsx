51269dfe0d9164c8f01306044009461b980052de
asalem <ariel.salem1989@gmail.com>
2021-07-27 15:27:52 -0700

    chore: WIP until other meta data changes are available

--- src/flows/components/FlowCard.tsx
+++ src/flows/components/FlowCard.tsx
@@ -1,10 +1,15 @@
-import React, {FC} from 'react'
+import React, {FC, useContext} from 'react'
 import {useParams, useHistory} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 // Components
 import {ResourceCard} from '@influxdata/clockface'
 import FlowContextMenu from 'src/flows/components/FlowContextMenu'
 import {PROJECT_NAME, PROJECT_NAME_PLURAL} from 'src/flows'
+import {FlowListContext} from 'src/flows/context/flow.list'
+
+// Utils
+import {getMe} from 'src/me/selectors'
 
 interface Props {
   id: string
@@ -13,7 +18,10 @@ interface Props {
 
 const FlowCard: FC<Props> = ({id, name}) => {
   const {orgID} = useParams<{orgID: string}>()
+  const {update} = useContext(FlowListContext)
+
   const history = useHistory()
+  const user = useSelector(getMe)
 
   const handleClick = () => {
     history.push(`/orgs/${orgID}/${PROJECT_NAME_PLURAL.toLowerCase()}/${id}`)
@@ -21,15 +29,28 @@ const FlowCard: FC<Props> = ({id, name}) => {
 
   const contextMenu = <FlowContextMenu id={id} name={name} />
 
+  let userName = user.name
+
+  if (name.includes('@')) {
+    userName = userName.split('@')[0]
+  }
+  userName = `${userName}-${PROJECT_NAME.toLowerCase()}-${new Date().toISOString()}`
+
+  const handleRenameNotebook = (name: string) => {
+    // TODO(ariel): once we get the flows PR up and running integrate this
+    update(id, {...flow, name})
+  }
+  // TODO(ariel): set the default different than NAME this PROJECT
   return (
     <ResourceCard
       key={`flow-card--${id}`}
       contextMenu={contextMenu}
       testID={`flow-card--${name}`}
     >
-      <ResourceCard.Name
-        name={name || `Name this ${PROJECT_NAME}`}
+      <ResourceCard.EditableName
+        name={name || userName}
         onClick={handleClick}
+        onUpdate={handleRenameTask}
       />
     </ResourceCard>
   )

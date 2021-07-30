83fd810140a885b365787513218b12fd3b514014
drdelambre <drdelambre@gmail.com>
2021-07-30 11:19:08 -0700

    feat: updating icon for sidebar

--- src/flows/components/Sidebar.tsx
+++ src/flows/components/Sidebar.tsx
@@ -104,7 +104,7 @@ export const MenuButton: FC<ButtonProps> = ({id}) => {
   return (
     <div ref={ref} style={{position: 'relative'}}>
       <Button
-        icon={IconFont.CogThick}
+        icon={IconFont.More}
         onClick={toggleSidebar}
         color={
           id === focused ? ComponentColor.Secondary : ComponentColor.Default

96b936b2a1a34b3de1662ab9b2e2367bf074a5a7
drdelambre <drdelambre@gmail.com>
2021-08-03 11:44:45 -0700

    fix: normalize where default notebooks are defined

---
+++ src/flows/templates/types/default.ts
@@ -0,0 +1,40 @@
+import {DEFAULT_TIME_RANGE} from 'src/shared/constants/timeRanges'
+import {AUTOREFRESH_DEFAULT} from 'src/shared/constants'
+import {PIPE_DEFINITIONS} from 'src/flows'
+
+export default register =>
+  register({
+    type: 'default',
+    init: () => ({
+      name: 'Welcome to Notebooks',
+      spec: {
+        readOnly: false,
+        range: DEFAULT_TIME_RANGE,
+        refresh: AUTOREFRESH_DEFAULT,
+        pipes: [
+          {
+            title: 'Select a Metric',
+            visible: true,
+            type: 'metricSelector',
+            ...JSON.parse(
+              JSON.stringify(PIPE_DEFINITIONS['metricSelector'].initial)
+            ),
+          },
+          {
+            title: 'Validate the Data',
+            visible: true,
+            type: 'visualization',
+            properties: {type: 'simple-table', showAll: false},
+          },
+          {
+            title: 'Visualize the Result',
+            visible: true,
+            type: 'visualization',
+            ...JSON.parse(
+              JSON.stringify(PIPE_DEFINITIONS['visualization'].initial)
+            ),
+          },
+        ],
+      },
+    }),
+  })

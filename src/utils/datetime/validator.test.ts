7c1fb2dd4d9743264e558a841a5355a02658e2c0
ChitlangeSahas <chitlangesahas@gmail.com>
2021-08-04 13:19:03 -0700

    chore: prettier and more test cases and validator cases

--- src/utils/datetime/validator.test.ts
+++ src/utils/datetime/validator.test.ts
@@ -3,13 +3,50 @@ import {isValid} from './validator'
 describe('validation tests', () => {
   it('should do the thing', function() {
     expect(isValid('1999-02-09 23:00:00', 'YYYY-MM-DD HH:mm:ss')).toBeTruthy()
-    // expect(isValid('1999-02-09 12:00:00 PM MST', 'YYYY-MM-DD hh:mm:ss a ZZ')).toBeTruthy()
-    expect(isValid('09/02/1999 23:00:00.000', 'DD/MM/YYYY HH:mm:ss.sss')).toBeTruthy()
-    expect(isValid('09/02/1999 11:00:00.000 PM', 'DD/MM/YYYY hh:mm:ss.sss a')).toBeTruthy()
-    expect(isValid('02/09/1999 23:00:00.000', 'MM/DD/YYYY HH:mm:ss.sss')).toBeTruthy()
-    expect(isValid('02/09/1999 11:00:00.000 PM', 'MM/DD/YYYY hh:mm:ss.sss a')).toBeTruthy()
-    expect(isValid('1999/02/09 23:00:00',  'YYYY/MM/DD HH:mm:ss')).toBeTruthy()
-    expect(isValid('1999/02/09 11:00:00 PM',  'YYYY/MM/DD hh:mm:ss a')).toBeTruthy()
-    expect(isValid('23:00',  'HH:mm')).toBeTruthy()
+    expect(
+      isValid('1999-02-09 12:00:00 PM -0700', 'YYYY-MM-DD hh:mm:ss a ZZ')
+    ).toBeTruthy()
+    expect(
+      isValid('09/02/1999 23:00:00.000', 'DD/MM/YYYY HH:mm:ss.sss')
+    ).toBeTruthy()
+    expect(
+      isValid('09/02/1999 11:00:00.000 PM', 'DD/MM/YYYY hh:mm:ss.sss a')
+    ).toBeTruthy()
+    expect(
+      isValid('02/09/1999 23:00:00.000', 'MM/DD/YYYY HH:mm:ss.sss')
+    ).toBeTruthy()
+    expect(
+      isValid('02/09/1999 11:00:00.000 PM', 'MM/DD/YYYY hh:mm:ss.sss a')
+    ).toBeTruthy()
+    expect(isValid('1999/02/09 23:00:00', 'YYYY/MM/DD HH:mm:ss')).toBeTruthy()
+    expect(
+      isValid('1999/02/09 11:00:00 PM', 'YYYY/MM/DD hh:mm:ss a')
+    ).toBeTruthy()
+    expect(isValid('23:00', 'HH:mm')).toBeTruthy()
+    expect(isValid('11:00 PM', 'hh:mm a')).toBeTruthy()
+    expect(isValid('23:00:00', 'HH:mm:ss')).toBeTruthy()
+    expect(isValid('11:00:00 PM', 'hh:mm:ss a')).toBeTruthy()
+    expect(isValid('23:00:00 -0700', 'HH:mm:ss ZZ')).toBeTruthy()
+    expect(isValid('11:00:00 PM -0700', 'hh:mm:ss a ZZ')).toBeTruthy()
+    expect(isValid('23:00:00.000', 'HH:mm:ss.sss')).toBeTruthy()
+    expect(isValid('11:00:00.000 PM', 'hh:mm:ss.sss a')).toBeTruthy()
+    expect(
+      isValid('February 9, 1999 23:00:00', 'MMMM D, YYYY HH:mm:ss')
+    ).toBeTruthy()
+    expect(
+      isValid('February 9, 1999 11:00:00 PM', 'MMMM D, YYYY hh:mm:ss a')
+    ).toBeTruthy()
+    expect(
+      isValid(
+        'Tuesday, February 9, 1999 23:00:00',
+        'dddd, MMMM D, YYYY HH:mm:ss'
+      )
+    ).toBeTruthy()
+    expect(
+      isValid(
+        'Tuesday, February 9, 1999 11:00:00 PM',
+        'dddd, MMMM D, YYYY hh:mm:ss a'
+      )
+    ).toBeTruthy()
   })
 })
--- src/utils/datetime/validator.ts
+++ src/utils/datetime/validator.ts
@@ -1,11 +1,9 @@
-import { isMatch } from 'date-fns'
+import {isMatch} from 'date-fns'
 import {DEFAULT_TIME_FORMAT} from '../../shared/constants'
-import {createDateTimeFormatter} from './formatters'
-
 
 const formatterToDateFnsMap: Map<string, string> = new Map([
   [DEFAULT_TIME_FORMAT, 'yyyy-MM-dd HH:mm:ss'],
-  ['YYYY-MM-DD hh:mm:ss a ZZ', 'yyyy-MM-dd hh:mm:ss a xxx'], // TBD
+  ['YYYY-MM-DD hh:mm:ss a ZZ', 'yyyy-MM-dd hh:mm:ss a xxxx'], // TBD
   ['DD/MM/YYYY HH:mm:ss.sss', 'dd/MM/yyyy HH:mm:ss.SSS'],
   ['DD/MM/YYYY hh:mm:ss.sss a', 'dd/MM/yyyy hh:mm:ss.SSS bb'],
   ['MM/DD/YYYY HH:mm:ss.sss', 'MM/dd/yyyy HH:mm:ss.SSS'],
@@ -13,21 +11,32 @@ const formatterToDateFnsMap: Map<string, string> = new Map([
   ['YYYY/MM/DD HH:mm:ss', 'yyyy/MM/dd HH:mm:ss'],
   ['YYYY/MM/DD hh:mm:ss a', 'yyyy/MM/dd hh:mm:ss bb'],
   ['HH:mm', 'HH:mm'],
-]);
+  ['hh:mm a', 'hh:mm bb'],
+  ['HH:mm:ss', 'HH:mm:ss'],
+  ['hh:mm:ss a', 'hh:mm:ss bb'],
+  ['HH:mm:ss ZZ', 'HH:mm:ss xxxx'],
+  ['hh:mm:ss a ZZ', 'hh:mm:ss bb xxxx'],
+  ['HH:mm:ss.sss', 'HH:mm:ss.SSS'],
+  ['hh:mm:ss.sss a', 'hh:mm:ss.SSS bb'],
+  ['MMMM D, YYYY HH:mm:ss', 'LLLL d, yyyy HH:mm:ss'],
+  ['MMMM D, YYYY hh:mm:ss a', 'LLLL d, yyyy hh:mm:ss bb'],
+  ['dddd, MMMM D, YYYY HH:mm:ss', 'EEEE, LLLL d, yyyy HH:mm:ss'],
+  ['dddd, MMMM D, YYYY hh:mm:ss a', 'EEEE, LLLL d, yyyy hh:mm:ss bb'],
+])
 
 const getDateFnsFormatString = (format: string): string => {
-
-  const dateFnsFormatString = formatterToDateFnsMap.get(format) ? formatterToDateFnsMap.get(format) : DEFAULT_TIME_FORMAT
+  const dateFnsFormatString = formatterToDateFnsMap.get(format)
+    ? formatterToDateFnsMap.get(format)
+    : DEFAULT_TIME_FORMAT
 
   return dateFnsFormatString
 }
 
-export const isValid = (formattedDateTimeString: string, format: string): boolean => {
+export const isValid = (
+  formattedDateTimeString: string,
+  format: string
+): boolean => {
   const dateFnsFormatString = getDateFnsFormatString(format)
-
-  const formatter = createDateTimeFormatter(format)
-
-  console.log(formatter.format(new Date(formattedDateTimeString)))
 
   return isMatch(formattedDateTimeString, dateFnsFormatString)
 }

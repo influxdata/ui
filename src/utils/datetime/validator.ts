afdea8b6217e33a6425b34c3c0d5a42cf163d247
ChitlangeSahas <chitlangesahas@gmail.com>
2021-08-09 14:58:39 -0700

    fix: one regex

--- src/utils/datetime/validator.ts
+++ src/utils/datetime/validator.ts
@@ -5,7 +5,7 @@ const formatterToDateFnsMap = {
     format: 'yyyy-MM-dd HH:mm:ss',
     regex: /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/,
   }, // DEFAULT_TIME_FORMAT
-  'YYYY-MM-DD': {format: 'yyyy-MM-dd', regex: /\d{4}-\d{2}-\d{2} \d{2}:\d{2}/},
+  'YYYY-MM-DD': {format: 'yyyy-MM-dd', regex: /\d{4}-\d{2}-\d{2}/},
   'YYYY-MM-DD HH:mm': {
     format: 'yyyy-MM-dd HH:mm',
     regex: /\d{4}-\d{2}-\d{2} \d{2}:\d{2}/,

64511c7e8d260ca9061b5c02cb1c105fb3513a88
ChitlangeSahas <chitlangesahas@gmail.com>
2021-08-09 10:04:20 -0700

    fix: use plain javascript object instead.

--- src/utils/datetime/validator.ts
+++ src/utils/datetime/validator.ts
@@ -1,32 +1,32 @@
 import {isMatch} from 'date-fns'
-import {DEFAULT_TIME_FORMAT} from '../../shared/constants'
+import {DEFAULT_TIME_FORMAT} from 'src/shared/constants'
 
-const formatterToDateFnsMap: Map<string, string> = new Map([
-  [DEFAULT_TIME_FORMAT, 'yyyy-MM-dd HH:mm:ss'],
-  ['YYYY-MM-DD hh:mm:ss a ZZ', 'yyyy-MM-dd hh:mm:ss a xxxx'], // TBD
-  ['DD/MM/YYYY HH:mm:ss.sss', 'dd/MM/yyyy HH:mm:ss.SSS'],
-  ['DD/MM/YYYY hh:mm:ss.sss a', 'dd/MM/yyyy hh:mm:ss.SSS bb'],
-  ['MM/DD/YYYY HH:mm:ss.sss', 'MM/dd/yyyy HH:mm:ss.SSS'],
-  ['MM/DD/YYYY hh:mm:ss.sss a', 'MM/dd/yyyy hh:mm:ss.SSS bb'],
-  ['YYYY/MM/DD HH:mm:ss', 'yyyy/MM/dd HH:mm:ss'],
-  ['YYYY/MM/DD hh:mm:ss a', 'yyyy/MM/dd hh:mm:ss bb'],
-  ['HH:mm', 'HH:mm'],
-  ['hh:mm a', 'hh:mm bb'],
-  ['HH:mm:ss', 'HH:mm:ss'],
-  ['hh:mm:ss a', 'hh:mm:ss bb'],
-  ['HH:mm:ss ZZ', 'HH:mm:ss xxxx'],
-  ['hh:mm:ss a ZZ', 'hh:mm:ss bb xxxx'],
-  ['HH:mm:ss.sss', 'HH:mm:ss.SSS'],
-  ['hh:mm:ss.sss a', 'hh:mm:ss.SSS bb'],
-  ['MMMM D, YYYY HH:mm:ss', 'LLLL d, yyyy HH:mm:ss'],
-  ['MMMM D, YYYY hh:mm:ss a', 'LLLL d, yyyy hh:mm:ss bb'],
-  ['dddd, MMMM D, YYYY HH:mm:ss', 'EEEE, LLLL d, yyyy HH:mm:ss'],
-  ['dddd, MMMM D, YYYY hh:mm:ss a', 'EEEE, LLLL d, yyyy hh:mm:ss bb'],
-])
+const formatterToDateFnsMap = {
+  'YYYY-MM-DD HH:mm:ss': 'yyyy-MM-dd HH:mm:ss', // DEFAULT_TIME_FORMAT
+  'YYYY-MM-DD hh:mm:ss a ZZ': 'yyyy-MM-dd hh:mm:ss a xxxx',
+  'DD/MM/YYYY HH:mm:ss.sss': 'dd/MM/yyyy HH:mm:ss.SSS',
+  'DD/MM/YYYY hh:mm:ss.sss a': 'dd/MM/yyyy hh:mm:ss.SSS bb',
+  'MM/DD/YYYY HH:mm:ss.sss': 'MM/dd/yyyy HH:mm:ss.SSS',
+  'MM/DD/YYYY hh:mm:ss.sss a': 'MM/dd/yyyy hh:mm:ss.SSS bb',
+  'YYYY/MM/DD HH:mm:ss': 'yyyy/MM/dd HH:mm:ss',
+  'YYYY/MM/DD hh:mm:ss a': 'yyyy/MM/dd hh:mm:ss bb',
+  'HH:mm': 'HH:mm',
+  'hh:mm a': 'hh:mm bb',
+  'HH:mm:ss': 'HH:mm:ss',
+  'hh:mm:ss a': 'hh:mm:ss bb',
+  'HH:mm:ss ZZ': 'HH:mm:ss xxxx',
+  'hh:mm:ss a ZZ': 'hh:mm:ss bb xxxx',
+  'HH:mm:ss.sss': 'HH:mm:ss.SSS',
+  'hh:mm:ss.sss a': 'hh:mm:ss.SSS bb',
+  'MMMM D, YYYY HH:mm:ss': 'LLLL d, yyyy HH:mm:ss',
+  'MMMM D, YYYY hh:mm:ss a': 'LLLL d, yyyy hh:mm:ss bb',
+  'dddd, MMMM D, YYYY HH:mm:ss': 'EEEE, LLLL d, yyyy HH:mm:ss',
+  'dddd, MMMM D, YYYY hh:mm:ss a': 'EEEE, LLLL d, yyyy hh:mm:ss bb',
+}
 
 const getDateFnsFormatString = (format: string): string => {
-  const dateFnsFormatString = formatterToDateFnsMap.get(format)
-    ? formatterToDateFnsMap.get(format)
+  const dateFnsFormatString = formatterToDateFnsMap[format]
+    ? formatterToDateFnsMap[format]
     : DEFAULT_TIME_FORMAT
 
   return dateFnsFormatString

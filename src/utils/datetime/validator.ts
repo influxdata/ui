610b806fb682f7543aa96c93429c454c4c6a4950
ChitlangeSahas <chitlangesahas@gmail.com>
2021-08-09 15:45:55 -0700

    fix: Default time format case

--- src/utils/datetime/validator.ts
+++ src/utils/datetime/validator.ts
@@ -1,10 +1,11 @@
 import {isMatch} from 'date-fns'
+import {DEFAULT_TIME_FORMAT} from 'src/utils/datetime/constants'
 
 const formatterToDateFnsMap = {
-  'YYYY-MM-DD HH:mm:ss': {
+  [DEFAULT_TIME_FORMAT]: {
     format: 'yyyy-MM-dd HH:mm:ss',
     regex: /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/,
-  }, // DEFAULT_TIME_FORMAT
+  },
   'YYYY-MM-DD': {format: 'yyyy-MM-dd', regex: /\d{4}-\d{2}-\d{2}/},
   'YYYY-MM-DD HH:mm': {
     format: 'yyyy-MM-dd HH:mm',

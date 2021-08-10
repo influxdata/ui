d94f983e015b42f1cb8b656b640a28b3420c0d14
Alex Boatwright <drdelambre@gmail.com>
2021-08-10 10:26:55 -0700

    feat: rename notifications but keep the type (#2303)

--- src/flows/pipes/Notification/index.ts
+++ src/flows/pipes/Notification/index.ts
@@ -7,7 +7,7 @@ export default register => {
     priority: 1,
     component: View,
     featureFlag: 'flow-panel--notification',
-    button: 'Notification',
+    button: 'Alert',
     initial: {
       interval: '10m',
       offset: '0s',
@@ -17,7 +17,7 @@ export default register => {
         value: 0,
       },
       message:
-        'Notification Rule: ${ r._notification_rule_name } triggered by check: ${ r._check_name }: ${ r._message }',
+        '${strings.title(v: r._type)} for ${r._source_measurement} triggered at ${time(v: r._source_timestamp)}!',
       endpoint: 'slack',
       endpointData: {
         url: 'https://hooks.slack.com/services/X/X/X',

cf852797275247c2eb0269143b926b8c992a023d
Joel Fried <jfried@influxdata.com>
2021-08-18 11:19:18 -0400

    chore: making a small no op coommit to a test file for pipeline testing purposes

--- cypress/e2e/shared/secrets.test.ts
+++ cypress/e2e/shared/secrets.test.ts
@@ -1,6 +1,6 @@
 import {Organization} from '../../src/types'
 
-describe('Secrets', () => {
+describe('Secrets test', () => {
   beforeEach(() => {
     cy.flush()
     return cy.signin().then(() => {

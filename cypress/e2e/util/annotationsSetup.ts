2a667d0c3be4b33e237cd82ca4fcbfbb9e7bf7e1
Gene Hynson <ghynson@influxdata.com>
2021-08-19 15:50:43 -0700

    test: edit the annotation via api body instead of ui (#2429)

--- cypress/e2e/util/annotationsSetup.ts
+++ cypress/e2e/util/annotationsSetup.ts
@@ -104,17 +104,36 @@ export const editAnnotation = (cy: Cypress.Chainable) => {
   cy.getByTestID('overlay--container')
     .filter(':visible')
     .within(() => {
-      cy.getByTestID('edit-annotation-message')
-        .should('be.visible')
-        .clear()
+      if (Cypress.browser.name === 'firefox') {
+        // interacting with the 'edit-annotation-message' box is unstable in Firefox, causes flaky tests.
+        // to work around the issue, lets update the annotation by intercepting the request body.
+        cy.intercept(
+          'PUT',
+          '**/annotations/*',
+          (req: any) =>
+            (req.body = {
+              ...req.body,
+              summary: 'lets edit this annotation...',
+            })
+        ).as('updateAnnotation')
+      } else {
+        // for Chrome et. al., continue interacting with the UI per usual
+        cy.getByTestID('edit-annotation-message')
+          .should('be.visible')
+          .clear()
 
-      cy.getByTestID('edit-annotation-message')
-        .should('be.visible')
-        .type('lets edit this annotation...')
+        cy.getByTestID('edit-annotation-message')
+          .should('be.visible')
+          .type('lets edit this annotation...')
+
+        cy.intercept('PUT', '**/annotations/*').as('updateAnnotation')
+      }
 
       cy.getByTestID('annotation-submit-button')
         .should('be.visible')
         .click()
+
+      cy.wait('@updateAnnotation')
     })
 }
 

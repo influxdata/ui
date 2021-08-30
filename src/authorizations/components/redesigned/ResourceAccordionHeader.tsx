2b19efaf48fe40004e78db4120c58d7210208ac0
ChitlangeSahas <chitlangesahas@gmail.com>
2021-08-30 09:40:08 -0700

    fix: ResourceAccordionHeader.tsx

--- src/authorizations/components/redesigned/ResourceAccordionHeader.tsx
+++ src/authorizations/components/redesigned/ResourceAccordionHeader.tsx
@@ -65,6 +65,9 @@ export const ResourceAccordionHeader: FC<OwnProps> = props => {
         stretchToFitWidth={true}
         alignItems={AlignItems.Center}
         style={{textAlign: 'start'}}
+        onClick={(e: any) => {
+          e.stopPropagation()
+        }}
       >
         <FlexBox.Child basis={40} grow={8}>
           <InputLabel size={ComponentSize.Medium}>{title}</InputLabel>

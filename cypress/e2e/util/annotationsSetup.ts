635b0f6c1050155c5c7c008d3390d4bde2e77c14
Dong Kyun David Kim <dkim@influxdata.com>
2021-09-01 11:01:01 -0400

    chore: fix annotations test by making cypress press enter after typing cell name

--- cypress/e2e/util/annotationsSetup.ts
+++ cypress/e2e/util/annotationsSetup.ts
@@ -54,7 +54,7 @@ export const setupData = (cy: Cypress.Chainable, plotTypeSuffix = '') => {
                 cy.getByTestID('page-title').click()
                 cy.getByTestID('renamable-page-title--input')
                   .clear()
-                  .type('blah')
+                  .type('blah{enter}')
                 cy.getByTestID('save-cell--button').click()
               })
 

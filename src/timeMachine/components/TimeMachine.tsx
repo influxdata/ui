8d242b819c825aef739bd2ccb743918d2fa22a29
ChitlangeSahas <chitlangesahas@gmail.com>
2021-09-01 22:11:14 -0700

    fix: simpleTable tests

--- src/timeMachine/components/TimeMachine.tsx
+++ src/timeMachine/components/TimeMachine.tsx
@@ -18,7 +18,7 @@ import {getActiveTimeMachine} from 'src/timeMachine/selectors'
 import {AppState, TimeMachineTab} from 'src/types'
 import ErrorBoundary from 'src/shared/components/ErrorBoundary'
 
-const INITIAL_RESIZER_HANDLE = 0.35
+const INITIAL_RESIZER_HANDLE = 0.30
 
 interface StateProps {
   activeTab: TimeMachineTab

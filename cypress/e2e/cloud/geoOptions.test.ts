1131b8679c23ed431d51b3bbc0070dab96e8754a
Gene Hynson <ghynson@influxdata.com>
2021-09-03 12:00:03 -0700

    chore: mas

--- cypress/e2e/cloud/geoOptions.test.ts
+++ cypress/e2e/cloud/geoOptions.test.ts
@@ -1,18 +1,18 @@
 import {Organization} from '../../../src/types'
 
 describe.skip('DataExplorer - Geo Map Type Customization Options', () => {
-  beforeEach(() => {
-    cy.flush()
-
-    cy.signin().then(() => {
-      cy.get('@org').then(({id}: Organization) => {
-        cy.createMapVariable(id)
-        cy.fixture('routes').then(({orgs, explorer}) => {
-          cy.visit(`${orgs}/${id}${explorer}`)
+  beforeEach(() =>
+    cy.flush().then(() =>
+      cy.signin().then(() => {
+        cy.get('@org').then(({id}: Organization) => {
+          cy.createMapVariable(id)
+          cy.fixture('routes').then(({orgs, explorer}) => {
+            cy.visit(`${orgs}/${id}${explorer}`)
+          })
         })
       })
-    })
-  })
+    )
+  )
   describe('map type options customizations for geo type', () => {
     beforeEach(() => {
       cy.getByTestID('view-type--dropdown').click()

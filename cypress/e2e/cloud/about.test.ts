3cf542531ca5f55a32c53c789c85ea371949181e
Joel Fried <jfried@influxdata.com>
2021-09-03 16:53:44 -0400

    chore: ok let's run this baby

--- cypress/e2e/cloud/about.test.ts
+++ cypress/e2e/cloud/about.test.ts
@@ -1,7 +1,7 @@
 import {Organization} from '../../../src/types'
 
 // resolve flakey behavior for the feature flag setting
-describe.skip('About Page for free users with only 1 user', () => {
+describe.skip('About Page for free users with only 1 user ', () => {
   beforeEach(() =>
     cy.flush().then(() =>
       cy.signin().then(() => {

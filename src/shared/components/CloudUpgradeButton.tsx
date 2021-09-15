7f7cef8493b0a603f141c27b2c1dc82bb02fccdf
David Kim <dkim@influxdata.com>
2021-09-15 11:58:50 -0400

    fix: cosmetic issues on homepage (#2617)
    * fix: cosmetic issues on homepage
    
    * chore: remove nav bar upgrade button

--- src/shared/components/CloudUpgradeButton.tsx
+++ src/shared/components/CloudUpgradeButton.tsx
@@ -8,7 +8,6 @@ import {useHistory} from 'react-router-dom'
 import {
   IconFont,
   Button,
-  ComponentColor,
   ComponentSize,
   ButtonShape,
 } from '@influxdata/clockface'
@@ -51,11 +50,14 @@ const CloudUpgradeButton: FC<OwnProps> = ({
         <Button
           icon={IconFont.CrownSolid}
           className={cloudUpgradeButtonClass}
-          color={ComponentColor.Success}
           size={size}
           shape={ButtonShape.Default}
           onClick={handleClick}
           text={buttonText}
+          style={{
+            background:
+              'linear-gradient(45deg, rgb(52, 187, 85) 0%, rgb(0, 163, 255) 100%)',
+          }}
           testID="cloud-upgrade--button"
         />
       )}

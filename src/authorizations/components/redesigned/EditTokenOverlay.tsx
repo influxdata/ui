2fa3294572455bf488b4862f423dfca8ccf93945
Bethelhem Legesse <legessebl@gmail.com>
2021-09-16 11:32:11 -0500

    feat: Edit overlay permissions (#2614)
    * fix: prettier
    
    * feat: manipulate perms
    
    * feat: permissions object logic
    
    * feat: fetching all resources
    
    * fix: prettier
    
    * fix: refactor with reduce function
    
    * feat: add accordion on the overlay
    
    * feat: added accordion body
    
    * feat: disabled accordion toggle
    
    * chore: moved permission func into utils
    
    * fix: prettier
    
    * chore: clean up
    
    * fix: prettier
    
    * chore: clean up
    
    * fix: prettier
    
    Co-authored-by: Palak Bhojani <palak@influxdata.com>

--- src/authorizations/components/redesigned/EditTokenOverlay.tsx
+++ src/authorizations/components/redesigned/EditTokenOverlay.tsx
@@ -18,12 +18,16 @@ import {
   ComponentColor,
   Page,
 } from '@influxdata/clockface'
+import {EditResourceAccordion} from 'src/authorizations/components/redesigned/EditResourceAccordion'
 
 // Types
 import {Authorization} from 'src/types'
 
 // Actions
 import {updateAuthorization} from 'src/authorizations/actions/thunks'
+
+// Utills
+import {formatPermissionsObj} from 'src/authorizations/utils/permissions'
 
 interface OwnProps {
   auth: Authorization
@@ -99,6 +103,41 @@ const EditTokenOverlay: FC<Props> = props => {
                 />
               </Form.Element>
             </FlexBox>
+            <FlexBox.Child className="main-flexbox-child">
+              <FlexBox
+                margin={ComponentSize.Large}
+                justifyContent={JustifyContent.SpaceBetween}
+                direction={FlexDirection.Row}
+                stretchToFitWidth={true}
+                alignItems={AlignItems.Center}
+                className="flex-box-label"
+              >
+                <FlexBox.Child basis={40} grow={8}>
+                  <InputLabel size={ComponentSize.ExtraSmall}>
+                    Resources
+                  </InputLabel>
+                </FlexBox.Child>
+                <FlexBox.Child grow={1} className="flexbox-child-label-read">
+                  <InputLabel
+                    className="input-label-read"
+                    size={ComponentSize.ExtraSmall}
+                  >
+                    Read
+                  </InputLabel>
+                </FlexBox.Child>
+                <FlexBox.Child grow={1} className="flexbox-child-label-write">
+                  <InputLabel
+                    className="input-label-write"
+                    size={ComponentSize.ExtraSmall}
+                  >
+                    Write
+                  </InputLabel>
+                </FlexBox.Child>
+              </FlexBox>
+              <EditResourceAccordion
+                permissions={formatPermissionsObj(props.auth.permissions)}
+              />
+            </FlexBox.Child>
             <Page.ControlBarCenter>
               <FlexBox margin={ComponentSize.Medium}>
                 <Button

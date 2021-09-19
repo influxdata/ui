cb04dc5918f64edc0870f665a00a0f16765b2dc3
Gene Hynson <ghynson@influxdata.com>
2021-09-18 20:36:16 -0700

    fix: add key to comp in list, lint

--- src/flows/pipes/QueryBuilder/readOnly.tsx
+++ src/flows/pipes/QueryBuilder/readOnly.tsx
@@ -1,10 +1,10 @@
 // Libraries
 import React, {FC, useContext} from 'react'
 import {
-    ComponentStatus,
-    DapperScrollbars,
-    Dropdown,
-    IconFont,
+  ComponentStatus,
+  DapperScrollbars,
+  Dropdown,
+  IconFont,
 } from '@influxdata/clockface'
 
 // Types
@@ -20,68 +20,78 @@ import BuilderCard from 'src/timeMachine/components/builderCard/BuilderCard'
 import SelectorList from 'src/timeMachine/components/SelectorList'
 
 const QueryBuilder: FC<PipeProp> = ({Context}) => {
-    const {data} = useContext(PipeContext)
+  const {data} = useContext(PipeContext)
 
-    return (
-        <BucketProvider panel={data.id}>
-            <QueryBuilderProvider>
-                <Context resizes>
-                    <div className="query-builder" data-testid="query-builder">
-                        <div className="query-builder--cards">
-                            <DapperScrollbars noScrollY={true}>
-                                <div className="builder-card--list">
-                                    <BuilderCard testID="bucket-selector">
-                                    <BuilderCard.Header title="From" />
-                                    <BuilderCard.Menu>
-                                    <Dropdown
-                                        button={(active, onClick) => (
-                                            <Dropdown.Button
-                                                onClick={onClick}
-                                                active={active}
-                                                icon={IconFont.BucketSolid}
-                                                status={ComponentStatus.Disabled}
-                                            >
-                                                {data.buckets[0] ?? 'No Bucket Selected'}
-                                            </Dropdown.Button>
-                                        )}
-                                        menu={onCollapse => <Dropdown.Menu onCollapse={onCollapse} />}
-                                        style={{width: '250px', flex: '0 0 250px'}}
-                                    />
-                                    </BuilderCard.Menu>
-                                    </BuilderCard>
-                                    {data.tags.map(tag => (
-                                        <BuilderCard>
-                                            <BuilderCard.Header title={tag.aggregateFunctionType.toUpperCase()} />
-                                            <Dropdown
-                                                button={(active, onClick) => (
-                                                    <Dropdown.Button
-                                                        onClick={onClick}
-                                                        active={active}
-                                                        icon={tag.aggregateFunctionType === 'filter' ? IconFont.FunnelSolid : IconFont.Link}
-                                                        status={ComponentStatus.Disabled}
-                                                    >
-                                                        {tag.key}
-                                                    </Dropdown.Button>
-                                                )}
-                                                menu={onCollapse => <Dropdown.Menu onCollapse={onCollapse} />}
-                                                style={{width: '250px'}}
-                                            />
-                                            <SelectorList
-                                                items={tag.values}
-                                                selectedItems={tag.values}
-                                                onSelectItem={() => {}}
-                                                multiSelect
-                                            />
-                                        </BuilderCard>
-                                    ))}
-                                </div>
-                            </DapperScrollbars>
-                        </div>
-                    </div>
-                </Context>
-            </QueryBuilderProvider>
-        </BucketProvider>
-    )
+  return (
+    <BucketProvider panel={data.id}>
+      <QueryBuilderProvider>
+        <Context resizes>
+          <div className="query-builder" data-testid="query-builder">
+            <div className="query-builder--cards">
+              <DapperScrollbars noScrollY={true}>
+                <div className="builder-card--list">
+                  <BuilderCard testID="bucket-selector">
+                    <BuilderCard.Header title="From" />
+                    <BuilderCard.Menu>
+                      <Dropdown
+                        button={(active, onClick) => (
+                          <Dropdown.Button
+                            onClick={onClick}
+                            active={active}
+                            icon={IconFont.BucketSolid}
+                            status={ComponentStatus.Disabled}
+                          >
+                            {data.buckets[0] ?? 'No Bucket Selected'}
+                          </Dropdown.Button>
+                        )}
+                        menu={onCollapse => (
+                          <Dropdown.Menu onCollapse={onCollapse} />
+                        )}
+                        style={{width: '250px', flex: '0 0 250px'}}
+                      />
+                    </BuilderCard.Menu>
+                  </BuilderCard>
+                  {data.tags.map(tag => (
+                    <BuilderCard key={tag.key}>
+                      <BuilderCard.Header
+                        title={tag.aggregateFunctionType.toUpperCase()}
+                      />
+                      <Dropdown
+                        button={(active, onClick) => (
+                          <Dropdown.Button
+                            onClick={onClick}
+                            active={active}
+                            icon={
+                              tag.aggregateFunctionType === 'filter'
+                                ? IconFont.FunnelSolid
+                                : IconFont.Link
+                            }
+                            status={ComponentStatus.Disabled}
+                          >
+                            {tag.key}
+                          </Dropdown.Button>
+                        )}
+                        menu={onCollapse => (
+                          <Dropdown.Menu onCollapse={onCollapse} />
+                        )}
+                        style={{width: '250px'}}
+                      />
+                      <SelectorList
+                        items={tag.values}
+                        selectedItems={tag.values}
+                        onSelectItem={() => {}}
+                        multiSelect
+                      />
+                    </BuilderCard>
+                  ))}
+                </div>
+              </DapperScrollbars>
+            </div>
+          </div>
+        </Context>
+      </QueryBuilderProvider>
+    </BucketProvider>
+  )
 }
 
 export default QueryBuilder

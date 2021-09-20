44a15ae9b714c6d46179b1d5756fd25c8c6f4256
drdelambre <drdelambre@gmail.com>
2021-09-20 15:24:34 -0700

    fix: moving read onlys out of there

--- src/flows/pipes/Notification/PagerDutyReadOnly.tsx
+++ src/flows/pipes/Notification/PagerDutyReadOnly.tsx
@@ -1,39 +1,18 @@
 import React, {FC, useContext} from 'react'
-import {useSelector} from 'react-redux'
 import {
   Form,
   Input,
   InputType,
   ComponentSize,
+  ComponentStatus,
   FlexBox,
   FlexDirection,
 } from '@influxdata/clockface'
 
 import {PipeContext} from 'src/flows/context/pipe'
-import {getOrg} from 'src/organizations/selectors'
 
 const PagerDuty: FC = () => {
-  const {data, update} = useContext(PipeContext)
-  const org = useSelector(getOrg)
-
-  const updater = (field, value) => {
-    update({
-      endpointData: {
-        ...data.endpointData,
-        [field]: value,
-      },
-    })
-  }
-
-  const updateUrl = evt => {
-    updater('url', evt.target.value)
-  }
-
-  const updateKey = evt => {
-    updater('key', evt.target.value)
-  }
-
-  const defaultEndpoint = `${window.location.origin}/org/${org.id}/alert-history`
+  const {data} = useContext(PipeContext)
 
   return (
     <FlexBox className="http-endpoint--flex" direction={FlexDirection.Column}>
@@ -41,10 +20,9 @@ const PagerDuty: FC = () => {
         <Input
           name="url"
           type={InputType.Text}
-          placeholder={defaultEndpoint}
           value={data.endpointData.url}
-          onChange={updateUrl}
           size={ComponentSize.Medium}
+          status={ComponentStatus.Disabled}
         />
       </Form.Element>
       <Form.Element label="Routing Key">
@@ -52,8 +30,8 @@ const PagerDuty: FC = () => {
           name="key"
           type={InputType.Password}
           value={data.endpointData.key}
-          onChange={updateKey}
           size={ComponentSize.Medium}
+          status={ComponentStatus.Disabled}
         />
       </Form.Element>
     </FlexBox>

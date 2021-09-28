0bcc21b62d34bcd25526ee2633f4cb49854ef68e
Jill R Pelavin <jpelavin@influxdata.com>
2021-09-28 19:17:49 -0400

    better testing

--- src/buckets/components/createBucketForm/MeasurementSchemaSection.test.tsx
+++ src/buckets/components/createBucketForm/MeasurementSchemaSection.test.tsx
@@ -13,25 +13,54 @@ const oneColumns = [
 ]
 
 const unkosher1 = [
+  {name: 'time', type: 'timestamp'},
+  {name: 23, type: 'tag'},
+  {name: 'service', type: 'tag'},
+  {name: 'fsRead', type: 'field', dataType: 'float'},
+  {name: 'fsWrite', type: 'field', dataType: 'float'},
+  {name: 'timeSignature', type: 'tag'},
+  {name: 'composer', type: 'tag'},
+  {name: 'instrument', type: 'field', dataType: 'string'},
+  {name: 'one', type: 'field', dataType: 'string'},
+]
+
+const columns1 = [
+  {name: 'time', type: 'timestamp'},
+  {name: 'host', type: 'tag'},
+  {name: 'service', type: 'tag'},
+  {name: 'fsRead', type: 'field', dataType: 'float'},
+  {name: 'fsWrite', type: 'field', dataType: 'float'},
+  {name: 'timeSignature', type: 'time'},
+]
+
+const columns2 = [
     {name: 'time', type: 'timestamp'},
-    {name: 23, type: 'tag'},
+    {name: 'host', type: 'tag'},
     {name: 'service', type: 'tag'},
-    {name: 'fsRead', type: 'field', dataType: 'float'},
+    {name: 'fsRead', type: 'field', dataType: 'double'},
     {name: 'fsWrite', type: 'field', dataType: 'float'},
     {name: 'timeSignature', type: 'tag'},
-    {name: 'composer', type: 'tag'},
-    {name: 'instrument', type: 'field', dataType: 'string'},
-    {name: 'one', type: 'field', dataType: 'string'},
 ]
 
 describe('test data validity function', () => {
+  const doTest = (data, expected) => {
+    const actual = areColumnsKosher(data)
+    expect(actual).toBe(expected)
+  }
+
   it('should be kosher', () => {
-    const actual = areColumnsKosher(oneColumns)
-    expect(actual).toBe(true)
+    doTest(oneColumns, true)
   })
 
-    it('should not be kosher, the name is not as string', () => {
-        const actual = areColumnsKosher(unkosher1)
-        expect(actual).toBe(false)
+  it('should not be kosher, the name is not as string', () => {
+    doTest(unkosher1, false)
+  })
+
+  it('should not be kosher, the type is wrong', () => {
+    doTest(columns1, false)
+  })
+
+    it('should not be kosher, the dataType is wrong', () => {
+        doTest(columns2, false)
     })
 })
--- src/buckets/components/createBucketForm/MeasurementSchemaUtils.ts
+++ src/buckets/components/createBucketForm/MeasurementSchemaUtils.ts
@@ -1,11 +1,41 @@
+// import {CLOUD} from 'src/shared/constants'
+//
+// let ColumnSemanticType = null
+//
+// if (CLOUD){
+//     ColumnSemanticType =   require('src/client/generatedRoutes').ColumnSemanticType
+// }
+
+//import {ColumnSemanticType} from "../../../client";
+
+const typeStrings = ['timestamp', 'tag', 'field']
+const dataTypeStrings = ['integer', 'float', 'boolean', 'string', 'unsigned']
+
+/**
+ * manually checking that the literal string union types are correct
+ *
+ * our types are generated, and changing it so there is an exported array that is *then* used
+ * for literal union type creation is out of scope.
+ * https://newbedev.com/checking-validity-of-string-literal-union-type-at-runtime
+ *
+ *
+ * right now, just checking true/false; is it valid?  later on, may add a message saying which part(s) are invalid
+ * */
 export const areColumnsKosher = columns => {
   if (Array.isArray(columns)) {
     const validArray = columns.map(item => {
       // for each item, does it have the necessary stuff?
-      const hasName = ('name' in item) && ((typeof item.name) === 'string')
-      const hasType = 'type' in item
+      const hasName = 'name' in item && typeof item.name === 'string'
+      const hasType = 'type' in item && typeStrings.includes(item.type)
 
-      return hasName && hasType
+      // optional part: if it isn't there; it is fine; but if it is check it!
+      let dataTypePartIsValid = true
+
+      if ('dataType' in item) {
+        dataTypePartIsValid = dataTypeStrings.includes(item.dataType)
+      }
+
+      return hasName && hasType && dataTypePartIsValid
     })
 
     return validArray.reduce((prevVal, curVal) => prevVal && curVal, true)

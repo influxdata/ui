2b33d3267fc007ed4c51638dce060b82bac4844d
Jill R Pelavin <jpelavin@influxdata.com>
2021-09-28 16:51:05 -0400

    starting to test kosherness....(3)

--- src/buckets/components/createBucketForm/MeasurementSchemaSection.test.tsx
+++ src/buckets/components/createBucketForm/MeasurementSchemaSection.test.tsx
@@ -12,9 +12,26 @@ const oneColumns = [
   {name: 'one', type: 'field', dataType: 'string'},
 ]
 
+const unkosher1 = [
+    {name: 'time', type: 'timestamp'},
+    {name: 23, type: 'tag'},
+    {name: 'service', type: 'tag'},
+    {name: 'fsRead', type: 'field', dataType: 'float'},
+    {name: 'fsWrite', type: 'field', dataType: 'float'},
+    {name: 'timeSignature', type: 'tag'},
+    {name: 'composer', type: 'tag'},
+    {name: 'instrument', type: 'field', dataType: 'string'},
+    {name: 'one', type: 'field', dataType: 'string'},
+]
+
 describe('test data validity function', () => {
   it('should be kosher', () => {
     const actual = areColumnsKosher(oneColumns)
     expect(actual).toBe(true)
   })
+
+    it('should not be kosher, the name is not as string', () => {
+        const actual = areColumnsKosher(unkosher1)
+        expect(actual).toBe(false)
+    })
 })
--- src/buckets/components/createBucketForm/MeasurementSchemaUtils.ts
+++ src/buckets/components/createBucketForm/MeasurementSchemaUtils.ts
@@ -2,7 +2,7 @@ export const areColumnsKosher = columns => {
   if (Array.isArray(columns)) {
     const validArray = columns.map(item => {
       // for each item, does it have the necessary stuff?
-      const hasName = 'name' in item
+      const hasName = ('name' in item) && ((typeof item.name) === 'string')
       const hasType = 'type' in item
 
       return hasName && hasType

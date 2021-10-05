f8763f9fa43b5e9504e6ff51b73339f02d30f797
karel rehor <karl.koerner@bonitoo.io>
2021-10-05 09:48:37 +0200

    test: start cypress notification history tests

--- cypress/e2e/shared/notificationRules.test.ts
+++ cypress/e2e/shared/notificationRules.test.ts
@@ -1,4 +1,13 @@
-import {SlackNotificationEndpoint, Organization} from '../../../src/types'
+import {
+  SlackNotificationEndpoint,
+  Organization,
+  GenEndpoint,
+  GenRule,
+  GenCheck,
+  NotificationEndpoint, NotificationRule,
+} from '../../../src/types'
+import {Bucket} from '../../../src/client'
+import {calcNanoTimestamp} from '../../support/Utils'
 
 // skipping these tests until we have a local vault instance running
 describe('NotificationRules', () => {
@@ -291,5 +300,240 @@ describe('NotificationRules', () => {
     // Remove the filter
     cy.getByTestID('filter--input rules').clear()
     cy.getByTestID('rule-card--name').should('have.length', 0)
+  })
+
+  describe.only('notification history', () => {
+    const endp: GenEndpoint= {
+      orgID: '',
+      name: 'Test HTTP Endpoint',
+      userID: '',
+      description: 'Special chars',
+      status: 'active',
+      type: 'http',
+      url: 'http://13.57.248.121:3000',
+      authMethod: 'none',
+      method: 'POST',
+    }
+
+    const rule: GenRule & { activeStatus: string } = {
+      type: 'http',
+      every: '1m',
+      offset: '0m',
+      url: '',
+      orgID: '',
+      name: 'Test Rule',
+      activeStatus: 'active',
+      status: 'active',
+      endpointID: "",
+      tagRules: [],
+      labels: [],
+      statusRules: [
+        {currentLevel: 'CRIT', period: '1h', count: 1},
+      ],
+      description: '',
+    }
+
+    const check: GenCheck = {
+      type: "deadman",
+      name: 'Ghost Check',
+      status: 'active',
+      orgID: '',
+      query: {
+        name: '',
+        text: '',
+        editMode: 'builder',
+        builderConfig: {
+          buckets: [],
+          tags: [
+            {key: '_measurement', values: ['wumpus'], aggregateFunctionType: 'filter'},
+            {key: '_field', values: ['mag'], aggregateFunctionType: 'filter'},
+            {key: 'foo', values: [], aggregateFunctionType: 'filter'}
+          ],
+          functions: []
+        },
+//        hidden: false
+      },
+      labels: [],
+      every: '1m',
+      level: 'CRIT',
+      offset: '0s',
+      reportZero: false,
+      staleTime: '10m',
+      statusMessageTemplate: 'Check: ${ r._check_name } is: ${ r._level }',
+      tags: [],
+      timeSince: '90s'
+    }
+
+    interface MonitoringRec {
+      check: GenCheck
+      endp: NotificationEndpoint
+      rule: GenRule
+      level: string
+      source: string
+      sent: string
+      valField: {key: string, val: string}
+      sourceTimestamp: number
+      statusTimestamp: number
+      message: string
+    }
+
+    const writeMonitoringRecord = (rec: MonitoringRec, offset: string) => {
+      const lp = `notifications,_level=${rec.level},_notification_endpoint_id=${rec.endp.id},` +
+        `_notification_endpoint_name=${rec.endp.name.replace(/ /g, "\\ ")},_notification_rule_id=${rec.rule.id},` +
+        `_notification_rule_name=${rec.rule.name.replace(/ /g, "\\ ")},` +
+        `_check_id=${rec.check.id},_check_name=${rec.check.name.replace(/ /g, "\\ ")},` +
+        `_sent=${rec.sent},_source_measurement=${rec.source},_type=deadman,${rec.valField.key}=${rec.valField.val.replace(/ /g, "\\ ")} ` + // end tags start fields
+        `_source_timestamp=${rec.sourceTimestamp},_status_timestamp=${rec.statusTimestamp},_message="${rec.message.replace(/ /g, "\ ")}",dead="true"`
+
+      cy.writeLPData({lines: [lp], offset: offset, namedBucket: '_monitoring'})
+
+    }
+
+    beforeEach('Generate alert records', () => {
+      cy.writeLPDataFromFile({
+        filename: 'data/wumpus01.lp',
+        offset: '20m',
+        stagger: '1m',
+      }).then(r => {
+        cy.log(`TSHOOT write data from file result ${JSON.stringify(r)}`)
+      })
+
+      cy.get<Organization>('@org').then( (org: Organization) => {
+        cy.get<Bucket>('@bucket').then((bucket: Bucket) => {
+          // get default org and bucket
+          // 2. create check
+          const queryText = `from(bucket: \"${bucket.name}\")\n  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)\n  |> filter(fn: (r) => r[\"_measurement\"] == \"wumpus\")\n  |> filter(fn: (r) => r[\"_field\"] == \"mag\")`
+          cy.createCheck({
+            ...check,
+            orgID: org.id,
+            query: {
+              ...check.query,
+              text: queryText,
+              builderConfig: {
+                ...check.query.builderConfig,
+                buckets: [bucket.id]
+              }
+            },
+          }).then(resp => {
+            cy.wrap(resp.body).as('check')
+          })
+
+          cy.createEndpoint({
+            ...(endp as NotificationEndpoint),
+            orgID: org.id
+          }).then(resp => {
+            cy.wrap(resp.body).as('endp')
+          })
+
+          cy.get<NotificationEndpoint>('@endp').then( endp => {
+            cy.createRule({
+              ...(rule as NotificationRule),
+              orgID: org.id,
+              endpointID: endp.id
+            }).then( resp => {
+              cy.wrap(resp.body).as('rule')
+            })
+
+          })
+        })
+      })
+      cy.get<GenCheck>('@check').then(check => {
+        cy.get<NotificationEndpoint>('@endp').then( endp => {
+          cy.get<GenRule>('@rule').then( rule => {
+
+            const now = new Date().getTime();
+
+            cy.log(`DEBUG now ${now}`)
+            const sourceTS1 = now - (1000 * 60 * 10)
+            cy.log(`DEBUG sourceTS1 ${new Date(sourceTS1)}`)
+            const tsResult = calcNanoTimestamp('10m')
+            cy.log(`DEBUG tsResult ${tsResult}`)
+
+            writeMonitoringRecord( {
+                check: check,
+                endp: endp,
+                rule: rule,
+                level: 'crit',
+                source: 'wumpus',
+                sent: 'true',
+                valField: {key: 'foo', val: 'bar'},
+                sourceTimestamp: calcNanoTimestamp('17m'),
+                statusTimestamp: calcNanoTimestamp('16m'),
+                message: 'This is a fake critical message'},
+              '15m'
+            )
+
+            writeMonitoringRecord( {
+                check: check,
+                endp: endp,
+                rule: rule,
+                level: 'info',
+                source: 'wumpus',
+                sent: 'false',
+                valField: {key: 'foo', val: 'bar'},
+                sourceTimestamp: calcNanoTimestamp('12m'),
+                statusTimestamp: calcNanoTimestamp('11m'),
+                message: 'This is a fake info message'},
+              '10m'
+            )
+            writeMonitoringRecord( {
+                check: check,
+                endp: endp,
+                rule: rule,
+                level: 'warn',
+                source: 'wumpus',
+                sent: 'true',
+                valField: {key: 'foo', val: 'bar'},
+                sourceTimestamp: calcNanoTimestamp('22m'),
+                statusTimestamp: calcNanoTimestamp('21m'),
+                message: 'This is a fake warning message'},
+              '20m'
+            )
+            writeMonitoringRecord( {
+                check: check,
+                endp: endp,
+                rule: rule,
+                level: 'ok',
+                source: 'wumpus',
+                sent: 'true',
+                valField: {key: 'foo', val: 'bar'},
+                sourceTimestamp: calcNanoTimestamp('7m'),
+                statusTimestamp: calcNanoTimestamp('6m'),
+                message: 'This is a fake OK message'},
+              '5m'
+            )
+          })
+        })
+      })
+    })
+
+    it('shows and filters history', () => {
+      cy.log('shows history')
+      cy.getByTestID('alerting-tab--rules').click()
+      cy.getByTestID(`rule-card ${rule.name}`).within(card => {
+        card.trigger('mouseover')
+        cy.getByTestID('context-history-menu').click()
+        cy.getByTestID('context-history-task').click()
+      })
+      cy.get('.event-row').should('have.length', 4)
+      cy.get('.event-row--field:nth-of-type(1)').then(timestamps => {
+        const stamps = timestamps.toArray().map((n) => new Date(n.innerText).getTime())
+        cy.log(`DEBUG stamps length ${stamps.length} content ${stamps} tupeof ${typeof stamps[0]}`)
+        cy.log(`DEBUG timestamps ${timestamps.text()} typeof ${typeof timestamps.text()}`)
+        // assert sort desc by date
+        for(let i = 1; i < stamps.length; i++){
+          cy.log(stamps[i-1] + ' ' + stamps[i])
+          expect(stamps[i-1]).to.be.above(stamps[i])
+        }
+      })
+      //verify first row
+      cy.get('[class$=ScrollContainer] > div:nth-of-type(1)').within((row) => {
+         cy.get('.level-table-field--ok').should('be.visible')
+         cy.get('.sent-table-field--sent').should('be.visible')
+         cy.get('.event-row--field:nth-of-type(3)').should('have.text', check.name)
+         cy.get('.event-row--field:nth-of-type(4)').should('have.text', rule.name)
+         cy.get('.event-row--field:nth-of-type(5)').should('have.text', endp.name)
+      })
+    })
   })
 })

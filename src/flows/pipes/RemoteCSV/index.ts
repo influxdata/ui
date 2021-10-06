0a2f9ddd847e01e131b8e5a7222e814b76699ab7
Gene Hynson <ghynson@influxdata.com>
2021-10-06 10:51:01 -0700

    feat(notebooks): auto-populate remote csv time range (#2795)

--- src/flows/pipes/RemoteCSV/index.ts
+++ src/flows/pipes/RemoteCSV/index.ts
@@ -17,13 +17,29 @@ export default register => {
       if (!data.url?.length) {
         return ''
       }
-      return data.sampleName === 'Custom'
-        ? `import "experimental/csv"
-      csv.from(url: "${data.url}")`
-        : `import "influxdata/influxdb/sample"
-      sample.data(
+      const dataQuery =
+        data.sampleName === 'Custom'
+          ? `import "experimental/csv"
+      data = csv.from(url: "${data.url}")`
+          : `import "influxdata/influxdb/sample"
+      data = sample.data(
         set: "${data.sampleName}"
       )`
+      return `${dataQuery}
+      csvRange = data
+          |> keep(columns: ["_time"])
+          |> group()
+          |> reduce(
+              fn: (r, accumulator) => ({
+                  min: if r._time < accumulator.min then r._time else accumulator.min,
+                  max: if r._time > accumulator.max then r._time else accumulator.max,
+              }),
+              identity: {min: now(), max: time(v: 0)},
+          )
+          |> findRecord(fn: (key) => true, idx: 0)
+
+      data
+          |> range(start: csvRange.min, stop: csvRange.max)`
     },
   })
 }

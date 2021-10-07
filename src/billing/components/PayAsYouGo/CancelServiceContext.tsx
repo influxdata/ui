bd7ad6bbccc62303687e18ac862f0ca92f580cd0
Subir Jolly <subirjolly@gmail.com>
2021-10-07 18:00:59 -0400

    feat: add feedback form to cancel service popup (#2827)
    * feat: add feedback form to cancel service popup
    
    * style: fix lint issues
    
    * chore: update per review
    
    * Update src/billing/components/PayAsYouGo/CancelServiceContext.tsx
    
    Co-authored-by: asalem1 <19984220+asalem1@users.noreply.github.com>
    
    * chore: update per review
    
    Co-authored-by: asalem1 <19984220+asalem1@users.noreply.github.com>

---
+++ src/billing/components/PayAsYouGo/CancelServiceContext.tsx
@@ -0,0 +1,90 @@
+// Libraries
+import React, {FC, createContext, useState, ReactChild} from 'react'
+
+interface Props {
+  children: ReactChild
+}
+
+export enum VariableItems {
+  NO_OPTION = '----',
+  USE_CASE_DIFFERENT = "It doesn't work for my use case",
+  SWITCHING_ORGANIZATION = 'I want to join my account to another organization',
+  ALTERNATIVE_PRODUCT = 'I found an alternative product',
+  RE_SIGNUP = 'I want to sign up for a new account using a marketplace option',
+  TOO_EXPENSIVE = 'It’s too expensive',
+  USABILITY_ISSUE = 'I found Flux hard to use',
+  UNSTABLE_PLATFORM = 'Platform isn’t stable enough',
+  UNSUPPORTED_HIGH_CARDINALITY = 'Platform doesn’t support my high Cardinality',
+  OTHER_REASON = 'Other reason',
+}
+
+export interface CancelServiceContextType {
+  shortSuggestion: string
+  isShortSuggestionEnabled: boolean
+  setShortSuggestionFlag: (_: boolean) => void
+  setShortSuggestion: (_: string) => void
+  suggestions: string
+  setSuggestions: (_: string) => void
+  reason: string
+  setReason: (_: string) => void
+  canContactForFeedback: boolean
+  toggleCanContactForFeedback: () => void
+}
+
+export const DEFAULT_CANCEL_SERVICE_CONTEXT: CancelServiceContextType = {
+  shortSuggestion: '',
+  isShortSuggestionEnabled: false,
+  setShortSuggestionFlag: (_: boolean) => null,
+  setShortSuggestion: (_: string) => null,
+  suggestions: '',
+  setSuggestions: (_: string) => null,
+  reason: 'NO_OPTION',
+  setReason: (_: string) => null,
+  canContactForFeedback: false,
+  toggleCanContactForFeedback: () => null,
+}
+
+export const CancelServiceContext = createContext<CancelServiceContextType>(
+  DEFAULT_CANCEL_SERVICE_CONTEXT
+)
+
+const CancelServiceProvider: FC<Props> = ({children}) => {
+  const [shortSuggestion, setShortSuggestion] = useState(
+    DEFAULT_CANCEL_SERVICE_CONTEXT.shortSuggestion
+  )
+  const [isShortSuggestionEnabled, setShortSuggestionFlag] = useState(
+    DEFAULT_CANCEL_SERVICE_CONTEXT.isShortSuggestionEnabled
+  )
+  const [suggestions, setSuggestions] = useState(
+    DEFAULT_CANCEL_SERVICE_CONTEXT.suggestions
+  )
+  const [reason, setReason] = useState(DEFAULT_CANCEL_SERVICE_CONTEXT.reason)
+  const [canContactForFeedback, setCanContactForFeedback] = useState(
+    DEFAULT_CANCEL_SERVICE_CONTEXT.canContactForFeedback
+  )
+
+  const toggleCanContactForFeedback = () => {
+    setCanContactForFeedback(prev => !prev)
+  }
+
+  return (
+    <CancelServiceContext.Provider
+      value={{
+        shortSuggestion,
+        isShortSuggestionEnabled,
+        setShortSuggestionFlag,
+        setShortSuggestion,
+        suggestions,
+        setSuggestions,
+        reason,
+        setReason,
+        canContactForFeedback,
+        toggleCanContactForFeedback,
+      }}
+    >
+      {children}
+    </CancelServiceContext.Provider>
+  )
+}
+
+export default CancelServiceProvider
---
+++ src/billing/components/PayAsYouGo/CancelServiceReasonsForm.tsx
@@ -0,0 +1,143 @@
+// Libraries
+import React, {useContext, useMemo} from 'react'
+
+// Components
+import {
+  AlignItems,
+  ComponentSize,
+  Dropdown,
+  FlexBox,
+  FlexDirection,
+  Form,
+  Input,
+  InputLabel,
+  InputType,
+  JustifyContent,
+  TextArea,
+} from '@influxdata/clockface'
+
+// Utilities
+import {CancelServiceContext, VariableItems} from './CancelServiceContext'
+
+// Types
+
+function CancelServiceReasonsForm() {
+  const {
+    shortSuggestion,
+    isShortSuggestionEnabled,
+    suggestions,
+    setShortSuggestionFlag,
+    setShortSuggestion,
+    setSuggestions,
+    reason,
+    setReason,
+    canContactForFeedback,
+    toggleCanContactForFeedback,
+  } = useContext(CancelServiceContext)
+
+  const isDropdownOptionValid = useMemo(() => {
+    return VariableItems[reason] !== VariableItems.NO_OPTION
+  }, [reason])
+
+  const onChange = (selected: string) => {
+    const isAlternateProductSelected =
+      VariableItems[selected] === VariableItems.ALTERNATIVE_PRODUCT
+    if (!isAlternateProductSelected) {
+      setShortSuggestion('')
+    }
+
+    setShortSuggestionFlag(isAlternateProductSelected)
+    setReason(selected)
+  }
+
+  return (
+    <div className="service-cancellation-reasons-form">
+      <Form.Element
+        label="What is your primary reason for leaving?"
+        required={true}
+      >
+        <Dropdown
+          button={(active, onClick) => (
+            <Dropdown.Button
+              active={active}
+              onClick={onClick}
+              testID="variable-type-dropdown--button"
+            >
+              {VariableItems[reason]}
+            </Dropdown.Button>
+          )}
+          menu={onCollapse => (
+            <Dropdown.Menu onCollapse={onCollapse}>
+              {Object.keys(VariableItems).map(key => (
+                <Dropdown.Item
+                  key={key}
+                  id={key}
+                  value={key}
+                  onClick={onChange}
+                  testID={`variable-type-dropdown-${key}`}
+                >
+                  {VariableItems[key]}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          )}
+        />
+      </Form.Element>
+      {isShortSuggestionEnabled && (
+        <Form.Element
+          label="What is the alternative?"
+          className="element alternate-product--input"
+        >
+          <Input
+            type={InputType.Text}
+            titleText="What is the alternative?"
+            onChange={e => setShortSuggestion(e.target.value)}
+            value={shortSuggestion}
+          />
+        </Form.Element>
+      )}
+      {isDropdownOptionValid && (
+        <Form.Element
+          label="How can we improve?"
+          className="element improvement-suggestion--input"
+        >
+          <TextArea
+            className="improvement-suggestions-input"
+            onChange={e => setSuggestions(e.target.value)}
+            testID="improvement-suggestions-input"
+            value={suggestions}
+            placeholder="How can we improve?"
+          />
+        </Form.Element>
+      )}
+      {isDropdownOptionValid && (
+        <span onClick={toggleCanContactForFeedback}>
+          <FlexBox
+            alignItems={AlignItems.Center}
+            direction={FlexDirection.Row}
+            justifyContent={JustifyContent.FlexStart}
+            margin={ComponentSize.Medium}
+          >
+            <Input
+              className="agree-terms-input"
+              checked={canContactForFeedback}
+              onChange={toggleCanContactForFeedback}
+              size={ComponentSize.Small}
+              titleText="I understand and agree to these conditions"
+              type={InputType.Checkbox}
+              testID="agree-terms--checkbox"
+            />
+            <InputLabel
+              active={canContactForFeedback}
+              size={ComponentSize.Small}
+            >
+              I understand and agree to these conditions
+            </InputLabel>
+          </FlexBox>
+        </span>
+      )}
+    </div>
+  )
+}
+
+export default CancelServiceReasonsForm

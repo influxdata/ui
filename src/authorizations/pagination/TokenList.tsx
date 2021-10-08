d394410e39fe715ccc71df92c590cc17fefd1314
Bucky Schwarz <hoorayimhelping@users.noreply.github.com>
2021-10-08 09:28:25 -0700

    feat(flagged): paginate tokens in a way that respects the redesign flag (#2832)

--- src/authorizations/pagination/TokenList.tsx
+++ src/authorizations/pagination/TokenList.tsx
@@ -1,11 +1,14 @@
 // Libraries
-import React, {PureComponent} from 'react'
+import React, {PureComponent, RefObject} from 'react'
 import memoizeOne from 'memoize-one'
+import isEqual from 'lodash/isEqual'
 
 // Components
-import {Overlay, ResourceList} from '@influxdata/clockface'
+import {Overlay, PaginationNav, ResourceList} from '@influxdata/clockface'
 import TokenRow from 'src/authorizations/components/TokenRow'
+import {TokenRow as TokenRowRedesigned} from 'src/authorizations/components/redesigned/TokenRow'
 import ViewTokenOverlay from 'src/authorizations/components/ViewTokenOverlay'
+import EditTokenOverlay from 'src/authorizations/components/redesigned/EditTokenOverlay'
 
 // Types
 import {Authorization} from 'src/types'
@@ -14,16 +17,20 @@ import {Sort} from '@influxdata/clockface'
 
 // Utils
 import {getSortedResources} from 'src/shared/utils/sort'
+import {isFlagEnabled} from 'src/shared/utils/featureFlag'
 
 type SortKey = keyof Authorization
 
 interface Props {
   auths: Authorization[]
   emptyState: JSX.Element
+  pageHeight: number
+  pageWidth: number
   searchTerm: string
   sortKey: string
   sortDirection: Sort
   sortType: SortTypes
+  tokenCount: number
   onClickColumn: (nextSort: Sort, sortKey: SortKey) => void
 }
 
@@ -37,6 +44,11 @@ export default class TokenList extends PureComponent<Props, State> {
     getSortedResources
   )
 
+  private paginationRef: RefObject<HTMLDivElement>
+  public currentPage: number = 1
+  public rowsPerPage: number = 10
+  public totalPages: number
+
   constructor(props) {
     super(props)
     this.state = {
@@ -45,28 +57,94 @@ export default class TokenList extends PureComponent<Props, State> {
     }
   }
 
+  public componentDidMount() {
+    const params = new URLSearchParams(window.location.search)
+    const urlPageNumber = parseInt(params.get('page'), 10)
+
+    const passedInPageIsValid =
+      urlPageNumber && urlPageNumber <= this.totalPages && urlPageNumber > 0
+
+    if (passedInPageIsValid) {
+      this.currentPage = urlPageNumber
+    }
+  }
+
+  public componentDidUpdate(prevProps) {
+    // if the user filters the list while on a page that is
+    // outside the new filtered list put them on the last page of the new list
+    if (this.currentPage > this.totalPages) {
+      this.paginate(this.totalPages)
+    }
+
+    if (isFlagEnabled('tokensUIRedesign')) {
+      const {auths: prevAuths} = prevProps
+      const {auths: nextAuths} = this.props
+
+      if (!isEqual(prevAuths, nextAuths)) {
+        const authInView = nextAuths.find(
+          auth => auth.id === this.state.authInView?.id
+        )
+        this.setState({authInView})
+      }
+    }
+  }
+
   public render() {
     const {isTokenOverlayVisible, authInView} = this.state
+    this.totalPages = Math.max(
+      Math.ceil(this.props.auths.length / this.rowsPerPage),
+      1
+    )
 
     return (
       <>
-        <ResourceList>
+        <ResourceList style={{width: this.props.pageWidth}}>
           <ResourceList.Body
             emptyState={this.props.emptyState}
+            style={{
+              maxHeight: this.props.pageHeight,
+              minHeight: this.props.pageHeight,
+              overflow: 'scroll',
+            }}
             testID="token-list"
           >
             {this.rows}
           </ResourceList.Body>
         </ResourceList>
+        <PaginationNav.PaginationNav
+          ref={this.paginationRef}
+          style={{width: this.props.pageWidth}}
+          totalPages={this.totalPages}
+          currentPage={this.currentPage}
+          pageRangeOffset={1}
+          onChange={this.paginate}
+        />
 
-        <Overlay visible={isTokenOverlayVisible}>
-          <ViewTokenOverlay
-            auth={authInView}
-            onDismissOverlay={this.handleDismissOverlay}
-          />
-        </Overlay>
+        {isFlagEnabled('tokensUIRedesign') ? (
+          <Overlay visible={isTokenOverlayVisible}>
+            <EditTokenOverlay
+              auth={authInView}
+              onDismissOverlay={this.handleDismissOverlay}
+            />
+          </Overlay>
+        ) : (
+          <Overlay visible={isTokenOverlayVisible}>
+            <ViewTokenOverlay
+              auth={authInView}
+              onDismissOverlay={this.handleDismissOverlay}
+            />
+          </Overlay>
+        )}
       </>
     )
+  }
+
+  public paginate = page => {
+    this.currentPage = page
+    const url = new URL(location.href)
+    url.searchParams.set('page', page)
+    history.replaceState(null, '', url.toString())
+    this.forceUpdate()
   }
 
   private get rows(): JSX.Element[] {
@@ -78,13 +156,38 @@ export default class TokenList extends PureComponent<Props, State> {
       sortType
     )
 
-    return sortedAuths.map(auth => (
-      <TokenRow
-        key={auth.id}
-        auth={auth}
-        onClickDescription={this.handleClickDescription}
-      />
-    ))
+    const startIndex = this.rowsPerPage * Math.max(this.currentPage - 1, 0)
+    const endIndex = Math.min(
+      startIndex + this.rowsPerPage,
+      this.props.tokenCount
+    )
+
+    const paginatedAuths = []
+    for (let i = startIndex; i < endIndex; i++) {
+      const auth = sortedAuths[i]
+
+      if (auth) {
+        if (isFlagEnabled('tokensUIRedesign')) {
+          paginatedAuths.push(
+            <TokenRowRedesigned
+              key={auth.id}
+              auth={auth}
+              onClickDescription={this.handleClickDescription}
+            />
+          )
+        } else {
+          paginatedAuths.push(
+            <TokenRow
+              key={auth.id}
+              auth={auth}
+              onClickDescription={this.handleClickDescription}
+            />
+          )
+        }
+      }
+    }
+
+    return paginatedAuths
   }
 
   private handleDismissOverlay = () => {

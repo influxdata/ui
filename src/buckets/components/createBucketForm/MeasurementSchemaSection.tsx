74a3a33d686f5108b3e034b592733fdb5045c428
ChitlangeSahas <chitlangesahas@gmail.com>
2021-10-08 11:42:23 -0700

    test: should be able to create an explicit bucket using one schema file

--- src/buckets/components/createBucketForm/MeasurementSchemaSection.tsx
+++ src/buckets/components/createBucketForm/MeasurementSchemaSection.tsx
@@ -238,7 +238,12 @@ const EditingPanel: FC<PanelProps> = ({measurementSchema, index}) => {
       >
         <div> name</div>
         <FlexBox direction={FlexDirection.Row} className="schema-row">
-          <div className="value-text">{measurementSchema.name}</div>
+          <div
+            className="value-text"
+            data-testid={`measurement-schema-name-${index}`}
+          >
+            {measurementSchema.name}
+          </div>
           <Button
             icon={IconFont.Download}
             color={ComponentColor.Secondary}

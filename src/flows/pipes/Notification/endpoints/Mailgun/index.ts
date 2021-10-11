323c7027fe1caa6e025a3ffa3782d666a752f413
Gene Hynson <ghynson@influxdata.com>
2021-10-11 11:00:15 -0700

    feat: mailgun

---
+++ src/flows/pipes/Notification/endpoints/Mailgun/index.ts
@@ -0,0 +1,61 @@
+import {TEST_NOTIFICATION} from 'src/flows/pipes/Notification/endpoints'
+import Mailgun from './view'
+import MailgunReadOnly from './readOnly'
+
+export default register => {
+  register({
+    type: 'mailgun',
+    name: 'Mailgun Email',
+    data: {
+      domain: '',
+      apiKey: '',
+      email: '',
+    },
+    component: Mailgun,
+    readOnlyComponent: MailgunReadOnly,
+    generateImports: () => ['http'].map(i => `import "${i}"`).join('\n'),
+    generateTestImports: () =>
+      ['array', 'http'].map(i => `import "${i}"`).join('\n'),
+    generateQuery: data => `task_data
+	|> schema["fieldsAsCols"]()
+      |> set(key: "_notebook_link", value: "${window.location.href}")
+	|> monitor["check"](
+		data: check,
+		messageFn: messageFn,
+		crit: trigger,
+	)
+	|> monitor["notify"](
+    data: notification,
+    endpoint: ((r) => {
+        http.post(
+            url: "https://api.mailgun.net/v3/${data.domain}/messages",
+            headers: {
+              "Content-type": "application/json",
+              "Authorization": "Basic api:${data.apiKey}"
+            },
+            data: bytes(v: "{
+              \\"from\\": \\"Username mailgun@${data.domain}\\",
+              \\"to\\": \\"${data.email}\\",
+              \\"subject\\": \\"InfluxDB critical alert\\",
+              \\"text\\": \\"\${ r._message }\\"
+            }"))
+    })
+  )`,
+    generateTestQuery: data => `
+    http.post(
+        url: "https://api.mailgun.net/v3/${data.domain}/messages",
+        headers: {
+          "Content-type": "application/json",
+          "Authorization": "Basic api:${data.apiKey}"
+        },
+        data: bytes(v: "{
+          \"from\": \"Username mailgun@${data.domain}\",
+          \"to\": \"${data.email}\",
+          \"subject\": \"InfluxDB critical alert\",
+          \"text\": \"${TEST_NOTIFICATION}\"
+        }"))
+
+    array.from(rows: [{value: 0}])
+        |> yield(name: "ignore")`,
+  })
+}
---
+++ src/flows/pipes/Notification/endpoints/Mailgun/view.tsx
@@ -0,0 +1,69 @@
+import React, {FC, useContext} from 'react'
+import {Form, Input, InputType, ComponentSize} from '@influxdata/clockface'
+
+import {PipeContext} from 'src/flows/context/pipe'
+
+const Mailgun: FC = () => {
+  const {data, update} = useContext(PipeContext)
+
+  const updateDomain = evt => {
+    update({
+      endpointData: {
+        ...data.endpointData,
+        domain: evt.target.value,
+      },
+    })
+  }
+
+  const updateAPIKey = evt => {
+    update({
+      endpointData: {
+        ...data.endpointData,
+        apiKey: evt.target.value,
+      },
+    })
+  }
+
+  const updateEmail = evt => {
+    update({
+      endpointData: {
+        ...data.endpointData,
+        email: evt.target.value,
+      },
+    })
+  }
+
+  return (
+    <div className="slack-endpoint-details--flex">
+      <Form.Element label="Domain" required={true}>
+        <Input
+          name="url"
+          type={InputType.Text}
+          value={data.endpointData.domain}
+          onChange={updateDomain}
+          size={ComponentSize.Medium}
+        />
+      </Form.Element>
+      <Form.Element label="API Key" required={true}>
+        <Input
+          name="apiKey"
+          type={InputType.Password}
+          value={data.endpointData.apiKey}
+          onChange={updateAPIKey}
+          size={ComponentSize.Medium}
+        />
+      </Form.Element>
+      <Form.Element label="Email" required={true}>
+        <Input
+          name="email"
+          type={InputType.Email}
+          value={data.endpointData.email}
+          onChange={updateEmail}
+          size={ComponentSize.Medium}
+        />
+      </Form.Element>
+    </div>
+  )
+}
+
+export default Mailgun

483cfd142d1593a284afc8ab5953d57841d5cde8
ChitlangeSahas <chitlangesahas@gmail.com>
2021-10-11 14:06:06 -0700

    chore: remove useGiraffe Flag from Gauge

--- src/visualization/types/Gauge/view.tsx
+++ src/visualization/types/Gauge/view.tsx
@@ -1,24 +1,16 @@
 // Libraries
-import React, {FC, useContext} from 'react'
+import React, {FC} from 'react'
 
 // Components
 import {Config, Plot} from '@influxdata/giraffe'
 // Types
 import {GaugeViewProperties} from 'src/types/dashboards'
 import {VisualizationProps} from 'src/visualization'
-import {isFlagEnabled} from '../../../shared/utils/featureFlag'
 
-// delete after testing
-import {AutoSizer} from 'react-virtualized'
 import {
   GAUGE_ARC_LENGTH_DEFAULT,
-  GAUGE_THEME_LIGHT,
-  GAUGE_THEME_DARK,
   GAUGE_VALUE_POSITION_Y_OFFSET_DEFAULT,
 } from './constants'
-import {AppSettingContext} from 'src/shared/contexts/app'
-import Gauge from './Gauge'
-import LatestValueTransform from 'src/visualization/components/LatestValueTransform'
 
 interface Props extends VisualizationProps {
   properties: GaugeViewProperties
@@ -34,56 +26,25 @@ const GaugeChart: FC<Props> = ({result, properties}) => {
     decimalPlaces,
   } = properties
 
-  // delete after testing code
-  const {theme} = useContext(AppSettingContext)
-  const currentTheme = theme === 'light' ? GAUGE_THEME_LIGHT : GAUGE_THEME_DARK
-
-  if (isFlagEnabled('useGiraffeGraphs')) {
-    const config: Config = {
-      table: result.table,
-      layers: [
-        {
-          type: 'gauge',
-          prefix: prefix,
-          suffix: suffix,
-          tickPrefix: tickPrefix,
-          tickSuffix: tickSuffix,
-          decimalPlaces: decimalPlaces,
-          gaugeColors: colors,
-          gaugeSize: GAUGE_ARC_LENGTH_DEFAULT,
-          gaugeTheme: {
-            valuePositionYOffset: GAUGE_VALUE_POSITION_Y_OFFSET_DEFAULT,
-          },
+  const config: Config = {
+    table: result.table,
+    layers: [
+      {
+        type: 'gauge',
+        prefix: prefix,
+        suffix: suffix,
+        tickPrefix: tickPrefix,
+        tickSuffix: tickSuffix,
+        decimalPlaces: decimalPlaces,
+        gaugeColors: colors,
+        gaugeSize: GAUGE_ARC_LENGTH_DEFAULT,
+        gaugeTheme: {
+          valuePositionYOffset: GAUGE_VALUE_POSITION_Y_OFFSET_DEFAULT,
         },
-      ],
-    }
-
-    return <Plot config={config} />
+      },
+    ],
   }
-  return (
-    <LatestValueTransform table={result.table} allowString={false}>
-      {latestValue => (
-        <AutoSizer>
-          {({width, height}) => (
-            <div className="gauge">
-              <Gauge
-                width={width}
-                height={height}
-                colors={colors}
-                prefix={prefix}
-                tickPrefix={tickPrefix}
-                suffix={suffix}
-                tickSuffix={tickSuffix}
-                gaugePosition={latestValue}
-                decimalPlaces={decimalPlaces}
-                theme={currentTheme}
-              />
-            </div>
-          )}
-        </AutoSizer>
-      )}
-    </LatestValueTransform>
-  )
+  return <Plot config={config} />
 }
 
 export default GaugeChart

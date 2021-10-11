22c85f2d9167fcd37851d34c68b85ff868ad6ad5
ChitlangeSahas <chitlangesahas@gmail.com>
2021-10-11 14:17:09 -0700

    chore: prettier

--- cypress/e2e/util/annotationsSetup.ts
+++ cypress/e2e/util/annotationsSetup.ts
@@ -9,53 +9,53 @@ export const setupData = (cy: Cypress.Chainable, plotTypeSuffix = '') =>
           cy.fixture('routes').then(({orgs}) => {
             cy.visit(`${orgs}/${orgID}/dashboards/${body.id}`)
             return cy.then(() => {
-                cy.createBucket(orgID, name, 'devbucket')
-                // have to add large amount of data to fill the window so that the random click for annotation works
-                cy.writeData(lines(3000), 'devbucket')
+              cy.createBucket(orgID, name, 'devbucket')
+              // have to add large amount of data to fill the window so that the random click for annotation works
+              cy.writeData(lines(3000), 'devbucket')
 
-                // make a dashboard cell
-                cy.getByTestID('add-cell--button').click()
-                cy.getByTestID('selector-list devbucket').should(
-                  'have.length.of.at.least',
-                  1
-                )
-                cy.getByTestID('selector-list devbucket').click()
+              // make a dashboard cell
+              cy.getByTestID('add-cell--button').click()
+              cy.getByTestID('selector-list devbucket').should(
+                'have.length.of.at.least',
+                1
+              )
+              cy.getByTestID('selector-list devbucket').click()
 
-                cy.getByTestID('selector-list m').should(
-                  'have.length.of.at.least',
-                  1
-                )
-                cy.getByTestID('selector-list m').clickAttached()
+              cy.getByTestID('selector-list m').should(
+                'have.length.of.at.least',
+                1
+              )
+              cy.getByTestID('selector-list m').clickAttached()
 
-                cy.getByTestID('selector-list v').should(
-                  'have.length.of.at.least',
-                  1
-                )
-                cy.getByTestID('selector-list v').clickAttached()
+              cy.getByTestID('selector-list v').should(
+                'have.length.of.at.least',
+                1
+              )
+              cy.getByTestID('selector-list v').clickAttached()
 
-                if (plotTypeSuffix) {
-                  cy.getByTestID('view-type--dropdown').click()
-                  cy.getByTestID(`view-type--${plotTypeSuffix}`).click()
-                }
+              if (plotTypeSuffix) {
+                cy.getByTestID('view-type--dropdown').click()
+                cy.getByTestID(`view-type--${plotTypeSuffix}`).click()
+              }
 
-                cy.getByTestID(`selector-list tv1`).should(
-                  'have.length.of.at.least',
-                  1
-                )
-                cy.getByTestID(`selector-list tv1`).clickAttached()
+              cy.getByTestID(`selector-list tv1`).should(
+                'have.length.of.at.least',
+                1
+              )
+              cy.getByTestID(`selector-list tv1`).clickAttached()
 
-                cy.getByTestID('time-machine-submit-button').click()
+              cy.getByTestID('time-machine-submit-button').click()
 
-                cy.getByTestID('overlay').within(() => {
-                  cy.getByTestID('page-title').click()
-                  cy.getByTestID('renamable-page-title--input')
-                    .clear()
-                    .type('blah')
-                  cy.getByTestID('save-cell--button').click()
-                })
-
-                cy.getByTestID('toggle-annotations-controls').click()
+              cy.getByTestID('overlay').within(() => {
+                cy.getByTestID('page-title').click()
+                cy.getByTestID('renamable-page-title--input')
+                  .clear()
+                  .type('blah')
+                cy.getByTestID('save-cell--button').click()
               })
+
+              cy.getByTestID('toggle-annotations-controls').click()
+            })
           })
         )
       )

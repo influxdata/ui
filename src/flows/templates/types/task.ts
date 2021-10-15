d545ef2a7e15be04ed79e1ac156b28543be361e5
drdelambre <drdelambre@gmail.com>
2021-10-15 13:12:10 -0700

    feat: open tasks as notebooks

--- src/flows/templates/types/task.ts
+++ src/flows/templates/types/task.ts
@@ -1,44 +1,117 @@
 import {DEFAULT_TIME_RANGE} from 'src/shared/constants/timeRanges'
 import {AUTOREFRESH_DEFAULT} from 'src/shared/constants'
+import {parse, format_from_js_file} from '@influxdata/flux'
+import {remove} from 'src/shared/contexts/query'
 
 export default register =>
   register({
     type: 'task',
-    init: () =>
-      Promise.resolve({
-        spec: {
-          readOnly: false,
-          range: DEFAULT_TIME_RANGE,
-          refresh: AUTOREFRESH_DEFAULT,
-          pipes: [
-            {
-              activeQuery: 0,
-              queries: [
-                {
-                  text: '',
-                  editMode: 'advanced',
-                  builderConfig: {
-                    buckets: [],
-                    tags: [],
-                    functions: [],
+    init: id => {
+      if (!id) {
+        return Promise.resolve({
+          spec: {
+            readOnly: false,
+            range: DEFAULT_TIME_RANGE,
+            refresh: AUTOREFRESH_DEFAULT,
+            pipes: [
+              {
+                activeQuery: 0,
+                queries: [
+                  {
+                    text: '',
+                    editMode: 'advanced',
+                    builderConfig: {
+                      buckets: [],
+                      tags: [],
+                      functions: [],
+                    },
                   },
+                ],
+                type: 'rawFluxEditor',
+                title: 'Query to Run',
+                visible: true,
+              },
+              {
+                title: 'Validate the Data',
+                visible: true,
+                type: 'table',
+              },
+              {
+                type: 'schedule',
+                title: 'Schedule',
+                visible: true,
+              },
+            ],
+          },
+        })
+      }
+
+      return fetch(`/api/v2/tasks/${id}`)
+        .then(resp => resp.json())
+        .then(resp => {
+          const ast = parse(resp.flux)
+          const taskParams = remove(
+            ast,
+            node =>
+              node.type === 'OptionStatement' &&
+              node.assignment.id.name === 'task'
+          )
+            .reverse()
+            .reduce((acc, curr) => {
+              // eslint-disable-next-line no-extra-semi
+              ;(curr.assignment?.init?.properties || []).reduce(
+                (_acc, _curr) => {
+                  if (_curr.key?.name && _curr.value?.location?.source) {
+                    _acc[_curr.key.name] = _curr.value.location.source
+                  }
+
+                  return _acc
+                },
+                acc
+              )
+
+              return acc
+            }, {})
+
+          return {
+            name: resp.name,
+            spec: {
+              readOnly: false,
+              range: DEFAULT_TIME_RANGE,
+              refresh: AUTOREFRESH_DEFAULT,
+              pipes: [
+                {
+                  activeQuery: 0,
+                  queries: [
+                    {
+                      text: format_from_js_file(ast),
+                      editMode: 'advanced',
+                      builderConfig: {
+                        buckets: [],
+                        tags: [],
+                        functions: [],
+                      },
+                    },
+                  ],
+                  type: 'rawFluxEditor',
+                  title: 'Query to Run',
+                  visible: true,
+                },
+                {
+                  title: 'Validate the Data',
+                  visible: true,
+                  type: 'table',
+                },
+                {
+                  type: 'schedule',
+                  title: 'Schedule',
+                  visible: true,
+                  interval: taskParams.every,
+                  offset: taskParams.offset,
                 },
               ],
-              type: 'rawFluxEditor',
-              title: 'Query to Run',
-              visible: true,
             },
-            {
-              title: 'Validate the Data',
-              visible: true,
-              type: 'table',
-            },
-            {
-              type: 'schedule',
-              title: 'Schedule',
-              visible: true,
-            },
-          ],
-        },
-      }),
+          }
+        })
+    },
   })

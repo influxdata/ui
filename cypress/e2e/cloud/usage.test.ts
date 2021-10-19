d238e95f38703bfe73db85ea92f9e3e90b95e533
ChitlangeSahas <chitlangesahas@gmail.com>
2021-10-19 12:35:14 -0700

    chore: fix usage tests

--- cypress/e2e/cloud/usage.test.ts
+++ cypress/e2e/cloud/usage.test.ts
@@ -139,7 +139,7 @@ describe('Usage Page PAYG With Data', () => {
     const stats = ['0.78 MB', '32,424', '2.06 GB-hr', '0.01 GB']
 
     // Check that the stats are returned and rendered for a user with data
-    cy.getByTestID('single-stat')
+    cy.getByTestID('giraffe-layer-single-stat')
       .should('have.length', 4)
       .each((child, index) => {
         expect(child.text().trim()).to.be.oneOf([

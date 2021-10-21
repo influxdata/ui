f9d6413d9b1efd55664a6346e3663ae142e897f6
Timmy Luong <TCL735@users.noreply.github.com>
2021-10-21 16:14:09 -0700

    test: force massive rerun of Annotations singlestat tests

--- cypress/e2e/shared/annotations.singlestat.test.ts
+++ cypress/e2e/shared/annotations.singlestat.test.ts
@@ -18,16 +18,20 @@ describe('The Annotations UI functionality on a graph + single stat graph type',
   it('can create an annotation on the single stat + line graph', () => {
     testAddAnnotation(cy)
   })
+
   it('can edit an annotation for the single stat + line graph', () => {
     testEditAnnotation(cy)
   })
+
   it('can delete an annotation for the single stat + line graph', () => {
     testDeleteAnnotation(cy)
   })
+
   it('can add a range annotation for the xy single stat + line graph', () => {
     addRangeAnnotation(cy)
     checkAnnotationText(cy, RANGE_ANNOTATION_TEXT)
   })
+
   it('can add and edit a range annotation for the single stat + line graph', () => {
     testEditRangeAnnotation(cy)
   })

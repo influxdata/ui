01ca36384f7b02a19c712c8ec10ecbc27a8671b3
Joel Fried <jfried@influxdata.com>
2021-10-25 16:50:00 -0400

    chore: move to custom renderer for OSS

--- src/shared/components/views/MarkdownRenderer.tsx
+++ src/shared/components/views/MarkdownRenderer.tsx
@@ -11,22 +11,34 @@ interface Props {
   escapeHtml?: boolean
 }
 
+interface ImageProps {
+  src?: string
+  alt?: string
+}
+
 // In cloud environments, we want to render the literal markdown image tag
 // but ReactMarkdown expects a React element wrapping an image to be returned,
 // so we use any
 // see: https://github.com/rexxars/react-markdown/blob/master/index.d.ts#L101
-const imageRenderer: any = (): any => MARKDOWN_UNSUPPORTED_IMAGE
+const cloudImageRenderer: any = (): any => MARKDOWN_UNSUPPORTED_IMAGE
+
+// In OSS we want to allow users to render images to external sources, requiring
+// this custom renderer.  If you want to disallow these, remove this renderer or
+// change to the cloudImageRenderer instead.
+const ossImageRenderer: FC<ImageProps> = ({src, alt}) => {
+  return <img src={src} alt={alt}/>
+}
 
 export const MarkdownRenderer: FC<Props> = ({className = '', text}) => {
   // don't parse images in cloud environments to prevent arbitrary script execution via images
   if (CLOUD) {
     return (
-      <ReactMarkdown className={className} components={{img: imageRenderer}}>
+      <ReactMarkdown className={className} components={{img: cloudImageRenderer}}>
         {text}
       </ReactMarkdown>
     )
   }
 
   // load images locally to your heart's content. caveat emptor
-  return <ReactMarkdown className={className}>{text}</ReactMarkdown>
+  return <ReactMarkdown className={className} components={{img: ossImageRenderer}}>{text}</ReactMarkdown>
 }

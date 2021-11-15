24e363b45a3fd1b68e12fadbdd99dbf355355df5
ChitlangeSahas <chitlangesahas@gmail.com>
2021-11-15 14:36:34 -0700

    chore: prettier

--- src/external/monaco.flux.theme.ts
+++ src/external/monaco.flux.theme.ts
@@ -63,7 +63,7 @@ function addTheme(monaco: MonacoType) {
       'editor.lineHighlightBackground': '#353640',
       'editorCursor.foreground': '#ffffff',
       'editorActiveLineNumber.foreground': '#bec2cc',
-      "minimap.background": InfluxColors.Grey15,
+      'minimap.background': InfluxColors.Grey15,
     },
   })
 }

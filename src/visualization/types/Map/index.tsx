6d8baca760233ce55e5c5c862465146727411b74
Will Baker <wbaker@gmail.com>
2021-11-16 09:43:39 -0700

    fix: remove maps as a vis option for OSS

--- src/visualization/types/Map/index.tsx
+++ src/visualization/types/Map/index.tsx
@@ -2,14 +2,17 @@ import icon from 'src/visualization/types/Map/icon'
 import properties from 'src/visualization/types/Map/properties'
 import view from 'src/visualization/types/Map/view'
 import {GeoOptions as options} from 'src/visualization/types/Map/GeoOptions'
+import {CLOUD} from 'src/shared/constants'
 
 export default register => {
-  register({
-    type: 'geo',
-    name: 'Map',
-    graphic: icon,
-    initial: properties,
-    component: view,
-    options,
-  })
+  // The maps visualization is not supported on OSS.
+  CLOUD &&
+    register({
+      type: 'geo',
+      name: 'Map',
+      graphic: icon,
+      initial: properties,
+      component: view,
+      options,
+    })
 }

db9c460ea9e4e6d288a9c1b08c64d19b79d51bda
Alex Boatwright <aboatwright@influxdata.com>
2021-12-01 13:57:20 -0800

    fix: missed that one

--- cypress/e2e/shared/simpleTable.test.ts
+++ cypress/e2e/shared/simpleTable.test.ts
@@ -12,15 +12,15 @@ describe('simple table interactions', () => {
       cy.fixture('routes').then(({orgs, explorer}) => {
         cy.visit(`${orgs}/${orgID}${explorer}`)
       })
+      cy.getByTestID('tree-nav')
+      cy.createBucket(orgID, name, simpleLarge)
+      cy.writeData(points(300), simpleLarge)
+      cy.createBucket(orgID, name, simpleSmall)
+      cy.writeData(points(30), simpleSmall)
+      cy.createBucket(orgID, name, simpleOverflow)
+      cy.writeData(points(31), simpleOverflow)
+      cy.reload()
     })
-    cy.getByTestID('tree-nav')
-    cy.createBucket(orgID, name, simpleLarge)
-    cy.writeData(points(300), simpleLarge)
-    cy.createBucket(orgID, name, simpleSmall)
-    cy.writeData(points(30), simpleSmall)
-    cy.createBucket(orgID, name, simpleOverflow)
-    cy.writeData(points(31), simpleOverflow)
-    cy.reload()
   })
 
   it('should render correctly after switching from a dataset with more pages to one with fewer', () => {

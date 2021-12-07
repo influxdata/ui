deddddcf725705c60027363439b695a4ec41d220
Subir Jolly <subirjolly@Subirs-MacBook-Pro.local>
2021-12-07 16:24:47 -0500

    chore: 404 instead of routing to /

--- src/checkout/CheckoutPage.tsx
+++ src/checkout/CheckoutPage.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useEffect, useMemo} from 'react'
+import React, {FC} from 'react'
 
 // Components
 import CheckoutForm from 'src/checkout/CheckoutForm'
@@ -6,42 +6,18 @@ import SuccessOverlay from 'src/checkout/SuccessOverlay'
 import CheckoutProvider from 'src/checkout/context/checkout'
 import {isFlagEnabled} from 'src/shared/utils/featureFlag'
 import ZuoraOutagePage from 'src/shared/components/zuora/ZuoraOutagePage'
-import {useSelector} from 'react-redux'
-import {AppState} from 'src/types'
-import {useHistory} from 'react-router'
 
-const CheckoutV2: FC = () => {
-  const history = useHistory()
-  const accountType = useSelector(
-    (state: AppState) => state.me?.quartzMe?.accountType
-  )
-
-  const shouldContinue = useMemo(() => {
-    return accountType === 'free' || accountType === 'cancelled'
-  }, [accountType])
-
-  useEffect(() => {
-    if (!shouldContinue) {
-      history.push('/')
-    }
-  }, [shouldContinue])
-
-  if (!shouldContinue) {
-    return null
-  }
-
-  return (
-    <CheckoutProvider>
-      <>
-        <SuccessOverlay />
-        {isFlagEnabled('quartzZuoraDisabled') ? (
-          <ZuoraOutagePage />
-        ) : (
-          <CheckoutForm />
-        )}
-      </>
-    </CheckoutProvider>
-  )
-}
+const CheckoutV2: FC = () => (
+  <CheckoutProvider>
+    <>
+      <SuccessOverlay />
+      {isFlagEnabled('quartzZuoraDisabled') ? (
+        <ZuoraOutagePage />
+      ) : (
+        <CheckoutForm />
+      )}
+    </>
+  </CheckoutProvider>
+)
 
 export default CheckoutV2

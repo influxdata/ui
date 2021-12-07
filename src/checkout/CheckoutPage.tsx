c7fb87c0b39c26f5c50688cc750fee200a2153d1
Subir Jolly <subirjolly@Subirs-MacBook-Pro.local>
2021-12-07 15:47:59 -0500

    feat: change checkout routing and billing(page) for contract accountType

--- src/checkout/CheckoutPage.tsx
+++ src/checkout/CheckoutPage.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react'
+import React, {FC, useEffect, useMemo} from 'react'
 
 // Components
 import CheckoutForm from 'src/checkout/CheckoutForm'
@@ -6,8 +6,30 @@ import SuccessOverlay from 'src/checkout/SuccessOverlay'
 import CheckoutProvider from 'src/checkout/context/checkout'
 import {isFlagEnabled} from 'src/shared/utils/featureFlag'
 import ZuoraOutagePage from 'src/shared/components/zuora/ZuoraOutagePage'
+import {useSelector} from 'react-redux'
+import {AppState} from 'src/types'
+import {useHistory} from 'react-router'
 
 const CheckoutV2: FC = () => {
+  const history = useHistory()
+  const accountType = useSelector(
+    (state: AppState) => state.me?.quartzMe?.accountType
+  )
+
+  const shouldContinue = useMemo(() => {
+    return accountType === 'free' || accountType === 'cancelled'
+  }, [accountType])
+
+  useEffect(() => {
+    if (!shouldContinue) {
+      history.push('/')
+    }
+  }, [shouldContinue])
+
+  if (!shouldContinue) {
+    return null
+  }
+
   return (
     <CheckoutProvider>
       <>

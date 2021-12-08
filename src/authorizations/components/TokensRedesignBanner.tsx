4199028fca2e54dc67f2dc79904121d46f322ad7
Bethelhem Legesse <legessebl@gmail.com>
2021-12-08 17:34:10 -0600

    fix: update tokens banner date

--- src/authorizations/components/TokensRedesignBanner.tsx
+++ src/authorizations/components/TokensRedesignBanner.tsx
@@ -5,7 +5,7 @@ import React, {FunctionComponent} from 'react'
 
 const TokensBanner: FunctionComponent = () => (
   <div>
-    Our Tokens UI is changing on <b>DD/MM/YYYY</b>. After that day you'll only
+    Our Tokens UI is changing on <b>Jan 10th, 2022</b>. After that day you'll only
     be able to view and safely store token details at the point of creation. If
     you lose access to token credentials you can generate a new token. Please
     copy any existing token credentials in your chosen safe location.

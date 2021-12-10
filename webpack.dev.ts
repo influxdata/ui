82db3b2e0fe0a78422c53d9a875bc12a673ed787
asalem <ariel.salem1989@gmail.com>
2021-12-10 06:25:08 -0800

    chore: remove the bundle analyzer since it doesnt play nice with webpack-dev-server

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -5,8 +5,6 @@ const common = require('./webpack.common.ts')
 const PORT = parseInt(process.env.PORT, 10) || 8080
 const PUBLIC = process.env.PUBLIC || undefined
 const {BASE_PATH} = require('./src/utils/env')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin
 const webpack = require('webpack')
 
 module.exports = merge(common, {
@@ -51,12 +49,6 @@ module.exports = merge(common, {
     new webpack.DllReferencePlugin({
       context: path.join(__dirname, 'build'),
       manifest: require('./build/vendor-manifest.json'),
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'server',
-      analyzerHost: '0.0.0.0',
-      analyzerPort: '9998',
-      openAnalyzer: false,
     }),
   ],
 })

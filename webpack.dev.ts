28181383c81a234250be8331295c8e9b5ed73583
asalem <ariel.salem1989@gmail.com>
2021-12-13 09:00:04 -0800

    chore: readd the bundler

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -5,12 +5,16 @@ const common = require('./webpack.common.ts')
 const PORT = parseInt(process.env.PORT, 10) || 8080
 const PUBLIC = process.env.PUBLIC || undefined
 const {BASE_PATH} = require('./src/utils/env')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
+
 const webpack = require('webpack')
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'cheap-inline-source-map',
   output: {
+    path: path.join(__dirname, 'build'),
     filename: '[name].js',
   },
   watchOptions: {
@@ -49,6 +53,12 @@ module.exports = merge(common, {
     new webpack.DllReferencePlugin({
       context: path.join(__dirname, 'build'),
       manifest: require('./build/vendor-manifest.json'),
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerHost: '0.0.0.0',
+      analyzerPort: '9998',
+      openAnalyzer: false,
+      generateStatsFile: true,
     }),
   ],
 })

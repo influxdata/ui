bf597b88a0f22ff973699676c26c5b1069ccac24
Beth Legesse <legessebl@gmail.com>
2021-12-13 15:50:13 -0600

    fix: removed example section from flyover panel

--- src/timeMachine/components/fluxFunctionsToolbar/FunctionTooltipContents.tsx
+++ src/timeMachine/components/fluxFunctionsToolbar/FunctionTooltipContents.tsx
@@ -5,7 +5,6 @@ import React, {FunctionComponent} from 'react'
 import {DapperScrollbars} from '@influxdata/clockface'
 import TooltipDescription from 'src/timeMachine/components/fluxFunctionsToolbar/TooltipDescription'
 import TooltipArguments from 'src/timeMachine/components/fluxFunctionsToolbar/TooltipArguments'
-import TooltipExample from 'src/timeMachine/components/fluxFunctionsToolbar/TooltipExample'
 import TooltipLink from 'src/timeMachine/components/fluxFunctionsToolbar/TooltipLink'
 
 // Types
@@ -16,7 +15,7 @@ interface Props {
 }
 
 const FunctionTooltipContents: FunctionComponent<Props> = ({
-  func: {desc, args, example, link, name},
+  func: {desc, args, link, name},
 }) => {
   return (
     <div className="flux-function-docs" data-testid={`flux-docs--${name}`}>
@@ -24,7 +23,6 @@ const FunctionTooltipContents: FunctionComponent<Props> = ({
         <div className="flux-toolbar--popover">
           <TooltipDescription description={desc} />
           <TooltipArguments argsList={args} />
-          <TooltipExample example={example} />
           <TooltipLink link={link} />
         </div>
       </DapperScrollbars>

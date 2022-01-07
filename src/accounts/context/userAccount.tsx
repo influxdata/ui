75f4c0f487072b917b6a8b10361264fa4928bf54
Jill R. Pelavin <jpelavin@influxdata.com>
2022-01-07 15:59:58 -0500

    feat: multiAccount- adding switch button and supporting UI (#3509)
    * Added new Account settings page, and adjusted the billing page too
    
     * Billing Page:  tabbed in with the account settings page if the flag is on
       * if the flag is off, no changes at all on the page
    * added e2e tests, adjusted existing tests
    * update openApi SHA

---
+++ src/accounts/context/userAccount.tsx
@@ -0,0 +1,96 @@
+// Libraries
+import React, {FC, useCallback, useEffect, useState} from 'react'
+import {useDispatch} from 'react-redux'
+
+// Types
+import {Account as UserAccount} from 'src/client/unityRoutes'
+
+// Utils
+import {getAccounts} from 'src/client/unityRoutes'
+
+// Metrics
+import {event} from 'src/cloud/utils/reporting'
+
+export type Props = {
+  children: JSX.Element
+}
+
+export interface UserAccountContextType {
+  userAccounts: UserAccount[]
+  handleGetAccounts: () => void
+
+  defaultAccountId: number
+  activeAccountId: number
+}
+//   todo: add to above when implementing:
+//    setDefaultAccountId: (id: number) => void
+
+export const DEFAULT_CONTEXT: UserAccountContextType = {
+  userAccounts: [],
+  defaultAccountId: -1,
+  activeAccountId: -1,
+  handleGetAccounts: () => {},
+}
+
+export const UserAccountContext = React.createContext<UserAccountContextType>(
+  DEFAULT_CONTEXT
+)
+
+// todo:  put in dependency array:  whenever the default account changes, should redo the call.
+// put this in *after* the ability to change the default account has been added
+
+export const UserAccountProvider: FC<Props> = React.memo(({children}) => {
+  const [userAccounts, setUserAccounts] = useState<UserAccount[]>(null)
+  const [defaultAccountId, setDefaultAccountId] = useState<number>(null)
+  const [activeAccountId, setActiveAccountId] = useState<number>(null)
+
+  const dispatch = useDispatch()
+
+  const handleGetAccounts = useCallback(async () => {
+    try {
+      const resp = await getAccounts({})
+      if (resp.status !== 200) {
+        // set user account status to error;...TODO
+        throw new Error(resp.data.message)
+      }
+      const {data} = resp
+      if (Array.isArray(data)) {
+        setUserAccounts(data)
+
+        const defaultAcctArray = data.filter(line => line.isDefault)
+        if (defaultAcctArray && defaultAcctArray.length === 1) {
+          const defaultId = defaultAcctArray[0].id
+          setDefaultAccountId(defaultId)
+        }
+
+        // isActive: true is for the currently logged in/active account
+        const activeAcctArray = data.filter(line => line.isActive)
+        if (activeAcctArray && activeAcctArray.length === 1) {
+          const activeId = activeAcctArray[0].id
+          setActiveAccountId(activeId)
+        }
+      }
+    } catch (error) {
+      event('multiAccount.retrieveAccounts.error', {error})
+    }
+  }, [dispatch])
+
+  useEffect(() => {
+    handleGetAccounts()
+  }, [handleGetAccounts, defaultAccountId, activeAccountId])
+
+  // todo: add to the value object when restoring/doing the default account setting:
+  //         setDefaultAccountId,
+  return (
+    <UserAccountContext.Provider
+      value={{
+        userAccounts,
+        defaultAccountId,
+        activeAccountId,
+        handleGetAccounts,
+      }}
+    >
+      {children}
+    </UserAccountContext.Provider>
+  )
+})

95c5e053945ef318bbf669627268740adb35d02c
Dustin Eaton <deaton@influxdata.com>
2022-01-11 11:51:39 -0600

    feat: allow self removal from account with ff on
    Co-authored-by: Luke Barbuto <luke.barbuto@gmail.com>

--- src/shared/selectors/flags.ts
+++ src/shared/selectors/flags.ts
@@ -45,6 +45,7 @@ export const OSS_FLAGS = {
   boardWithFlows: false,
   createWithFlows: false,
   leadWithFlows: false,
+  selfRemovalFromAccount: false,
 }
 
 export const CLOUD_FLAGS = {
@@ -91,6 +92,7 @@ export const CLOUD_FLAGS = {
   boardWithFlows: false,
   createWithFlows: false,
   leadWithFlows: false,
+  selfRemovalFromAccount: false,
 }
 
 export const activeFlags = (state: AppState): FlagMap => {

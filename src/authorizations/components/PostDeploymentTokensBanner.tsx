b45954cc34ab768c7592ac0142df84cde515a764
Palak Bhojani <palak@influxdata.com>
2022-01-11 14:23:52 -0800

    fix: wording

--- src/authorizations/components/PostDeploymentTokensBanner.tsx
+++ src/authorizations/components/PostDeploymentTokensBanner.tsx
@@ -6,7 +6,7 @@ import React, {FunctionComponent} from 'react'
 const PostDeploymentTokensBanner: FunctionComponent = () => (
   <div>
     Our Tokens UI has changed. You are only able to view and safely store token
-    details at the point of creation. If you lose access to token credentials
+    details at the point of creation. If you lose access to token credentials,
     you can generate a new token.
   </div>
 )

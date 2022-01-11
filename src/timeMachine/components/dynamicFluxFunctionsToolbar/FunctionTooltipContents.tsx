b0c234f25b9b1e5236c1416c1b4e5857242368b6
Bethelhem Legesse <legessebl@gmail.com>
2022-01-11 14:44:21 -0600

    feat: remove example section from flux function's flyover panel  (#3434)
    * feat: adding feature flag in FluxToolbar
    
    * fix: removed example section from flyover panel
    
    * fix: removed extra space from new folder
    
    * chore: update the file path for components
    
    * fix: prettier
    
    * feat: removing category from flux functions
    
    * fix: prettier
    
    * fix: accidently removed example from original folder. added it back
    
    * fix: prettier
    
    * fix: undoing changes made to notebook's script editor

---
+++ src/timeMachine/components/dynamicFluxFunctionsToolbar/FunctionTooltipContents.tsx
@@ -0,0 +1,33 @@
+// Libraries
+import React, {FunctionComponent} from 'react'
+
+// Components
+import {DapperScrollbars} from '@influxdata/clockface'
+import TooltipDescription from 'src/timeMachine/components/dynamicFluxFunctionsToolbar/TooltipDescription'
+import TooltipArguments from 'src/timeMachine/components/dynamicFluxFunctionsToolbar/TooltipArguments'
+import TooltipLink from 'src/timeMachine/components/dynamicFluxFunctionsToolbar/TooltipLink'
+
+// Types
+import {FluxToolbarFunction} from 'src/types/shared'
+
+interface Props {
+  func: FluxToolbarFunction
+}
+
+const FunctionTooltipContents: FunctionComponent<Props> = ({
+  func: {desc, args, link, name},
+}) => {
+  return (
+    <div className="flux-function-docs" data-testid={`flux-docs--${name}`}>
+      <DapperScrollbars autoHide={false}>
+        <div className="flux-toolbar--popover">
+          <TooltipDescription description={desc} />
+          <TooltipArguments argsList={args} />
+          <TooltipLink link={link} />
+        </div>
+      </DapperScrollbars>
+    </div>
+  )
+}
+
+export default FunctionTooltipContents

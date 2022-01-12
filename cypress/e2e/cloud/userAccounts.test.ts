a549000a00151d2d2920d790fb2809051da5cbd5
Jill R Pelavin <jpelavin@influxdata.com>
2022-01-12 15:21:37 -0500

    chore: condensed 2 tests into one

--- cypress/e2e/cloud/userAccounts.test.ts
+++ cypress/e2e/cloud/userAccounts.test.ts
@@ -18,47 +18,10 @@ const doSetup = (cy, numAccounts: number) => {
   })
 }
 
-describe('Account Page; user with 3 accounts', () => {
-  beforeEach(() => doSetup(cy, 3))
-
-  it('can get to the page and get the accounts, and the switch button is showing', () => {
-    cy.getByTestID('account-settings--header').should('be.visible')
-    cy.getByTestID('user-account-switch-btn').should('be.visible')
-    cy.getByTestID('account-active-name--block').contains('Influx')
-
-    cy.getByTestID('user-account-switch-btn').click()
-
-    cy.getByTestID('switch-account--dialog').within(() => {
-      const prefix = 'accountSwitch-toggle-choice'
-
-      cy.getByTestID(`${prefix}-0-ID`).should('be.visible')
-      cy.getByTestID(`${prefix}-0-ID`).contains('Influx')
-      cy.getByTestID(`${prefix}-0-ID--input`).should('be.checked')
-
-      cy.getByTestID(`${prefix}-1-ID`).should('be.visible')
-      cy.getByTestID(`${prefix}-1-ID`).contains('Veganomicon (default)')
-
-      cy.getByTestID(`${prefix}-2-ID`).should('be.visible')
-      cy.getByTestID(`${prefix}-2-ID`).contains('Stradivarius')
-
-      // at first; the switch button should be disabled:
-      cy.getByTestID('actually-switch-account--btn').should('be.disabled')
-
-      // now:  select another option:
-      cy.getByTestID(`${prefix}-2-ID`).click()
-
-      // check that it is selected before checking the button enabled state:
-      cy.getByTestID(`${prefix}-2-ID--input`).should('be.checked')
-
-      // now; the button should be enabled:
-      cy.getByTestID('actually-switch-account--btn').should('be.enabled')
-    })
-  })
-})
 describe('Account Page; user with 4 accounts', () => {
   beforeEach(() => doSetup(cy, 4))
 
-  it('can change the default account, and then see that it changed on the switch dialog', () => {
+  it('can change the default account, and then see that it changed on the switch dialog; also checks that the switch button is enabled and disabled correctly', () => {
     cy.getByTestID('account-settings--header').should('be.visible')
     cy.getByTestID('user-account-switch-btn').should('be.visible')
     cy.getByTestID('account-active-name--block').contains('Influx')

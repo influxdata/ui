ad795a49d6b6b5af6ee51fe43ad33a91c3ee61e5
Jill R Pelavin <jpelavin@influxdata.com>
2022-01-12 09:54:41 -0500

    added e2e test for phase 2: default account setting"

--- cypress/e2e/cloud/userAccounts.test.ts
+++ cypress/e2e/cloud/userAccounts.test.ts
@@ -54,9 +54,77 @@ describe('Account Page; user with 3 accounts', () => {
       cy.getByTestID('actually-switch-account--btn').should('be.enabled')
     })
   })
+})
+describe('Account Page; user with 4 accounts', () => {
+  beforeEach(() => doSetup(cy, 4))
+
   it('can change the default account, and then see that it changed on the switch dialog', () => {
-    // TODO:  do this test next!
-    expect(5).equal(5)
+    cy.getByTestID('account-settings--header').should('be.visible')
+    cy.getByTestID('user-account-switch-btn').should('be.visible')
+    cy.getByTestID('account-active-name--block').contains('Influx')
+
+    cy.getByTestID('user-account-switch-btn').click()
+
+    const prefix = 'accountSwitch-toggle-choice'
+
+    cy.getByTestID('switch-account--dialog').within(() => {
+      cy.getByTestID(`${prefix}-0-ID`).should('be.visible')
+      cy.getByTestID(`${prefix}-0-ID`).contains('Influx')
+      cy.getByTestID(`${prefix}-0-ID--input`).should('be.checked')
+
+      cy.getByTestID(`${prefix}-1-ID`).should('be.visible')
+      cy.getByTestID(`${prefix}-1-ID`).contains('Veganomicon (default)')
+
+      cy.getByTestID(`${prefix}-2-ID`).should('be.visible')
+      cy.getByTestID(`${prefix}-2-ID`).contains('Stradivarius')
+
+      cy.getByTestID(`${prefix}-3-ID`).should('be.visible')
+      cy.getByTestID(`${prefix}-3-ID`).contains('Yamaha')
+
+      // at first; the switch button should be disabled:
+      cy.getByTestID('actually-switch-account--btn').should('be.disabled')
+
+      // the set default button should be *enabled*
+      cy.getByTestID('switch-default-account--btn').should('be.enabled')
+
+      // now:  select another option:
+      cy.getByTestID(`${prefix}-1-ID`).click()
+
+      // check that it is selected before checking the button enabled state:
+      cy.getByTestID(`${prefix}-1-ID--input`).should('be.checked')
+
+      // now; the button should be enabled:
+      cy.getByTestID('actually-switch-account--btn').should('be.enabled')
+
+      // and the default button should be *disabled* b/c just chose the default acct:
+      cy.getByTestID('switch-default-account--btn').should('be.disabled')
+
+      // ok; now pick the third option:
+      cy.getByTestID(`${prefix}-2-ID`).click()
+
+      // check that it is selected before going to the next step:
+      cy.getByTestID(`${prefix}-2-ID--input`).should('be.checked')
+      cy.getByTestID('switch-default-account--btn').should('be.enabled')
+
+      cy.getByTestID('switch-default-account--btn').click()
+    })
+    // test that the notification is up:
+    cy.getByTestID('notification-success').should('be.visible')
+
+    // now; bring up the dialog again, the default one should be changed:
+    cy.getByTestID('user-account-switch-btn').click()
+
+    cy.getByTestID('switch-account--dialog').within(() => {
+      cy.getByTestID(`${prefix}-0-ID`).should('be.visible')
+      cy.getByTestID(`${prefix}-0-ID`).contains('Influx')
+      cy.getByTestID(`${prefix}-0-ID--input`).should('be.checked')
+
+      cy.getByTestID(`${prefix}-1-ID`).should('be.visible')
+      cy.getByTestID(`${prefix}-1-ID`).contains('Veganomicon')
+
+      cy.getByTestID(`${prefix}-2-ID`).should('be.visible')
+      cy.getByTestID(`${prefix}-2-ID`).contains('Stradivarius (default)')
+    })
   })
 })
 

81936d75b9a22bdc663760e853488b83d040dcca
Subir Jolly <sjolly@influxdata.com>
2022-01-13 16:44:35 -0500

    chore: update bucket selector to fetch all buckets

--- src/flows/context/bucket.scoped.tsx
+++ src/flows/context/bucket.scoped.tsx
@@ -2,16 +2,15 @@
 import React, {
   FC,
   createContext,
-  useContext,
   useEffect,
   useState,
   useMemo,
   useRef,
 } from 'react'
+import {useSelector} from 'react-redux'
+import {getSortedBuckets} from 'src/buckets/selectors'
 
 // Contexts
-import {FlowQueryContext} from 'src/flows/context/flow.query'
-import {PipeContext} from 'src/flows/context/pipe'
 
 // Types
 import {Bucket, RemoteDataState} from 'src/types'
@@ -29,14 +28,54 @@ const DEFAULT_CONTEXT: BucketContextType = {
 }
 
 export const BucketContext = createContext<BucketContextType>(DEFAULT_CONTEXT)
+const SAMPLE_BUCKETS: any = [
+  {
+    type: 'sample',
+    name: 'Air Sensor Data',
+    id: 'airSensor',
+  },
+  {
+    type: 'sample',
+    name: 'Coinbase bitcoin price',
+    id: 'bitcoin',
+  },
+  {
+    type: 'sample',
+    name: 'NOAA National Buoy Data',
+    id: 'noaa',
+  },
+  {
+    type: 'sample',
+    name: 'USGS Earthquakes',
+    id: 'usgs',
+  },
+]
 
 export const BucketProvider: FC = ({children}) => {
   const [loading, setLoading] = useState(RemoteDataState.NotStarted)
   const [buckets, setBuckets] = useState<Bucket[]>([])
-  const {getPanelQueries} = useContext(FlowQueryContext)
-  const {id} = useContext(PipeContext)
-  const scope = getPanelQueries(id)?.scope ?? {}
   const controller = useRef(new AbortController())
+  const allBuckets = useSelector(getSortedBuckets)
+
+  useEffect(() => {
+    if (buckets.length) {
+      return
+    }
+
+    setLoading(RemoteDataState.Loading)
+
+    const tempBuckets = allBuckets.map(bucket => {
+      return {
+        id: bucket.id,
+        name: bucket.name,
+        orgID: bucket.orgID,
+        type: bucket.type,
+      }
+    })
+
+    setLoading(RemoteDataState.Done)
+    setBuckets([...tempBuckets, ...SAMPLE_BUCKETS])
+  }, [buckets, allBuckets])
 
   useEffect(() => {
     return () => {
@@ -48,92 +87,6 @@ export const BucketProvider: FC = ({children}) => {
       }
     }
   }, [controller])
-
-  useEffect(() => {
-    if (!scope.region || !scope.org) {
-      return
-    }
-
-    // TODO: cancel any active queries
-
-    setLoading(RemoteDataState.Loading)
-
-    const headers = {
-      'Content-Type': 'application/json',
-      'Accept-Encoding': 'gzip',
-    }
-
-    if (scope.token) {
-      headers['Authorization'] = `Token ${scope.token}`
-    }
-
-    fetch(`${scope.region}/api/v2/buckets?limit=100&orgID=${scope.org}`, {
-      method: 'GET',
-      headers,
-      signal: controller.current.signal,
-    })
-      .then(response => {
-        return response.json()
-      })
-      .then(response => {
-        const bucks = response.buckets
-          .map(bucket => ({
-            id: bucket.id,
-            orgID: bucket.orgID,
-            type: bucket.type,
-            name: bucket.name,
-          }))
-          .reduce(
-            (acc, curr) => {
-              if (curr.type === 'system') {
-                acc.system.push(curr)
-              } else {
-                acc.user.push(curr)
-              }
-              return acc
-            },
-            {
-              system: [],
-              user: [],
-              sample: [
-                {
-                  type: 'sample',
-                  name: 'Air Sensor Data',
-                  id: 'airSensor',
-                },
-                {
-                  type: 'sample',
-                  name: 'Coinbase bitcoin price',
-                  id: 'bitcoin',
-                },
-                {
-                  type: 'sample',
-                  name: 'NOAA National Buoy Data',
-                  id: 'noaa',
-                },
-                {
-                  type: 'sample',
-                  name: 'USGS Earthquakes',
-                  id: 'usgs',
-                },
-              ],
-            }
-          )
-
-        bucks.system.sort((a, b) =>
-          `${a.name}`.toLowerCase().localeCompare(`${b.name}`.toLowerCase())
-        )
-        bucks.user.sort((a, b) =>
-          `${a.name}`.toLowerCase().localeCompare(`${b.name}`.toLowerCase())
-        )
-        bucks.sample.sort((a, b) =>
-          `${a.name}`.toLowerCase().localeCompare(`${b.name}`.toLowerCase())
-        )
-        setLoading(RemoteDataState.Done)
-        setBuckets([...bucks.user, ...bucks.system, ...bucks.sample])
-      })
-      .catch(() => {})
-  }, [scope.region, scope.org, controller])
 
   const addBucket = (bucket: Bucket) => {
     setBuckets([...buckets, bucket])

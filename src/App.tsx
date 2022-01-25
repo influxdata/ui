6bcad4e2003edbbb027d13f3d9aad2012c4e8517
asalem1 <19984220+asalem1@users.noreply.github.com>
2022-01-25 04:57:10 -0800

    chore: remove GlobalRefresh feature flag (#3676)

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ const App: FC = () => {
 
   return (
     <AppWrapper presentationMode={presentationMode} className={appWrapperClass}>
-      {isFlagEnabled('globalSearch') && <GlobalSearch />}
+      {CLOUD && <GlobalSearch />}
       <Notifications />
       <TooltipPortal />
       <NotesPortal />

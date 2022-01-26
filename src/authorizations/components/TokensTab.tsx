0f954d48764c0ffd0df89f06af5d12e598a3ca08
Palak Bhojani <palak@influxdata.com>
2022-01-26 15:20:04 -0800

    fix: typing for tokens

--- src/authorizations/components/TokensTab.tsx
+++ src/authorizations/components/TokensTab.tsx
@@ -231,7 +231,7 @@ class TokensTab extends PureComponent<Props, State> {
       return (
         <EmptyState size={ComponentSize.Large}>
           <EmptyState.Text>
-            Looks like there aren't any <b>Tokens</b>, why not generate one?
+            Looks like there aren't any <b>API Tokens</b>, why not generate one?
           </EmptyState.Text>
           <GenerateTokenDropdown />
         </EmptyState>
@@ -240,7 +240,7 @@ class TokensTab extends PureComponent<Props, State> {
 
     return (
       <EmptyState size={ComponentSize.Large}>
-        <EmptyState.Text>No Tokens match your query</EmptyState.Text>
+        <EmptyState.Text>No API Tokens match your query</EmptyState.Text>
       </EmptyState>
     )
   }

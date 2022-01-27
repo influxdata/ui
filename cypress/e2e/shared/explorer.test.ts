f69490897269911698f4fd364aff430a7a55d97e
Chunchun <yccvivo@gmail.com>
2022-01-27 09:10:35 -0600

    chore: remove unused function makeGraphSnapshot

--- cypress/e2e/shared/explorer.test.ts
+++ cypress/e2e/shared/explorer.test.ts
@@ -1,5 +1,5 @@
 import {Organization} from '../../../src/types'
-import {points, makeGraphSnapshot} from '../../support/commands'
+import {points} from '../../support/commands'
 import {
   FROM,
   RANGE,

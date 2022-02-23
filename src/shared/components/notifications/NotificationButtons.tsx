bc4688ca4a429726d2b77e9350d12a41e4ffeeef
ChitlangeSahas <chitlangesahas@gmail.com>
2022-02-22 21:05:12 -0700

    chore: remove more unused code

--- src/shared/components/notifications/NotificationButtons.tsx
+++ src/shared/components/notifications/NotificationButtons.tsx
@@ -1,11 +1,6 @@
 // Libraries
-import React, {FC} from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom'
-import {Button, ComponentSize} from '@influxdata/clockface'
-import {useDispatch} from 'react-redux'
-
-// Utils
-import {setFunctions} from 'src/timeMachine/actions/queryBuilder'
 
 // Types
 import {NotificationDismiss} from 'src/types'
@@ -19,32 +14,4 @@ export const getDeleteAccountWarningButton = (
       Go to Users page
     </Link>
   )
-}
-
-interface AggregateTypeErrorButtonProps {
-  onDismiss: NotificationDismiss
-}
-
-export const AggregateTypeErrorButton: FC<AggregateTypeErrorButtonProps> = ({
-  onDismiss,
-}) => {
-  const dispatch = useDispatch()
-  const onClick = () => {
-    dispatch(setFunctions(['last']))
-    onDismiss()
-  }
-
-  return (
-    <Button
-      text="Update Aggregate Type"
-      onClick={onClick}
-      size={ComponentSize.ExtraSmall}
-    />
-  )
-}
-
-export const getAggregateTypeErrorButton = (
-  onDismiss: NotificationDismiss
-): JSX.Element => {
-  return <AggregateTypeErrorButton onDismiss={onDismiss} />
 }

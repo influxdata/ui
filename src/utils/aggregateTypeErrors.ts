bcaaa44cb4fe197bb0b9aa57908936267a451783
Chitlange Sahas <chitlangesahas@gmail.com>
2022-02-24 11:23:15 -0700

    fix: remove hard-coded error message for Aggregate Type error (#3932)

--- src/utils/aggregateTypeErrors.ts
+++
@@ -1,7 +0,0 @@
-const unsupportedAggregateType = 'unsupported aggregate column type'
-
-export const isAggregateTypeError = (
-  errorCode: string,
-  errorMessage: string
-): boolean =>
-  errorCode === 'invalid' && errorMessage.includes(unsupportedAggregateType)

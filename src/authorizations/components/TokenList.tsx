6dae7bfbc59ab89e5a50389bacf538fe9855ad64
Chunchun <yccvivo@gmail.com>
2022-03-01 17:23:06 -0600

    chore: add functional component for TokenList

--- src/authorizations/components/TokenList.tsx
+++ src/authorizations/components/TokenList.tsx
@@ -1,5 +1,13 @@
 // Libraries
-import React, {PureComponent, RefObject} from 'react'
+import React, {
+  PureComponent,
+  RefObject,
+  FC,
+  useState,
+  useEffect,
+  useMemo,
+  useReducer,
+} from 'react'
 import memoizeOne from 'memoize-one'
 import isEqual from 'lodash/isEqual'
 
@@ -23,12 +31,12 @@ interface Props {
   emptyState: JSX.Element
   pageHeight: number
   pageWidth: number
-  searchTerm: string
+  searchTerm: string // why not used?
   sortKey: string
   sortDirection: Sort
   sortType: SortTypes
   tokenCount: number
-  onClickColumn: (nextSort: Sort, sortKey: SortKey) => void
+  onClickColumn: (nextSort: Sort, sortKey: SortKey) => void // why not used?
 }
 
 interface State {
@@ -36,7 +44,7 @@ interface State {
   authInView: Authorization
 }
 
-class TokenList extends PureComponent<Props, State> {
+class TokenList_T extends PureComponent<Props, State> {
   private memGetSortedResources = memoizeOne<typeof getSortedResources>(
     getSortedResources
   )
@@ -175,4 +183,129 @@ class TokenList extends PureComponent<Props, State> {
   }
 }
 
-export default TokenList
+const TokenList: FC<Props> = ({
+  auths,
+  emptyState,
+  pageHeight,
+  pageWidth,
+  sortKey,
+  sortDirection,
+  sortType,
+  tokenCount,
+}) => {
+  const [isTokenOverlayVisible, setIsTokenOverlayVisible] = useState(false)
+  const [authInView, setAuthInView] = useState(null)
+  const [, forceUpdate] = useReducer(x => x + 1, 0)
+
+  let paginationRef: RefObject<HTMLDivElement>
+  let currentPage: number = 1
+  const rowsPerPage: number = 10
+  const totalPages: number = Math.max(Math.ceil(auths.length / rowsPerPage), 1)
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const urlPageNumber = parseInt(params.get('page'), 10)
+
+    const passedInPageIsValid =
+      urlPageNumber && urlPageNumber <= totalPages && urlPageNumber > 0
+
+    if (passedInPageIsValid) {
+      currentPage = urlPageNumber
+    }
+  }, [])
+
+  useEffect(() => {
+    // TODO
+    // if the user filters the list while on a page that is
+    // outside the new filtered list put them on the last page of the new list
+    if (currentPage > totalPages) {
+      paginate(totalPages)
+    }
+  })
+
+  useEffect(() => {
+    const newAuthInView = auths.find(auth => auth.id === authInView?.id)
+    setAuthInView(newAuthInView)
+  }, [auths])
+
+  const sortedAuths = useMemo(
+    () => getSortedResources(auths, sortKey, sortDirection, sortType),
+    [auths, sortKey, sortDirection, sortType]
+  )
+
+  const paginate = page => {
+    console.log(page)
+    currentPage = page
+    const url = new URL(location.href)
+    url.searchParams.set('page', page)
+    history.replaceState(null, '', url.toString())
+    forceUpdate()
+  }
+
+  const rows = (): JSX.Element[] => {
+    const startIndex = rowsPerPage * Math.max(currentPage - 1, 0)
+    const endIndex = Math.min(startIndex + rowsPerPage, tokenCount)
+
+    const paginatedAuths = []
+    for (let i = startIndex; i < endIndex; i++) {
+      const auth = sortedAuths[i]
+
+      if (auth) {
+        paginatedAuths.push(
+          <TokenRow
+            key={auth.id}
+            auth={auth}
+            onClickDescription={handleClickDescription}
+          />
+        )
+      }
+    }
+
+    return paginatedAuths
+  }
+
+  const handleDismissOverlay = () => {
+    setIsTokenOverlayVisible(false)
+  }
+
+  const handleClickDescription = (authID: string): void => {
+    const newAuthInView = auths.find(a => a.id === authID)
+    setAuthInView(newAuthInView)
+    setIsTokenOverlayVisible(true)
+  }
+
+  return (
+    <>
+      <ResourceList style={{width: pageWidth}}>
+        <ResourceList.Body
+          emptyState={emptyState}
+          style={{
+            maxHeight: pageHeight,
+            minHeight: pageHeight,
+            overflow: 'auto',
+          }}
+          testID="token-list"
+        >
+          {rows()}
+        </ResourceList.Body>
+      </ResourceList>
+      <PaginationNav.PaginationNav
+        ref={paginationRef}
+        style={{width: pageWidth}}
+        totalPages={totalPages}
+        currentPage={currentPage}
+        pageRangeOffset={1}
+        onChange={paginate}
+      />
+      <Overlay visible={isTokenOverlayVisible}>
+        <EditTokenOverlay
+          auth={authInView}
+          onDismissOverlay={handleDismissOverlay}
+        />
+      </Overlay>
+    </>
+  )
+}
+
+export default React.memo(TokenList)
+// export default TokenList_T

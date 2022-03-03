f4e10fec47e69b8d14bb54040c87707621d826ea
ChitlangeSahas <chitlangesahas@gmail.com>
2022-03-03 13:52:30 -0700

    feat: Added Overview.tsx content

--- src/homepageExperience/components/steps/InstallDependencies.tsx
+++ src/homepageExperience/components/steps/InstallDependencies.tsx
@@ -6,7 +6,11 @@ export class InstallDependencies extends PureComponent {
     return (
       <>
         <h1>Install Dependencies</h1>
-        <p>First, you need to install the <code style={{color: '#B7B8FF'}}>influxdb-client</code> module. Run the command below in your terminal.</p>
+        <p>
+          First, you need to install the{' '}
+          <code style={{color: '#B7B8FF'}}>influxdb-client</code> module. Run
+          the command below in your terminal.
+        </p>
         <CodeSnippet text="pip3 install influxdb-client" onCopy={null} />
       </>
     )
--- src/homepageExperience/components/steps/Overview.tsx
+++ src/homepageExperience/components/steps/Overview.tsx
@@ -2,6 +2,25 @@ import React, {PureComponent} from 'react'
 
 export class Overview extends PureComponent {
   render() {
-    return <h1>Overview</h1>
+    return (
+      <>
+        <h1>Hello, Time-Series World!</h1>
+        <p>
+          Welcome and thanks for checking out InfluxData!
+          <br /> <br />
+          In the next 5 minutes, you will set up InfluxData on your machine and
+          write and execute some basic queries.
+          <br /> <br />
+          If you ever stray away from this set up process, worry not! You can
+          always refer to the “Help & Support” item in the side navigation menu
+          to return here.
+          <br /> <br />
+          Without further ado, let’s get started.
+          <br /> <br /> <br /> <br /> <br /> <br />
+          Want to just look at code? Check out the code snippets involved in
+          this guide on <a href="https://github.com/influxdata/ui">Github</a>.
+        </p>
+      </>
+    )
   }
 }

2bac60994a24938ea110204a7784a1048626994e
ChitlangeSahas <chitlangesahas@gmail.com>
2022-03-09 11:08:57 -0700

    feat: adds a retry button to the connection information

--- src/homepageExperience/components/DataListening.tsx
+++ src/homepageExperience/components/DataListening.tsx
@@ -1,15 +1,14 @@
 // Libraries
 import React, {PureComponent} from 'react'
-import {withRouter, RouteComponentProps} from 'react-router-dom'
+import {RouteComponentProps, withRouter} from 'react-router-dom'
 
 // Apis
 import {runQuery} from 'src/shared/apis/query'
 
 // Components
 import {ErrorHandling} from 'src/shared/decorators/errors'
-import ConnectionInformation, {
-  LoadingState,
-} from 'src/dataLoaders/components/verifyStep/ConnectionInformation'
+import ConnectionInformation, {LoadingState} from './ConnectionInformation'
+import {Button} from '@influxdata/clockface'
 
 interface OwnProps {
   bucket: string
@@ -20,6 +19,7 @@ interface State {
   timePassedInSeconds: number
   secondsLeft: number
   previousBucket: string
+  retry: boolean
 }
 
 const MINUTE = 60000
@@ -43,6 +43,7 @@ class DataListening extends PureComponent<Props, State> {
       timePassedInSeconds: 0,
       secondsLeft: SECONDS,
       previousBucket: null,
+      retry: false,
     }
   }
 
@@ -57,19 +58,33 @@ class DataListening extends PureComponent<Props, State> {
 
   componentDidUpdate() {
     const {bucket} = this.props
-    if (bucket !== '<BUCKET>' && this.state.previousBucket !== bucket) {
+    if (
+      (bucket !== '<BUCKET>' && this.state.previousBucket !== bucket) ||
+      this.state.retry
+    ) {
       // clear timer when bucket changes
       clearInterval(this.intervalID)
       clearInterval(this.timer)
+      this.setState({
+        timePassedInSeconds: 0,
+        secondsLeft: SECONDS,
+      })
       this.startListeningForData()
-      this.setState({previousBucket: bucket})
+      this.setState({previousBucket: bucket, retry: false})
     }
+  }
+
+  private handleRetry = () => {
+    this.setState({retry: true})
   }
 
   public render() {
     return (
       <div className="wizard-step--body-streaming" data-testid="streaming">
         {this.connectionInfo}
+        {this.state.loading === LoadingState.NotFound && (
+          <Button onClick={this.handleRetry} text="Retry" />
+        )}
       </div>
     )
   }
@@ -92,9 +107,10 @@ class DataListening extends PureComponent<Props, State> {
 
   private startListeningForData = (): void => {
     this.startTimer()
-    this.setState({loading: LoadingState.Loading})
-    this.startTime = Number(new Date())
-    this.checkForData()
+    this.setState({loading: LoadingState.Loading}, () => {
+      this.startTime = Number(new Date())
+      this.checkForData()
+    })
   }
 
   private checkForData = async (): Promise<void> => {

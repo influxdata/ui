854e19b3c6275bd574f00d43292a4d4cc7e5fbb5
ChitlangeSahas <chitlangesahas@gmail.com>
2022-03-09 09:47:22 -0700

    feat: listen for data activated right after page loads

--- src/homepageExperience/components/steps/WriteData.tsx
+++ src/homepageExperience/components/steps/WriteData.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {useSelector} from 'react-redux'
+import React, {useContext, useEffect, useState} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import {
   Columns,
   ComponentSize,
@@ -9,16 +9,17 @@ import {
 } from '@influxdata/clockface'
 
 import {SafeBlankLink} from 'src/utils/SafeBlankLink'
-import GetResources from 'src/resources/components/GetResources'
 import WriteDataHelperBuckets from 'src/writeData/components/WriteDataHelperBuckets'
 import CodeSnippet from 'src/shared/components/CodeSnippet'
-import WriteDataDetailsContextProvider from 'src/writeData/components/WriteDataDetailsContext'
+import WriteDataDetailsContextProvider, {
+  WriteDataDetailsContext,
+} from 'src/writeData/components/WriteDataDetailsContext'
 
 import {getOrg} from 'src/organizations/selectors'
-import {ResourceType} from 'src/types'
-import {ListeningDataHeartbeatIcon} from '../HomepageIcons'
+import DataListening from '../DataListening'
+import {getBuckets} from '../../../buckets/actions/thunks'
 
-export const WriteData = () => {
+export const WriteDataComponent = () => {
   const org = useSelector(getOrg)
 
   const codeSnippet = `for value in range(5):
@@ -29,59 +30,65 @@ export const WriteData = () => {
     )
     write_api.write(bucket=bucket, org=org, record=point)
     time.sleep(1)`
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getBuckets())
+  }, [])
+
+  const {bucket} = useContext(WriteDataDetailsContext)
+
+  const [selectedBucket, setSelectedBucket] = useState(bucket)
+
+  useEffect(() => {
+    setSelectedBucket(bucket)
+  }, [bucket])
+
 
   return (
     <>
-      <h1>Write Data</h1>
-      <p>
-        To start writing data, we need a place to our time-series store data. We
-        call these{' '}
-        <SafeBlankLink href={`orgs/${org.id}/load-data/buckets`}>
-          buckets.
-        </SafeBlankLink>
-      </p>
-      <GetResources resources={[ResourceType.Buckets]}>
-        <WriteDataDetailsContextProvider>
-          <Panel backgroundColor={InfluxColors.Grey15}>
-            <Panel.Body size={ComponentSize.ExtraSmall}>
-              <Grid>
-                <Grid.Row>
-                  <Grid.Column widthSM={Columns.Twelve}>
-                    <WriteDataHelperBuckets />
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Panel.Body>
-          </Panel>
-        </WriteDataDetailsContextProvider>
-      </GetResources>
+        <h1>Write Data</h1>
+        <p>
+          To start writing data, we need a place to our time-series store data.
+          We call these{' '}
+          <SafeBlankLink href={`orgs/${org.id}/load-data/buckets`}>
+            buckets.
+          </SafeBlankLink>
+        </p>
 
-      <p>
-        You’re ready to write data! In this code, we define five data points and
-        write each one for InfluxDB. Paste the following code after the prompt
-        (>>>) and press Enter.
-      </p>
-      <CodeSnippet text={codeSnippet} />
-      <p style={{marginTop: '40px'}}>
-        Once you this data, you’ll begin to see the confirmation below
-      </p>
+        <Panel backgroundColor={InfluxColors.Grey15}>
+          <Panel.Body size={ComponentSize.ExtraSmall}>
+            <Grid>
+              <Grid.Row>
+                <Grid.Column widthSM={Columns.Twelve}>
+                  <WriteDataHelperBuckets />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Panel.Body>
+        </Panel>
+        <p>
+          You’re ready to write data! In this code, we define five data points
+          and write each one for InfluxDB. Paste the following code after the
+          prompt (>>>) and press Enter.
+        </p>
+        <CodeSnippet text={codeSnippet} />
+        <p style={{marginTop: '40px'}}>
+          Once you write this data, you’ll begin to see the confirmation below
+        </p>
 
-      <Panel backgroundColor={InfluxColors.Grey15}>
-        <Panel.Body>
-          <p
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              fontFamily: 'IBMPlexMono',
-              fontSize: '14px',
-              justifyContent: 'center',
-            }}
-          >
-            {ListeningDataHeartbeatIcon}{' '}
-            <span style={{marginLeft: '10px'}}>Listening for data...</span>
-          </p>
-        </Panel.Body>
-      </Panel>
+        <Panel backgroundColor={InfluxColors.Grey15}>
+          <Panel.Body>
+            <DataListening bucket={selectedBucket.name} />
+          </Panel.Body>
+        </Panel>
     </>
   )
 }
+
+export const WriteData = () => {
+  return <WriteDataDetailsContextProvider>
+    <WriteDataComponent/>
+  </WriteDataDetailsContextProvider>
+}
+

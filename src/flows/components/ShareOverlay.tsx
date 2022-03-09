a2fbd5af1d1dde2680e2bff672fbce8f73434afc
Chunchun <yccvivo@gmail.com>
2022-03-09 11:23:30 -0600

    feat: add simple context in the overlay body

--- src/flows/components/ShareOverlay.tsx
+++ src/flows/components/ShareOverlay.tsx
@@ -2,16 +2,53 @@
 import React, {FC, useContext} from 'react'
 
 // Components
-import {Overlay} from '@influxdata/clockface'
+import {
+  AlignItems,
+  FlexBox,
+  FlexDirection,
+  Overlay,
+  Icon,
+  IconFont,
+  Button,
+} from '@influxdata/clockface'
 
 import {OverlayContext} from 'src/overlays/components/OverlayController'
+
+import './ShareOverlay.scss'
 
 const ShareOverlay: FC = () => {
   const {onClose} = useContext(OverlayContext)
   return (
-    <Overlay.Container maxWidth={600}>
+    <Overlay.Container maxWidth={800}>
       <Overlay.Header title="Share Notebook" onDismiss={onClose} />
-      <Overlay.Body>hello</Overlay.Body>
+      <Overlay.Body>
+        <FlexBox
+          direction={FlexDirection.Column}
+          alignItems={AlignItems.Stretch}
+        >
+          <FlexBox.Child className="share-section--alert">
+            <Icon glyph={IconFont.AlertTriangle_New} />
+            <span className="share-text">
+              By sharing this link, your org may incur charges when a user
+              visits the page and the query is run.
+            </span>
+          </FlexBox.Child>
+          <FlexBox.Child className="share-section--link">
+            Share Link
+            <Icon glyph={IconFont.Eye_New} />
+            <span className="share-text">
+              Anyone with this link can view this Notebook, but will not have
+              access to autorefresh updates.
+            </span>
+          </FlexBox.Child>
+          <FlexBox.Child className="share-section--delete">
+            <Button text="Delete Link" icon={IconFont.Trash_New} />
+            <span className="share-text">
+              If deleted, viewers with this link will no longer have access.
+            </span>
+          </FlexBox.Child>
+        </FlexBox>
+      </Overlay.Body>
       <Overlay.Footer>Done</Overlay.Footer>
     </Overlay.Container>
   )

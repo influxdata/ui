284840f333a3609574795449eab020fac5442d20
wiedld <dlw405@gmail.com>
2022-03-09 19:57:34 -0800

    fix(4021): join multiple threshold conditions correctly

--- src/flows/pipes/Visualization/threshold.ts
+++ src/flows/pipes/Visualization/threshold.ts
@@ -18,31 +18,28 @@ export interface ErrorThreshold extends Threshold {
   fieldType: string
 }
 
+export const lambdaPrefix = '(r) =>'
+
 export const COMMON_THRESHOLD_TYPES = {
   greater: {
     name: 'greater than',
     format: ThresholdFormat.Value,
-    condition: data => `(r) => (r["${data.field}"] > ${data.value})`,
+    condition: data => `${lambdaPrefix} (r["${data.field}"] > ${data.value})`,
   },
   'greater-equal': {
     name: 'greater than or equal to',
     format: ThresholdFormat.Value,
-    condition: data => `(r) => (r["${data.field}"] >= ${data.value})`,
+    condition: data => `${lambdaPrefix} (r["${data.field}"] >= ${data.value})`,
   },
   less: {
     name: 'less than',
     format: ThresholdFormat.Value,
-    condition: data => `(r) => (r["${data.field}"] < ${data.value})`,
+    condition: data => `${lambdaPrefix} (r["${data.field}"] < ${data.value})`,
   },
   'less-equal': {
     name: 'less than or equal to',
     format: ThresholdFormat.Value,
-    condition: data => `(r) => (r["${data.field}"] <= ${data.value})`,
-  },
-  equal: {
-    name: 'equal to',
-    format: ThresholdFormat.Value,
-    condition: data => `(r) => (r["${data.field}"] == ${data.value})`,
+    condition: data => `${lambdaPrefix} (r["${data.field}"] <= ${data.value})`,
   },
   'not-equal': {
     name: 'not equal to',
@@ -53,13 +50,13 @@ export const COMMON_THRESHOLD_TYPES = {
     name: 'between',
     format: ThresholdFormat.Range,
     condition: data =>
-      `(r) => (r["${data.field}"] > ${data.min} and r["${data.field}"] < ${data.max})`,
+      `${lambdaPrefix} (r["${data.field}"] > ${data.min} and r["${data.field}"] < ${data.max})`,
   },
   'not-between': {
     name: 'not between',
     format: ThresholdFormat.Range,
     condition: data =>
-      `(r) => (r["${data.field}"] < ${data.min} or r["${data.field}"] > ${data.max})`,
+      `${lambdaPrefix} (r["${data.field}"] < ${data.min} or r["${data.field}"] > ${data.max})`,
   },
 }
 
@@ -67,12 +64,12 @@ export const EQUALITY_THRESHOLD_TYPES = {
   equal: {
     name: 'equal to',
     format: ThresholdFormat.Value,
-    condition: data => `(r) => (r["${data.field}"] == ${data.value})`,
+    condition: data => `${lambdaPrefix} (r["${data.field}"] == ${data.value})`,
   },
   'not-equal': {
     name: 'not equal to',
     format: ThresholdFormat.Value,
-    condition: data => `(r) => (r["${data.field}"] != ${data.value})`,
+    condition: data => `${lambdaPrefix} (r["${data.field}"] != ${data.value})`,
   },
 }
 
@@ -83,6 +80,6 @@ export const THRESHOLD_TYPES = {
   [deadmanType]: {
     name: 'missing for longer than',
     format: ThresholdFormat.Deadman,
-    condition: _ => `(r) => (r["dead"])`,
+    condition: _ => `${lambdaPrefix} (r["dead"])`,
   },
 }

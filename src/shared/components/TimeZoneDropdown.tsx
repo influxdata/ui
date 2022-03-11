727acfbdeda63af1aa0780660a47d1bd6b3f1237
drdelambre <drdelambre@gmail.com>
2022-03-11 10:58:46 -0800

    fix: add eventing around controversal dropdown

--- src/shared/components/TimeZoneDropdown.tsx
+++ src/shared/components/TimeZoneDropdown.tsx
@@ -7,15 +7,31 @@ import {AppSettingContext} from 'src/shared/contexts/app'
 
 // Constants
 import {TIME_ZONES} from 'src/shared/constants/timeZones'
+import {TimeZone} from 'src/types'
+import {event} from 'src/cloud/utils/reporting'
 
 const TimeZoneDropdown: FC = () => {
   const {timeZone, setTimeZone} = useContext(AppSettingContext)
+
+  const onSelect = (zone: TimeZone) => {
+    let loc = 'other'
+    if (/notebooks/.test(window.location.pathname)) {
+      loc = 'notebooks'
+    } else if (/data-explorer/.test(window.location.pathname)) {
+      loc = 'data explorer'
+    } else if (/dashboard/.test(window.location.pathname)) {
+      loc = 'dashboard'
+    }
+
+    event('user selected new timezone', {location: loc})
+    setTimeZone(zone)
+  }
 
   return (
     <SelectDropdown
       options={TIME_ZONES.map(tz => tz.timeZone)}
       selectedOption={timeZone}
-      onSelect={setTimeZone}
+      onSelect={onSelect}
       buttonIcon={IconFont.Annotate_New}
       style={{width: '115px'}}
     />

7ac208797631253df15e8493bf400c7613feceab
Beth Legesse <legessebl@gmail.com>
2022-03-14 11:44:16 -0500

    feat: add useMemo to sorted func

--- src/timeMachine/components/dynamicFluxFunctionsToolbar/TransformToolbarFunctions.tsx
+++ src/timeMachine/components/dynamicFluxFunctionsToolbar/TransformToolbarFunctions.tsx
@@ -16,25 +16,22 @@ interface Props {
 const TransformToolbarFunctions: FC<Props> = props => {
   const {searchTerm, funcs, children} = props
 
-  const sortedFunctions = funcs.sort((a, b) => {
+  const sortFunctions = funcs.sort((a, b) => {
     if (a.package.toLowerCase() === b.package.toLowerCase()) {
       return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
     } else {
       return a.package.toLowerCase() < b.package.toLowerCase() ? -1 : 1
     }
   })
+  const sortedFunctions = useMemo(() => sortFunctions, [funcs])
 
-  const filteredFunctions = useMemo(
-    () =>
-      sortedFunctions.filter(fn => {
-        return (
-          !searchTerm.length ||
-          fn.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          fn.package.toLowerCase().includes(searchTerm.toLowerCase())
-        )
-      }),
-    [searchTerm, sortedFunctions]
-  )
+  const filteredFunctions = sortedFunctions.filter(fn => {
+    return (
+      !searchTerm.length ||
+      fn.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      fn.package.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  })
 
   if (filteredFunctions.length === 0) {
     return (

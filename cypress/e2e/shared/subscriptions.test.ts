64d950eb3a76b30f5efd9b07d752374ba3181777
helenosheaa <38860767+helenosheaa@users.noreply.github.com>
2022-03-15 14:57:37 -0400

    fix: test ids

--- cypress/e2e/shared/subscriptions.test.ts
+++ cypress/e2e/shared/subscriptions.test.ts
@@ -43,15 +43,17 @@ describe('Subscriptions', () => {
 
   //   cy.getByTestID('subscriptions-empty-state').should('exist')
   // })
-  it('Create a subscription from the index page from load data nav', () => {
+  it('Create a LP subscription from the index page from load data nav', () => {
     cy.get('.cf-tree-nav--toggle').click({force: true})
     cy.getByTestID('nav-item-load-data').should('be.visible')
     cy.getByTestID('nav-item-load-data').click()
     cy.getByTestID('subscriptions--tab').should('be.visible')
     cy.getByTestID('subscriptions--tab').click()
 
-    cy.getByTestID('create-subscription-button').should('be.visible')
-    cy.getByTestID('create-subscription-button').first().click()
+    cy.getByTestID('create-subscription-button--empty').should('be.visible')
+    cy.getByTestID('create-subscription-button--empty')
+      .first()
+      .click()
 
     // broker form
     cy.getByTestID('create-broker-form-overlay').should('be.visible')
@@ -61,11 +63,13 @@ describe('Subscriptions', () => {
     cy.getByTestID('create-broker-form--cancel').click()
 
     // return to broker form
-    cy.getByTestID('create-subscription-button').should('be.visible')
-    cy.getByTestID('create-subscription-button').first().click()
+    cy.getByTestID('create-subscription-button--control-bar').should('be.visible')
+    cy.getByTestID('create-subscription-button--control-bar')
+      .first()
+      .click()
     cy.getByTestID('create-broker-form-overlay').should('be.visible')
 
-    // fill in broker form 
+    // fill in broker form
     cy.getByTestID('create-broker-form--name').type('My Subscription')
     cy.getByTestID('create-broker-form--description').type('My Description')
     cy.getByTestID('dropdown')
@@ -76,21 +80,27 @@ describe('Subscriptions', () => {
     cy.getByTestID('create-broker-form--submit').click()
 
     // subscription form
-    cy.getByTestID('create-subscription-form--overlay-form').should('be.visible')
+    cy.getByTestID('create-subscription-form--overlay-form').should(
+      'be.visible'
+    )
 
     // back to broker form
     cy.getByTestID('create-subscription-form--back').should('be.visible')
     cy.getByTestID('create-subscription-form--back').click()
     cy.getByTestID('create-broker-form-overlay').should('be.visible')
-   
+
     // return to subscription
     cy.getByTestID('create-broker-form--submit').should('be.visible')
     cy.getByTestID('create-broker-form--submit').click()
-    cy.getByTestID('create-subscription-form--overlay-form').should('be.visible')
+    cy.getByTestID('create-subscription-form--overlay-form').should(
+      'be.visible'
+    )
 
     // fill in subscription form
     cy.getByTestID('create-subscription-form--topic').type('topic')
-    cy.getByTestID('list-item').contains('defbuck').click()
+    cy.getByTestID('list-item')
+      .contains('defbuck')
+      .click()
     cy.getByTestID('create-subscription-form--submit').click()
 
     // parsing form
@@ -99,7 +109,9 @@ describe('Subscriptions', () => {
     // back to subscription
     cy.getByTestID('create-parsing-form--back').should('be.visible')
     cy.getByTestID('create-parsing-form--back').click()
-    cy.getByTestID('create-subscription-form--overlay-form').should('be.visible')
+    cy.getByTestID('create-subscription-form--overlay-form').should(
+      'be.visible'
+    )
 
     // return to parsing
     cy.getByTestID('create-subscription-form--submit').click()
@@ -111,4 +123,124 @@ describe('Subscriptions', () => {
     cy.getByTestID('subscription-card').should('be.visible')
     cy.getByTestID('subscription-card').contains('My Subscription')
   })
+  // it('Create a JSON subscription from the index page from load data nav', () => {
+  //   cy.get('.cf-tree-nav--toggle').click({force: true})
+  //   cy.getByTestID('nav-item-load-data').should('be.visible')
+  //   cy.getByTestID('nav-item-load-data').click()
+  //   cy.getByTestID('subscriptions--tab').should('be.visible')
+  //   cy.getByTestID('subscriptions--tab').click()
+
+  //   cy.getByTestID('create-subscription-button--control-bar').should('be.visible')
+  //   cy.getByTestID('create-subscription-button--control-bar')
+  //     .first()
+  //     .click()
+
+  //   // broker form
+  //   cy.getByTestID('create-broker-form-overlay').should('be.visible')
+  //   cy.getByTestID('create-broker-form--name').type('My Subscription 2')
+  //   cy.getByTestID('create-broker-form--description').type('My Description')
+  //   cy.getByTestID('dropdown')
+  //     .contains('MQTT')
+  //     .click()
+  //   cy.getByTestID('create-broker-form--host').type('localhost')
+  //   cy.getByTestID('create-broker-form--port').type('1883')
+  //   cy.getByTestID('create-broker-form--submit').click()
+
+  //   // subscription form
+  //   cy.getByTestID('create-subscription-form--overlay-form').should(
+  //     'be.visible'
+  //   )
+  //   cy.getByTestID('create-subscription-form--topic').type('topic')
+  //   cy.getByTestID('list-item')
+  //     .contains('defbuck')
+  //     .click()
+  //   cy.getByTestID('create-subscription-form--submit').click()
+
+  //   // parsing form
+  //   cy.getByTestID('create-parsing-form-overlay').should('be.visible')
+
+  //   // json
+  //   cy.getByTestID('create-parsing-form-json--button').click()
+  //   cy.getByTestID('timestamp-json-parsing').type('0123456789')
+  //   cy.getByTestID('measurement-json-parsing-name').type('measurement')
+  //   cy.getByTestID('measurement-json-parsing-type')
+  //     .contains('string')
+  //     .click()
+  //   cy.getByTestID('measurement-json-parsing-path').type('$.m')
+  //   cy.getByTestID('field-json-parsing-name').type('field')
+  //   cy.getByTestID('field-json-parsing-type')
+  //     .first()
+  //     .contains('string')
+  //     .click()
+  //   cy.getByTestID('field-json-parsing-path').type('$.f')
+  //   cy.getByTestID('tag-json-parsing-name').type('tag')
+  //   cy.getByTestID('tag-json-parsing-type')
+  //     .contains('string')
+  //     .click()
+  //   cy.getByTestID('tag-json-parsing-path').type('$.t')
+  //   cy.getByTestID('json-parsing-add-rule')
+  //     .contains('field')
+  //     .click()
+  //   cy.getByTestID('field-json-delete-label').should('be.visible')
+  //   cy.getByTestID('field-json-delete-label').click()
+  //   cy.getByTestID('create-parsing-form--submit').click()
+
+  //   cy.getByTestID('subscription-card').should('be.visible')
+  //   cy.getByTestID('subscription-card').contains('My Subscription 2')
+  // })
+  // it('Create a String subscription from the index page from load data nav', () => {
+  //   cy.get('.cf-tree-nav--toggle').click({force: true})
+  //   cy.getByTestID('nav-item-load-data').should('be.visible')
+  //   cy.getByTestID('nav-item-load-data').click()
+  //   cy.getByTestID('subscriptions--tab').should('be.visible')
+  //   cy.getByTestID('subscriptions--tab').click()
+
+  //   cy.getByTestID('create-subscription-button--control-bar').should('be.visible')
+  //   cy.getByTestID('create-subscription-button--control-bar')
+  //     .first()
+  //     .click()
+
+  //   // broker form
+  //   cy.getByTestID('create-broker-form-overlay').should('be.visible')
+  //   cy.getByTestID('create-broker-form--name').type('My Subscription 3')
+  //   cy.getByTestID('create-broker-form--description').type('My Description')
+  //   cy.getByTestID('dropdown')
+  //     .contains('MQTT')
+  //     .click()
+  //   cy.getByTestID('create-broker-form--host').type('localhost')
+  //   cy.getByTestID('create-broker-form--port').type('1883')
+  //   cy.getByTestID('create-broker-form--submit').click()
+
+  //   // subscription form
+  //   cy.getByTestID('create-subscription-form--overlay-form').should(
+  //     'be.visible'
+  //   )
+  //   cy.getByTestID('create-subscription-form--topic').type('topic')
+  //   cy.getByTestID('list-item')
+  //     .contains('defbuck')
+  //     .click()
+  //   cy.getByTestID('create-subscription-form--submit').click()
+
+  //   // parsing form
+  //   cy.getByTestID('create-parsing-form-overlay').should('be.visible')
+
+  //   // string
+  //   cy.getByTestID('create-parsing-form-string--button').click()
+  //   cy.getByTestID('timestamp-string-parsing').type('0123456789')
+  //   cy.getByTestID('measurment-string-parsing-pattern').type('m=//m')
+  //   cy.getByTestID('Field-string-parsing-name').type('field')
+  //   cy.getByTestID('Field-string-parsing-pattern').type('f=//f')
+  //   cy.getByTestID('Tag-string-parsing-name').type('tag')
+  //   cy.getByTestID('Tag-string-parsing-pattern').type('t=//t')
+
+  //   cy.getByTestID('string-parsing-add-rule')
+  //     .contains('tag')
+  //     .click()
+  //   cy.getByTestID('Field-string-delete-label').should('be.visible')
+  //   cy.getByTestID('Field-string-delete-label').click()
+  //   cy.getByTestID('create-parsing-form--submit').click()
+
+  //   cy.getByTestID('subscription-card').should('be.visible')
+  //   cy.getByTestID('subscription-card').contains('My Subscription 3')
+  // })
 })

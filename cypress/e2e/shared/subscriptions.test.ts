dea6f768c121ad5e6d9cd14eada0b1ae60bfc88d
helenosheaa <38860767+helenosheaa@users.noreply.github.com>
2022-03-15 13:44:24 -0400

    feat: more tests

--- cypress/e2e/shared/subscriptions.test.ts
+++ cypress/e2e/shared/subscriptions.test.ts
@@ -1,57 +1,49 @@
 import {Organization} from '../../../src/types'
 
 describe('Subscriptions', () => {
-  beforeEach(() => {
-    cy.flush()
-    cy.signin()
-    cy.get<Organization>('@org').then(({id}: Organization) =>
-      cy.fixture('routes').then(({orgs}) => {
-        cy.visit(`${orgs}/${id}`)
+  beforeEach(() =>
+    cy.flush().then(() =>
+      cy.signin().then(() => {
+        cy.get('@org').then(({id}: Organization) => {
+          cy.fixture('routes').then(({orgs}) => {
+            cy.setFeatureFlags({
+              subscriptionsResourceType: true,
+            }).then(() => {
+              cy.quartzProvision({
+                accountType: 'pay_as_you_go',
+              }).then(() => {
+                cy.visit(`${orgs}/${id}`)
+              })
+            })
+          })
+        })
       })
     )
-  })
+  )
 
-  it('Navigate to empty subscriptions page via sources tab', () => {
-    cy.setFeatureFlags({
-      subscriptionsResourceType: true,
-    })
-    cy.quartzProvision({
-      accountType: 'pay_as_you_go',
-    })
-    cy.get('.cf-tree-nav--toggle').click({force: true})
-    cy.getByTestID('nav-item-load-data').should('be.visible')
-    cy.getByTestID('nav-item-load-data').click()
-    cy.getByTestID('subscriptions--tab').should('be.visible')
+  // it('Navigate to empty subscriptions page via sources tab', () => {
+  //   cy.get('.cf-tree-nav--toggle').click({force: true})
+  //   cy.getByTestID('nav-item-load-data').should('be.visible')
+  //   cy.getByTestID('nav-item-load-data').click()
+  //   cy.getByTestID('subscriptions--tab').should('be.visible')
 
-    cy.getByTestID('load-data-item mqtt')
-      .scrollIntoView()
-      .should('be.visible')
-    cy.getByTestID('load-data-item mqtt').click()
+  //   cy.getByTestID('load-data-item mqtt')
+  //     .scrollIntoView()
+  //     .should('be.visible')
+  //   cy.getByTestID('load-data-item mqtt').click()
 
-    cy.getByTestID('subscriptions-empty-state').should('exist')
-  })
-  it('Navigate to subscriptions page via load data dropdown', () => {
-    cy.setFeatureFlags({
-      subscriptionsResourceType: true,
-    })
-    cy.quartzProvision({
-      accountType: 'pay_as_you_go',
-    })
-    cy.get('.cf-tree-nav--toggle').click({force: true})
-    cy.getByTestID('nav-item-load-data').should('be.visible')
-    cy.getByTestID('nav-item-load-data').click()
-    cy.getByTestID('nav-subitem--subscriptions').should('be.visible')
-    cy.getByTestID('nav-subitem--subscriptions').click({force: true})
+  //   cy.getByTestID('subscriptions-empty-state').should('exist')
+  // })
+  // it('Navigate to subscriptions page via load data dropdown', () => {
+  //   // cy.get('.cf-tree-nav--toggle').click({force: true})
+  //   cy.getByTestID('nav-item-load-data').should('be.visible')
+  //   cy.getByTestID('nav-item-load-data').click()
+  //   cy.getByTestID('nav-subitem-subscriptions').should('be.visible')
+  //   cy.getByTestID('nav-subitem-subscriptions').click()
 
-    cy.getByTestID('subscriptions-empty-state').should('exist')
-  })
+  //   cy.getByTestID('subscriptions-empty-state').should('exist')
+  // })
   it('Create a subscription from the index page from load data nav', () => {
-    cy.setFeatureFlags({
-      subscriptionsResourceType: true,
-    })
-    cy.quartzProvision({
-      accountType: 'pay_as_you_go',
-    })
     cy.get('.cf-tree-nav--toggle').click({force: true})
     cy.getByTestID('nav-item-load-data').should('be.visible')
     cy.getByTestID('nav-item-load-data').click()
@@ -59,34 +51,64 @@ describe('Subscriptions', () => {
     cy.getByTestID('subscriptions--tab').click()
 
     cy.getByTestID('create-subscription-button').should('be.visible')
-    cy.getByTestID('create-subscription-button').click()
+    cy.getByTestID('create-subscription-button').first().click()
 
-    cy.getByTestID('create-broker-form').should('be.visible')
+    // broker form
+    cy.getByTestID('create-broker-form-overlay').should('be.visible')
 
+    // back to create landing
+    cy.getByTestID('create-broker-form--cancel').should('be.visible')
+    cy.getByTestID('create-broker-form--cancel').click()
+
+    // return to broker form
+    cy.getByTestID('create-subscription-button').should('be.visible')
+    cy.getByTestID('create-subscription-button').first().click()
+    cy.getByTestID('create-broker-form-overlay').should('be.visible')
+
+    // fill in broker form 
     cy.getByTestID('create-broker-form--name').type('My Subscription')
     cy.getByTestID('create-broker-form--description').type('My Description')
-    cy.getByTestID('create-broker-form--protocol').type('mqtt')
+    cy.getByTestID('dropdown')
+      .contains('MQTT')
+      .click()
     cy.getByTestID('create-broker-form--host').type('localhost')
     cy.getByTestID('create-broker-form--port').type('1883')
+    cy.getByTestID('create-broker-form--submit').click()
 
-    cy.getByTestID('create-broker-form--dropdown-button')
-      .click()
-      .then(() => {
-        cy.getByTestID('create-broker-form-1').click()
-      })
+    // subscription form
+    cy.getByTestID('create-subscription-form--overlay-form').should('be.visible')
 
-    cy.getByTestID('create-subscription-form').should('be.visible')
+    // back to broker form
+    cy.getByTestID('create-subscription-form--back').should('be.visible')
+    cy.getByTestID('create-subscription-form--back').click()
+    cy.getByTestID('create-broker-form-overlay').should('be.visible')
+   
+    // return to subscription
+    cy.getByTestID('create-broker-form--submit').should('be.visible')
+    cy.getByTestID('create-broker-form--submit').click()
+    cy.getByTestID('create-subscription-form--overlay-form').should('be.visible')
 
+    // fill in subscription form
     cy.getByTestID('create-subscription-form--topic').type('topic')
-    cy.getByTestID('list-item').click()
+    cy.getByTestID('list-item').contains('defbuck').click()
+    cy.getByTestID('create-subscription-form--submit').click()
 
-    cy.getByTestID('create-parsing-form').should('be.visible')
+    // parsing form
+    cy.getByTestID('create-parsing-form-overlay').should('be.visible')
 
+    // back to subscription
+    cy.getByTestID('create-parsing-form--back').should('be.visible')
+    cy.getByTestID('create-parsing-form--back').click()
+    cy.getByTestID('create-subscription-form--overlay-form').should('be.visible')
+
+    // return to parsing
+    cy.getByTestID('create-subscription-form--submit').click()
+
+    // line protocol
     cy.getByTestID('create-parsing-form-line-protocol--button').click()
     cy.getByTestID('create-parsing-form--submit').click()
 
-    cy.getByTestID('subscriptions-list').should('exist')
-    cy.getByTestID('subscription-card').should('exist')
-    cy.getByTestID('resource-card').contains('My Subscription')
+    cy.getByTestID('subscription-card').should('be.visible')
+    cy.getByTestID('subscription-card').contains('My Subscription')
   })
 })

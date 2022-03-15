1fb8e388f0718de081a765faaeb38daa032e0c83
Beth Legesse <legessebl@gmail.com>
2022-03-15 11:27:45 -0500

    chore: replace a tag href with docs link

--- src/flows/pipes/RawFluxEditor/DynamicFunctions/FunctionToolTipContent.tsx
+++ src/flows/pipes/RawFluxEditor/DynamicFunctions/FunctionToolTipContent.tsx
@@ -42,7 +42,11 @@ const FunctionTooltipContent: FC<TooltipProps> = ({item: func}) => {
           </article>
           <p className="tooltip--link">
             Still have questions? Check out the{' '}
-            <a target="_blank" rel="noreferrer" href={'n/a'}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://docs.influxdata.com/flux/v0.x/stdlib/"
+            >
               Flux Docs
             </a>
             .

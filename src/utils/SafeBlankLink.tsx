394afa6f1c37ca93dba6472698027ef8bdfddcf3
ChitlangeSahas <chitlangesahas@gmail.com>
2022-03-17 09:01:09 -0700

    fix: don't use index for key

--- src/utils/SafeBlankLink.tsx
+++ src/utils/SafeBlankLink.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 // The purpose of this component is to provide a safe way to open links in a new tab/window
 // that aren't vulnerable to reverse tabnabbing https://owasp.org/www-community/attacks/Reverse_Tabnabbing
-export const SafeBlankLink: FC<Props> = (props) => {
+export const SafeBlankLink: FC<Props> = props => {
   const {children} = props
   return (
     <a target="_blank" rel="noopener noreferrer" {...props}>

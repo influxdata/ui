c53845712c692d37c513e82aed5f50f466722780
wiedld <dlw405@gmail.com>
2022-03-22 12:21:31 -0700

    fix(3707): only send 1 notification

--- src/flows/pipes/Notification/endpoints/Mailjet/index.ts
+++ src/flows/pipes/Notification/endpoints/Mailjet/index.ts
@@ -34,6 +34,8 @@ export default register => {
 		crit: trigger,
 	)
   |> filter(fn: trigger)
+  |> keep(columns: ["_value", "_time", "_measurement"])
+  |> limit(n: 1, offset: 0)
 	|> monitor["notify"](
     data: notification,
     endpoint: http.endpoint(url: "${data.url}")(

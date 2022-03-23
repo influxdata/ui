1e7244d81d6f1e8eeb7c10e462b5675cf23b705e
ChitlangeSahas <chitlangesahas@gmail.com>
2022-03-23 13:28:38 -0700

    fix: tab spacing for code

--- src/homepageExperience/components/steps/WriteData.tsx
+++ src/homepageExperience/components/steps/WriteData.tsx
@@ -58,8 +58,8 @@ write_api = client.write_api(write_options=SYNCHRONOUS)
 for value in range(5):
   point = (
     Point("measurement1")
-      .tag("tagname1", "tagvalue1")
-      .field("field1", value)
+    .tag("tagname1", "tagvalue1")
+    .field("field1", value)
   )
 write_api.write(bucket=bucket, org="${org.name}", record=point)
 time.sleep(1) # separate points by 1 second`

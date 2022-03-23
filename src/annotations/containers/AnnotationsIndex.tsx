6472c009375b05c844026c6d0c1e2a65215ecd9e
drdelambre <drdelambre@gmail.com>
2022-03-23 10:46:11 -0700

    fix: missed one

--- src/annotations/containers/AnnotationsIndex.tsx
+++ src/annotations/containers/AnnotationsIndex.tsx
@@ -1,6 +1,5 @@
 // Libraries
 import React, {FC} from 'react'
-import {useSelector} from 'react-redux'
 
 // Components
 import SettingsTabbedPage from 'src/settings/components/SettingsTabbedPage'
@@ -8,15 +7,10 @@ import SettingsHeader from 'src/settings/components/SettingsHeader'
 import {Page} from '@influxdata/clockface'
 import {AnnotationsTab} from 'src/annotations/components/AnnotationsTab'
 
-// Selectors
-import {getOrg} from 'src/organizations/selectors'
-
 // Utils
 import {pageTitleSuffixer} from 'src/shared/utils/pageTitles'
 
 export const AnnotationsIndex: FC = () => {
-  const org = useSelector(getOrg)
-
   return (
     <>
       <Page titleTag={pageTitleSuffixer(['Annotations', 'Settings'])}>

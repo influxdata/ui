2f96a57fdf7f86b2ffbad61bc913b51f63a52d6f
Chunchun <yccvivo@gmail.com>
2022-03-24 10:19:22 -0500

    test: run prettier and add initial setup before each test

--- cypress/e2e/oss/members.test.ts
+++ cypress/e2e/oss/members.test.ts
@@ -1,19 +1,35 @@
+import {Organization} from '../../../src/types'
+
+// a generous commitment to delivering this page in a loaded state
+const PAGE_LOAD_SLA = 10000
+
 describe('Member List', () => {
-  // set up
-
-  it('can sort by name', () => {
-    // in ascending order
-
-    // in descending order    
+  beforeEach(() => {
+    cy.flush().then(() =>
+      cy.signin().then(() => {
+        cy.get('@org').then(({id}: Organization) =>
+          cy.fixture('routes').then(({orgs}) => {
+            cy.visit(`${orgs}/${id}/members`)
+            cy.getByTestID('tree-nav')
+            cy.get('[data-testid="resource-list--body"]', {
+              timeout: PAGE_LOAD_SLA,
+            })
+          })
+        )
+      })
+    )
   })
 
-  it('can sort by role', () => {
+  it('can sort by name', () => {
     // in ascending order
 
     // in descending order
   })
 
-  it('can delete a member', () => {
-
+  it('can sort by role', () => {
+    // in ascending order
+    // in descending order
   })
+
+  it('can delete a member', () => {})
 })

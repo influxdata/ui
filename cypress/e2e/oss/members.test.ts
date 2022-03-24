a043394912cea5ee33eca7ab492f6b35cbef7cb2
Chunchun <yccvivo@gmail.com>
2022-03-24 09:49:00 -0500

    test: add test structure

---
+++ cypress/e2e/oss/members.test.ts
@@ -0,0 +1,19 @@
+describe('Member List', () => {
+  // set up
+
+  it('can sort by name', () => {
+    // in ascending order
+
+    // in descending order    
+  })
+
+  it('can sort by role', () => {
+    // in ascending order
+
+    // in descending order
+  })
+
+  it('can delete a member', () => {
+
+  })
+})

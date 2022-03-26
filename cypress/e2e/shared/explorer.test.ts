e93221babcb639a660f9b1c023eb832b3f8c3487
Beth Legesse <legessebl@gmail.com>
2022-03-26 15:16:09 -0500

    test: e2e test for dynamic flux panel in Data explorer

--- cypress/e2e/shared/explorer.test.ts
+++ cypress/e2e/shared/explorer.test.ts
@@ -430,6 +430,45 @@ describe('DataExplorer', () => {
         .click()
     })
 
+    // test for Flux Dynamic Help Panels
+
+    it('can use the dynamic flux function selector to build a query', () => {
+      cy.setFeatureFlags({
+        fluxDynamicDocs: true,
+      }).then(() => {
+        cy.get('.view-line').should('be.visible')
+
+        cy.getByTestID('flux-toolbar-search--input')
+          .clear()
+          .type('microseconds') // purposefully misspell "mean" so all functions are filtered out
+  
+        cy.getByTestID('flux-toolbar--list').within(() => {
+          cy.getByTestID('empty-state').should('be.visible')
+        })
+        cy.getByTestID('flux-toolbar-search--input').type('{backspace}')
+  
+        cy.get('.flux-toolbar--list-item').should('contain', 'microsecond')
+        cy.get('.flux-toolbar--list-item').should('have.length', 1)
+  
+        // hovers over function and see a tooltip
+        cy.get('.flux-toolbar--list-item').trigger('mouseover')
+        cy.getByTestID('flux-docs--microsecond').should('be.visible')
+        
+        // inject function into script editor 
+        cy.getByTestID('flux--microsecond--inject').click()
+        
+        getTimeMachineText().then(text => {
+          const expected = 'import "date" |> undefined'
+  
+          cy.fluxEqual(text, expected).should('be.true')
+        })
+        
+      })
+      cy.setFeatureFlags({
+        fluxDynamicDocs: false,
+      })
+    })
+
     it('shows the proper errors and query button state', () => {
       cy.getByTestID('time-machine-submit-button').should('be.disabled')
 

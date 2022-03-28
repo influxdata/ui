6a6755320472fd7a194dee966a2b2ad9dc6168ab
Beth Legesse <legessebl@gmail.com>
2022-03-28 10:55:31 -0500

    fix: prettier

--- cypress/e2e/shared/explorer.test.ts
+++ cypress/e2e/shared/explorer.test.ts
@@ -440,29 +440,28 @@ describe('DataExplorer', () => {
 
         cy.getByTestID('flux-toolbar-search--input')
           .clear()
-          .type('microseconds') // purposefully misspell "mean" so all functions are filtered out
-  
+          .type('microsecondd') // purposefully misspell "microsecond" so all functions are filtered out
+
         cy.getByTestID('flux-toolbar--list').within(() => {
           cy.getByTestID('empty-state').should('be.visible')
         })
         cy.getByTestID('flux-toolbar-search--input').type('{backspace}')
-  
+
         cy.get('.flux-toolbar--list-item').should('contain', 'microsecond')
         cy.get('.flux-toolbar--list-item').should('have.length', 1)
-  
+
         // hovers over function and see a tooltip
         cy.get('.flux-toolbar--list-item').trigger('mouseover')
         cy.getByTestID('flux-docs--microsecond').should('be.visible')
-        
-        // inject function into script editor 
+
+        // inject function into script editor
         cy.getByTestID('flux--microsecond--inject').click()
-        
+
         getTimeMachineText().then(text => {
-          const expected = 'import "date" |> undefined'
-  
+          const expected = 'import "date" |> undefined' // issue still in progress
+
           cy.fluxEqual(text, expected).should('be.true')
         })
-        
       })
       cy.setFeatureFlags({
         fluxDynamicDocs: false,

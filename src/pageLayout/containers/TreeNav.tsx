7f2ab71f3bd342d0757556ea719dcd761a9f2456
Beth Legesse <legessebl@gmail.com>
2022-04-05 18:00:28 -0500

    chore: clean up

--- src/pageLayout/containers/TreeNav.tsx
+++ src/pageLayout/containers/TreeNav.tsx
@@ -60,135 +60,121 @@ const TreeSidebar: FC = () => {
 
   return (
     <OrgSettings>
-      <>
-        <TreeNav
-          expanded={navbarMode === 'expanded'}
-          headerElement={<NavHeader link={`/orgs/${org.id}`} />}
-          userElement={<UserWidget />}
-          onToggleClick={handleToggleNavExpansion}
-        >
-          {generateNavItems().map((item: NavItem) => {
-            const linkElement = (className: string): JSX.Element => (
-              <Link
-                to={item.link}
-                className={className}
-                title={item.label}
-                onClick={() => {
-                  event('nav clicked', {which: item.id})
-                }}
-              />
-            )
-            return (
-              <TreeNav.Item
-                key={item.id}
-                id={item.id}
-                testID={item.testID}
-                icon={<Icon glyph={item.icon} />}
-                label={item.label}
-                shortLabel={item.shortLabel}
-                active={getNavItemActivation(
-                  item.activeKeywords,
-                  location.pathname
-                )}
-                linkElement={linkElement}
-              >
-                {Boolean(item.menu) && (
-                  <TreeNav.SubMenu>
-                    {item.menu.map((menuItem: NavSubItem) => {
-                      const linkElement = (className: string): JSX.Element => (
-                        <Link
-                          to={menuItem.link}
-                          className={className}
-                          onClick={() => {
-                            event('nav clicked', {
-                              which: `${item.id} - ${menuItem.id}`,
-                            })
-                          }}
-                        />
-                      )
+      <TreeNav
+        expanded={navbarMode === 'expanded'}
+        headerElement={<NavHeader link={`/orgs/${org.id}`} />}
+        userElement={<UserWidget />}
+        onToggleClick={handleToggleNavExpansion}
+      >
+        {generateNavItems().map((item: NavItem) => {
+          const linkElement = (className: string): JSX.Element => (
+            <Link
+              to={item.link}
+              className={className}
+              title={item.label}
+              onClick={() => {
+                event('nav clicked', {which: item.id})
+              }}
+            />
+          )
+          return (
+            <TreeNav.Item
+              key={item.id}
+              id={item.id}
+              testID={item.testID}
+              icon={<Icon glyph={item.icon} />}
+              label={item.label}
+              shortLabel={item.shortLabel}
+              active={getNavItemActivation(
+                item.activeKeywords,
+                location.pathname
+              )}
+              linkElement={linkElement}
+            >
+              {Boolean(item.menu) && (
+                <TreeNav.SubMenu>
+                  {item.menu.map((menuItem: NavSubItem) => {
+                    const linkElement = (className: string): JSX.Element => (
+                      <Link
+                        to={menuItem.link}
+                        className={className}
+                        onClick={() => {
+                          event('nav clicked', {
+                            which: `${item.id} - ${menuItem.id}`,
+                          })
+                        }}
+                      />
+                    )
 
-                      return (
-                        <TreeNav.SubItem
-                          key={menuItem.id}
-                          id={menuItem.id}
-                          testID={menuItem.testID}
-                          active={getNavItemActivation(
-                            [menuItem.id],
-                            location.pathname
-                          )}
-                          label={menuItem.label}
-                          linkElement={linkElement}
-                        />
-                      )
-                    })}
-                  </TreeNav.SubMenu>
-                )}
-              </TreeNav.Item>
-            )
-          })}
-          <TreeNav.Item
-            id={'support'}
-            testID={'nav-item-support'}
-            icon={<Icon glyph={IconFont.QuestionMark_New} />}
-            label={'Help & Support'}
-            shortLabel={'Support'}
-          >
-            <TreeNav.SubMenu>
-              <TreeNav.SubHeading label="Support" />
-              <TreeNav.SubItem
-                id="documentation"
-                label="Documentation"
-                testID="nav-subitem-documentation"
-                linkElement={className => (
-                  <Link className={className} to={``} />
-                )}
-              />
-              <TreeNav.SubItem
-                id="faqs"
-                label="FAQs"
-                testID="nav-subitem-faqs"
-                linkElement={className => (
-                  <Link className={className} to={``} />
-                )}
-              />
-              <TreeNav.SubItem
-                id="contactSupport"
-                label="Contact Support"
-                testID="nav-subitem-contact-support"
-                linkElement={className => (
-                  <Link className={className} to={``} />
-                )}
-              />
-              <TreeNav.SubHeading label="Community" />
-              <TreeNav.SubItem
-                id="offcialForum"
-                label="Official Forum"
-                testID="nav-subitem-forum"
-                linkElement={className => (
-                  <Link className={className} to={``} />
-                )}
-              />
-              <TreeNav.SubItem
-                id="influxdbSlack"
-                label="InfluxDB Slack"
-                testID="nav-subitem-influxdb-slack"
-                linkElement={className => (
-                  <Link className={className} to={``} />
-                )}
-              />
-              <TreeNav.SubHeading label="Feedback" />
-              <TreeNav.SubItem
-                id="feedback"
-                label="Feedback & Questions"
-                testID="nav-subitem-feedback-questions"
-                linkElement={className => (
-                  <Link className={className} to={``} />
-                )}
-              />
-            </TreeNav.SubMenu>
-          </TreeNav.Item>
-        </TreeNav>
-      </>
+                    return (
+                      <TreeNav.SubItem
+                        key={menuItem.id}
+                        id={menuItem.id}
+                        testID={menuItem.testID}
+                        active={getNavItemActivation(
+                          [menuItem.id],
+                          location.pathname
+                        )}
+                        label={menuItem.label}
+                        linkElement={linkElement}
+                      />
+                    )
+                  })}
+                </TreeNav.SubMenu>
+              )}
+            </TreeNav.Item>
+          )
+        })}
+        <TreeNav.Item
+          id={'support'}
+          testID={'nav-item-support'}
+          icon={<Icon glyph={IconFont.QuestionMark_New} />}
+          label={'Help & Support'}
+          shortLabel={'Support'}
+        >
+          <TreeNav.SubMenu>
+            <TreeNav.SubHeading label="Support" />
+            <TreeNav.SubItem
+              id="documentation"
+              label="Documentation"
+              testID="nav-subitem-documentation"
+              linkElement={className => <Link className={className} to={``} />}
+            />
+            <TreeNav.SubItem
+              id="faqs"
+              label="FAQs"
+              testID="nav-subitem-faqs"
+              linkElement={className => <Link className={className} to={``} />}
+            />
+            <TreeNav.SubItem
+              id="contactSupport"
+              label="Contact Support"
+              testID="nav-subitem-contact-support"
+              linkElement={className => <Link className={className} to={``} />}
+            />
+            <TreeNav.SubHeading label="Community" />
+            <TreeNav.SubItem
+              id="offcialForum"
+              label="Official Forum"
+              testID="nav-subitem-forum"
+              linkElement={className => <Link className={className} to={``} />}
+            />
+            <TreeNav.SubItem
+              id="influxdbSlack"
+              label="InfluxDB Slack"
+              testID="nav-subitem-influxdb-slack"
+              linkElement={className => <Link className={className} to={``} />}
+            />
+            <TreeNav.SubHeading label="Feedback" />
+            <TreeNav.SubItem
+              id="feedback"
+              label="Feedback & Questions"
+              testID="nav-subitem-feedback-questions"
+              linkElement={className => <Link className={className} to={``} />}
+            />
+          </TreeNav.SubMenu>
+        </TreeNav.Item>
+      </TreeNav>
     </OrgSettings>
   )
 }

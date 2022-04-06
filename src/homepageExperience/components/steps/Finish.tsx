62d8285d91fd5fb3df22cf239409d38bb106a9be
Bucky Schwarz <hoorayimhelping@users.noreply.github.com>
2022-04-06 12:32:29 -0700

    feat(firstMile): add nodejs wizard execute aggregate query step (#4329)

--- src/homepageExperience/components/steps/Finish.tsx
+++ src/homepageExperience/components/steps/Finish.tsx
@@ -7,10 +7,13 @@ import {
   IconFont,
   ResourceCard,
 } from '@influxdata/clockface'
+
 import {
   InfluxDBUniversityIcon,
   VSCodePluginIcon,
 } from 'src/homepageExperience/components/HomepageIcons'
+import {SafeBlankLink} from 'src/utils/SafeBlankLink'
+
 import {event} from 'src/cloud/utils/reporting'
 
 export const Finish = () => {
@@ -24,29 +27,31 @@ export const Finish = () => {
       <p>Curious to learn more? Try these next steps!</p>
       <FlexBox margin={ComponentSize.Medium} alignItems={AlignItems.Stretch}>
         <ResourceCard className="homepage-wizard-next-steps">
-          <a href="https://influxdbu.com/" target="_blank">
+          <SafeBlankLink href="https://influxdbu.com/">
             <h4>{InfluxDBUniversityIcon}InfluxDB University</h4>
-          </a>
+          </SafeBlankLink>
           <p>
             Our free hands-on courses teach you the technical skills and best
             practices to get the most out of your real-time data with InfluxDB."
           </p>
         </ResourceCard>
         <ResourceCard className="homepage-wizard-next-steps">
-          <a
-            href="https://docs.influxdata.com/influxdb/cloud/tools/flux-vscode"
-            target="_blank"
-          >
+          <SafeBlankLink href="https://docs.influxdata.com/influxdb/cloud/tools/flux-vscode">
             <h4>{VSCodePluginIcon}Install VSCode Plugin</h4>
-          </a>
+          </SafeBlankLink>
           <p>Streamline your workflow even further with our VSCode plugin!</p>
         </ResourceCard>
         <ResourceCard className="homepage-wizard-next-steps">
           <div
-            style={{display: 'flex', justifyContent: 'center', height: '100%'}}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              height: '100%',
+              textTransform: 'uppercase',
+            }}
           >
             <h5>
-              MORE OPTIONS{' '}
+              More Options{' '}
               <Icon
                 glyph={IconFont.ArrowRight_New}
                 style={{marginLeft: '9px'}}

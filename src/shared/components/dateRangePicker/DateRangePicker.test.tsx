373320a2b4fcf244e3f0e7a10c61ca3299ab4c3f
ChitlangeSahas <chitlangesahas@gmail.com>
2022-04-06 22:06:08 -0700

    chore: prettier

--- src/shared/components/dateRangePicker/DateRangePicker.test.tsx
+++ src/shared/components/dateRangePicker/DateRangePicker.test.tsx
@@ -8,35 +8,32 @@ import {renderWithReduxAndRouter} from 'src/mockState'
 import {screen} from '@testing-library/dom'
 import {fireEvent} from '@testing-library/react'
 
-jest
-  .useFakeTimers()
-  .setSystemTime(new Date('2022-03-10 20:00').getTime());
+jest.useFakeTimers().setSystemTime(new Date('2022-03-10 20:00').getTime())
 
 describe('Date Range Picker', function() {
   test('should have input values in the correct format after user changes the date selection', () => {
-    const {getAllByText} = renderWithReduxAndRouter(<DateRangePicker
-      timeRange={convertTimeRangeToCustom(pastHourTimeRange)}
-    onSetTimeRange={() => {}}
-    onClose={() => {}}
-    position={{position: 'relative'}}
-    />)
+    const {getAllByText} = renderWithReduxAndRouter(
+      <DateRangePicker
+        timeRange={convertTimeRangeToCustom(pastHourTimeRange)}
+        onSetTimeRange={() => {}}
+        onClose={() => {}}
+        position={{position: 'relative'}}
+      />
+    )
 
     // YYYY-MM-DD HH:mm format by default
-    expect(screen.getByTitle("Start")).toHaveValue("2022-03-10 19:00");
-    expect(screen.getByTitle("Stop")).toHaveValue("2022-03-10 20:00");
+    expect(screen.getByTitle('Start')).toHaveValue('2022-03-10 19:00')
+    expect(screen.getByTitle('Stop')).toHaveValue('2022-03-10 20:00')
 
     // change the time and the format should stay the same
 
-    const lowerTime = getAllByText("18:00")[0]
-    const upperTime = getAllByText("21:00")[1]
+    const lowerTime = getAllByText('18:00')[0]
+    const upperTime = getAllByText('21:00')[1]
 
     fireEvent.click(lowerTime)
     fireEvent.click(upperTime)
 
-    expect(screen.getByTitle("Start")).toHaveValue("2022-03-10 18:00");
-    expect(screen.getByTitle("Stop")).toHaveValue("2022-03-10 21:00");
-
-
-
+    expect(screen.getByTitle('Start')).toHaveValue('2022-03-10 18:00')
+    expect(screen.getByTitle('Stop')).toHaveValue('2022-03-10 21:00')
   })
 })

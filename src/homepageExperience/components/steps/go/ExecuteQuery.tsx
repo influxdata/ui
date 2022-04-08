93ce33a910ef08d69c2607e9cd2013d0c524caae
ChitlangeSahas <chitlangesahas@gmail.com>
2022-04-08 11:07:46 -0700

    feat: install dependencies page and other structure for go wizard

---
+++ src/homepageExperience/components/steps/go/ExecuteQuery.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+import CodeSnippet from 'src/shared/components/CodeSnippet'
+import {event} from 'src/cloud/utils/reporting'
+
+const logCopyCodeSnippet = () => {
+  event('firstMile.nodejsWizard.executeQuery.code.copied')
+}
+
+type OwnProps = {
+  bucket: string
+}
+
+export const ExecuteQuery = (props: OwnProps) => {
+  const {bucket} = props
+
+  const fromBucketSnippet = `from(bucket: "${bucket}")
+  |> range(start: -10m)`
+
+  const query = `const queryClient = client.getQueryApi(org)
+const fluxQuery = \`from(bucket: "fooo")
+ |> range(start: -10m)
+ |> filter(fn: (r) => r._measurement == "measurement1")\`
+
+queryClient.queryRows(fluxQuery, {
+  next: (row, tableMeta) => {
+    const tableObject = tableMeta.toObject(row)
+    console.log(row, tableObject)
+  },
+  error: (error) => {
+    console.error('\\nError', error)
+  },
+  complete: () => {
+    console.log('\\nSuccess')
+  },
+})`
+
+  return (
+    <>
+      <h1>Execute a Flux Query</h1>
+      <p>
+        Now let’s query the numbers we wrote into the database. We use the Flux
+        scripting language to query data. Flux is designed for querying,
+        analyzing, and acting on data.
+        <br />
+        <br />
+        Here is what a simple Flux query looks like on its own:
+      </p>
+      <CodeSnippet
+        text={fromBucketSnippet}
+        showCopyControl={false}
+        onCopy={logCopyCodeSnippet}
+      />
+      <p>
+        In this query, we are looking for data points within last 10 minutes
+        with field key of "field1".
+        <br />
+        <br />
+        Let’s use that Flux query in our Nodejs code!
+        <br />
+        <br />
+        Run the following:
+      </p>
+      <CodeSnippet text={query} onCopy={logCopyCodeSnippet} />
+    </>
+  )
+}
---
+++ src/homepageExperience/components/steps/go/InstallDependencies.tsx
@@ -0,0 +1,34 @@
+import React, {PureComponent} from 'react'
+import CodeSnippet from 'src/shared/components/CodeSnippet'
+import {SafeBlankLink} from 'src/utils/SafeBlankLink'
+
+import {event} from 'src/cloud/utils/reporting'
+
+export class InstallDependencies extends PureComponent {
+  private logCopyCodeSnippet = () => {
+    event('firstMile.nodejsWizard.installDependencies.code.copied')
+  }
+  render() {
+    return (
+      <>
+        <h1>Install Dependencies</h1>
+        <p>
+          First, you need to install the{' '}
+          <code style={{color: '#B7B8FF'}}>influxdb-client-go</code> module. Run
+          the command below in your terminal.
+        </p>
+        <CodeSnippet
+          text="go get github.com/influxdata/influxdb-client-go/v2"
+          onCopy={this.logCopyCodeSnippet}
+        />
+        <p style={{fontStyle: 'italic'}}>
+          You’ll need to have{' '}
+          <SafeBlankLink href="https://go.dev/doc/go1.17/">
+            Go 1.17
+          </SafeBlankLink>{' '}
+          or higher installed.
+        </p>
+      </>
+    )
+  }
+}

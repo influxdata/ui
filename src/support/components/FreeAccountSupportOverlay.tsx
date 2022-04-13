d1327f51c802dc7f7273c16a75f08c3a776b15ba
Beth Legesse <legessebl@gmail.com>
2022-04-13 10:16:46 -0500

    feat: fill in content for free support overlay

--- src/support/components/FreeAccountSupportOverlay.tsx
+++ src/support/components/FreeAccountSupportOverlay.tsx
@@ -1,14 +1,16 @@
 import React, {FC, useContext} from 'react'
 
 // Components
-import {Overlay} from '@influxdata/clockface'
+import {Overlay, List, Icon, IconFont} from '@influxdata/clockface'
 
 // Contexts
 import {OverlayContext} from 'src/overlays/components/OverlayController'
 
 // Components
 import CloudUpgradeButton from 'src/shared/components/CloudUpgradeButton'
+import {SafeBlankLink} from 'src/utils/SafeBlankLink'
 
+import './ContactSupport.scss'
 interface OwnProps {
   onClose: () => void
 }
@@ -24,7 +26,42 @@ const FreeAccountSupportOverlay: FC<OwnProps> = () => {
         onDismiss={onClose}
       />
 
-      <Overlay.Body></Overlay.Body>
+      <p className="status-page-text">
+        <span>
+          {' '}
+          <Icon glyph={IconFont.Info_New} />{' '}
+        </span>
+        Check our{' '}
+        <SafeBlankLink href="https://status.influxdata.com">
+          status page
+        </SafeBlankLink>{' '}
+        to see if there is an outage impacting your region.
+      </p>
+      <Overlay.Body>
+        <p>
+          Contact support is currently only available for Usage-Based Plan and
+          Annual customers. Please try our community resources below to recieve
+          help or file a feedback & questions form.
+        </p>
+        <List className="support-links">
+          <List.Item>
+            <div className="community-logo" />
+            <SafeBlankLink href="https://community.influxdata.com">
+              InfluxDB Community Forums
+            </SafeBlankLink>
+          </List.Item>
+          <List.Item>
+            <div className="slack-logo" />
+            <SafeBlankLink href="https://influxcommunity.slack.com/join/shared_invite/zt-156zm7ult-LcIW2T4TwLYeS8rZbCP1mw#/shared-invite/email">
+              InfluxDB Slack
+            </SafeBlankLink>
+          </List.Item>
+          <List.Item>
+            <div className="feedback-logo" />
+            <SafeBlankLink href="">Feedback & Questions Form</SafeBlankLink>
+          </List.Item>
+        </List>
+      </Overlay.Body>
       <Overlay.Footer>
         <CloudUpgradeButton />
       </Overlay.Footer>

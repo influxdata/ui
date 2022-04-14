67c0ea26c9aa78249c1fffc3cedff96032106d5e
ChitlangeSahas <chitlangesahas@gmail.com>
2022-04-13 17:23:05 -0700

    fix: InitalizeClient code snippet for python

--- src/homepageExperience/components/steps/python/InitalizeClient.tsx
+++ src/homepageExperience/components/steps/python/InitalizeClient.tsx
@@ -18,9 +18,9 @@ export const InitalizeClient = () => {
   const url =
     me.quartzMe?.clusterHost || 'https://us-west-2-1.aws.cloud2.influxdata.com/'
 
-  const pythonCode = `import os
+  const pythonCode = `import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client import SYNCHRONOUS
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 token = os.environ.get("INFLUXDB_TOKEN")
 org = "${org.name}"

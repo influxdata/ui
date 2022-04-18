7b638d7820f85814bdfd52d9523fc3cb249d3891
Gene Hynson <ghynson@influxdata.com>
2022-04-18 08:37:46 -0700

    chore(sub): handle errors from GET and DELETE endpoints (#4399)

--- src/shared/copy/notifications/categories/subscriptions.ts
+++ src/shared/copy/notifications/categories/subscriptions.ts
@@ -16,3 +16,13 @@ export const subscriptionStatusUpdateFail = (): Notification => ({
   ...defaultErrorNotification,
   message: `Failed to update Subscription status, please try again.`,
 })
+
+export const subscriptionsGetFail = (): Notification => ({
+  ...defaultErrorNotification,
+  message: `Failed to get Subscriptions, please try again.`,
+})
+
+export const subscriptionsDeleteFail = (): Notification => ({
+  ...defaultErrorNotification,
+  message: `Failed to delete the Subscription, please try again.`,
+})

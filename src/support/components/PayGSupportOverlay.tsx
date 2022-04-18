7ee57687f2e6e327e6b608d9d0c5bbad58518a87
Beth Legesse <legessebl@gmail.com>
2022-04-18 14:00:57 -0500

    feat: move overlay footer outside body

--- src/support/components/PayGSupportOverlay.tsx
+++ src/support/components/PayGSupportOverlay.tsx
@@ -31,7 +31,7 @@ interface OwnProps {
 
 const PayGSupportOverlay: FC<OwnProps> = () => {
   const [severity, setSeverity] = useState('')
-  const [input, setInput] = useState('')
+  const [textInput, setTextInput] = useState('')
   const {onClose} = useContext(OverlayContext)
 
   const severityLevel = [
@@ -41,8 +41,13 @@ const PayGSupportOverlay: FC<OwnProps> = () => {
     '4 - Request',
   ]
 
+  const submitButtonStatus =
+    textInput.length && severity.length
+      ? ComponentStatus.Default
+      : ComponentStatus.Disabled
+
   const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setInput(e.target.value)
+    setTextInput(e.target.value)
   }
   const handleSubmit = (): void => {
     // submit support form
@@ -58,16 +63,9 @@ const PayGSupportOverlay: FC<OwnProps> = () => {
     }
 
     if (value.length >= 2500) {
-      return 'Must be 2500 characters or less'
+      return 'Must be 2500 characters or fewer'
     }
     return null
-  }
-
-  const submitButtonStatus = (): ComponentStatus => {
-    if (!input.length || !severity) {
-      return ComponentStatus.Disabled
-    }
-    return ComponentStatus.Default
   }
 
   const severityTip = (): JSX.Element => {
@@ -110,12 +108,12 @@ const PayGSupportOverlay: FC<OwnProps> = () => {
         to see if there is an outage impacting your region.
       </p>
       <ErrorBoundary>
-        <Overlay.Body>
-          <Form
-            onSubmit={handleSubmit}
-            action="https://influxdata--full.my.salesforce.com/servlet/servlet.WebToCase?encoding=UTF-8"
-            method={Method.Post}
-          >
+        <Form
+          onSubmit={handleSubmit}
+          action="https://influxdata--full.my.salesforce.com/servlet/servlet.WebToCase?encoding=UTF-8"
+          method={Method.Post}
+        >
+          <Overlay.Body>
             <Form.Element
               label="Severity"
               required={true}
@@ -131,7 +129,7 @@ const PayGSupportOverlay: FC<OwnProps> = () => {
             <Form.ValidationElement
               label="Description"
               required={true}
-              value={input}
+              value={textInput}
               validationFunc={handleValidation}
             >
               {status => (
@@ -140,29 +138,29 @@ const PayGSupportOverlay: FC<OwnProps> = () => {
                   rows={10}
                   testID="support-description--textarea"
                   name="description"
-                  value={input}
+                  value={textInput}
                   onChange={handleInputChange}
                 />
               )}
             </Form.ValidationElement>
-            <Overlay.Footer>
-              <Button
-                text="Cancel"
-                color={ComponentColor.Tertiary}
-                onClick={onClose}
-                type={ButtonType.Button}
-                testID="payg-contact-support--cancel"
-              />
-              <Button
-                text="Submit"
-                color={ComponentColor.Success}
-                type={ButtonType.Submit}
-                testID="payg-contact-support--submit"
-                status={submitButtonStatus()}
-              />
-            </Overlay.Footer>
-          </Form>
-        </Overlay.Body>
+          </Overlay.Body>
+          <Overlay.Footer>
+            <Button
+              text="Cancel"
+              color={ComponentColor.Tertiary}
+              onClick={onClose}
+              type={ButtonType.Button}
+              testID="payg-contact-support--cancel"
+            />
+            <Button
+              text="Submit"
+              color={ComponentColor.Success}
+              type={ButtonType.Submit}
+              testID="payg-contact-support--submit"
+              status={submitButtonStatus}
+            />
+          </Overlay.Footer>
+        </Form>
       </ErrorBoundary>
     </Overlay.Container>
   )

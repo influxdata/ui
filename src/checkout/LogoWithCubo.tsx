4e3e9d4c38966f3fa269cd31ef0eb4dd754aad0e
Subir Jolly <sjolly@influxdata.com>
2022-04-19 19:14:17 +0530

    chore: update per ui-ux requirement

--- src/checkout/LogoWithCubo.tsx
+++ src/checkout/LogoWithCubo.tsx
@@ -1,9 +1,23 @@
-import {InfluxColors, InfluxDataLogo} from '@influxdata/clockface'
+import {
+  InfluxColors,
+  InfluxLogo,
+  LogoAuxiliaryText,
+  LogoBaseText,
+  LogoMarks,
+  LogoSymbols,
+} from '@influxdata/clockface'
 import React, {FC} from 'react'
 
 const LogoWithCubo: FC = () => (
   <div className="logo-with-cubo">
-    <InfluxDataLogo fill={InfluxColors.White} />
+    <InfluxLogo
+      logoMark={LogoMarks.Kubo}
+      baseText={LogoBaseText.InfluxDb}
+      auxiliaryText={LogoAuxiliaryText.Cloud}
+      symbol={LogoSymbols.Trademark}
+      fill={InfluxColors.White}
+      centeredLogo={false}
+    />
   </div>
 )
 

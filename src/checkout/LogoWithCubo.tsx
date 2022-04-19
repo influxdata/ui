7c43e73b289692e66917f5afc7b8591bafa70377
Subir Jolly <sjolly@influxdata.com>
2022-04-19 18:28:43 +0530

    chore: update logo to use clockface component

--- src/checkout/LogoWithCubo.tsx
+++ src/checkout/LogoWithCubo.tsx
@@ -1,19 +1,9 @@
-import {
-  Icon,
-  IconFont,
-  InfluxColors,
-  InfluxDBCloudLogo,
-} from '@influxdata/clockface'
+import {InfluxColors, InfluxDataLogo} from '@influxdata/clockface'
 import React, {FC} from 'react'
 
 const LogoWithCubo: FC = () => (
   <div className="logo-with-cubo">
-    <Icon glyph={IconFont.CuboSolid} className="cubo-logo" />
-    <InfluxDBCloudLogo
-      fill={InfluxColors.White}
-      cloud={true}
-      className="influx-cloud-logo"
-    />
+    <InfluxDataLogo fill={InfluxColors.White} />
   </div>
 )
 

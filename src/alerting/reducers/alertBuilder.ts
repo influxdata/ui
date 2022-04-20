d3639051867a6214a2cb3b47f8ce2f40f3124521
Chunchun Ye <14298407+appletreeisyellow@users.noreply.github.com>
2022-04-20 14:45:00 -0500

    fix(alert): keep alert description saved after an alert is edited (#4436)
    * fix(alert): add alert description in api.putCheck data
    
    * test(alderBuilder): add description in mock state

--- src/alerting/reducers/alertBuilder.ts
+++ src/alerting/reducers/alertBuilder.ts
@@ -17,7 +17,7 @@ import {
 } from 'src/alerting/constants'
 
 type FromBase = Required<
-  Pick<CheckBase, 'name' | 'id' | 'activeStatus' | 'status'>
+  Pick<CheckBase, 'name' | 'id' | 'activeStatus' | 'status' | 'description'>
 >
 
 type FromThreshold = Required<
@@ -53,6 +53,7 @@ export const initialState = (): AlertBuilderState => ({
   staleTime: '10m',
   level: DEFAULT_DEADMAN_LEVEL,
   thresholds: [],
+  description: '',
 })
 
 export default (
@@ -73,7 +74,7 @@ export default (
     }
 
     case 'SET_ALERT_BUILDER_CHECK': {
-      const {id, type, name, query} = action.payload.check
+      const {id, type, name, query, description} = action.payload.check
 
       const newState = {
         ...initialState(),
@@ -82,6 +83,7 @@ export default (
         name,
         query: {...query},
         type,
+        description,
       }
 
       if (action.payload.check.type === 'custom') {

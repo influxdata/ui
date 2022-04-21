f97f23bf32a064dcca5da065a6a6c6387e7fd3c3
Chitlange Sahas <chitlangesahas@gmail.com>
2022-04-21 15:26:16 -0700

    feat: updated Finish Page for first mile (#4438)

---
+++ src/homepageExperience/components/FeedbackBar.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import {
+  ComponentSize,
+  FlexBox,
+  InfluxColors,
+  JustifyContent,
+} from '@influxdata/clockface'
+import {
+  ThumbsDown,
+  ThumbsUp,
+} from 'src/homepageExperience/components/HomepageIcons'
+import {event} from 'src/cloud/utils/reporting'
+
+import 'src/homepageExperience/components/HomepageExperience.scss'
+
+type OwnProps = {
+  wizardEventName: string
+}
+type State = {
+  selectedFeedback: number
+}
+
+enum feedbackValue {
+  THUMBS_DOWN,
+  THUMBS_UP,
+}
+
+export default class FeedbackBar extends React.Component<OwnProps, State> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedFeedback: null,
+    }
+  }
+
+  private handleThumbsUpClick = () => {
+    event(`firstMile.${this.props.wizardEventName}.thumbsUp.clicked`)
+    this.setState({selectedFeedback: feedbackValue.THUMBS_UP})
+  }
+  private handleThumbsDownClick = () => {
+    event(`firstMile.${this.props.wizardEventName}.thumbsDown.clicked`)
+    this.setState({selectedFeedback: feedbackValue.THUMBS_DOWN})
+  }
+
+  render() {
+    const {selectedFeedback} = this.state
+    return (
+      <FlexBox
+        margin={ComponentSize.Large}
+        justifyContent={JustifyContent.SpaceBetween}
+        className="feedback-bar"
+      >
+        <p>What did you think about the set up process?</p>
+        <FlexBox margin={ComponentSize.Large}>
+          <button
+            onClick={this.handleThumbsUpClick}
+            style={{
+              fill:
+                selectedFeedback === feedbackValue.THUMBS_UP
+                  ? InfluxColors.Rainforest
+                  : InfluxColors.White,
+            }}
+          >
+            {ThumbsUp}
+          </button>
+          <button
+            onClick={this.handleThumbsDownClick}
+            style={{
+              fill:
+                selectedFeedback === feedbackValue.THUMBS_DOWN
+                  ? InfluxColors.Fire
+                  : InfluxColors.White,
+            }}
+          >
+            {ThumbsDown}
+          </button>
+        </FlexBox>
+      </FlexBox>
+    )
+  }
+}
--- src/homepageExperience/components/steps/Finish.tsx
+++ src/homepageExperience/components/steps/Finish.tsx
@@ -3,71 +3,110 @@ import {
   AlignItems,
   ComponentSize,
   FlexBox,
-  Icon,
-  IconFont,
   ResourceCard,
 } from '@influxdata/clockface'
 
+import confetti from 'canvas-confetti'
+
 import {
-  InfluxDBUniversityIcon,
-  VSCodePluginIcon,
+  BookIcon,
+  CodeTerminalIcon,
 } from 'src/homepageExperience/components/HomepageIcons'
 import {SafeBlankLink} from 'src/utils/SafeBlankLink'
 
 import {event} from 'src/cloud/utils/reporting'
+import FeedbackBar from 'src/homepageExperience/components/FeedbackBar'
 
 type OwnProps = {
   wizardEventName: string
+  markStepAsCompleted: () => void
+  finishStepCompleted: boolean
+}
+
+const fireConfetti = () => {
+  const count = 200
+  const defaults = {
+    origin: {y: 0.7},
+  }
+
+  const fire = (particleRatio, options) => {
+    confetti({
+      ...defaults,
+      ...options,
+      particleCount: Math.floor(count * particleRatio),
+    })
+  }
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  })
+  fire(0.2, {
+    spread: 60,
+  })
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  })
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  })
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  })
 }
 
 export const Finish = (props: OwnProps) => {
   useEffect(() => {
-    event(`firstMile.${props.wizardEventName}.finished`)
+    // if the finish step was opened during the session,
+    // this check prevents from multiple logging of finish events
+    // in case user navigates back and forth
+    if (!props.finishStepCompleted) {
+      event(`firstMile.${props.wizardEventName}.finished`)
+      props.markStepAsCompleted()
+      fireConfetti()
+    }
   }, [])
+
   return (
     <>
       <h1>Congrats!</h1>
       <p>You completed setting up, writing, and querying data.</p>
-      <p>Curious to learn more? Try these next steps!</p>
+      <FeedbackBar wizardEventName={props.wizardEventName} />
+      <p style={{marginTop: '80px'}}>
+        Curious to learn more? Try these next steps!
+      </p>
       <FlexBox margin={ComponentSize.Medium} alignItems={AlignItems.Stretch}>
         <ResourceCard className="homepage-wizard-next-steps">
-          <SafeBlankLink href="https://influxdbu.com/">
-            <h4>{InfluxDBUniversityIcon}InfluxDB University</h4>
+          <SafeBlankLink href="https://github.com/InfluxCommunity/sample-flask/blob/main/app.py">
+            <h4>{CodeTerminalIcon}Sample App</h4>
           </SafeBlankLink>
           <p>
-            Our free hands-on courses teach you the technical skills and best
-            practices to get the most out of your real-time data with InfluxDB."
+            Play around with our template code of sample app to streamline your
+            own data into InfluxData.
           </p>
         </ResourceCard>
         <ResourceCard className="homepage-wizard-next-steps">
-          <SafeBlankLink href="https://docs.influxdata.com/influxdb/cloud/tools/flux-vscode">
-            <h4>{VSCodePluginIcon}Install VSCode Plugin</h4>
+          <SafeBlankLink href="https://docs.influxdata.com">
+            <h4>{BookIcon}Key Concepts</h4>
           </SafeBlankLink>
-          <p>Streamline your workflow even further with our VSCode plugin!</p>
+          <p>Learn about important concepts for writing time-series data.</p>
         </ResourceCard>
         <ResourceCard className="homepage-wizard-next-steps">
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              height: '100%',
-              textTransform: 'uppercase',
-            }}
-          >
-            <h5>
-              More Options{' '}
-              <Icon
-                glyph={IconFont.ArrowRight_New}
-                style={{marginLeft: '9px'}}
-              />
-            </h5>
-          </div>
+          <SafeBlankLink href="https://influxdbu.com/">
+            <h4>{CodeTerminalIcon}InfluxDB University</h4>
+          </SafeBlankLink>
+          <p>
+            Our free hands-on courses teach you the technical skills and best
+            practices to get the most out of your real-time data with InfluxDB.
+          </p>
         </ResourceCard>
       </FlexBox>
-
-      <p style={{marginTop: '150px'}}>
-        What did you think about the set up process? Give us feedback to improve
-      </p>
     </>
   )
 }

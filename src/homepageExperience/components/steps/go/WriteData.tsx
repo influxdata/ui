3ddd2e66d0dad33bc5677a311b94b38bf114c6be
Bucky Schwarz <d.w.schwarz@gmail.com>
2022-04-25 16:58:18 -0400

    chore(firstMile): convert spaces to tabs for golang writedata

--- src/homepageExperience/components/steps/go/WriteData.tsx
+++ src/homepageExperience/components/steps/go/WriteData.tsx
@@ -55,17 +55,17 @@ org := "${org.name}"
 bucket := "${bucket.name}"
 writeAPI := client.WriteAPIBlocking(org, bucket)
 for value := 0; value < 5; value++ {
-    tags := map[string]string{
-        "tagname1": "tagvalue1",
-    }
-    fields := map[string]interface{}{
-        "field1": value,
-    }
-    point := write.NewPoint("measurement1", tags, fields, time.Now())
+	tags := map[string]string{
+		"tagname1": "tagvalue1",
+	}
+	fields := map[string]interface{}{
+		"field1": value,
+	}
+	point := write.NewPoint("measurement1", tags, fields, time.Now())
 
-    if err := writeAPI.WritePoint(context.Background(), point); err != nil {
-        log.Fatal(err)
-    }
+	if err := writeAPI.WritePoint(context.Background(), point); err != nil {
+		log.Fatal(err)
+	}
 }`
 
   return (

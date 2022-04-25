705bc34315da0ee594dc0f90217cbfaa3167af5a
Bucky Schwarz <d.w.schwarz@gmail.com>
2022-04-25 16:19:31 -0400

    chore(firstMile): add code snippet for setting up a go module

--- src/homepageExperience/components/steps/go/InstallDependencies.tsx
+++ src/homepageExperience/components/steps/go/InstallDependencies.tsx
@@ -4,8 +4,18 @@ import {SafeBlankLink} from 'src/utils/SafeBlankLink'
 
 import {event} from 'src/cloud/utils/reporting'
 
+const goModuleSnippet = `mkdir -p influxdb_go_client
+cd influxdb_go_client
+go mod init influxdb_go_client
+touch main.go
+`
+
 export class InstallDependencies extends PureComponent {
-  private logCopyCodeSnippet = () => {
+  private logCopyInitializeModuleSnippet = () => {
+    event('firstMile.goWizard.initializeModule.code.copied')
+  }
+
+  private logCopyInstallCodeSnippet = () => {
     event('firstMile.goWizard.installDependencies.code.copied')
   }
   render() {
@@ -13,13 +23,20 @@ export class InstallDependencies extends PureComponent {
       <>
         <h1>Install Dependencies</h1>
         <p>
-          First, you need to install the{' '}
+          First, you need to create a new go module. Run the commands below in your terminal.
+        </p>
+        <CodeSnippet
+          text={goModuleSnippet}
+          onCopy={this.logCopyInitializeModuleSnippet}
+        />
+        <p style={{marginTop: '40px'}}>
+          Then, install the{' '}
           <code style={{color: '#B7B8FF'}}>influxdb-client-go</code> module. Run
           the command below in your terminal.
         </p>
         <CodeSnippet
           text="go get github.com/influxdata/influxdb-client-go/v2"
-          onCopy={this.logCopyCodeSnippet}
+          onCopy={this.logCopyInstallCodeSnippet}
         />
         <p style={{fontStyle: 'italic'}}>
           You’ll need to have{' '}

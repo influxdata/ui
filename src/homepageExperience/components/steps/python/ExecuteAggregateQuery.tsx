3e52adc7ad58f0427b011cd521f2411a4c94da75
Bucky Schwarz <d.w.schwarz@gmail.com>
2022-04-26 14:22:09 -0400

    chore(firstMile): ExecuteAggregateQuery - fix text explaining query being written

--- src/homepageExperience/components/steps/python/ExecuteAggregateQuery.tsx
+++ src/homepageExperience/components/steps/python/ExecuteAggregateQuery.tsx
@@ -59,8 +59,9 @@ for table in tables:
         onCopy={logCopyCodeSnippet}
       />
       <p>
-        In this example, we use the mean() function to calculate the average of
-        data points in last 10 minutes.
+        In this example, we use the{' '}
+        <code className="homepage-wizard--code-highlight">mean()</code> function
+        to calculate the average value of data points in the last 10 minutes.
         <br />
         <br />
         Run the following:

c62c36cfe06522129edf2fef0f40f953164acac9
ChitlangeSahas <chitlangesahas@gmail.com>
2022-04-28 04:55:47 -0700

    fix: #4491 data listening component clear old interval id's when bucket changed

--- src/homepageExperience/components/DataListening.tsx
+++ src/homepageExperience/components/DataListening.tsx
@@ -120,7 +120,7 @@ class DataListening extends PureComponent<Props, State> {
     } = this.props
 
     this.setState({loading: LoadingState.Loading})
-    continuouslyCheckForData(orgID, bucket, this.updateResponse)
+    this.intervalID = continuouslyCheckForData(orgID, bucket, this.updateResponse)
   }
 }
 

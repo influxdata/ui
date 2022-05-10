9eec07faf8120f77013c6964469669751d2950d0
wdoconnell <91283923+wdoconnell@users.noreply.github.com>
2022-05-10 09:42:41 -0400

    fix: Update LogoWithCubo to use LoginPage.scss file.

--- src/onboarding/components/LogoWithCubo.tsx
+++ src/onboarding/components/LogoWithCubo.tsx
@@ -6,6 +6,8 @@ import {
 } from '@influxdata/clockface'
 import React, {FC} from 'react'
 
+import 'src/onboarding/containers/LoginPage.scss'
+
 const LogoWithCubo: FC = () => (
   <div className="logo-with-cubo">
     <Icon glyph={IconFont.CuboSolid} className="cubo-logo" />

321aad7354fa5400d92ea2249fc8ad9d2ee4d980
wiedld <dlw405@gmail.com>
2022-05-12 15:57:20 -0700

    feat(4300): tests confirming communication flows btwn editor and LSP

---
+++ cypress/e2e/shared/editor.test.ts
@@ -0,0 +1,84 @@
+import {Organization} from '../../../src/types'
+
+// to see list of monaco-editor widgets to check:
+// document.querySelectorAll('[widgetid]')
+
+describe('Editor+LSP communication', () => {
+  const runTest = editorSelector => {
+    it('receives LSP-triggered server events', () => {
+      cy.getByTestID(editorSelector).then(() => {
+        cy.getByTestID('flux-editor', {timeout: 30000})
+          .should('be.visible')
+          .monacoType('foo |> bar')
+          .within(() => {
+            cy.get('.squiggly-error', {timeout: 30000}).should('be.visible')
+          })
+        cy.getByTestID('flux-editor').monacoType('{selectall}{del}')
+      })
+    })
+
+    it('has a roundtrip request-response, from the editor to the LSP server', () => {
+      cy.getByTestID(editorSelector).then(() => {
+        cy.getByTestID('flux-editor', {timeout: 30000})
+          .monacoType('{selectall} {backspace}')
+          .monacoType('from(')
+          .within(() => {
+            cy.get('[widgetid="editor.widget.suggestWidget"]', {
+              timeout: 30000,
+            }).should('be.visible')
+          })
+          .monacoType(`{selectall}{del}`)
+      })
+    })
+  }
+
+  describe('in Flows:', () => {
+    before(() => {
+      cy.flush()
+      cy.signin()
+      cy.get('@org').then(({id}: Organization) =>
+        cy.fixture('routes').then(({orgs}) => {
+          cy.visit(`${orgs}/${id}`)
+        })
+      )
+      cy.getByTestID('version-info')
+      cy.getByTestID('nav-item-flows').should('be.visible')
+      cy.getByTestID('nav-item-flows').click()
+
+      cy.getByTestID('preset-new')
+        .first()
+        .click()
+      cy.getByTestID('time-machine-submit-button').should('be.visible')
+
+      cy.get('.flow-divider--button')
+        .first()
+        .click()
+      cy.get('.insert-cell-menu.always-on').contains('Add Another Panel')
+      cy.getByTestID('add-flow-btn--rawFluxEditor')
+        .last()
+        .click()
+      cy.getByTestID('flux-editor').should('be.visible')
+    })
+
+    runTest('flux-editor')
+  })
+
+  describe('in DataExplorer', () => {
+    before(() => {
+      cy.flush()
+      cy.signin()
+      cy.get('@org').then(({id}: Organization) => {
+        cy.createMapVariable(id)
+        cy.fixture('routes').then(({orgs, explorer}) => {
+          cy.visit(`${orgs}/${id}${explorer}`)
+          cy.getByTestID('tree-nav').should('be.visible')
+        })
+      })
+      cy.getByTestID('switch-to-script-editor')
+        .should('be.visible')
+        .click()
+    })
+
+    runTest('time-machine--bottom')
+  })
+})

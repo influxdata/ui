5f77194cbcd5ab63d11e8cec4cff06f871c6d520
Bucky Schwarz <d.w.schwarz@gmail.com>
2022-05-12 09:54:09 -0400

    chore: remove unneeded type delcaration; change IBMPlexMono => RobotoMono

--- src/onboarding/components/HelperText.tsx
+++ src/onboarding/components/HelperText.tsx
@@ -3,13 +3,11 @@ import {
   FontWeight,
   Heading,
   HeadingElement,
-  Typeface,
 } from '@influxdata/clockface'
 
 const HelperText: FC = ({children}) => (
   <Heading
     element={HeadingElement.H6}
-    type={Typeface.Rubik}
     weight={FontWeight.Regular}
     className="helper-link-text"
     selectable={true}

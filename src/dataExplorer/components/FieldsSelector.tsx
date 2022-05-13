ea79789111d9cc013f5e83297e759bab6e787463
Chunchun <yccvivo@gmail.com>
2022-05-13 09:44:30 -0500

    feat: add empty view

--- src/dataExplorer/components/FieldsSelector.tsx
+++ src/dataExplorer/components/FieldsSelector.tsx
@@ -9,18 +9,30 @@ import './Schema.scss'
 
 const FieldsSelector: FC = () => {
   const fields = ['air_temp_degc', 'avg_wave_period_sec', 'dewpoint_temp_degc']
-  const list = fields.map(field => (
-    <div key={field} className="fields-selector--list-item">
-      {field}
-    </div>
-  ))
+
+  let list: JSX.Element | JSX.Element[] = (
+    <div className="fields-selector--list-item">No Fields Found</div>
+  )
+
+  if (fields.length) {
+    list = fields.map(field => (
+      <div key={field} className="fields-selector--list-item">
+        {field}
+      </div>
+    ))
+    list.push(
+      <div key="load-more" className="fields-selector--list-item">
+        Load More
+      </div>
+    )
+  }
+
   return (
     <Accordion className="fields-selector">
       <Accordion.AccordionHeader className="fields-selector--header">
         <SelectorTitle title="Fields" />
       </Accordion.AccordionHeader>
       {list}
-      <div className="fields-selector--list-item">Load More</div>
     </Accordion>
   )
 }

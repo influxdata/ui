64e001c35598e224132c63b566b67ef7205d8f20
Chunchun <yccvivo@gmail.com>
2022-05-13 09:48:31 -0500

    feat: add empty view for TagKeysSelector

--- src/dataExplorer/components/TagKeysSelector.tsx
+++ src/dataExplorer/components/TagKeysSelector.tsx
@@ -9,18 +9,29 @@ const TagKeysSelector: FC = () => {
     {key: 'station_id', value: []},
     {key: 'station_owner', value: ['COMPS']},
   ]
-  const list = tags.map(tag => (
-    <div key={tag.key} className="tag-keys-selector--list-item">
-      {tag.key}
-    </div>
-  ))
+
+  let list: JSX.Element | JSX.Element[] = (
+    <div className="tag-keys-selector--list-item">No Tags Found</div>
+  )
+
+  if (tags.length) {
+    list = tags.map(tag => (
+      <div key={tag.key} className="tag-keys-selector--list-item">
+        {tag.key}
+      </div>
+    ))
+    list.push(
+      <div key="load-more" className="tag-keys-selector--list-item">
+        Load More
+      </div>
+    )
+  }
   return (
     <Accordion className="tag-keys-selector">
       <Accordion.AccordionHeader className="tag-keys-selector--header">
         <SelectorTitle title="Tag Keys" />
       </Accordion.AccordionHeader>
       {list}
-      <div className="fields-selector--list-item">Load More</div>
     </Accordion>
   )
 }

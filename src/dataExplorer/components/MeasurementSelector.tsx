44ec0e2d35d376c510260949b52d197ee737ea88
Chunchun <yccvivo@gmail.com>
2022-05-13 10:50:36 -0500

    feat: add sample measurement and enable search bar

--- src/dataExplorer/components/MeasurementSelector.tsx
+++ src/dataExplorer/components/MeasurementSelector.tsx
@@ -8,26 +8,37 @@ import SearchableDropdown from 'src/shared/components/SearchableDropdown'
 const MeasurementSelector: FC = () => {
   // TODO: change to context later
   const [selectedMeasurement, setSelectedMeasurement] = useState(null)
+  const [searchTerm, setSearchTerm] = useState('')
 
-  const measurements = []
+  const measurements = [
+    'airSensors',
+    'average_temperature',
+    'coindesk',
+    'earthquake',
+    'explosion',
+    'h2o_feet',
+    'h2o_pH',
+    'h2o_quality',
+    'h2o_temperature',
+    'ice quake',
+    'ndbc',
+    'quarry blast',
+  ]
 
   const handleSelect = (option: string) => {
-    const selected = measurements.find(m => m.name === option)
+    const selected = measurements.find(m => m === option)
     setSelectedMeasurement(selected)
   }
 
   const handleChangeSearchTerm = (value: string) => {
-    // TODO
-    /* eslint-disable no-console */
-    console.log(value)
-    /* eslint-disable no-console */
+    setSearchTerm(value)
   }
 
   return (
     <div>
       <SelectorTitle title="Measurement" />
       <SearchableDropdown
-        searchTerm="" // TODO: variable
+        searchTerm={searchTerm}
         searchPlaceholder="Search measurements"
         selectedOption={selectedMeasurement || 'Select measurement...'}
         onSelect={handleSelect}
@@ -37,7 +48,7 @@ const MeasurementSelector: FC = () => {
         testID="measurement-selector--dropdown"
         buttonTestID="measurement-selector--dropdown-button"
         menuTestID="measurement-selector--dropdown--menu"
-        emptyText="No Measurement Found"
+        emptyText="No Measurements Found"
         iconOn={true}
       />
     </div>

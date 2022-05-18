94afa11ade92ad759625cca85fc25cb566e64a63
asalem <ariel.salem1989@gmail.com>
2022-05-18 06:16:45 -0700

    chore: readd the whitespace

--- src/timeMachine/apis/queryBuilder.ts
+++ src/timeMachine/apis/queryBuilder.ts
@@ -107,6 +107,7 @@ export function findValues({
   // TODO: Use the `v1.tagValues` function from the Flux standard library once
   // this issue is resolved: https://github.com/influxdata/flux/issues/1071
   const query = `import "regexp"
+
   from(bucket: "${bucket}")
   |> range(${timeRangeArguments})
   |> filter(fn: ${tagFilters})

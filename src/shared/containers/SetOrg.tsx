f2c5f42dfabdf7b083ba3b292610ee6e7cd1e5e0
wdoconnell <91283923+wdoconnell@users.noreply.github.com>
2022-05-26 10:16:38 -0400

    fix: add provider to account page, WIP.

--- src/shared/containers/SetOrg.tsx
+++ src/shared/containers/SetOrg.tsx
@@ -110,6 +110,9 @@ const SetOrg: FC = () => {
     }
     updateReportingContext({orgID: null})
 
+    console.log('content of orgs at the setOrg page')
+    console.log(orgs)
+
     if (!orgs.length) {
       history.push(`/no-orgs`)
       return

300d062325b40facffd6e38cda4a6303afd7ebab
Bucky Schwarz <d.w.schwarz@gmail.com>
2022-05-27 14:35:44 -0400

    feat: add sfdc api layer to connect to new sfdc functionality

---
+++ src/shared/apis/sfdc.ts
@@ -0,0 +1,20 @@
+import {postUiproxySfdcSupport, PostUiproxySfdcSupportParams} from 'src/client/uiproxydRoutes'
+
+export const createSfdcSupportCase = async (
+  description: string,
+  email: string,
+  severity: string,
+  subject: string
+) => {
+  const params: PostUiproxySfdcSupportParams = {
+    data: {description, email, severity, subject},
+  }
+
+  const response = await postUiproxySfdcSupport(params)
+
+  if (response.status !== 204) {
+    throw new Error(response.data.message)
+  }
+
+  return response
+}

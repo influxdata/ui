ef19b2b3ef3981f4d7e37011d0ab09a6f296cdb9
Beth Legesse <legessebl@gmail.com>
2022-05-30 12:57:59 -0500

    test: add tests for payg overlay

--- cypress/e2e/shared/helpBar.test.ts
+++ cypress/e2e/shared/helpBar.test.ts
@@ -1,6 +1,4 @@
-import {Organization} from '../../../src/types'
-
-describe('navigation', () => {
+describe('Help bar menu', () => {
   beforeEach(() => {
     cy.flush()
     cy.signin()
@@ -8,75 +6,37 @@ describe('navigation', () => {
     cy.setFeatureFlags({
         helpBar: true
     })
-    
   })
 
   it('can navigate to Help Bar sub nav items ', () => {
-
-    // cy.getByTestID('nav-tree').should('be.visible')
-    
-    // cy.window().then(win => {
-    //   cy.stub(win, 'open').as('windowOpenSpy')
-    // })
     cy.getByTestID('nav-item-support').get('.cf-tree-nav--sub-menu-trigger').eq(3).trigger('mouseover')
-    // cy.getByTestID('nav-subitem-documentation').eq(1).click({force: true})
 
-    // checks if documentation link is live
     cy.getByTestID('nav-subitem-documentation').should('exist').then(link => {
       cy.request(link.prop('href'))
       .its('status')
       .should('eq', 200)
     })
-    // checks if faq link is live 
+     
     cy.getByTestID('nav-subitem-faqs').should('exist').then(link => {
       cy.request(link.prop('href'))
       .its('status')
       .should('eq', 200)
     })
 
-    // checks if forum link is live
     cy.getByTestID('nav-subitem-forum').should('exist').then(link => {
       cy.request(link.prop('href'))
       .its('status')
       .should('eq', 200)
     })
 
-    // checks if slack link is live
     cy.getByTestID('nav-subitem-influxdb-slack').should('exist').then(link => {
       cy.request(link.prop('href'))
       .its('status')
       .should('eq', 200)
     })
-
-      // cy.get('@windowOpenSpy').should(
-      //     'have.been.calledOnceWithExactly',
-      //     'https://docs.influxdata.com/'
-      //   )
-    
-        
-
-            // cy.get('.cf-tree-nav--sub-menu-trigger').trigger('mouseover')
-        
-        // cy.get('cf-tree-nav--sub-menu-trigger').trigger('mouseover')
-            // cy.getByTestID('nav-subitem-documentation').click()
-
-        
-
-            // cy.get('.tree-nav--sub-menu').click()
-        
-        // cy.getByTestID('nav-item-load-data').click({force: true})
-        
-        // cy.getByTestID('documentation').click()
-        // cy.get('@windowOpenSpy').should(
-        //   'be.calledWith',
-        //   'https://docs.influxdata.com/'
-        // )
-    
-
-
   })
 
-  it.only('can submit feedback and questions form', () => {
+  it('can submit feedback and questions form', () => {
     cy.getByTestID('nav-item-support').get('.cf-tree-nav--sub-menu-trigger').eq(3).trigger('mouseover')
     cy.getByTestID('nav-subitem-feedback-questions').eq(0).click({force: true})
 
@@ -85,13 +45,83 @@ describe('navigation', () => {
     cy.getByTestID('overlay--container').within(() => {
     cy.getByTestID('support-description--textarea').type('here is some feedback and questions from a cloud customer')
 
-      // cy.getByTestID('feedback-questions-overlay--submit').click()
+      cy.getByTestID('feedback-questions-overlay--submit').click()
     })
 
+    cy.getByTestID('confirmation-overlay-header').should('be.visible')
+    cy.getByTestID('confirmation-overlay-header').contains('Feedback & Questions')
+    cy.getByTestID('confirmation-overlay--OK').click()
+    cy.getByTestID('confirmation-overlay-header').should('not.exist')
   })
+})
 
+describe('Help bar support for free users', () => {
+  beforeEach(() =>
+    cy.flush().then(() =>
+      cy.signin().then(() => {
+        cy.get('@org').then(() => {
+          cy.setFeatureFlags({
+            helpBar: true,
+          }).then(() => {
+            cy.quartzProvision({
+              accountType: 'free'
+            }).then(() => {
+              cy.visit('/')
+              cy.getByTestID('nav-item-support').should('be.visible')
+            })
+          })
+        })
+      })
+    )
+  )
+  it('displays important links free users can use to seek help', () => {
+    cy.getByTestID('nav-item-support').get('.cf-tree-nav--sub-menu-trigger').eq(3).trigger('mouseover')
+    cy.getByTestID('nav-subitem-contact-support').eq(1).click({force: true})
+    cy.getByTestID('free-support-overlay-header').should('exist')
   
-  
+    cy.getByTestID('overlay--container').within(() => {
+      cy.getByTestID('free-account-links').eq(0).contains('InfluxDB Community Forums')
+      cy.getByTestID('free-account-links').eq(0).contains('InfluxDB Slack')
+      cy.getByTestID('free-account-links').eq(0).contains('Feedback & Questions Form')
+      })
+  })
+})
+
+describe('Help bar support for PAYG users', () => {
+  beforeEach(() =>
+    cy.flush().then(() =>
+      cy.signin().then(() => {
+        cy.get('@org').then(() => {
+          cy.setFeatureFlags({
+            helpBar: true,
+          }).then(() => {
+            cy.quartzProvision({
+              accountType: 'pay_as_you_go',
+            }).then(() => {
+              cy.visit('/')
+              cy.getByTestID('nav-item-support').should('be.visible')
+            })
+          })
+        })
+      })
+    )
+  )
+
+  it('Allows PAYG users to submit a support request', () => {
+    cy.getByTestID('nav-item-support').get('.cf-tree-nav--sub-menu-trigger').eq(3).trigger('mouseover')
+    cy.getByTestID('nav-subitem-contact-support').eq(1).click({force: true})
+    cy.getByTestID('payg-support-overlay-header')
+      .should('exist')
+
+    cy.getByTestID('contact-support-subject-input').clear().type('testing help bar')
+    cy.getByTestID('severity-level-dropdown').click()
+    cy.getByTitle('1 - Critical').click()
+
+    cy.getByTestID('support-description--textarea').clear().type('here is a description from user about something they need help with')
+    cy.getByTestID('payg-contact-support--submit').click()
+
+    cy.getByTestID('confirmation-overlay-header').should('exist')
+  })
 })
 
 // help and support bar exists 

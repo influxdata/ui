9203de04b1b236b3c429fbd7307fc49b7c575123
wdoconnell <91283923+wdoconnell@users.noreply.github.com>
2022-05-31 20:04:41 -0400

    feat: adding backwards-compatible API updates

--- src/me/actions/thunks/index.ts
+++ src/me/actions/thunks/index.ts
@@ -62,53 +62,55 @@ export const getMe = () => async (
         throw new Error('Error')
       }
 
-      console.log('herei s the data coming from /identity')
-      console.log(identityEndPoint.data)
-      const arrayOfAccounts = await getAccounts({})
-      console.log('here is a list of all accounts')
-      console.log(arrayOfAccounts.data)
+      // console.log('herei s the data coming from /identity')
+      // console.log(identityEndPoint.data)
 
-      const specificAccountInfo = await getAccount({
-        accountId: arrayOfAccounts.data[0].id,
-      })
+      // const arrayOfAccounts = await getAccounts({})
+      // console.log('here is a list of all accounts')
+      // console.log(arrayOfAccounts.data)
 
-      if (specificAccountInfo.status !== 200) {
-        throw new Error(specificAccountInfo.data.message)
-      }
+      // const specificAccountInfo = await getAccount({
+      //   accountId: arrayOfAccounts.data[0].id,
+      // })
 
-      // Need a way to get the specific org id
-      const specificOrgInfo = await getOrgAPI({
-        orgId: identityEndPoint.data.org.id,
-      })
-      if (specificOrgInfo.status !== 200) {
-        throw new Error(specificOrgInfo.data.message)
-      }
+      // if (specificAccountInfo.status !== 200) {
+      //   throw new Error(specificAccountInfo.data.message)
+      // }
 
-      console.log('here is the specific account data for id 2')
-      console.log(specificAccountInfo.data)
+      // // Need a way to get the specific org id
+      // const specificOrgInfo = await getOrgAPI({
+      //   orgId: identityEndPoint.data.org.id,
+      // })
+      // if (specificOrgInfo.status !== 200) {
+      //   throw new Error(specificOrgInfo.data.message)
+      // }
 
-      const quartzMeObj = {
-        accountCreatedAt: identityEndPoint.data.account.accountCreatedAt,
-        accountType: identityEndPoint.data.account.type,
+      // console.log('here is the specific account data for id 2')
+      // console.log(specificAccountInfo.data)
 
-        // For billing provider, I'm getting an undefined when I should be getting null.
-        // Check with ecommerce team to make sure that's right.
-        billingProvider: specificAccountInfo.data.billing_provider,
-        clusterHost: identityEndPoint.data.org.clusterHost,
-        email: identityEndPoint.data.user.email,
-        id: identityEndPoint.data.user.id,
+      // // This information would need to be moved into the quartz function though.
+      // const quartzMeObj = {
+      //   accountCreatedAt: identityEndPoint.data.account.accountCreatedAt,
+      //   accountType: identityEndPoint.data.account.type,
 
-        // I don't see an isOperator. For now, I'm going to set it to false by default,
-        // unless operatorRole is true.
-        isOperator: identityEndPoint.data.user.operatorRole ? true : false,
-        isRegionBeta: specificOrgInfo.data.isRegionBeta,
-        operatorRole: identityEndPoint.data.user.operatorRole,
-        paygCreditStartDate: identityEndPoint.data.account.paygCreditStartDate,
-        regionCode: specificOrgInfo.data.regionCode,
-        regionName: specificOrgInfo.data.regionName,
-      }
-      console.log('here is the shoehorn for quartzMe')
-      console.log(quartzMeObj)
+      //   // For billing provider, I'm getting an undefined when I should be getting null.
+      //   // Check with ecommerce team to make sure that's right.
+      //   billingProvider: specificAccountInfo.data.billing_provider,
+      //   clusterHost: identityEndPoint.data.org.clusterHost,
+      //   email: identityEndPoint.data.user.email,
+      //   id: identityEndPoint.data.user.id,
+
+      //   // I don't see an isOperator. For now, I'm going to set it to false by default,
+      //   // unless operatorRole is true.
+      //   isOperator: identityEndPoint.data.user.operatorRole ? true : false,
+      //   isRegionBeta: specificOrgInfo.data.isRegionBeta,
+      //   operatorRole: identityEndPoint.data.user.operatorRole,
+      //   paygCreditStartDate: identityEndPoint.data.account.paygCreditStartDate,
+      //   regionCode: specificOrgInfo.data.regionCode,
+      //   regionName: specificOrgInfo.data.regionName,
+      // }
+      // console.log('here is the shoehorn for quartzMe')
+      // console.log(quartzMeObj)
 
       if (resp.status !== 200) {
         throw new Error(resp.data.message)
@@ -164,6 +166,12 @@ export const getMe = () => async (
 }
 
 export const getQuartzMe = () => async dispatch => {
+  // Ideally the best way to do this is:
+  // (1) Check if flag is enabled. If it isn't, keep the old logic.
+  // (2) If flag is enabled, then store a bunch of new information in a new piece of state
+  // That keeps track fo organization data (this is true of both functions.
+  // (3) If flag is enabled, also map over the data into a separate object.
+
   try {
     dispatch(setQuartzMeStatus(RemoteDataState.Loading))
     const resp = await apiGetQuartzMe({})
@@ -173,7 +181,69 @@ export const getQuartzMe = () => async dispatch => {
       throw new Error(resp.data.message)
     }
 
-    dispatch(setQuartzMe(resp.data, RemoteDataState.Done))
+    // This info should be pulled from state, not re-pulled from the API.
+    const identityEndPoint = await getIdentity({})
+
+    if (identityEndPoint.status !== 200) {
+      throw new Error('Error')
+    }
+
+    const arrayOfAccounts = await getAccounts({})
+    console.log('here is a list of all accounts')
+    console.log(arrayOfAccounts.data)
+
+    const specificAccountInfo = await getAccount({
+      accountId: arrayOfAccounts.data[0].id,
+    })
+
+    if (specificAccountInfo.status !== 200) {
+      throw new Error(specificAccountInfo.data.message)
+    }
+
+    // Need a way to get the specific org id
+    const specificOrgInfo = await getOrgAPI({
+      orgId: identityEndPoint.data.org.id,
+    })
+    if (specificOrgInfo.status !== 200) {
+      throw new Error(specificOrgInfo.data.message)
+    }
+
+    console.log('here is the specific account data for id 2')
+    console.log(specificAccountInfo.data)
+
+    // This information would need to be moved into the quartz function though.
+    const quartzMeObj = {
+      accountCreatedAt: identityEndPoint.data.account.accountCreatedAt,
+      accountType: identityEndPoint.data.account.type,
+
+      // For billing provider, I'm getting an undefined when I should be getting null.
+      // Check with ecommerce team to make sure that's right.
+      billingProvider: specificAccountInfo.data.billing_provider,
+      clusterHost: identityEndPoint.data.org.clusterHost,
+      email: identityEndPoint.data.user.email,
+      id: identityEndPoint.data.user.id,
+
+      // I don't see an isOperator. For now, I'm going to set it to false by default,
+      // unless operatorRole is true.
+      isOperator: identityEndPoint.data.user.operatorRole ? true : false,
+      isRegionBeta: specificOrgInfo.data.isRegionBeta,
+      operatorRole: identityEndPoint.data.user.operatorRole,
+      paygCreditStartDate: identityEndPoint.data.account.paygCreditStartDate,
+      regionCode: specificOrgInfo.data.regionCode,
+      regionName: specificOrgInfo.data.regionName,
+    }
+    console.log('here is the shoehorn for quartzMe')
+    console.log(quartzMeObj)
+
+    console.log('HERE IS THE BIG COMPARISON')
+    console.log('HERE IS RESP.DATA')
+    console.log(resp.data)
+    console.log('HERE IS THE CLONE')
+    console.log(quartzMeObj)
+
+    // Here's what happens when we replace it.
+    // dispatch(setQuartzMe(resp.data, RemoteDataState.Done))
+    dispatch(setQuartzMe(quartzMeObj, RemoteDataState.Done))
   } catch (error) {
     console.error(error)
     dispatch(setQuartzMeStatus(RemoteDataState.Error))

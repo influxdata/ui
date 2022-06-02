1cc20b4af16aadeb5de810a2e153c37ee0ef3975
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-06-02 08:48:39 -0500

    chore: run prettier

--- src/dataExplorer/shared/utils.ts
+++ src/dataExplorer/shared/utils.ts
@@ -1,4 +1,3 @@
-
 export const LOCAL_LIMIT = 8
 export const IMPORT_REGEXP = 'import "regexp"\n'
 export const IMPORT_INFLUX_SCHEMA = 'import "influxdata/influxdb/schema"'
@@ -10,4 +9,4 @@ export const FROM_BUCKET = (bucketName: string) =>
   `from(bucket: "${bucketName}")`
 
 export const SEARCH_STRING = (searchTerm: string): string =>
-`|> filter(fn: (r) => r._value =~ regexp.compile(v: "(?i:" + regexp.quoteMeta(v: "${searchTerm}") + ")"))`
+  `|> filter(fn: (r) => r._value =~ regexp.compile(v: "(?i:" + regexp.quoteMeta(v: "${searchTerm}") + ")"))`

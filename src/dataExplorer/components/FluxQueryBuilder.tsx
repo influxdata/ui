ade4554eadd278c9db37ac62c48ebc74b06240ab
wiedld <dlw405@gmail.com>
2022-06-06 11:48:05 -0700

    feat: bring editorContext into new FLuxQueryBuilder, to can use for function injection

--- src/dataExplorer/components/FluxQueryBuilder.tsx
+++ src/dataExplorer/components/FluxQueryBuilder.tsx
@@ -3,6 +3,7 @@ import React, {FC, useState} from 'react'
 // Components
 import {DraggableResizer, Orientation} from '@influxdata/clockface'
 import {QueryProvider} from 'src/shared/contexts/query'
+import {EditorProvider} from 'src/shared/contexts/editor'
 import {ResultsProvider} from 'src/dataExplorer/components/ResultsContext'
 import ResultsPane from 'src/dataExplorer/components/ResultsPane'
 import Schema from 'src/dataExplorer/components/Schema'
@@ -19,20 +20,22 @@ const FluxQueryBuilder: FC = () => {
 
   return (
     <QueryProvider>
-      <DraggableResizer
-        handleOrientation={Orientation.Vertical}
-        handlePositions={vertDragPosition}
-        onChangePositions={setVertDragPosition}
-      >
-        <DraggableResizer.Panel>
-          <Schema />
-        </DraggableResizer.Panel>
-        <DraggableResizer.Panel className="new-data-explorer-rightside">
-          <ResultsProvider>
-            <ResultsPane />
-          </ResultsProvider>
-        </DraggableResizer.Panel>
-      </DraggableResizer>
+      <EditorProvider>
+        <DraggableResizer
+          handleOrientation={Orientation.Vertical}
+          handlePositions={vertDragPosition}
+          onChangePositions={setVertDragPosition}
+        >
+          <DraggableResizer.Panel>
+            <Schema />
+          </DraggableResizer.Panel>
+          <DraggableResizer.Panel className="new-data-explorer-rightside">
+            <ResultsProvider>
+              <ResultsPane />
+            </ResultsProvider>
+          </DraggableResizer.Panel>
+        </DraggableResizer>
+      </EditorProvider>
     </QueryProvider>
   )
 }

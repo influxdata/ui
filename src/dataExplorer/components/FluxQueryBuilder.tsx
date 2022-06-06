90e8cac5250684f5e8f0f1c6b45ecaba46d16f5e
wiedld <dlw405@gmail.com>
2022-06-06 15:38:47 -0700

    feat: interactivity to allow the left & right panels to expand and contract, including the new right panel.

--- src/dataExplorer/components/FluxQueryBuilder.tsx
+++ src/dataExplorer/components/FluxQueryBuilder.tsx
@@ -1,15 +1,24 @@
 import React, {FC, useState} from 'react'
 
 // Components
-import {DraggableResizer, Orientation} from '@influxdata/clockface'
+import {
+  AlignItems,
+  DraggableResizer,
+  FlexBox,
+  FlexDirection,
+  Orientation,
+} from '@influxdata/clockface'
 import {QueryProvider} from 'src/shared/contexts/query'
 import {EditorProvider} from 'src/shared/contexts/editor'
 import {ResultsProvider} from 'src/dataExplorer/components/ResultsContext'
 import ResultsPane from 'src/dataExplorer/components/ResultsPane'
+import SidePane from 'src/dataExplorer/components/SidePane'
 import Schema from 'src/dataExplorer/components/Schema'
 
 // Styles
 import './FluxQueryBuilder.scss'
+import LeftArrow from 'assets/images/collapse-left.svg'
+import RightArrow from 'assets/images/collapse-right.svg'
 
 const INITIAL_VERT_RESIZER_HANDLE = 0.24
 
@@ -17,6 +26,8 @@ const FluxQueryBuilder: FC = () => {
   const [vertDragPosition, setVertDragPosition] = useState([
     INITIAL_VERT_RESIZER_HANDLE,
   ])
+  const [showLeft, toggleLeftPanel] = useState(true)
+  const [showRight, toggleRightPanel] = useState(true)
 
   return (
     <QueryProvider>
@@ -26,13 +37,32 @@ const FluxQueryBuilder: FC = () => {
           handlePositions={vertDragPosition}
           onChangePositions={setVertDragPosition}
         >
-          <DraggableResizer.Panel>
-            <Schema />
+          <DraggableResizer.Panel style={showLeft ? {} : {display: 'none'}}>
+            {showLeft && <Schema />}
           </DraggableResizer.Panel>
           <DraggableResizer.Panel className="new-data-explorer-rightside">
-            <ResultsProvider>
-              <ResultsPane />
-            </ResultsProvider>
+            <FlexBox
+              direction={FlexDirection.Row}
+              alignItems={AlignItems.FlexStart}
+              style={{height: '100%'}}
+            >
+              <FlexBox.Child>
+                <span onClick={() => toggleLeftPanel(!showLeft)}>
+                  <img src={showLeft ? LeftArrow : RightArrow} />
+                </span>
+              </FlexBox.Child>
+              <ResultsProvider>
+                <ResultsPane />
+              </ResultsProvider>
+              <FlexBox.Child>
+                <span onClick={() => toggleRightPanel(!showRight)}>
+                  <img src={showRight ? RightArrow : LeftArrow} />
+                </span>
+              </FlexBox.Child>
+            </FlexBox>
+          </DraggableResizer.Panel>
+          <DraggableResizer.Panel style={showRight ? {} : {display: 'none'}}>
+            {showRight && <SidePane />}
           </DraggableResizer.Panel>
         </DraggableResizer>
       </EditorProvider>
---
+++ src/dataExplorer/components/SidePane.tsx
@@ -0,0 +1,7 @@
+import React, {FC} from 'react'
+
+const SidePane: FC = () => {
+  return <div>TODO: SidePane</div>
+}
+
+export default SidePane

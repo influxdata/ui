04e4348401886f2e7d9f1147cac0e36049b8fc7b
Bucky Schwarz <d.w.schwarz@gmail.com>
2022-06-06 17:32:29 -0400

    chore: fire an event when a user's variant id is retrieved

--- src/cloud/components/experiments/GoogleOptimizeExperiment.tsx
+++ src/cloud/components/experiments/GoogleOptimizeExperiment.tsx
@@ -1,8 +1,14 @@
 // Libraries
 import {FC, useEffect, useState} from 'react'
+import {useSelector} from 'react-redux'
 
 // Utils
 import {getExperimentVariantId} from 'src/cloud/utils/experiments'
+import {event} from 'src/cloud/utils/reporting'
+
+// Selectors
+import {getMe} from 'src/me/selectors'
+import {getOrg} from 'src/organizations/selectors'
 
 interface Props {
   /** Experiment ID found in the Measurements and Objectives section of the Google Optimize experiment Details page */
@@ -22,11 +28,26 @@ export const GoogleOptimizeExperiment: FC<Props> = ({
   variants,
 }) => {
   const [variantID, setExperimentVariantId] = useState<string>('')
+  const me = useSelector(getMe)
+  const org = useSelector(getOrg)
 
   useEffect(() => {
     setExperimentVariantId(
       getExperimentVariantId(experimentID, activationEvent)
     )
+
+    if (variantID) {
+      event(
+        'optimize.user_variant',
+        {},
+        {
+          experiment_id: experimentID,
+          variant: variantID,
+          user_id: me.id,
+          org_id: org.id,
+        }
+      )
+    }
   })
 
   if (variantID) {

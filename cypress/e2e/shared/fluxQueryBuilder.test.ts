46f8cb888dc587dada218f7381adcbaac3b7b4a8
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-06-07 14:50:10 -0500

    test: add test structure

--- cypress/e2e/shared/fluxQueryBuilder.test.ts
+++ cypress/e2e/shared/fluxQueryBuilder.test.ts
@@ -10,15 +10,70 @@ describe('FluxQueryBuilder', () => {
           cy.visit(`${orgs}/${id}${explorer}`)
           cy.getByTestID('tree-nav').should('be.visible')
           // Switch to Flux Query Builder
-          cy.getByTestID('slide-toggle').should('be.visible').click()
+          cy.getByTestID('slide-toggle')
+            .should('be.visible')
+            .click()
         })
       })
     })
   })
 
-  describe('schema browser', () => {
-    it("exists", () => {
-      cy.getByTestID('schema-browser').should('be.visible')
+  describe('Schema browser', () => {
+    it('bucket selector', () => {
+      it('no other selectors are shown if no bucket is selected', () => {})
+
+      it('show empty list if fetching buckets failed', () => {})
+
+      it('allows one (and only one) bucket to be selected from the list', () => {})
+
+      it('upon selection, will show measurement selector', () => {})
+    })
+
+    describe('measurement selector', () => {
+      beforeEach(() => {
+        // select a bucket
+      })
+
+      it('show empyt list if fetching measurements failed', () => {})
+
+      it('allow the user to select one (and only one) measurement', () => {})
+
+      it('upon selection, will show a list of fields and tag keys to the user', () => {})
+    })
+
+    describe('field selector', () => {
+      beforeEach(() => {
+        // select a bucket
+        // select a measurement
+      })
+
+      it('if less than 8 items, show all the items', () => {})
+
+      it("if more than 8 items, show a 'Load More' option to load more", () => {})
+
+      it('when load more is chosen, up to 25 additional entries will be shown', () => {})
+
+      it('if more than 25 more exists in the list, the user has the option to load more', () => {})
+    })
+
+    describe('tag values', () => {
+      beforeEach(() => {
+        // select a bucket
+        // select a measurement
+      })
+
+      it('allow an expansion of tag keys into a list of tag values', () => {})
+
+      it('allow one or more tag values to be selected', () => {})
+    })
+
+    describe('search bar for fields and tag keys', () => {
+      beforeEach(() => {
+        // select a bucket
+        // select a measurement
+      })
+
+      it('can search dynamically', () => {})
     })
   })
 })

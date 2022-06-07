f6a8fa846af2dff394f3947c43c7449de7be18e3
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-06-07 09:20:09 -0500

    test: add basic test for schema browser

--- cypress/e2e/shared/fluxQueryBuilder.test.ts
+++ cypress/e2e/shared/fluxQueryBuilder.test.ts
@@ -5,9 +5,20 @@ describe('FluxQueryBuilder', () => {
     cy.flush()
     cy.signin()
     cy.get('@org').then(({id}: Organization) => {
-      cy.fixture('routes').then(({orgs}) => {
-        cy.visit(`${orgs}/${id}`)
+      cy.fixture('routes').then(({orgs, explorer}) => {
+        cy.setFeatureFlags({newDataExplorer: true}).then(() => {
+          cy.visit(`${orgs}/${id}${explorer}`)
+          cy.getByTestID('tree-nav').should('be.visible')
+          // Switch to Flux Query Builder
+          cy.getByTestID('slide-toggle').should('be.visible').click()
+        })
       })
+    })
+  })
+
+  describe('schema browser', () => {
+    it("exists", () => {
+      cy.getByTestID('schema-browser').should('be.visible')
     })
   })
 })

c802a57fb49592055737b91a84f3e2e9f9c0df9b
Bethelhem Legesse <legessebl@gmail.com>
2022-06-08 12:26:33 -0400

    feat: add status page in help bar sub menu (#4782)

--- src/pageLayout/containers/TreeNav.tsx
+++ src/pageLayout/containers/TreeNav.tsx
@@ -187,7 +187,17 @@ const TreeSidebar: FC<ReduxProps> = ({
                   />
                 )}
               />
-
+              <TreeNav.SubItem
+                id="status-page"
+                label="Status Page"
+                testID="nav-subitem-status"
+                linkElement={() => (
+                  <SafeBlankLink
+                    href="https://status.influxdata.com"
+                    onClick={() => handleEventing('status-page')}
+                  />
+                )}
+              />
               {isFlagEnabled('helpBarSfdcIntegration') && (
                 <TreeNav.SubItem
                   id="contactSupport"

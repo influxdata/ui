3808153d1a2c1c26364b0f31b6b9e95589d97d89
wiedld <dlw405@gmail.com>
2022-06-08 08:39:38 -0700

    fix: remove collapsing functionality of side panels

--- src/dataExplorer/components/FluxQueryBuilder.tsx
+++ src/dataExplorer/components/FluxQueryBuilder.tsx
@@ -1,13 +1,7 @@
 import React, {FC, useState} from 'react'
 
 // Components
-import {
-  AlignItems,
-  DraggableResizer,
-  FlexBox,
-  FlexDirection,
-  Orientation,
-} from '@influxdata/clockface'
+import {DraggableResizer, Orientation} from '@influxdata/clockface'
 import {QueryProvider} from 'src/shared/contexts/query'
 import {EditorProvider} from 'src/shared/contexts/editor'
 import {ResultsProvider} from 'src/dataExplorer/components/ResultsContext'
@@ -17,8 +11,6 @@ import Schema from 'src/dataExplorer/components/Schema'
 
 // Styles
 import './FluxQueryBuilder.scss'
-import LeftArrow from 'assets/images/collapse-left.svg'
-import RightArrow from 'assets/images/collapse-right.svg'
 
 const INITIAL_LEFT_VERT_RESIZER_HANDLE = 0.2
 const INITIAL_RIGHT_VERT_RESIZER_HANDLE = 0.8
@@ -28,20 +20,6 @@ const FluxQueryBuilder: FC = () => {
     INITIAL_LEFT_VERT_RESIZER_HANDLE,
     INITIAL_RIGHT_VERT_RESIZER_HANDLE,
   ])
-  const [showLeft, toggleLeftPanel] = useState(true)
-  const [showRight, toggleRightPanel] = useState(true)
-
-  const adjustMiddlePanel = grow => {
-    if (!grow) {
-      return
-    }
-    const el = document.querySelector(
-      '[data-testid="flux-query-builder-middle-panel"]'
-    )
-    if (el) {
-      ;(el as any).style.flexGrow = 0.8 // eslint-disable-line
-    }
-  }
 
   return (
     <QueryProvider>
@@ -51,51 +29,18 @@ const FluxQueryBuilder: FC = () => {
           handlePositions={vertDragPosition}
           onChangePositions={setVertDragPosition}
         >
-          <DraggableResizer.Panel style={showLeft ? {} : {display: 'none'}}>
+          <DraggableResizer.Panel>
             <Schema />
           </DraggableResizer.Panel>
           <DraggableResizer.Panel
             testID="flux-query-builder-middle-panel"
             className="new-data-explorer-rightside"
           >
-            <FlexBox
-              direction={FlexDirection.Row}
-              alignItems={AlignItems.FlexStart}
-              style={{height: '100%', width: '100%', position: 'absolute'}}
-            >
-              <FlexBox.Child>
-                <span
-                  onClick={() => {
-                    toggleLeftPanel(!showLeft)
-                    adjustMiddlePanel(showLeft)
-                  }}
-                  style={{marginLeft: '-20px', float: 'left'}}
-                >
-                  <img src={showLeft ? LeftArrow : RightArrow} />
-                </span>
-              </FlexBox.Child>
-              <ResultsProvider>
-                <ResultsPane />
-              </ResultsProvider>
-              <FlexBox.Child>
-                <span
-                  onClick={() => {
-                    toggleRightPanel(!showRight)
-                    adjustMiddlePanel(showRight)
-                  }}
-                  style={{
-                    marginRight: '-20px',
-                    float: 'right',
-                    zIndex: '999',
-                    position: 'absolute',
-                  }}
-                >
-                  <img src={showRight ? RightArrow : LeftArrow} />
-                </span>
-              </FlexBox.Child>
-            </FlexBox>
+            <ResultsProvider>
+              <ResultsPane />
+            </ResultsProvider>
           </DraggableResizer.Panel>
-          <DraggableResizer.Panel style={showRight ? {} : {display: 'none'}}>
+          <DraggableResizer.Panel>
             <SidePane />
           </DraggableResizer.Panel>
         </DraggableResizer>

1cd779aad8ff74449696c084820469bbe7084c11
Beth Legesse <legessebl@gmail.com>
2022-06-14 13:05:19 -0500

    test: mock rejected and resolved response for api

--- src/pageLayout/containers/TreeNav.Helpbar.test.tsx
+++ src/pageLayout/containers/TreeNav.Helpbar.test.tsx
@@ -4,6 +4,15 @@ import {renderWithReduxAndRouter} from 'src/mockState'
 import {Me} from 'src/client/unityRoutes'
 import {mocked} from 'ts-jest/utils'
 
+// Imported mocks
+import {
+  OverlayController,
+  OverlayProviderComp,
+} from '../../overlays/components/OverlayController'
+import TreeNav from 'src/pageLayout/containers/TreeNav'
+import {notify} from 'src/shared/actions/notifications'
+import {supportRequestError} from 'src/shared/copy/notifications'
+
 jest.mock('src/flows', () => {
   return () => <></>
 })
@@ -17,10 +26,20 @@ jest.mock('src/checks/components/NewThresholdCheckEO.tsx', () => () => null)
 jest.mock('src/checks/components/NewDeadmanCheckEO.tsx', () => () => null)
 
 jest.mock(
-  'src/client/uiProxydRoutes',
+  'src/client/uiproxydRoutes',
   () => ({
-    ...jest.requireActual('src/client/uiProxydRoutes'),
-    postUiproxySfdcSupport: jest.fn(),
+    postUiproxySfdcSupport: jest
+      .fn()
+      .mockResolvedValueOnce({
+        status: 204,
+        headers: {},
+        data: null,
+      })
+      .mockRejectedValueOnce({
+        status: 500,
+        headers: {},
+        data: null,
+      }),
   }),
   {virtual: true}
 )
@@ -35,15 +54,7 @@ jest.mock('src/cloud/utils/reporting', () => ({
   updateReportingContext: jest.fn(),
 }))
 jest.mock('src/shared/actions/notifications')
-
-// Imported mocks
-import {
-  OverlayController,
-  OverlayProviderComp,
-} from '../../overlays/components/OverlayController'
-import TreeNav from 'src/pageLayout/containers/TreeNav'
-import {notify} from 'src/shared/actions/notifications'
-import {supportRequestError} from 'src/shared/copy/notifications'
+jest.mock('src/shared/constants', () => ({CLOUD: true}))
 
 const setup = (quartzMe: Me) => {
   const newState = {
@@ -65,150 +76,13 @@ const setup = (quartzMe: Me) => {
     })
   )
 }
-jest.mock('src/shared/constants', () => ({CLOUD: true}))
-
-const mockPostUiproxySFDCSupport = (success: boolean) => {
-  switch (success) {
-    case true:
-      return Promise.resolve({
-        status: 204,
-        headers: {},
-        data: null,
-      })
-    case false:
-      return Promise.resolve({
-        status: 500,
-        headers: {},
-        data: null,
-      })
-  }
-}
-
-describe('PAYG Contact Support', () => {
-  afterEach(() => {
-    cleanup()
-  })
-
-  it('can open payg account Contact Support overlay', async () => {
-    const fakeAccount: Me = {
-      id: '',
-      email: '',
-      accountType: 'pay_as_you_go',
-      billingProvider: 'zuora',
-      clusterHost: 'string',
-      isRegionBeta: true,
-      regionCode: 'string',
-      regionName: 'string',
-      isOperator: false,
-      accountCreatedAt: 'string',
-    }
-    const {getByTestId} = setup(fakeAccount)
-
-    expect(getByTestId('nav-item-support')).toBeVisible()
-
-    const contactSupport = getByTestId('nav-subitem-contact-support')
-    fireEvent.click(contactSupport)
-
-    await waitFor(() => {
-      expect(screen.queryByTitle('Submit')).toBeVisible()
-    })
-
-    // subject
-    const supportSubject = getByTestId('contact-support-subject-input')
-    fireEvent.change(supportSubject, {target: {value: 'Testing Subject'}})
-
-    // severity
-    const severityDropDownButton = getByTestId('dropdown--button')
-    const severityDropDown = getByTestId('contact-support-dropdown')
-    fireEvent.click(severityDropDownButton)
-    fireEvent.click(severityDropDown)
-
-    const selectOption = await screen.findByText('1 - Critical')
-    fireEvent.click(selectOption)
-
-    // description
-    const supportDescription = getByTestId('support-description--textarea')
-    fireEvent.change(supportDescription, {
-      target: {value: 'Testing description area'},
-    })
-
-    // submit support request
-    const submitButton = getByTestId('payg-contact-support--submit')
-    expect(submitButton).not.toBeDisabled()
-
-    await mockPostUiproxySFDCSupport(true)
-    await waitFor(() => {
-      fireEvent.click(submitButton)
-    })
-
-    const confirmationOverlay = getByTestId('confirmation-overlay-header')
-    expect(confirmationOverlay).toBeVisible()
-  })
-
-  it('handles 500 error on submit', async () => {
-    const fakeAccount: Me = {
-      id: '',
-      email: '',
-      accountType: 'pay_as_you_go',
-      billingProvider: 'zuora',
-      clusterHost: 'string',
-      isRegionBeta: true,
-      regionCode: 'string',
-      regionName: 'string',
-      isOperator: false,
-      accountCreatedAt: 'string',
-    }
-    const {getByTestId} = setup(fakeAccount)
-
-    expect(getByTestId('nav-item-support')).toBeVisible()
-
-    const contactSupport = getByTestId('nav-subitem-contact-support')
-    fireEvent.click(contactSupport)
-
-    await waitFor(() => {
-      expect(screen.queryByTitle('Submit')).toBeVisible()
-    })
-
-    // subject
-    const supportSubject = getByTestId('contact-support-subject-input')
-    fireEvent.change(supportSubject, {target: {value: 'Testing Subject'}})
-
-    // severity
-    const severityDropDownButton = getByTestId('dropdown--button')
-    const severityDropDown = getByTestId('contact-support-dropdown')
-    fireEvent.click(severityDropDownButton)
-    fireEvent.click(severityDropDown)
-
-    const selectOption = await screen.findByText('1 - Critical')
-    fireEvent.click(selectOption)
-
-    // description
-    const supportDescription = getByTestId('support-description--textarea')
-    fireEvent.change(supportDescription, {
-      target: {value: 'Testing description area'},
-    })
-
-    // submit support request
-    const submitButton = getByTestId('payg-contact-support--submit')
-    expect(submitButton).not.toBeDisabled()
-
-    await mockPostUiproxySFDCSupport(false)
-    await waitFor(() => {
-      fireEvent.click(submitButton)
-    })
-
-    const [notifyCallArguments] = mocked(notify).mock.calls
-    const [notifyMessage] = notifyCallArguments
-    expect(notifyMessage).toEqual(supportRequestError())
-  })
-})
 
 describe('Free Account Contact Support', () => {
   afterEach(() => {
     cleanup()
   })
 
-  it('can open free account support overlay', async () => {
+  it('opens support overlay for free account users', async () => {
     const fakeAccount: Me = {
       id: '',
       email: '',
@@ -234,5 +108,118 @@ describe('Free Account Contact Support', () => {
       )
       expect(freeAccountOverlay).toBeVisible()
     })
+  })
+})
+
+describe('PAYG Contact Support', () => {
+  it('can submit a support request from payg account', async () => {
+    const fakeAccount: Me = {
+      id: '',
+      email: '',
+      accountType: 'pay_as_you_go',
+      billingProvider: 'zuora',
+      clusterHost: 'string',
+      isRegionBeta: true,
+      regionCode: 'string',
+      regionName: 'string',
+      isOperator: false,
+      accountCreatedAt: 'string',
+    }
+    const {getByTestId} = setup(fakeAccount)
+
+    expect(getByTestId('nav-item-support')).toBeVisible()
+
+    const contactSupport = getByTestId('nav-subitem-contact-support')
+    fireEvent.click(contactSupport)
+
+    await waitFor(() => {
+      expect(screen.queryByTitle('Submit')).toBeVisible()
+    })
+
+    // subject
+    const supportSubject = getByTestId('contact-support-subject-input')
+    fireEvent.change(supportSubject, {target: {value: 'Testing Subject'}})
+
+    // severity
+    const severityDropDownButton = getByTestId('dropdown--button')
+    const severityDropDown = getByTestId('severity-level-dropdown')
+    fireEvent.click(severityDropDownButton)
+    fireEvent.click(severityDropDown)
+
+    const selectOption = await screen.findByText('1 - Critical')
+    fireEvent.click(selectOption)
+
+    // description
+    const supportDescription = getByTestId('support-description--textarea')
+    fireEvent.change(supportDescription, {
+      target: {value: 'Testing description area'},
+    })
+
+    // submit support request
+    const submitButton = getByTestId('payg-contact-support--submit')
+    expect(submitButton).not.toBeDisabled()
+
+    await waitFor(() => {
+      fireEvent.click(submitButton)
+    })
+
+    const confirmationOverlay = getByTestId('confirmation-overlay-header')
+    expect(confirmationOverlay).toBeVisible()
+  })
+
+  it('notifies user when support request has not been successfully submitted', async () => {
+    const fakeAccount: Me = {
+      id: '',
+      email: '',
+      accountType: 'pay_as_you_go',
+      billingProvider: 'zuora',
+      clusterHost: 'string',
+      isRegionBeta: true,
+      regionCode: 'string',
+      regionName: 'string',
+      isOperator: false,
+      accountCreatedAt: 'string',
+    }
+    const {getByTestId} = setup(fakeAccount)
+
+    expect(getByTestId('nav-item-support')).toBeVisible()
+
+    const contactSupport = getByTestId('nav-subitem-contact-support')
+    fireEvent.click(contactSupport)
+
+    await waitFor(() => {
+      expect(screen.queryByTitle('Submit')).toBeVisible()
+    })
+
+    // subject
+    const supportSubject = getByTestId('contact-support-subject-input')
+    fireEvent.change(supportSubject, {target: {value: 'Testing Subject'}})
+
+    // severity
+    const severityDropDownButton = getByTestId('dropdown--button')
+    const severityDropDown = getByTestId('severity-level-dropdown')
+    fireEvent.click(severityDropDownButton)
+    fireEvent.click(severityDropDown)
+
+    const selectOption = await screen.findByText('1 - Critical')
+    fireEvent.click(selectOption)
+
+    // description
+    const supportDescription = getByTestId('support-description--textarea')
+    fireEvent.change(supportDescription, {
+      target: {value: 'Testing description area'},
+    })
+
+    // submit support request
+    const submitButton = getByTestId('payg-contact-support--submit')
+    expect(submitButton).not.toBeDisabled()
+
+    await waitFor(() => {
+      fireEvent.click(submitButton)
+    })
+
+    const [notifyCallArguments] = mocked(notify).mock.calls
+    const [notifyMessage] = notifyCallArguments
+    expect(notifyMessage).toEqual(supportRequestError())
   })
 })

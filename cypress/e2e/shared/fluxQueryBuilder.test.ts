84087d871cdd18112bbb298095d556e0dccb7a1e
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-06-14 10:51:01 -0500

    test: add then to flush, signin func

--- cypress/e2e/shared/fluxQueryBuilder.test.ts
+++ cypress/e2e/shared/fluxQueryBuilder.test.ts
@@ -2,19 +2,19 @@ import {Organization} from '../../../src/types'
 
 describe('FluxQueryBuilder', () => {
   beforeEach(() => {
-    cy.flush()
-    cy.signin()
-    cy.get('@org').then(({id}: Organization) => {
-      cy.fixture('routes').then(({orgs, explorer}) => {
-        cy.setFeatureFlags({
-          newDataExplorer: true,
-        }).then(() => {
-          cy.visit(`${orgs}/${id}${explorer}`)
-          cy.getByTestID('tree-nav').should('be.visible')
-          // Switch to Flux Query Builder
-          cy.getByTestID('flux-query-builder-toggle')
-            .should('be.visible')
-            .click()
+    cy.flush().then(() => {
+      cy.signin().then(() => {
+        cy.get('@org').then(({id}: Organization) => {
+          cy.setFeatureFlags({
+            newDataExplorer: true,
+          }).then(() => {
+            cy.visit(`/orgs/${id}/data-explorer`)
+            cy.getByTestID('tree-nav').should('be.visible')
+            // Switch to Flux Query Builder
+            cy.getByTestID('flux-query-builder-toggle')
+              .should('be.visible')
+              .click()
+          })
         })
       })
     })

1f8cb784589e38aecd8e64ae43618245b0206bbb
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-06-14 15:49:15 -0500

    test: swap set flag and get org - get org first

--- cypress/e2e/shared/fluxQueryBuilder.test.ts
+++ cypress/e2e/shared/fluxQueryBuilder.test.ts
@@ -4,10 +4,10 @@ describe('FluxQueryBuilder', () => {
   beforeEach(() => {
     cy.flush().then(() => {
       cy.signin().then(() => {
-        cy.setFeatureFlags({
-          newDataExplorer: true,
-        }).then(() => {
-          cy.get('@org').then(({id}: Organization) => {
+        cy.get('@org').then(({id}: Organization) => {
+          cy.setFeatureFlags({
+            newDataExplorer: true,
+          }).then(() => {
             cy.visit(`/orgs/${id}/data-explorer`)
             cy.getByTestID('tree-nav').should('be.visible')
             cy.wait(1000)

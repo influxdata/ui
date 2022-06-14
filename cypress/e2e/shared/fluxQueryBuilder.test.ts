0b13cf07cdb5fff569039ca9055d5a2575e55d26
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-06-14 11:03:44 -0500

    test: setFeatureFlag first then navigate to the data explorer page

--- cypress/e2e/shared/fluxQueryBuilder.test.ts
+++ cypress/e2e/shared/fluxQueryBuilder.test.ts
@@ -4,10 +4,10 @@ describe('FluxQueryBuilder', () => {
   beforeEach(() => {
     cy.flush().then(() => {
       cy.signin().then(() => {
-        cy.get('@org').then(({id}: Organization) => {
-          cy.setFeatureFlags({
-            newDataExplorer: true,
-          }).then(() => {
+        cy.setFeatureFlags({
+          newDataExplorer: true,
+        }).then(() => {
+          cy.get('@org').then(({id}: Organization) => {
             cy.visit(`/orgs/${id}/data-explorer`)
             cy.getByTestID('tree-nav').should('be.visible')
             // Switch to Flux Query Builder

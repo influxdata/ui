0c85aa0c6b3ef7403858f4a8be67a68010184a85
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-06-14 15:28:44 -0500

    test: add wait

--- cypress/e2e/shared/fluxQueryBuilder.test.ts
+++ cypress/e2e/shared/fluxQueryBuilder.test.ts
@@ -10,6 +10,7 @@ describe('FluxQueryBuilder', () => {
           cy.get('@org').then(({id}: Organization) => {
             cy.visit(`/orgs/${id}/data-explorer`)
             cy.getByTestID('tree-nav').should('be.visible')
+            cy.wait(1000)
             // Switch to Flux Query Builder
             cy.getByTestID('flux-query-builder-toggle')
               .should('be.visible')

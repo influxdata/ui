a0274adb02321d776c230964c52ff28716789525
Bethelhem Legesse <legessebl@gmail.com>
2022-06-15 10:39:19 -0400

    test: integration test for help bar (#4758)

--- src/flows/index.ts
+++ src/flows/index.ts
@@ -11,12 +11,14 @@ export const UNPROCESSED_PANEL_TEXT =
   'This cell will display results from the previous cell after selecting Run.'
 
 export const PIPE_DEFINITIONS: TypeLookup = {}
-export const PROJECT_NAME: string = 'Notebook'
-export const DEFAULT_PROJECT_NAME: string = `Untitled ${PROJECT_NAME}`
-export const PROJECT_NAME_SHORT: string = 'Books'
-export const PROJECT_NAME_PLURAL: string = `${PROJECT_NAME}s`
-
 export const NOTEBOOKS_DOCUMENTATION_LINK = `https://docs.influxdata.com/influxdb/${DOCS_URL_VERSION}/notebooks/create-notebook/`
+
+export {
+  PROJECT_NAME,
+  DEFAULT_PROJECT_NAME,
+  PROJECT_NAME_SHORT,
+  PROJECT_NAME_PLURAL,
+} from 'src/flows/constants'
 
 // NOTE: this loads in all the modules under the current directory
 // to make it easier to add new types

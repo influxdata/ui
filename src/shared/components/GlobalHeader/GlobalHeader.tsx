a1b269e013d43e23c4ec5f64f404a441a3d905d8
ChitlangeSahas <chitlangesahas@gmail.com>
2022-06-24 09:21:45 -0700

    feat: half of dropdown works

--- src/shared/components/GlobalHeader/GlobalHeader.tsx
+++ src/shared/components/GlobalHeader/GlobalHeader.tsx
@@ -3,6 +3,7 @@ import {ComponentSize, FlexBox, Icon, IconFont, JustifyContent, SelectDropdown} 
 import {UserAccountContext} from '../../../accounts/context/userAccount'
 import {useSelector} from 'react-redux'
 import {AppState, ResourceType} from '../../../types'
+import {MenuDropdown, SubMenuItem} from '@influxdata/clockface'
 
 const globalHeaderStyle = {
   padding: '0 32px 0 32px',
@@ -12,16 +13,46 @@ const globalHeaderStyle = {
 
 const GlobalHeader = () => {
   const {userAccounts} = useContext(UserAccountContext)
-  const [selectedOption, setSelectedOption] = useState('Loading...')
+  const [selectedOption, setSelectedOption] = useState({name: 'Loading...' , id : ''} as SubMenuItem)
 
-  const userOrgs = useSelector((state :AppState) => state.resources[ResourceType.Orgs])
-  console.log(userOrgs)
-  const options = userAccounts?.map(acct => acct.name) || []
+  const options = userAccounts?.map(acct => {return {name: acct.name, id: acct.id.toString()} as SubMenuItem}) || []
 
+  console.log(userAccounts)
   useEffect(() => {
-    const initialPick = userAccounts?.filter(acct => acct.isActive)[0].name
-    setSelectedOption(initialPick)
+    const initialPick = userAccounts?.filter(acct => acct.isActive)[0]
+    setSelectedOption({name: initialPick?.name, id: initialPick?.id.toString()})
   }, [userAccounts])
+
+  const menuHrefOptions = [
+    {
+      name: 'Settings',
+      iconFont: IconFont.CogOutline,
+      href: '/settings',
+    },
+    {
+      name: 'Members',
+      iconFont: IconFont.UserOutline_New,
+      href: '/members',
+    },
+    {
+      name: 'Billing',
+      iconFont: IconFont.Bill,
+      href: '/billing',
+    },
+  ]
+
+
+  const accountDropdown = <MenuDropdown
+    largeListSearch={true}
+    largeListCeiling={15}
+    selectedOption={selectedOption}
+    options={menuHrefOptions}
+    subMenuOptions={options}
+    menuHeaderIcon={IconFont.Switch_New}
+    menuHeaderText={'Switch Account'}
+    style={{width: '110px'}}
+    menuStyle={{width: '250px'}}
+  />
 
     return (
       <FlexBox
@@ -30,7 +61,8 @@ const GlobalHeader = () => {
         style={globalHeaderStyle}
       >
         <FlexBox margin={ComponentSize.Medium}>
-          <SelectDropdown selectedOption={selectedOption} options={options} onSelect={setSelectedOption} />
+
+          {accountDropdown}
           <Icon glyph={IconFont.CaretRight}></Icon>
           <div>Org Dropdown</div>
         </FlexBox>

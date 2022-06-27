8d03bab1ea787729a01f09aa4a33b124d983e1ff
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-06-27 14:18:35 -0500

    chore: remove the front-end injection for bucket and time range

--- src/shared/contexts/editor/index.tsx
+++ src/shared/contexts/editor/index.tsx
@@ -40,7 +40,6 @@ export interface EditorContextType {
     cmd: ExecuteCommand,
     data: Omit<ExecuteCommandArgument, 'textDocument'>
   ) => void
-  injectDataSourceAndTimeRange: (dataSource: Bucket) => void
 }
 
 const DEFAULT_CONTEXT: EditorContextType = {
@@ -50,7 +49,6 @@ const DEFAULT_CONTEXT: EditorContextType = {
   injectFunction: (_, __) => {},
   injectVariable: (_, __) => {},
   injectViaLsp: (_, __) => {},
-  injectDataSourceAndTimeRange: _ => {},
 }
 
 export const EditorContext = createContext<EditorContextType>(DEFAULT_CONTEXT)
@@ -139,30 +137,6 @@ export const EditorProvider: FC = ({children}) => {
     [editor]
   )
 
-  const injectDataSourceAndTimeRange = useCallback(
-    (dataSource: Bucket): void => {
-      let _source = ''
-      if (dataSource.type === 'sample') {
-        _source += SAMPLE_DATA_SET(dataSource.id)
-      } else {
-        _source += FROM_BUCKET(dataSource.name)
-      }
-
-      const text = `${_source}\n  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)`
-
-      const type = InjectionType.OnOwnLine
-
-      const options = {
-        text,
-        type,
-        triggerSuggest: false,
-        cbParentOnUpdateText: _ => {},
-      }
-      inject(options)
-    },
-    [inject, editor]
-  )
-
   const injectFunction = useCallback(
     (
       rawFn: FluxToolbarFunction | FluxFunction,
@@ -222,7 +196,6 @@ export const EditorProvider: FC = ({children}) => {
         injectFunction,
         injectVariable,
         injectViaLsp,
-        injectDataSourceAndTimeRange,
       }}
     >
       {children}

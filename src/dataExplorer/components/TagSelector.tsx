6383679f4aa803e5156ec0f73a2e35b144cc530c
asalem <ariel.salem1989@gmail.com>
2022-07-01 05:32:52 -0700

    chore: add events for tag and field selection in the view layer

--- src/dataExplorer/components/TagSelector.tsx
+++ src/dataExplorer/components/TagSelector.tsx
@@ -17,6 +17,7 @@ import {
   LOAD_MORE_LIMIT_INITIAL,
   LOAD_MORE_LIMIT,
 } from 'src/dataExplorer/shared/utils'
+import {event} from 'src/cloud/utils/reporting'
 
 const TAG_KEYS_TOOLTIP = `Tags and Tag Values are indexed key values \
 pairs within a measurement. For SQL users, this is conceptually \
@@ -29,7 +30,7 @@ interface Prop {
 }
 
 const TagValues: FC<Prop> = ({loading, tagKey, tagValues}) => {
-  const {selectedBucket, selectedMeasurement, selectTagValue} = useContext(
+  const {selectedBucket, selectedMeasurement, selectTagValue, searchTerm} = useContext(
     FluxQueryBuilderContext
   )
   const {getTagValues} = useContext(TagsContext)
@@ -42,6 +43,7 @@ const TagValues: FC<Prop> = ({loading, tagKey, tagValues}) => {
 
   const handleSelectTagValue = (tagValue: string) => {
     // Inject tag key and value into editor
+    event('handleSelectTagValue')
     selectTagValue(tagKey, tagValue)
   }
 
@@ -52,6 +54,7 @@ const TagValues: FC<Prop> = ({loading, tagKey, tagValues}) => {
       return
     }
 
+    event('handleSelectTagKey', {searchTerm: searchTerm.length})
     getTagValues(selectedBucket, selectedMeasurement, key)
   }
 

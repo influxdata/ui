ce1736ba3155b38a2b8bd7a013199ad8cb89b983
wiedld <dlw405@gmail.com>
2022-07-15 14:12:00 -0700

    fix(5062): don't take up option v namespace, when don't have variables to inject

--- src/variables/utils/buildVarsOption.ts
+++ src/variables/utils/buildVarsOption.ts
@@ -2,7 +2,13 @@
 import {OPTION_NAME} from 'src/variables/constants'
 
 // Types
-import {File, Property, VariableAssignment, Variable} from 'src/types'
+import {
+  File,
+  Statement,
+  Property,
+  VariableAssignment,
+  Variable,
+} from 'src/types'
 
 // Utils
 import {filterUnusedVarsBasedOnQuery} from 'src/shared/utils/filterUnusedVars'
@@ -28,27 +34,33 @@ export const buildUsedVarsOption = (
   return buildVarsOption([...filteredAssignmentVars, ...(windowVars ?? [])])
 }
 
-export const buildVarsOption = (variables: VariableAssignment[]): File => ({
-  type: 'File',
-  package: null,
-  imports: null,
-  body: [
-    {
-      type: 'OptionStatement',
-      assignment: {
-        type: 'VariableAssignment',
-        id: {
-          type: 'Identifier',
-          name: OPTION_NAME,
+export const buildVarsOption = (variables: VariableAssignment[]): File => {
+  const body: Statement[] = !variables.length
+    ? []
+    : [
+        {
+          type: 'OptionStatement',
+          assignment: {
+            type: 'VariableAssignment',
+            id: {
+              type: 'Identifier',
+              name: OPTION_NAME,
+            },
+            init: {
+              type: 'ObjectExpression',
+              properties: variables.filter(v => !!v).map(assignmentToProperty),
+            },
+          },
         },
-        init: {
-          type: 'ObjectExpression',
-          properties: variables.filter(v => !!v).map(assignmentToProperty),
-        },
-      },
-    },
-  ],
-})
+      ]
+
+  return {
+    type: 'File',
+    package: null,
+    imports: null,
+    body,
+  }
+}
 
 const assignmentToProperty = (variable: VariableAssignment): Property => {
   return {

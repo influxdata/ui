1304c29ba1fa223cc3995a4956d85ce91e3cf5f9
Subir Jolly <sjolly@influxdata.com>
2022-07-14 20:19:02 -0400

    chore: remove unused code

--- src/writeData/subscriptions/components/BrokerDetails.tsx
+++ src/writeData/subscriptions/components/BrokerDetails.tsx
@@ -1,37 +1,21 @@
 // Libraries
-import React, {FC, useContext} from 'react'
-import {useHistory} from 'react-router-dom'
-import {useSelector} from 'react-redux'
+import React, {FC} from 'react'
 
 // Components
 import {
-  Button,
   Form,
   Overlay,
-  ButtonType,
-  ComponentColor,
-  ComponentStatus,
   Heading,
   HeadingElement,
   FontWeight,
   SpinnerContainer,
   TechnoSpinner,
-  FlexBox,
-  FlexDirection,
-  ComponentSize,
-  JustifyContent,
 } from '@influxdata/clockface'
-import StatusHeader from 'src/writeData/subscriptions/components/StatusHeader'
 import BrokerFormContent from 'src/writeData/subscriptions/components/BrokerFormContent'
 
 // Utils
-import {getOrg} from 'src/organizations/selectors'
-import {event} from 'src/cloud/utils/reporting'
-import {AppSettingContext} from 'src/shared/contexts/app'
-import {checkRequiredFields} from 'src/writeData/subscriptions/utils/form'
 
 // Types
-import {SUBSCRIPTIONS, LOAD_DATA} from 'src/shared/constants/routes'
 import {Subscription} from 'src/types/subscriptions'
 
 // Styles

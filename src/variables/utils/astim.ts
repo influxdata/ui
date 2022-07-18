ab22220c6a1079c2c90068ebe069cdb5a7d921a1
wiedld <dlw405@gmail.com>
2022-07-18 13:03:51 -0700

    chore(5062): provide more debugging information when parseASTIM fails

--- src/variables/utils/astim.ts
+++ src/variables/utils/astim.ts
@@ -23,7 +23,7 @@ export const parseASTIM = (query: string): ASTIM => {
   try {
     ast = parse(query)
   } catch (e) {
-    console.error(e)
+    console.error(`ERROR: parseASTIM errored on script ${query} with error:`, e)
   }
   const variables: MemberExpression[] = ast ? parseAllVariables(ast) : []
   const variableNames = new Set()

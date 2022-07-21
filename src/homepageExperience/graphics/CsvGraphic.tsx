e0c11f21f2922764c0ed0ae4a0554475268cc18a
David R <drusnak@influxdata.com>
2022-07-21 16:03:05 -0400

    feat: add write data step to cli onboarding

---
+++ src/homepageExperience/graphics/CsvGraphic.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import sampleCsv from 'assets/images/sample-csv.png'
+
+export const CsvGraphic = () => {
+  return <img style={{width: '100%', marginBottom: '48px'}} src={sampleCsv} />
+}

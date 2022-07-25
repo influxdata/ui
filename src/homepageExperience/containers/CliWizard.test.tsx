96dbc7200a10b9e2a5b82b0174c9d117d60ea9d9
David R <drusnak@influxdata.com>
2022-07-25 16:18:43 -0400

    fix: add await to tests that need it

--- src/homepageExperience/containers/CliWizard.test.tsx
+++ src/homepageExperience/containers/CliWizard.test.tsx
@@ -22,13 +22,13 @@ describe('Navigation', () => {
   describe('Next and Previous Buttons', () => {
     it('cannot click next on final step', async () => {
       setup()
-      fireEvent.click(screen.getByText('Finished!'))
+      await fireEvent.click(screen.getByText('Finished!'))
       const nextButton = screen.getByTestId('cli-next-button')
       expect(nextButton).toHaveAttribute('disabled')
     })
     it('cannot click previous on first step', async () => {
       setup()
-      fireEvent.click(screen.getByText('Overview'))
+      await fireEvent.click(screen.getByText('Overview'))
       const prevButton = screen.getByTestId('cli-prev-button')
       expect(prevButton).toHaveAttribute('disabled')
     })

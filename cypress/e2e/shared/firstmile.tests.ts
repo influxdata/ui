7366ad8d3ac4df4fa51177d4ecc1ac5a0b75fee1
David R <drusnak@influxdata.com>
2022-07-29 09:28:52 -0400

    chore: removed for loop in test for flakiness

--- cypress/e2e/shared/firstmile.tests.ts
+++ cypress/e2e/shared/firstmile.tests.ts
@@ -1,86 +1,84 @@
-for (let i = 0; i < 20; i++) {
-  describe('First mile experience', () => {
+describe('First mile experience', () => {
+  beforeEach(() => {
+    cy.flush()
+    cy.signin()
+    cy.setFeatureFlags({firstMile: true})
+  })
+
+  it('should load the main home-page that has the language tiles', function() {
+    cy.getByTestID('home-page--header').should('exist')
+  })
+
+  describe('Python', () => {
     beforeEach(() => {
-      cy.flush()
-      cy.signin()
-      cy.setFeatureFlags({firstMile: true})
+      cy.getByTestID('homepage-wizard-language-tile--python').click()
+    })
+    it("runs through the python's wizard pages", function() {
+      cy.getByTestID('subway-nav').should('exist')
+
+      // first page is overview
+      cy.contains('Hello, Time-Series World!')
+
+      // second page is install dependencies
+      cy.getByTestID('python-next-button').click()
+      cy.contains('Install Dependencies')
+
+      // third page is create token
+      cy.getByTestID('python-next-button').click()
+      cy.contains('Tokens')
+
+      // fourth page is initalize client
+      cy.getByTestID('python-next-button').click()
+      cy.contains('Initialize Client')
+
+      // fifth page is write data
+      cy.getByTestID('python-next-button').click()
+      cy.contains('Write Data')
+
+      // sixth page is execute query
+      cy.getByTestID('python-next-button').click()
+      cy.contains('Execute a Flux Query')
+
+      // seventh page is execute aggregate query
+      cy.getByTestID('python-next-button').click()
+      cy.contains('Execute an Aggregate Query')
+
+      // eight page is finish
+      cy.getByTestID('python-next-button').click()
+      cy.contains('Congrats!')
     })
 
-    it('should load the main home-page that has the language tiles', function() {
-      cy.getByTestID('home-page--header').should('exist')
-    })
+    describe('Subway Nav Bar', () => {
+      it('navigates to the correct page when the respective navigation button is clicked', function() {
+        cy.get('h1').contains('Hello, Time-Series World!')
 
-    describe('Python', () => {
-      beforeEach(() => {
-        cy.getByTestID('homepage-wizard-language-tile--python').click()
-      })
-      it("runs through the python's wizard pages", function() {
-        cy.getByTestID('subway-nav').should('exist')
+        cy.getByTestID('python-prev-button').should('be.disabled')
 
-        // first page is overview
-        cy.contains('Hello, Time-Series World!')
+        cy.contains(/^Install Dependencies$/).click()
+        cy.get('h1').contains('Install Dependencies')
 
-        // second page is install dependencies
-        cy.getByTestID('python-next-button').click()
-        cy.contains('Install Dependencies')
+        cy.contains('Previous').should('not.be.disabled')
 
-        // third page is create token
-        cy.getByTestID('python-next-button').click()
-        cy.contains('Tokens')
+        cy.contains(/^Tokens$/).click()
+        cy.get('h1').contains('Tokens')
 
-        // fourth page is initalize client
-        cy.getByTestID('python-next-button').click()
-        cy.contains('Initialize Client')
+        cy.contains(/^Initialize Client$/).click()
+        cy.get('h1').contains('Initialize Client')
 
-        // fifth page is write data
-        cy.getByTestID('python-next-button').click()
-        cy.contains('Write Data')
+        cy.contains(/^Write Data$/).click()
+        cy.get('h1').contains('Write Data')
 
-        // sixth page is execute query
-        cy.getByTestID('python-next-button').click()
+        cy.contains(/^Execute a Simple Query$/).click()
         cy.contains('Execute a Flux Query')
 
-        // seventh page is execute aggregate query
-        cy.getByTestID('python-next-button').click()
-        cy.contains('Execute an Aggregate Query')
+        cy.contains(/^Execute an Aggregate Query$/).click()
+        cy.get('h1').contains('Execute an Aggregate Query')
 
-        // eight page is finish
-        cy.getByTestID('python-next-button').click()
+        cy.contains(/^Finish$/).click()
         cy.contains('Congrats!')
-      })
 
-      describe('Subway Nav Bar', () => {
-        it('navigates to the correct page when the respective navigation button is clicked', function() {
-          cy.get('h1').contains('Hello, Time-Series World!')
-
-          cy.getByTestID('python-prev-button').should('be.disabled')
-
-          cy.contains(/^Install Dependencies$/).click()
-          cy.get('h1').contains('Install Dependencies')
-
-          cy.contains('Previous').should('not.be.disabled')
-
-          cy.contains(/^Tokens$/).click()
-          cy.get('h1').contains('Tokens')
-
-          cy.contains(/^Initialize Client$/).click()
-          cy.get('h1').contains('Initialize Client')
-
-          cy.contains(/^Write Data$/).click()
-          cy.get('h1').contains('Write Data')
-
-          cy.contains(/^Execute a Simple Query$/).click()
-          cy.contains('Execute a Flux Query')
-
-          cy.contains(/^Execute an Aggregate Query$/).click()
-          cy.get('h1').contains('Execute an Aggregate Query')
-
-          cy.contains(/^Finish$/).click()
-          cy.contains('Congrats!')
-
-          cy.getByTestID('python-next-button').should('be.disabled')
-        })
+        cy.getByTestID('python-next-button').should('be.disabled')
       })
     })
   })
-}
+})

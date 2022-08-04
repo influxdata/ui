e60c11000dc8947050c8fe7f5c4d5cad94091eef
Beth Legesse <legessebl@gmail.com>
2022-08-04 14:28:35 -0500

    feat: use eventName property to fire event

--- src/me/components/DocSearchWidget.tsx
+++ src/me/components/DocSearchWidget.tsx
@@ -18,28 +18,38 @@ const supportLinks = [
   {
     link: `https://docs.influxdata.com/influxdb/${DOCS_URL_VERSION}/query-data/get-started/`,
     title: 'Get Started with Flux',
+    eventName: 'QueryData',
   },
   {
     link: `https://docs.influxdata.com/influxdb/${DOCS_URL_VERSION}/query-data/execute-queries/data-explorer/`,
     title: 'Explore Metrics',
+    eventName: 'DataExplorer',
   },
   {
     link: `https://docs.influxdata.com/influxdb/${DOCS_URL_VERSION}/visualize-data/dashboards/`,
     title: 'Build a Dashboard',
+    eventName: 'Dashboards',
   },
   {
     link: `https://docs.influxdata.com/influxdb/${DOCS_URL_VERSION}/process-data/get-started/`,
     title: 'Write a Task',
+    eventName: 'WriteTask',
   },
   {
     link: 'https://github.com/influxdata/ui/issues/new',
     title: 'Report a bug',
+    eventName: 'GitBugs',
   },
-  {link: 'https://community.influxdata.com', title: 'Community Forum'},
+  {
+    link: 'https://community.influxdata.com',
+    title: 'Community Forum',
+    eventName: 'Community',
+  },
   {
     link:
       'https://github.com/influxdata/influxdb/issues/new?template=feature_request.md',
     title: 'Feature Requests',
+    eventName: 'FeatureRequest',
   },
 ]
 
@@ -47,12 +57,12 @@ if (CLOUD && isFlagEnabled('requestPoc')) {
   supportLinks.push({
     link: 'https://www.influxdata.com/proof-of-concept/',
     title: 'Request Proof of Concept',
+    eventName: 'POC',
   })
 }
 
 const handleEventing = eventName => {
-  const normalizedEventName = normalizeEventName(eventName)
-  event(`HomePage.${normalizedEventName}.clicked`)
+  event(`HomePage.${eventName}.clicked`)
 }
 
 const DocSearchWidget: FC = () => {
@@ -63,13 +73,13 @@ const DocSearchWidget: FC = () => {
       <div className="useful-links">
         <h4 style={{textTransform: 'uppercase'}}>Useful Links</h4>
         <ul className="docslinks-list">
-          {supportLinks.map(({link, title}) => (
+          {supportLinks.map(({link, title, eventName}) => (
             <li key={title}>
               <a
                 href={link}
                 target="_blank"
                 rel="noreferrer"
-                onClick={() => handleEventing(title)}
+                onClick={() => handleEventing(eventName)}
               >
                 {title}
               </a>

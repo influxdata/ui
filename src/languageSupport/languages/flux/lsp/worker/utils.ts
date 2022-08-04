cb24d9a52151cbe22b1880a0b1575ad0aad194e6
wiedld <wiedld@users.noreply.github.com>
2022-08-04 12:00:45 -0700

    fix(5073): enable handling of empty LSP responses (#5299)

--- src/languageSupport/languages/flux/lsp/worker/utils.ts
+++ src/languageSupport/languages/flux/lsp/worker/utils.ts
@@ -1,5 +1,9 @@
 export const respond = (msg, cb) => {
   try {
+    // LSP spec permits a blank response
+    if (!msg) {
+      return
+    }
     const d = JSON.parse(msg)
     cb(d)
   } catch (e) {

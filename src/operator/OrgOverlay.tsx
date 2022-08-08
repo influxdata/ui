d43652bf235ea108299c5ae854a176375084aa0d
David R <drusnak@influxdata.com>
2022-08-08 15:54:55 -0400

    refactor: make tools url an exported const

--- src/operator/OrgOverlay.tsx
+++ src/operator/OrgOverlay.tsx
@@ -31,6 +31,9 @@ import {fromDisplayLimits} from 'src/operator/utils'
 // Components
 import LimitsField from 'src/operator/LimitsField'
 
+// Constants
+import {TOOLS_URL} from 'src/shared/constants'
+
 const OrgOverlay: FC = () => {
   const {
     limits,
@@ -108,7 +111,7 @@ const OrgOverlay: FC = () => {
                       text="View Usage Dashboard"
                       target={LinkTarget.Blank}
                       className="overlay-button--link"
-                      href={`https://us-east-1-2.aws.cloud2.influxdata.com/orgs/5d59ccc5163fc318/dashboards/0988da0fd78a7003?vars%5Borgid%5D=${orgID}`}
+                      href={`${TOOLS_URL}orgs/5d59ccc5163fc318/dashboards/0988da0fd78a7003?vars%5Borgid%5D=${orgID}`}
                     />
                   </Grid.Column>
                 </Grid.Row>

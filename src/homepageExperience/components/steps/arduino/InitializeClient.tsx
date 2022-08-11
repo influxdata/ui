b6b1f85d6babff50fd43af9971c37f2f305c9d0f
David R <drusnak@influxdata.com>
2022-08-11 15:33:18 -0400

    feat: add init client page to arduino wizard

--- src/homepageExperience/components/steps/arduino/InitializeClient.tsx
+++ src/homepageExperience/components/steps/arduino/InitializeClient.tsx
@@ -7,17 +7,24 @@ import {
   createAuthorization,
   getAllResources,
 } from 'src/authorizations/actions/thunks'
+import {getBuckets} from 'src/buckets/actions/thunks'
+
+// Components
+import CodeSnippet from 'src/shared/components/CodeSnippet'
+import {
+  Columns,
+  ComponentSize,
+  Grid,
+  InfluxColors,
+  Panel,
+} from '@influxdata/clockface'
+import WriteDataHelperBuckets from 'src/writeData/components/WriteDataHelperBuckets'
+import {WriteDataDetailsContext} from 'src/writeData/components/WriteDataDetailsContext'
 
 // Selectors
 import {getOrg} from 'src/organizations/selectors'
 import {getMe} from 'src/me/selectors'
 import {getAllTokensResources} from 'src/resources/selectors'
-
-// Thunks
-import {getBuckets} from 'src/buckets/actions/thunks'
-
-// Helper Components
-import {WriteDataDetailsContext} from 'src/writeData/components/WriteDataDetailsContext'
 
 // Utils
 import {allAccessPermissions} from 'src/authorizations/utils/permissions'
@@ -26,9 +33,6 @@ import {keyboardCopyTriggered, userSelection} from 'src/utils/crossPlatform'
 
 // Types
 import {AppState, Authorization} from 'src/types'
-
-// Styles
-import './ArduinoSteps.scss'
 
 type OwnProps = {
   setTokenValue: (tokenValue: string) => void
@@ -47,31 +51,28 @@ export const InitializeClient: FC<OwnProps> = ({
   const me = useSelector(getMe)
   const allPermissionTypes = useSelector(getAllTokensResources)
   const dispatch = useDispatch()
-
+  const url =
+    me.quartzMe?.clusterHost || 'https://us-west-2-1.aws.cloud2.influxdata.com/'
   const currentAuth = useSelector((state: AppState) => {
     return state.resources.tokens.currentAuth.item
   })
+  const token = currentAuth.token
 
   const sortedPermissionTypes = useMemo(
     () => allPermissionTypes.sort((a, b) => collator.compare(a, b)),
     [allPermissionTypes]
   )
+
   const {bucket} = useContext(WriteDataDetailsContext)
 
   useEffect(() => {
     dispatch(getBuckets())
+    dispatch(getAllResources())
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
     onSelectBucket(bucket.name)
-  }, [bucket.name, onSelectBucket])
-
-  useEffect(() => {
-    const fetchResources = async () => {
-      await dispatch(getAllResources())
-    }
-    fetchResources()
-  }, [])
+  }, [bucket, onSelectBucket])
 
   useEffect(() => {
     if (sortedPermissionTypes.length && tokenValue === null) {
@@ -95,11 +96,7 @@ export const InitializeClient: FC<OwnProps> = ({
 
   useEffect(() => {
     const fireKeyboardCopyEvent = event => {
-      if (
-        keyboardCopyTriggered(event) &&
-        (userSelection().includes('influx config create') ||
-          userSelection().includes('influx bucket create'))
-      ) {
+      if (keyboardCopyTriggered(event) && userSelection().includes('#define')) {
         logCopyCodeSnippet()
       }
     }
@@ -107,10 +104,102 @@ export const InitializeClient: FC<OwnProps> = ({
     return () => document.removeEventListener('keydown', fireKeyboardCopyEvent)
   }, [])
 
+  const codeSnippet = `#if defined(ESP32)
+  #include <WiFiMulti.h>
+  WiFiMulti wifiMulti;
+  #define DEVICE "ESP32"
+  #elif defined(ESP8266)
+  #include <ESP8266WiFiMulti.h>
+  ESP8266WiFiMulti wifiMulti;
+  #define DEVICE "ESP8266"
+  #endif
+  
+  #include <InfluxDbClient.h>
+  #include <InfluxDbCloud.h>
+  
+  // WiFi AP SSID
+  #define WIFI_SSID "YOUR_WIFI_SSID"
+  // WiFi password
+  #define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+  
+  #define INFLUXDB_URL "${url}"
+  #define INFLUXDB_TOKEN "${token}"
+  #define INFLUXDB_ORG "${org.id}"
+  #define INFLUXDB_BUCKET "${
+    bucket.name === '<BUCKET>' ? 'YOUR_BUCKET' : bucket.name
+  }"
+  
+  // Time zone info
+  #define TZ_INFO "UTC−07:00"
+  
+  // Declare InfluxDB client instance with preconfigured InfluxCloud certificate
+  InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
+  
+  // Declare Data point
+  Point sensor("wifi_status");
+  
+  void setup() {
+    Serial.begin(115200);
+  
+    // Setup wifi
+    WiFi.mode(WIFI_STA);
+    wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  
+    Serial.print("Connecting to wifi");
+    while (wifiMulti.run() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(100);
+    }
+    Serial.println();
+  
+    // Accurate time is necessary for certificate validation and writing in batches
+    // We use the NTP servers in your area as provided by: https://www.pool.ntp.org/zone/
+    // Syncing progress and the time will be printed to Serial.
+    timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+  
+  
+    // Check server connection
+    if (client.validateConnection()) {
+      Serial.print("Connected to InfluxDB: ");
+      Serial.println(client.getServerUrl());
+    } else {
+      Serial.print("InfluxDB connection failed: ");
+      Serial.println(client.getLastErrorMessage());
+    }
+  }`
+
   // Events log handling
   const logCopyCodeSnippet = () => {
-    event(`firstMile.arduinoWizard.buckets.code.copied`)
+    event(`firstMile.arduinoWizard.buckets.code.copied`) // edit
   }
 
-  return <h1>Initialize Client</h1>
+  return (
+    <>
+      <h1>Initialize Client</h1>
+      <p>Select or create a bucket to write data to.</p>
+      <Panel backgroundColor={InfluxColors.Grey15}>
+        <Panel.Body size={ComponentSize.ExtraSmall}>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column widthSM={Columns.Twelve}>
+                <WriteDataHelperBuckets useSimplifiedBucketForm={true} />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Panel.Body>
+      </Panel>
+      <p className="small-margins">
+        Paste the following snippet into a blank Arduino sketch file.
+      </p>
+      <CodeSnippet
+        text={codeSnippet}
+        onCopy={logCopyCodeSnippet}
+        language="arduino"
+      />
+      <p style={{fontSize: '14px', marginTop: '8px', marginBottom: '48px'}}>
+        Note: you will need to set the WIFI_SSID and WIFI_PASSWORD variables to
+        the correct values for your wifi router.
+      </p>
+    </>
+  )
 }

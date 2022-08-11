d50f34f63a81c8a0d2b5353c972ffd75282fd484
alex wang <alexwang@alexs-MacBook-Pro.local>
2022-08-11 13:14:09 -0700

    feat: added ExecuteAggregateQuery framework and ExecuteQuery framework

--- src/homepageExperience/components/steps/arduino/ExecuteAggregateQuery.tsx
+++ src/homepageExperience/components/steps/arduino/ExecuteAggregateQuery.tsx
@@ -1,5 +1,6 @@
 // Libraries
 import React, {useEffect} from 'react'
+import CodeSnippet from 'src/shared/components/CodeSnippet'
 
 // Utils
 import {event} from 'src/cloud/utils/reporting'
@@ -15,6 +16,61 @@ type OwnProps = {
 
 export const ExecuteAggregateQuery = (props: OwnProps) => {
   const {bucket} = props
+
+  const codeSnippet = `void loop() {
+    // ... code from Write Data step
+    
+    // Query will find the min RSSI value for last minute for each connected WiFi network with this device
+      String query = "from(bucket: \"apis\")\n\
+    |> range(start: -1m)\n\
+    |> filter(fn: (r) => r._measurement == \"wifi_status\")\n\
+    |> min()";
+    
+      // Print composed query
+      Serial.println("Querying for the mean RSSI value written to the \"apis\" bucket in the last 1 min... ");
+      Serial.println(query);
+    
+      // Send query to the server and get result
+      FluxQueryResult result = client.query(query);
+    
+      Serial.println("Result : ");
+      // Iterate over rows.
+      while (result.next()) {
+        // Get converted value for flux result column 'SSID'
+        String ssid = result.getValueByName("SSID").getString();
+        Serial.print("SSID '");
+        Serial.print(ssid);
+    
+        Serial.print("' with RSSI ");
+        // Get value of column named '_value'
+        long value = result.getValueByName("_value").getLong();
+        Serial.print(value);
+    
+        // Get value for the _time column
+        FluxDateTime time = result.getValueByName("_time").getDateTime();
+    
+        String timeStr = time.format("%F %T");
+    
+        Serial.print(" at ");
+        Serial.print(timeStr);
+    
+        Serial.println();
+      }
+    
+      // Report any error
+      if (result.getError() != "") {
+        Serial.print("Query result error: ");
+        Serial.println(result.getError());
+      }
+    
+      // Close the result
+      result.close();
+    
+      Serial.println("==========");
+    
+      delay(5000);
+    
+    }`
 
   useEffect(() => {
     const fireKeyboardCopyEvent = event => {
@@ -32,6 +88,16 @@ export const ExecuteAggregateQuery = (props: OwnProps) => {
   return (
     <>
       <h1>Execute a Flux Aggregate Query on {bucket}</h1>
+      <p className="small-margins">
+        In this example, we use the{' '}
+        <code className="homepage-wizard--code-highlight">mean()</code> function
+        to calculate the average value of data points in the last 1 minute.
+      </p>
+      <CodeSnippet
+        text={codeSnippet}
+        onCopy={logCopyCodeSnippet}
+        language="properties"
+      />
     </>
   )
 }
--- src/homepageExperience/components/steps/arduino/ExecuteQuery.tsx
+++ src/homepageExperience/components/steps/arduino/ExecuteQuery.tsx
@@ -3,6 +3,7 @@ import React, {useEffect} from 'react'
 
 // Utils
 import {event} from 'src/cloud/utils/reporting'
+import CodeSnippet from 'src/shared/components/CodeSnippet'
 
 import {keyboardCopyTriggered, userSelection} from 'src/utils/crossPlatform'
 
@@ -16,6 +17,59 @@ type OwnProps = {
 
 export const ExecuteQuery = (props: OwnProps) => {
   const {bucket} = props
+
+  const codeSnippet = `void loop() {
+    // ... code from Write Data step 
+    
+    // Query will find the RSSI values for last minute for each connected WiFi network with this device
+     String query = "from(bucket: \"apis\")\n\
+   |> range(start: -1m)\n\
+   |> filter(fn: (r) => r._measurement == \"wifi_status\" and r._field == \"rssi\")";
+   
+     // Print composed query
+     Serial.println("Querying for RSSI values written to the \"apis\" bucket in the last 1 min... ");
+     Serial.println(query);
+   
+     // Send query to the server and get result
+     FluxQueryResult result = client.query(query);
+   
+     Serial.println("Results : ");
+     // Iterate over rows.
+     while (result.next()) {
+       // Get converted value for flux result column 'SSID'
+       String ssid = result.getValueByName("SSID").getString();
+       Serial.print("SSID '");
+       Serial.print(ssid);
+   
+       Serial.print("' with RSSI ");
+       // Get value of column named '_value'
+       long value = result.getValueByName("_value").getLong();
+       Serial.print(value);
+   
+       // Get value for the _time column
+       FluxDateTime time = result.getValueByName("_time").getDateTime();
+   
+       String timeStr = time.format("%F %T");
+   
+       Serial.print(" at ");
+       Serial.print(timeStr);
+   
+       Serial.println();
+     }
+   
+     // Report any error
+     if (result.getError() != "") {
+       Serial.print("Query result error: ");
+       Serial.println(result.getError());
+     }
+   
+     // Close the result
+     result.close();
+   
+     Serial.println("==========");
+   
+     delay(5000);
+   }`
 
   useEffect(() => {
     const fireKeyboardCopyEvent = event => {
@@ -33,6 +87,17 @@ export const ExecuteQuery = (props: OwnProps) => {
   return (
     <>
       <h1>Execute a Flux Query on {bucket}</h1>
+      <p className="small-margins">
+        In this query, we are looking for data points within the last 1 minute
+        with a measurement of{' '}
+        <code className="homepage-wizard--code-highlight">"wifi_status"</code>
+        .
+      </p>
+      <CodeSnippet
+        text={codeSnippet}
+        onCopy={logCopyCodeSnippet}
+        language="properties"
+      />
     </>
   )
 }

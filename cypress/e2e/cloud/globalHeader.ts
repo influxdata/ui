6a48aae542086ceb81b1a764f996103b276351f0
wdoconnell <91283923+wdoconnell@users.noreply.github.com>
2022-08-12 11:33:05 -0400

    fix: simplify tests, reduce risk of .click() flake

--- cypress/e2e/cloud/globalHeader.ts
+++ cypress/e2e/cloud/globalHeader.ts
@@ -15,8 +15,8 @@ describe('multi-account multi-org global header', () => {
         cy.get('@org').then(() => {
           cy.getByTestID('home-page--header').should('be.visible')
           cy.setFeatureFlags(globalHeaderFeatureFlags).then(() => {
-            // cy.wait(400) is necessary to ensure sufficient time for the feature flag override.
-            // The feature flag reset happens in redux, (it's not a network request), so we can't cy.wait an intercepted route.
+            // cy.wait is necessary to ensure sufficient time for the feature flag override.
+            // We cannot cy.wait an intercepted route because this isn't a network request.
             cy.wait(400).then(() => {
               cy.visit('/')
             })
@@ -27,31 +27,31 @@ describe('multi-account multi-org global header', () => {
   })
 
   describe('user profile avatar', () => {
-    it('displays the `user profile` avatar', () => {
-      cy.getByTestID('global-header--user-avatar').should('be.visible')
+    it('navigates to the `user profile` page', () => {
+      cy.getByTestID('global-header--user-avatar')
+        .should('be.visible')
+        .click({scrollBehavior: false})
+
+      cy.getByTestID('global-header--user-popover-profile-button')
+        .should('be.visible')
+        .click({scrollBehavior: false})
+
+      cy.getByTestID('global-header--user-avatar')
+        .should('be.visible')
+        .click({scrollBehavior: false})
     })
 
-    it('displays the popover links', () => {
-      cy.getByTestID('global-header--user-avatar').click()
-      cy.getByTestID('global-header--user-popover-profile-button').should(
-        'be.visible'
-      )
+    it('allows the user to log out', () => {
+      cy.getByTestID('global-header--user-avatar')
+        .should('be.visible')
+        .click({scrollBehavior: false})
 
       cy.getByTestID('global-header--user-popover-logout-button').should(
         'be.visible'
       )
-      cy.getByTestID('global-header--user-avatar').click()
-    })
-
-    it('navigates to the `user profile` page', () => {
-      cy.getByTestID('global-header--user-avatar').click()
-      cy.getByTestID('global-header--user-popover-profile-button').click()
-      cy.getByTestID('user-profile--page').should('be.visible')
-    })
-
-    it('allows the user to log out', () => {
-      cy.getByTestID('global-header--user-avatar').click()
-      cy.getByTestID('global-header--user-popover-logout-button').click()
+      cy.getByTestID('global-header--user-popover-logout-button').click({
+        scrollBehavior: false,
+      })
       // Logout in remocal looks like a 404 because there is no quartz. This tests the logout URL.
       cy.location('pathname').should('eq', '/logout')
     })

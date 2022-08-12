046304543c12cf9cd8292ac3be3d33ae38285c14
Bill O'Connell <91283923+wdoconnell@users.noreply.github.com>
2022-08-12 12:14:53 -0400

    feat: add e2e tests for user profile avatar (#5381)

--- src/identity/components/userprofile/UserProfilePage.tsx
+++ src/identity/components/userprofile/UserProfilePage.tsx
@@ -17,7 +17,7 @@ export const UserProfilePage: FC = () => {
   return (
     <Page titleTag={pageTitleSuffixer(['User Profile'])}>
       <Page.Header fullWidth={true}>
-        <Page.Title title="User Profile" />
+        <Page.Title title="User Profile" testID="user-profile--page" />
       </Page.Header>
       <Page.Contents fullWidth={true}>
         <UserDetails />

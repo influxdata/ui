df21539585f73a9e24d57f61cd71b3c4ada805a9
ChitlangeSahas <chitlangesahas@gmail.com>
2022-08-18 13:51:25 -0700

    chore: no +

--- src/homepageExperience/containers/HomepageContainer.tsx
+++ src/homepageExperience/containers/HomepageContainer.tsx
@@ -218,7 +218,7 @@ export const HomepageContainer: FC = () => {
                       onClick={logMoreButtonClick}
                       style={inlineViewMoreStyle}
                     >
-                      + View more
+                      View more
                     </Link>
                   </FlexBox>
                   <hr style={{marginTop: '8px'}} />

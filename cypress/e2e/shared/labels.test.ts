f3652d731115c18e699f265addeb8087b9940eff
ChitlangeSahas <chitlangesahas@gmail.com>
2022-08-19 11:50:17 -0700

    chore: prettier

--- cypress/e2e/shared/labels.test.ts
+++ cypress/e2e/shared/labels.test.ts
@@ -25,7 +25,8 @@ describe('labels', () => {
 
   it('can create a label', () => {
     const newLabelName = 'Substantia'
-    const labelNameWith43Characters = 'Substantia Substantia Substantia Substantia'
+    const labelNameWith43Characters =
+      'Substantia Substantia Substantia Substantia'
 
     const newLabelDescription =
       '(\u03943) quod in se est et per se concipitur hoc est id cujus conceptus non indiget conceptu alterius rei a quo formari debeat. '
@@ -60,10 +61,12 @@ describe('labels', () => {
     // Try to save without name (required field) button should be disabled
     cy.getByTestID('create-label-form--submit').should('be.disabled')
 
-
     // check that input limits name to 30 characters
     cy.getByTestID('create-label-form--name').type(labelNameWith43Characters)
-    cy.getByTestID('create-label-form--name').should('not.have.value', labelNameWith43Characters)
+    cy.getByTestID('create-label-form--name').should(
+      'not.have.value',
+      labelNameWith43Characters
+    )
     cy.getByTestID('create-label-form--name').clear()
 
     // enter name

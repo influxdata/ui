e07fecaccc5acf1235b89c0943b726a40b57a4e6
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-08-22 14:58:57 -0500

    feat: add heading and flux sync toggle button

--- src/dataExplorer/components/Schema.tsx
+++ src/dataExplorer/components/Schema.tsx
@@ -1,13 +1,21 @@
-import React, {FC, useContext, useEffect, useMemo} from 'react'
+import React, {FC, useContext, useEffect, useMemo, useState} from 'react'
 import {useSelector} from 'react-redux'
 
 // Components
-import {DapperScrollbars} from '@influxdata/clockface'
+import {
+  DapperScrollbars,
+  FlexBox,
+  InputLabel,
+  SlideToggle,
+  JustifyContent,
+  QuestionMarkTooltip,
+} from '@influxdata/clockface'
 import SearchWidget from 'src/shared/components/search_widget/SearchWidget'
 import BucketSelector from 'src/dataExplorer/components/BucketSelector'
 import MeasurementSelector from 'src/dataExplorer/components/MeasurementSelector'
 import FieldSelector from 'src/dataExplorer/components/FieldSelector'
 import TagSelector from 'src/dataExplorer/components/TagSelector'
+import SelectorTitle from 'src/dataExplorer/components/SelectorTitle'
 
 // Context
 import {
@@ -27,6 +35,30 @@ import {getOrg} from 'src/organizations/selectors'
 
 // Style
 import './Schema.scss'
+
+const FLUX_SYNC_TOOLTIP = `Flux Sync autopopulates the script editor to help you \
+start a query. You can turn this feature on and off, but typing within this \
+section will disable synchronization.`
+
+const Heading: FC = () => {
+  const [fluxSyncOn, setFluxSyncOn] = useState(true)
+
+  const handleFluxSyncToggle = () => {
+    setFluxSyncOn(!fluxSyncOn)
+  }
+
+  return (
+    <FlexBox justifyContent={JustifyContent.SpaceBetween}>
+      <div>Schema Browser</div>
+      <FlexBox>
+        <SlideToggle active={fluxSyncOn} onChange={handleFluxSyncToggle} />
+        <InputLabel>
+          <SelectorTitle title="Flux Sync" info={FLUX_SYNC_TOOLTIP} />
+        </InputLabel>
+      </FlexBox>
+    </FlexBox>
+  )
+}
 
 const FieldsTags: FC = () => {
   const {
@@ -80,6 +112,7 @@ const Schema: FC = () => {
               <div className="scroll--container">
                 <DapperScrollbars>
                   <div className="schema-browser" data-testid="schema-browser">
+                    <Heading />
                     <BucketSelector />
                     <div className="container-side-bar">
                       <MeasurementSelector />

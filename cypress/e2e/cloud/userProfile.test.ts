bf96dc83d3549fb0ebb000fee6e34ed1a9ec8dd9
wdoconnell <91283923+wdoconnell@users.noreply.github.com>
2022-08-22 18:39:49 -0400

    chore: add duration, remove unnecessary quartz-mock reset

--- cypress/e2e/cloud/userProfile.test.ts
+++ cypress/e2e/cloud/userProfile.test.ts
@@ -4,16 +4,12 @@ export const setupProfile = (): Promise<any> => {
     cy.flush().then(() =>
       cy.signin().then(() => {
         cy.get('@org').then(() => {
-          cy.request({
-            method: 'PUT',
-            url: 'api/v2/quartz/accounts/resetAllAccountOrgs',
-          })
           cy.visit('/')
           cy.getByTestID('home-page--header').should('be.visible')
           cy.setFeatureFlags(userProfileFeatureFlags).then(() => {
             // cy.wait($time) is necessary to consistently ensure sufficient time for the feature flag override.
             // The flag reset happens via redux, (it's not a network request), so we can't cy.wait($intercepted_route).
-            cy.wait(1000).then(() => {
+            cy.wait(1200).then(() => {
               cy.visit('/me/profile')
             })
           })

921ca5368ab0a03b48b1de0fda40d430442be1c6
drdelambre <drdelambre>
2022-08-25 22:32:24 +0000

    fix: fighting oss linting

--- src/dataExplorer/components/resources/types/script/index.ts
+++ src/dataExplorer/components/resources/types/script/index.ts
@@ -1,13 +1,21 @@
 import {ResourceType} from 'src/types/resources'
 import editor from './editor'
-import * as api from 'src/client/scriptsRoutes'
+import {CLOUD} from 'src/shared/constants'
+
+const {getScript, patchScript, postScript} = CLOUD
+  ? require('src/client/scriptsRoutes')
+  : {
+      getScript: false,
+      patchScript: false,
+      postScript: false,
+    }
 
 export default function script(register) {
   register({
     type: ResourceType.Scripts,
     editor,
     init: id => {
-      if (!id) {
+      if (!id || !CLOUD) {
         return Promise.resolve({
           type: ResourceType.Scripts,
           flux: '',
@@ -18,70 +26,68 @@ export default function script(register) {
         })
       }
 
-      return api
-        .getScript({
-          scriptID: id,
-        })
-        .then(resp => {
-          if (resp.status === 200) {
-            return {
-              type: ResourceType.Scripts,
-              flux: resp.data.script,
-              data: resp.data,
-            }
-          }
-
+      return getScript({
+        scriptID: id,
+      }).then(resp => {
+        if (resp.status === 200) {
           return {
             type: ResourceType.Scripts,
-            flux: '',
-            data: {
-              name: `Untitled Script: ${new Date().toISOString()}`,
-              description: 'default description',
-            },
+            flux: resp.data.script,
+            data: resp.data,
           }
-        })
+        }
+
+        return {
+          type: ResourceType.Scripts,
+          flux: '',
+          data: {
+            name: `Untitled Script: ${new Date().toISOString()}`,
+            description: 'default description',
+          },
+        }
+      })
     },
     persist: resource => {
       const data = JSON.parse(JSON.stringify(resource.data))
       data.script = resource.flux
 
-      if (data.id) {
-        return api
-          .patchScript({
-            scriptID: data.id,
-            data: {
-              name: data.name,
-              description: data.description,
-              script: data.script,
-            },
-          })
-          .then(() => {
-            return resource
-          })
+      if (!CLOUD) {
+        return resource
       }
 
-      return api
-        .postScript({
+      if (data.id) {
+        return patchScript({
+          scriptID: data.id,
           data: {
             name: data.name,
             description: data.description,
             script: data.script,
-            language: 'flux',
           },
-        })
-        .then(resp => {
-          if (resp.status === 201) {
-            return {
-              ...resource,
-              data: {
-                ...data,
-                id: resp.data.id,
-              },
-            }
-          }
-
+        }).then(() => {
           return resource
         })
+      }
+
+      return postScript({
+        data: {
+          name: data.name,
+          description: data.description,
+          script: data.script,
+          language: 'flux',
+        },
+      }).then(resp => {
+        if (resp.status === 201) {
+          return {
+            ...resource,
+            data: {
+              ...data,
+              id: resp.data.id,
+            },
+          }
+        }
+
+        return resource
+      })
     },
   })
 }

0d4e52663033e59460bf995b3d7be74ec6a8b200
ChitlangeSahas <chitlangesahas@gmail.com>
2022-09-19 15:39:05 -0700

    fix(firstMile): fixing listen data component to listen on componentDidMount as well

--- src/homepageExperience/components/DataListening.tsx
+++ src/homepageExperience/components/DataListening.tsx
@@ -7,12 +7,13 @@ import {ErrorHandling} from 'src/shared/decorators/errors'
 import ConnectionInformation, {
   LoadingState,
 } from 'src/shared/components/DataListening/ConnectionInformation'
-import {Button} from '@influxdata/clockface'
+import {Button, FlexBox, JustifyContent} from '@influxdata/clockface'
 
 import {
   continuouslyCheckForData,
   TIMEOUT_MILLISECONDS,
 } from 'src/shared/utils/dataListening'
+import {DEFAULT_BUCKET} from '../../writeData/components/WriteDataDetailsContext'
 
 interface OwnProps {
   bucket: string
@@ -55,6 +56,24 @@ class DataListening extends PureComponent<Props, State> {
     })
   }
 
+  componentDidMount() {
+    const {bucket} = this.props
+    if (
+      (bucket !== '<BUCKET>' && this.state.previousBucket !== bucket) ||
+      this.state.retry
+    ) {
+      // clear timer when bucket changes
+      clearInterval(this.intervalID)
+      clearInterval(this.timer)
+      this.setState({
+        timePassedInSeconds: 0,
+        secondsLeft: this.TIMEOUT_SECONDS,
+      })
+      this.startListeningForData()
+      this.setState({previousBucket: bucket, retry: false})
+    }
+  }
+
   componentDidUpdate() {
     const {bucket} = this.props
     if (
@@ -78,6 +97,16 @@ class DataListening extends PureComponent<Props, State> {
   }
 
   public render() {
+    if (
+      this.props.bucket === 'sample-bucket' ||
+      this.props.bucket === DEFAULT_BUCKET
+    ) {
+      return (
+        <FlexBox justifyContent={JustifyContent.Center}>
+          <h4>Please select a bucket to listen to</h4>
+        </FlexBox>
+      )
+    }
     return (
       <div className="wizard-step--body-streaming" data-testid="streaming">
         {this.connectionInfo}

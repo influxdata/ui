effb2b5a29f088313a67ea23b81939cfa49bec82
ChitlangeSahas <chitlangesahas@gmail.com>
2022-09-21 14:44:27 -0700

    test test

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,7 @@ const App: FC = () => {
   const {theme, presentationMode} = useContext(AppSettingContext)
   const currentPage = useSelector((state: AppState) => state.currentPage)
 
+  console.log("ENV VAR TEST", process.env.TEST_VAR, process.env)
   const appWrapperClass = classnames('', {
     'dashboard-light-mode': currentPage === 'dashboard' && theme === 'light',
     'multi-org': isFlagEnabled('multiOrg'),

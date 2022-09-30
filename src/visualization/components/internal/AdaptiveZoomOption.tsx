2df84c9b62551376b69d11d250b597faade86b07
ChitlangeSahas <chitlangesahas@gmail.com>
2022-09-29 23:25:46 -0700

    fix(dataExplorer): inconsistent spacing and component usage

--- src/visualization/components/internal/AdaptiveZoomOption.tsx
+++ src/visualization/components/internal/AdaptiveZoomOption.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import React, {CSSProperties, FC} from 'react'
+import React, {FC} from 'react'
 
 // Components
 import {
@@ -7,7 +7,6 @@ import {
   ComponentSize,
   FlexBox,
   FlexDirection,
-  InfluxColors,
   InputLabel,
   QuestionMarkTooltip,
   SlideToggle,
@@ -22,13 +21,6 @@ interface AdaptiveZoomToggleProps {
   update: (obj: any) => void
   testID?: string
   type: string
-}
-
-const getToggleColor = (toggle: boolean): CSSProperties => {
-  if (toggle) {
-    return {color: InfluxColors.Grey95}
-  }
-  return {color: InfluxColors.Grey65}
 }
 
 const adaptiveZoomTooltipStyle = {
@@ -73,15 +65,14 @@ export const AdaptiveZoomToggle: FC<AdaptiveZoomToggleProps> = ({
       stretchToFitWidth={true}
       className="adaptive-zoom-toggle"
       testID="adaptive-zoom-toggle"
+      style={{marginBottom: '16px'}}
     >
       <SlideToggle
         active={!adaptiveZoomHide}
         size={ComponentSize.ExtraSmall}
         onChange={handleSetAdaptiveZoom}
       />
-      <InputLabel style={getToggleColor(adaptiveZoomHide)}>
-        Adaptive Zoom
-      </InputLabel>
+      <InputLabel>Adaptive Zoom</InputLabel>
       <QuestionMarkTooltip
         diameter={16}
         tooltipContents={adaptiveZoomTooltip}
--- src/visualization/types/Graph/options.tsx
+++ src/visualization/types/Graph/options.tsx
@@ -1,15 +1,19 @@
 // Libraries
 import React, {FC, useMemo} from 'react'
 import {
-  Input,
-  Grid,
+  ButtonShape,
   Columns,
+  ComponentSize,
+  ComponentStatus,
+  Dropdown,
+  FlexBox,
   Form,
+  Grid,
+  Input,
+  InputLabel,
   SelectDropdown,
   SelectGroup,
-  Dropdown,
-  ComponentStatus,
-  ButtonShape,
+  SlideToggle,
 } from '@influxdata/clockface'
 import {createGroupIDColumn} from '@influxdata/giraffe'
 
@@ -35,7 +39,6 @@ import ColorSchemeDropdown from 'src/visualization/components/internal/ColorSche
 import HoverLegend from 'src/visualization/components/internal/HoverLegend'
 import StaticLegend from 'src/visualization/components/internal/StaticLegend'
 import AxisTicksGenerator from 'src/visualization/components/internal/AxisTicksGenerator'
-import Checkbox from 'src/shared/components/Checkbox'
 import {XYViewProperties} from 'src/types'
 import {VisualizationOptionProps} from 'src/visualization'
 
@@ -119,7 +122,9 @@ const GraphViewOptions: FC<Props> = ({properties, results, update}) => {
           widthMD={Columns.Six}
           widthLG={Columns.Four}
         >
-          <h5 className="view-options--header">Data</h5>
+          <h5 className="view-options--header" style={{marginTop: '0'}}>
+            Data
+          </h5>
           <Form.Element label="X Column">
             <SelectDropdown
               options={numericColumns}
@@ -253,15 +258,6 @@ const GraphViewOptions: FC<Props> = ({properties, results, update}) => {
               }}
             />
           </Form.Element>
-
-          <Checkbox
-            label="Shade Area Below Lines"
-            checked={!!properties.shadeBelow}
-            onSetChecked={shadeBelow => {
-              update({shadeBelow})
-            }}
-          />
-          <br />
           <Form.Element label="Hover Dimension">
             <Dropdown
               button={(active, onClick) => (
@@ -314,6 +310,17 @@ const GraphViewOptions: FC<Props> = ({properties, results, update}) => {
                 </Dropdown.Menu>
               )}
             />
+          </Form.Element>
+          <Form.Element label={''}>
+            <FlexBox margin={ComponentSize.Medium}>
+              <SlideToggle
+                active={!!properties.shadeBelow}
+                onChange={() => {
+                  update({shadeBelow: !properties.shadeBelow})
+                }}
+              />
+              <InputLabel>Shade area below graph</InputLabel>
+            </FlexBox>
           </Form.Element>
         </Grid.Column>
         <Grid.Column>

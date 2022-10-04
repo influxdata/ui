02c1a8ce7eddfbfe47760c9b085f45b2b57dc3b4
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-10-04 16:28:36 -0500

    feat: add fields as columns toggle

--- src/dataExplorer/components/ResultOptions.tsx
+++ src/dataExplorer/components/ResultOptions.tsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 // Components
-import {Accordion} from '@influxdata/clockface'
+import {
+  Accordion,
+  FlexBox,
+  InputLabel,
+  SlideToggle,
+} from '@influxdata/clockface'
+import SelectorTitle from 'src/dataExplorer/components/SelectorTitle'
 
 // Style
 import './Sidebar.scss'
 
+const TOOLTIP_CONTENT = {
+  FIELDS_AS_COLUMNS: `Tooltip content for Fields as Columns`,
+}
+
 const ResultOptions = () => {
-  console.log('test result options')
+  const [fieldsAsColumnsOn, setFieldsAsColumnsOn] = useState(false)
+
+  const fieldsAsColumns = (
+    <FlexBox>
+      <SlideToggle
+        active={fieldsAsColumnsOn}
+        onChange={() => setFieldsAsColumnsOn(current => !current)}
+      />
+      <InputLabel>
+        <SelectorTitle
+          label="Fields as Columns"
+          tooltipContents={TOOLTIP_CONTENT.FIELDS_AS_COLUMNS}
+        />
+      </InputLabel>
+    </FlexBox>
+  )
+
   return (
     <Accordion className="result-options" expanded={true}>
       <Accordion.AccordionHeader className="result-options--header">
         Result Options
       </Accordion.AccordionHeader>
-      items
+      {fieldsAsColumns}
     </Accordion>
   )
 }

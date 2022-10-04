5242b7f7797dae397212d1ab307f7d6ff1fbcb53
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-10-04 15:40:45 -0500

    chore: add accordion to result options

--- src/dataExplorer/components/ResultOptions.tsx
+++ src/dataExplorer/components/ResultOptions.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+// Components
+import {Accordion} from '@influxdata/clockface'
+
+// Style
+import './Sidebar.scss'
+
 const ResultOptions = () => {
   console.log('test result options')
-  return <div>Result Options</div>
+  return (
+    <Accordion className="result-options" expanded={true}>
+      <Accordion.AccordionHeader className="result-options--header">
+        Result Options
+      </Accordion.AccordionHeader>
+      items
+    </Accordion>
+  )
 }
 
 export {ResultOptions}

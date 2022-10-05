80d8e6afbe01c644c7827ea77fba2e1db357fc73
Bucky Schwarz <hoorayimhelping@users.noreply.github.com>
2022-10-05 13:10:37 -0400

    chore(dashboards): combine Dashboard.tsx and Cells.tsx into DashboardCells.tsx (#5996)

--- src/shared/components/cells/DashboardCells.tsx
+++ src/shared/components/cells/DashboardCells.tsx
@@ -4,11 +4,19 @@ import {connect, ConnectedProps} from 'react-redux'
 import ReactGridLayout, {WidthProvider, Layout} from 'react-grid-layout'
 import {get} from 'lodash'
 
+import {
+  Page,
+  SpinnerContainer,
+  TechnoSpinner,
+  RemoteDataState,
+} from '@influxdata/clockface'
+
 // Components
 const Grid = WidthProvider(ReactGridLayout)
 import CellComponent from 'src/shared/components/cells/Cell'
 import GradientBorder from 'src/shared/components/cells/GradientBorder'
 import ScrollDetector from 'src/perf/components/ScrollDetector'
+import DashboardEmpty from 'src/dashboards/components/dashboard_empty/DashboardEmpty'
 
 // Actions
 import {updateCells} from 'src/cells/actions/thunks'
@@ -21,7 +29,7 @@ import {getCells} from 'src/cells/selectors'
 import {LAYOUT_MARGIN, DASHBOARD_LAYOUT_ROW_HEIGHT} from 'src/shared/constants'
 
 // Types
-import {AppState, Cell, RemoteDataState} from 'src/types'
+import {AppState, Cell} from 'src/types'
 
 import {ErrorHandling} from 'src/shared/decorators/errors'
 
@@ -33,37 +41,47 @@ type ReduxProps = ConnectedProps<typeof connector>
 type Props = OwnProps & ReduxProps
 
 @ErrorHandling
-class Cells extends Component<Props> {
+class DashboardCellsUnconnected extends Component<Props> {
   public render() {
-    const {views, cells, manualRefresh} = this.props
+    const {cells, manualRefresh, status, views} = this.props
 
     return (
-      <>
-        <ScrollDetector component="dashboard" />
-        <Grid
-          cols={12}
-          layout={this.cells}
-          rowHeight={DASHBOARD_LAYOUT_ROW_HEIGHT}
-          useCSSTransforms={false}
-          containerPadding={[0, 0]}
-          margin={[LAYOUT_MARGIN, LAYOUT_MARGIN]}
-          onLayoutChange={this.handleLayoutChange}
-          draggableHandle=".cell--draggable"
-          isDraggable
-          isResizable
-        >
-          {fastMap(cells, cell => (
-            <div
-              key={cell.id}
-              className="cell"
-              data-testid={`cell ${views[cell.id]?.name}`}
-            >
-              <CellComponent cell={cell} manualRefresh={manualRefresh} />
-              <GradientBorder />
-            </div>
-          ))}
-        </Grid>
-      </>
+      <SpinnerContainer loading={status} spinnerComponent={<TechnoSpinner />}>
+        <Page.Contents fullWidth={true} scrollable={true} className="dashboard">
+          {!cells.length ? (
+            <DashboardEmpty />
+          ) : (
+            <>
+              <ScrollDetector component="dashboard" />
+              <Grid
+                cols={12}
+                layout={this.cells}
+                rowHeight={DASHBOARD_LAYOUT_ROW_HEIGHT}
+                useCSSTransforms={false}
+                containerPadding={[0, 0]}
+                margin={[LAYOUT_MARGIN, LAYOUT_MARGIN]}
+                onLayoutChange={this.handleLayoutChange}
+                draggableHandle=".cell--draggable"
+                isDraggable
+                isResizable
+              >
+                {fastMap(cells, cell => (
+                  <div
+                    key={cell.id}
+                    className="cell"
+                    data-testid={`cell ${views[cell.id]?.name}`}
+                  >
+                    <CellComponent cell={cell} manualRefresh={manualRefresh} />
+                    <GradientBorder />
+                  </div>
+                ))}
+              </Grid>
+            </>
+          )}
+          {/* This element is used as a portal container for note tooltips in cell headers */}
+          <div className="cell-header-note-tooltip-container" />
+        </Page.Contents>
+      </SpinnerContainer>
     )
   }
 
@@ -129,17 +147,18 @@ class Cells extends Component<Props> {
     }
   }
   private handlePositionChange = (cells: Cell[]) => {
-    const {dashboard, updateCells} = this.props
-    updateCells(dashboard, cells)
+    const {dashboardId, updateCells} = this.props
+    updateCells(dashboardId, cells)
   }
 }
 const mstp = (state: AppState) => {
-  const dashboard = state.currentDashboard.id
+  const dashboardId = state.currentDashboard.id
 
   return {
-    dashboard,
-    cells: getCells(state, dashboard),
+    dashboardId,
+    cells: getCells(state, dashboardId),
     views: state.resources.views.byID,
+    status: state.resources.cells.status,
   }
 }
 const mdtp = {
@@ -148,4 +167,4 @@ const mdtp = {
 
 const connector = connect(mstp, mdtp)
 
-export default connector(Cells)
+export const DashboardCells = connector(DashboardCellsUnconnected)

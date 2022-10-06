0cf64bc6df623efb729aec861d0e3d4348c2b0d2
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-10-06 16:30:09 -0500

    feat: add group option with SelectGroup cf component

--- src/dataExplorer/components/GroupBy.tsx
+++ src/dataExplorer/components/GroupBy.tsx
@@ -1,24 +1,47 @@
-import React, {FC, useState} from 'react'
+import React, {FC, useState, useMemo} from 'react'
 
 // Components
-import {ToggleWithLabelTooltip} from 'src/dataExplorer/components/ToggleWithLabelTooltip'
+import SelectorTitle from 'src/dataExplorer/components/SelectorTitle'
+import {SelectGroup} from '@influxdata/clockface'
 
 // Styles
 import './Sidebar.scss'
 
 const GROUP_TOOLTIP = `test`
 
-const GroupBy: FC = () => {
-  const [group, setGroup] = useState(false)
+enum GroupOptions {
+  Default = 'Default',
+  GroupBy = 'Group By',
+  Ungroup = 'Ungroup',
+}
 
-  return (
-    <ToggleWithLabelTooltip
-      label="Group"
-      active={group}
-      onChange={() => setGroup(current => !current)}
-      tooltipContents={GROUP_TOOLTIP}
-    />
+const GroupBy: FC = () => {
+  const [selectedGroupOption, setSelectedGroupOption] = useState<GroupOptions>(
+    GroupOptions.Default
   )
+
+  return useMemo(() => {
+    return (
+      <div className="result-options--item">
+        <SelectorTitle label="Group" tooltipContents={GROUP_TOOLTIP} />
+        <SelectGroup>
+          {(Object.keys(GroupOptions) as (keyof typeof GroupOptions)[]).map(
+            key => (
+              <SelectGroup.Option
+                key={key}
+                id={key}
+                active={selectedGroupOption === GroupOptions[key]}
+                value={GroupOptions[key]}
+                onClick={setSelectedGroupOption}
+              >
+                {GroupOptions[key]}
+              </SelectGroup.Option>
+            )
+          )}
+        </SelectGroup>
+      </div>
+    )
+  }, [selectedGroupOption])
 }
 
 export {GroupBy}

385464eb96b09e9ba91d7a6c324d2686b52ea1fb
Bucky Schwarz <hoorayimhelping@users.noreply.github.com>
2022-10-26 10:47:14 -0400

    feat(buckets): don't show explicit schema when an iox org creates a bucket (#6166)

--- src/buckets/components/createBucketForm/CreateBucketOverlay.tsx
+++ src/buckets/components/createBucketForm/CreateBucketOverlay.tsx
@@ -1,5 +1,6 @@
 // Libraries
 import React, {FC, useContext} from 'react'
+import {useSelector} from 'react-redux'
 
 // Components
 import {Overlay} from '@influxdata/clockface'
@@ -11,9 +12,15 @@ import {OverlayContext} from 'src/overlays/components/OverlayController'
 // Constants
 import {getBucketOverlayWidth} from 'src/buckets/constants'
 
+// Selectors
+import {isOrgIOx} from 'src/organizations/selectors'
+
 const CreateBucketOverlay: FC = () => {
   const {onClose, params} = useContext(OverlayContext)
-  const useSimplifiedBucketForm = params?.useSimplifiedBucketForm
+  const isIOx = useSelector(isOrgIOx)
+
+  // IOx currently doesn't have the capability to handle explicit schemas
+  const useSimplifiedBucketForm = isIOx ? true : params?.useSimplifiedBucketForm
   const callbackAfterBucketCreation = params?.callbackAfterBucketCreation
 
   return (

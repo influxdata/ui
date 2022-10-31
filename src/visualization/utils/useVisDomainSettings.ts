9a375790338702addb6bd67f2180b7e3e852e2ec
Timmy Luong <TCL735@users.noreply.github.com>
2022-10-31 09:35:15 -0700

    fix: move cross axis syncining into a separate hook (#6207)

--- src/visualization/utils/useVisDomainSettings.ts
+++ src/visualization/utils/useVisDomainSettings.ts
@@ -277,21 +277,24 @@ export const useZoomRequeryXDomainSettings = (args: ZoomRequeryArgs) => {
   //      - the time range has changed
   useEffect(() => {
     if (
-      !timeRange ||
-      (!preZoomResult &&
-        (isNotEqual(preZoomDomain, domain) ||
-          (timeRange && isNotEqual(timeRange, selectedTimeRange))))
+      !preZoomResult &&
+      (isNotEqual(preZoomDomain, domain) ||
+        (timeRange && isNotEqual(timeRange, selectedTimeRange)))
     ) {
       setSelectedTimeRange(timeRange)
       setDomain(preZoomDomain)
     }
   }, [domain, preZoomDomain, preZoomResult, selectedTimeRange, timeRange])
 
+  // send back the window period if it is the time axis, otherwise
+  //   cross syncing - time axis zoom changes the limits of the value axis
   useEffect(() => {
     if (timeRange) {
       transmitWindowPeriod(windowPeriod)
+    } else {
+      setDomain(preZoomDomain)
     }
-  }, [timeRange, transmitWindowPeriod, windowPeriod])
+  }, [preZoomDomain, timeRange, transmitWindowPeriod, windowPeriod])
 
   // Suppresses adaptive zoom feature; must come after all hooks
   if (
@@ -455,21 +458,24 @@ export const useZoomRequeryYDomainSettings = (args: ZoomRequeryArgs) => {
   //      - the time range has changed
   useEffect(() => {
     if (
-      !timeRange ||
-      (!preZoomResult &&
-        (isNotEqual(preZoomDomain, domain) ||
-          (timeRange && isNotEqual(timeRange, selectedTimeRange))))
+      !preZoomResult &&
+      (isNotEqual(preZoomDomain, domain) ||
+        (timeRange && isNotEqual(timeRange, selectedTimeRange)))
     ) {
       setSelectedTimeRange(timeRange)
       setDomain(preZoomDomain)
     }
   }, [domain, preZoomDomain, preZoomResult, selectedTimeRange, timeRange])
 
+  // send back the window period if it is the time axis, otherwise
+  //   cross syncing - time axis zoom changes the limits of the value axis
   useEffect(() => {
     if (timeRange) {
       transmitWindowPeriod(windowPeriod)
+    } else {
+      setDomain(preZoomDomain)
     }
-  }, [timeRange, transmitWindowPeriod, windowPeriod])
+  }, [preZoomDomain, timeRange, transmitWindowPeriod, windowPeriod])
 
   // Suppresses adaptive zoom feature; must come after all hooks
   if (

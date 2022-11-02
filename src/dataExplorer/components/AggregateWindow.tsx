6d30c3b7c2a2bc9447bc03a59578a97584570ece
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2022-11-02 14:09:57 -0500

    chore: add search functionality for column and function dropdown

--- src/dataExplorer/components/AggregateWindow.tsx
+++ src/dataExplorer/components/AggregateWindow.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useCallback, useContext, useMemo} from 'react'
+import React, {FC, useCallback, useContext, useMemo, useState} from 'react'
 
 // Components
 import {ToggleWithLabelTooltip} from 'src/dataExplorer/components/ToggleWithLabelTooltip'
@@ -17,6 +17,7 @@ import {
 import {
   AGG_WINDOW_AUTO,
   DURATIONS,
+  FUNCTIONS,
 } from 'src/timeMachine/constants/queryBuilder'
 
 // Utilities
@@ -30,6 +31,8 @@ const WINDOW_PERIOD_TOOLTIP = `test`
 
 const AggregateWindow: FC = () => {
   const {selection, setSelection} = useContext(PersistanceContext)
+  const [columnSearchTerm, setColumnSearchTerm] = useState('')
+  const [functionSearchTerm, setFunctionSearchTerm] = useState('')
   const {
     isOn,
     isAutoWindowPeriod,
@@ -70,7 +73,6 @@ const AggregateWindow: FC = () => {
     return (
       isOn && (
         <SearchableDropdown
-          // TODO: searchTerm
           options={[
             'column1',
             'column2',
@@ -81,15 +83,18 @@ const AggregateWindow: FC = () => {
           ]} // TODO: get column from backend
           selectedOption={selectedColumn || 'Select column'}
           onSelect={handleSelectColumn}
+          searchPlaceholder="Search columns"
+          searchTerm={columnSearchTerm}
+          onChangeSearchTerm={setColumnSearchTerm}
+          emptyText="No columns found"
           buttonStatus={ComponentStatus.Default} // TODO: use toComponentStatus
           testID="aggregate-window--column--dropdown"
           buttonTestID="aggregate-window--column--dropdown-button"
           menuTestID="aggregate-window--column--dropdown-menu"
-          emptyText="No columns found" // TODO: re-phrase it
         />
       )
     )
-  }, [isOn, selectedColumn, handleSelectColumn])
+  }, [isOn, selectedColumn, columnSearchTerm, handleSelectColumn])
 
   const handleSelectFunction = useCallback(
     (fn: string) => {
@@ -109,19 +114,21 @@ const AggregateWindow: FC = () => {
     return (
       isOn && (
         <SearchableDropdown
-          // TODO: searchTerm
-          options={['fn1', 'fn2', 'fn3', 'fn4']} // TODO: get aggregate functions from backend
+          options={FUNCTIONS.map(f => f.name)}
           selectedOption={selectedFunction || 'Select aggregate function'}
           onSelect={handleSelectFunction}
+          searchPlaceholder="Search aggregate function"
+          searchTerm={functionSearchTerm}
+          onChangeSearchTerm={setFunctionSearchTerm}
+          emptyText="No functions found"
           buttonStatus={ComponentStatus.Default} // TODO: use toComponentStatus
           testID="aggregate-window--function--dropdown"
           buttonTestID="aggregate-window--function--dropdown-button"
           menuTestID="aggregate-window--function--dropdown-menu"
-          emptyText="No functions find. Refresh the page and retry." // TODO: re-phrase it
         />
       )
     )
-  }, [isOn, selectedFunction, handleSelectFunction])
+  }, [isOn, selectedFunction, functionSearchTerm, handleSelectFunction])
 
   const handleToggleAutoWindowPeriod = useCallback(() => {
     const isAutoWindowPeriod =

d85c939c4284f87f290fd28b33904fc346d04aaf
Beth Legesse <legessebl@gmail.com>
2022-11-09 17:04:12 -0600

    chore: pull latest version of openAPI sha

--- src/scripts/apis/index.ts
+++ src/scripts/apis/index.ts
@@ -3,12 +3,14 @@ import {CLOUD} from 'src/shared/constants'
 
 // Types
 import {ServerError, UnauthorizedError} from 'src/types/error'
-import {Scripts} from 'src/types/scripts'
+import {Scripts, Params} from 'src/types/scripts'
 
 let getScripts
+let getScriptsParams
 
 if (CLOUD) {
   getScripts = require('src/client/scriptsRoutes').getScripts
+  getScriptsParams = require('src/client/scriptsRoutes').getScriptsParams
 }
 
 export const fetchScripts = async (): Promise<Scripts> => {
@@ -22,4 +24,18 @@ export const fetchScripts = async (): Promise<Scripts> => {
   }
 
   return response.data
+}
+
+export const fetchScriptParams = async (scriptID: string): Promise<Params> => {
+  const response = await getScriptsParams({scriptID})
+
+  if (response.status === 401) {
+    throw new UnauthorizedError(response.data.message)
+  }
+  if (response.status === 500) {
+    throw new ServerError(response.data.message)
+  }
+  const scriptParams = response.data as Params
+
+  return scriptParams
 }

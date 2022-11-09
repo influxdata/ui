b3985941531dd375ac16f1a21e99990c5d30ffe9
Beth Legesse <legessebl@gmail.com>
2022-11-09 09:45:57 -0600

    chore: require scriptsRoutes

--- src/scripts/apis/index.ts
+++ src/scripts/apis/index.ts
@@ -1,8 +1,31 @@
-// APIs
-import {getScripts, Scripts} from 'src/client/scriptsRoutes'
-
 // Types
 import {ServerError, UnauthorizedError} from 'src/types/error'
+
+// Constants
+import {CLOUD} from 'src/shared/constants'
+
+export interface Scripts {
+  scripts?: Script[]
+}
+export interface Script {
+  readonly id?: string
+  name: string
+  description?: string
+  orgID: string
+  script: string
+  language?: ScriptLanguage
+  url?: string
+  readonly createdAt?: string
+  readonly updatedAt?: string
+  labels?: string[]
+}
+export type ScriptLanguage = 'flux' | 'sql'
+
+let getScripts
+
+if (CLOUD) {
+  getScripts = require('src/client/scriptsRoutes').getScripts
+}
 
 export const fetchScripts = async (): Promise<Scripts> => {
   const response = await getScripts({query: {limit: 250}})

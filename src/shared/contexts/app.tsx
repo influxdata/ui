8f702d300dbb27f8c584ec9ad8a9e3160bc8a2c8
Bucky Schwarz <d.w.schwarz@gmail.com>
2022-12-07 09:06:34 -0500

    chore: make CSVExporter changes compatible with webpack 5

--- src/shared/contexts/app.tsx
+++ src/shared/contexts/app.tsx
@@ -30,13 +30,14 @@ import {event} from 'src/cloud/utils/reporting'
 
 // Worker -- load prior to page load event, in order to intercept fetch in http/2.
 // see service worker life cycle. https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import downloadWorker from 'worker-plugin/loader!../workers/downloadHelper'
 
 let workerRegistration = null
 if ('serviceWorker' in navigator) {
-  workerRegistration = navigator.serviceWorker.register(downloadWorker)
+  workerRegistration = navigator.serviceWorker.register(
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    new URL('../workers/downloadHelper.ts', import.meta.url)
+  )
 }
 
 interface AppSettingContextType {

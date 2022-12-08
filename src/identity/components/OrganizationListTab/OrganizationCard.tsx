538efbd891694a4b15413996b4cbb204c7e30f36
Bethelhem Legesse <legessebl@gmail.com>
2022-12-08 09:18:41 -0600

    feat(identity): show deleted org status in orgs list (#6378)
    * feat: styling for suspended org card
    
    * feat: show provisioningStatus of org
    
    * feat: tooltip for suspended org card
    
    * fix: prettier
    
    * fix: alphabetize and change suspended card color
    
    * chore: alphabetize and clean up

--- src/identity/components/OrganizationListTab/OrganizationCard.tsx
+++ src/identity/components/OrganizationListTab/OrganizationCard.tsx
@@ -3,8 +3,12 @@ import {
   FlexBox,
   FlexDirection,
   JustifyContent,
+  QuestionMarkTooltip,
   ResourceCard,
 } from '@influxdata/clockface'
+
+// Utils
+import {SafeBlankLink} from 'src/utils/SafeBlankLink'
 
 // Styles
 import './OrganizationCard.scss'
@@ -12,19 +16,38 @@ import './OrganizationCard.scss'
 interface OrgCardProps {
   name: string
   provider: string
+  provisioningStatus?: string
   regionCode: string
   regionName: string
 }
 
+const tooltipContent = (
+  <p>
+    Organizations can be reactivated within 7 days of deletion. Contact support
+    at{' '}
+    <SafeBlankLink href="mailto:support@influxdata.com">
+      support@influxdata.com
+    </SafeBlankLink>{' '}
+    to reactivate.
+  </p>
+)
+
 export const OrganizationCard: FC<OrgCardProps> = ({
   name,
   provider,
+  provisioningStatus,
   regionCode,
   regionName,
 }) => {
+  const isOrgSuspended = provisioningStatus === 'suspended'
+
   return (
     <ResourceCard
-      className="account--organizations-tab-orgs-card"
+      className={
+        isOrgSuspended
+          ? 'account--organizations-tab-suspended-orgs-card'
+          : 'account--organizations-tab-orgs-card'
+      }
       direction={FlexDirection.Row}
       justifyContent={JustifyContent.SpaceBetween}
       testID="account--organizations-tab-orgs-card"
@@ -42,6 +65,22 @@ export const OrganizationCard: FC<OrgCardProps> = ({
         </FlexBox>
         <FlexBox className="account-organizations-tab-orgs-card-location-data">
           <b>Location:</b> &nbsp;{regionName}
+        </FlexBox>
+        <FlexBox
+          justifyContent={JustifyContent.SpaceBetween}
+          className="account-organizations-tab-orgs-card-org-status"
+        >
+          {isOrgSuspended && (
+            <>
+              <b className="account-organizations-tab-orgs-status--text">
+                Deletion in progress
+              </b>
+              <QuestionMarkTooltip
+                diameter={15}
+                tooltipContents={tooltipContent}
+              />
+            </>
+          )}
         </FlexBox>
       </ResourceCard.Meta>
     </ResourceCard>

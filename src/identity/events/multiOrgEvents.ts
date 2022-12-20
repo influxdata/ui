a8d8a0e2d56b1bf65803a2d0dc2d3572d4999686
Beth Legesse <legessebl@gmail.com>
2022-12-20 10:26:22 -0600

    chore: alphabetize multi org events

--- src/identity/events/multiOrgEvents.ts
+++ src/identity/events/multiOrgEvents.ts
@@ -2,59 +2,44 @@ export const multiOrgTag = {
   initiative: 'multiOrg',
 }
 
-export enum HeaderNavEvent {
-  AccountDropdownClick = 'headerNav.accountDropdown.clicked',
-  AccountSwitch = 'headerNav.accountDropdown.accountSwitched',
-
-  OrgDropdownClick = 'headerNav.orgDropdown.clicked',
-  OrgSwitch = 'headerNav.orgDropdown.orgSwitched',
-
-  UserAvatarClick = 'headerNav.userAvatarIcon.clicked',
-  UserProfileClick = 'headerNav.userAvatarProfile.clicked',
-  UserLogoutClick = 'headerNav.userAvatarLogOut.clicked',
-
-  CreateOrgClick = 'headerNav.createOrg.clicked',
-  UpgradeButtonClick = 'headerNav.upgradeButton.clicked',
+export enum AccountUpgradeOverlay {
+  MarketoAccountUpgrade = 'userProfile.marketoAccountUpgrade.formSubmitted',
+  SalesFormLinkClick = 'userProfile.marketoAccountUpgrade.salesFormLinkClicked',
 }
 
 export enum CreateOrgOverlayEvent {
   OrgCreationSuccess = 'headerNav.createOrgOverlay.creationSuccess',
   OrgCreationFail = 'headerNav.createOrgOverlay.creationFail',
-  SwitchToNewOrg = 'headerNav.createOrg.switchedToNewOrg',
+  SwitchToNewOrg = 'headerNav.createOrgOverlay.switchedToNewOrg',
 }
 
 export enum DeleteOrgOverlay {
-  DeleteOrgFail = 'userProfile.orgProfileTab.orgDeletionFailed',
-  DeleteOrgSuccess = 'userProfile.orgProfileTab.orgDeletionSuccess',
+  DeleteOrg = 'userProfile.orgProfileTab.orgDeleted',
 }
 
-export enum AccountUpgradeOverlay {
-  MarketoAccountUpgradeFail = 'userProfile.marketoAccountUpgradeForm.submissionFail',
-  MarketoAccountUpgradeSuccess = 'userProfile.marketoAccountUpgradeForm.submissionSuccess',
-  SalesFormLinkClick = 'userProfile.marketoAccountUpgradeForm.salesFormLinkClicked',
+export enum HeaderNavEvent {
+  AccountDropdownClick = 'headerNav.accountDropdown.clicked',
+  AccountSwitch = 'headerNav.accountDropdown.accountSwitched',
+
+  CreateOrgClick = 'headerNav.createOrg.clicked',
+  OrgDropdownClick = 'headerNav.orgDropdown.clicked',
+  OrgSwitch = 'headerNav.orgDropdown.orgSwitched',
+  
+  UpgradeButtonClick = 'headerNav.upgradeButton.clicked',
+  UserAvatarClick = 'headerNav.userAvatarIcon.clicked',
+  UserProfileClick = 'headerNav.userAvatarProfile.clicked',
+  UserLogoutClick = 'headerNav.userAvatarLogOut.clicked',
+}
+
+export enum MainMenuEventPrefix {
+  ChangeDefaultAccount = 'userProfile.defaultAccountDropdown',
+  ChangeDefaultOrg = 'userProfile.defaultOrgDropdown',
+  SwitchOrg = 'headerNav.org',
+  SwitchAccount = 'headerNav.account',
 }
 
 export enum OrgListEvent {
   UpgradeAccount = 'accountSettings.orgsTab.upgradeBannerClicked',
-}
-
-export enum UserProfileEvent {
-  DefaultAccountChange = 'userProfile.defaultAccountDropdown.defaultAccountChanged',
-  DefaultOrgChange = 'userProfile.defaultOrgDropdown.defaultOrgChanged',
-}
-
-export enum UserProfileEventPrefix {
-  Default = 'userProfile.default',
-  Account = 'Account',
-  Organization = 'Org',
-}
-
-export enum MainMenuEventPrefix {
-  SwitchOrg = 'headerNav.org',
-  SwitchAccount = 'headerNav.account',
-
-  ChangeDefaultOrg = 'userProfile.defaultOrgDropdown',
-  ChangeDefaultAccount = 'userProfile.defaultAccountDropdown',
 }
 
 export enum TypeAheadEventPrefix {
@@ -63,4 +48,15 @@ export enum TypeAheadEventPrefix {
 
   UserProfileSearchAccount = 'userProfile.defaultAccountDropdown',
   UserProfileSearchOrg = 'userProfile.defaultOrgDropdown',
+}
+
+export enum UserProfileEvent {
+  DefaultAccountChange = 'userProfile.defaultAccountDropdown.defaultAccountChanged',
+  DefaultOrgChange = 'userProfile.defaultOrgDropdown.defaultOrgChanged',
+}
+
+export enum UserProfileEventPrefix {
+  Account = 'Account',
+  Default = 'userProfile.default',
+  Organization = 'Org',
 }

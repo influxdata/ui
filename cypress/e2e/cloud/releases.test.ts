76f3e36bbce410623aee27263d3a448a57a3bc1a
wiedld <wiedld@users.noreply.github.com>
2023-02-07 12:13:40 -0800

    feat(6562): marty release test (#6569)
    * fix(6562): leaving all other e2es to test the full UI compatibility with the backend, and have a marty-specific release test
    
    chore: remove unused feature flag
    
    chore: fix unhandled promise rejection
    
    * chore: handle webpack dependency resolution with oats geenrated files
    
    * chore: move the marty-specific release navigation and 404 tests into the single test location

---
+++ cypress/e2e/cloud/releases.test.ts
@@ -0,0 +1,173 @@
+import {Organization} from '../../../src/types'
+
+// webpack bundling error if try importing --> due to imports in the constants file
+const IOX_SWITCHOVER_CREATION_DATE = '2023-01-31T00:00:00Z'
+
+describe('Deprecations per cloud release', () => {
+  before(() => {
+    cy.flush().then(() =>
+      cy.signin().then(() =>
+        cy.isIoxOrg().then(isIox => {
+          cy.skipOn(!isIox)
+          cy.visit('/')
+          cy.getByTestID('home-page--header').should('be.visible')
+        })
+      )
+    )
+  })
+
+  describe('Marty-release', () => {
+    beforeEach(() => {
+      cy.signinWithoutUserReprovision()
+    })
+
+    describe('has specific navigation options', () => {
+      describe('depreciated features do not exist', () => {
+        ;[
+          {
+            route: orgID => `orgs/${orgID}/notebooks`,
+            navId: 'nav-item-flows',
+            name: 'notebooks',
+          },
+          {
+            route: orgID => `orgs/${orgID}/alerting`,
+            navId: 'nav-item-alerting',
+            name: 'alerts',
+          },
+          {
+            route: orgID => `orgs/${orgID}/dashboards-list`,
+            navId: 'nav-item-dashboards',
+            name: 'dashboards',
+          },
+          {
+            route: orgID => `orgs/${orgID}/tasks`,
+            navId: 'nav-item-tasks',
+            name: 'tasks',
+          },
+        ].forEach(({route, navId, name}) => {
+          it(`does not permit access to ${name}`, () => {
+            cy.getByTestID(navId).should('not.exist')
+            cy.get<Organization>('@org').then(({id: orgID}: Organization) => {
+              cy.visit(route(orgID))
+              cy.getByTestID('tree-nav').should('be.visible')
+              cy.contains('404: Page Not Found')
+            })
+          })
+        })
+      })
+
+      describe('supported features are still navigable', () => {
+        ;[
+          {navId: 'nav-item-data-explorer', title: 'Data Explorer'},
+          {navId: 'nav-item-load-data', title: 'Load Data'},
+          {navId: 'nav-item-settings', title: 'Settings'},
+        ].forEach(({navId, title}) => {
+          it(`for ${title} page`, () => {
+            cy.getByTestID(navId).should('be.visible').click({force: true})
+            cy.getByTestID('page-title').contains(title)
+          })
+        })
+      })
+
+      describe('nav subItems are navigable based upon selective support', () => {
+        ;[
+          {
+            navId: 'nav-item-load-data',
+            navSubId: 'nav-subitem-sources',
+            tabId: 'sources--tab',
+            supported: true,
+          },
+          {
+            navId: 'nav-item-load-data',
+            navSubId: 'nav-subitem-buckets',
+            tabId: 'buckets--tab',
+            supported: true,
+          },
+          {
+            navId: 'nav-item-load-data',
+            navSubId: 'nav-subitem-telegrafs',
+            tabId: 'telegrafs--tab',
+            supported: true,
+          },
+          {
+            navId: 'nav-item-load-data',
+            navSubId: 'nav-subitem-tokens',
+            tabId: 'tokens--tab',
+            supported: true,
+          },
+          {
+            navId: 'nav-item-load-data',
+            navSubId: 'nav-subitem-subscriptions',
+            tabId: 'subscriptions--tab',
+            supported: false,
+          },
+          {
+            navId: 'nav-item-settings',
+            navSubId: 'nav-subitem-labels',
+            tabId: 'labels--tab',
+            supported: true,
+          },
+          {
+            navId: 'nav-item-settings',
+            navSubId: 'nav-subitem-secrets',
+            tabId: 'secrets--tab',
+            supported: true,
+          },
+          {
+            navId: 'nav-item-settings',
+            navSubId: 'nav-subitem-templates',
+            tabId: 'templates--tab',
+            supported: false,
+          },
+        ].forEach(({navId, navSubId, tabId, supported}) => {
+          it(`${navSubId} is ${
+            supported ? 'supported' : 'not supported'
+          }`, () => {
+            cy.getByTestID(navId).should('be.visible').click({force: true})
+            cy.getByTestID(navSubId).should(supported ? 'exist' : 'not.exist')
+            cy.getByTestID(tabId).should(supported ? 'be.visible' : 'not.exist')
+          })
+        })
+      })
+    })
+  })
+
+  describe('iox users prior to the Marty-release date', () => {
+    const beforeCutoff = new Date(
+      new Date(IOX_SWITCHOVER_CREATION_DATE) - 10000
+    ).toISOString()
+    beforeEach(() => {
+      cy.intercept('GET', '/api/v2/orgs', req => {
+        req.continue(res => {
+          const orgs = res.body.orgs.map(org => ({
+            ...org,
+            createdAt: beforeCutoff,
+          }))
+          res.body.orgs = orgs
+        })
+      })
+      cy.signinWithoutUserReprovision()
+    })
+
+    it('have everything exist except notebooks', () => {
+      cy.getByTestID('nav-item-flows').should('not.exist')
+
+      cy.getByTestID('nav-item-dashboards').should('exist').click({force: true})
+      cy.getByTestID('page-title').contains('Dashboards')
+
+      cy.getByTestID('nav-item-tasks').should('exist').click({force: true})
+      cy.getByTestID('page-title').contains('Tasks')
+
+      cy.getByTestID('nav-item-alerting').should('exist').click({force: true})
+      cy.getByTestID('page-title').contains('Alerts')
+    })
+
+    it('if pre-existing notebooks --> then notebooks access does exist', () => {
+      cy.get('@org').then(({id}: Organization) =>
+        cy.createNotebook(id).then(() => cy.reload())
+      )
+      cy.getByTestID('nav-item-flows').should('exist').click({force: true})
+      cy.getByTestID('page-title').contains('Notebooks')
+    })
+  })
+})

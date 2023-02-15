c14705ab87d958152cbbae41bcce7fe78f1fd226
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2023-02-15 10:04:42 -0600

    feat: add InfluxQL as a LanguageType

--- src/dataExplorer/components/resources/index.ts
+++ src/dataExplorer/components/resources/index.ts
@@ -7,6 +7,7 @@ export const SCRIPT_EDITOR_PARAMS = '?fluxScriptEditor'
 export enum LanguageType {
   FLUX = 'flux',
   SQL = 'sql',
+  INFLUXQL = 'influxql',
 }
 export interface ResourceConnectedQuery<T> {
   type: ResourceType

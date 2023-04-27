f244dc635f2a8c8a51f0b453d05eae943a7f32cc
Daniel Campbell <metalwhirlwind@gmail.com>
2023-04-27 10:18:24 -0700

    feat(ui): rename iox to serverless (#6680)
    * feat(ui): rename iox to serverless
    
    * chore: lint

--- src/shared/components/VersionInfo.tsx
+++ src/shared/components/VersionInfo.tsx
@@ -13,18 +13,18 @@ import {VERSION, GIT_SHA, CLOUD} from 'src/shared/constants'
 import {isOrgIOx} from 'src/organizations/selectors'
 
 const VersionInfo: FC = () => {
-  const engineName = useSelector(isOrgIOx) ? 'IOx' : 'TSM'
+  const cloudEngine = useSelector(isOrgIOx)
+    ? 'InfluxDB Cloud Serverless'
+    : 'InfluxDB Cloud powered by TSM'
   const engineLink = useSelector(isOrgIOx)
-    ? 'https://docs.influxdata.com/influxdb/cloud-iox/'
+    ? 'https://docs.influxdata.com/influxdb/cloud-serverless/'
     : 'https://docs.influxdata.com/influxdb/latest/reference/internals/storage-engine/#time-structured-merge-tree-tsm'
 
   return (
     <div className="version-info" data-testid="version-info">
       {CLOUD ? (
         <div>
-          <SafeBlankLink href={engineLink}>
-            InfluxDB Cloud powered by {engineName}
-          </SafeBlankLink>
+          <SafeBlankLink href={engineLink}>{cloudEngine}</SafeBlankLink>
           <br />
           Version {VERSION} {GIT_SHA && <code>({GIT_SHA.slice(0, 7)})</code>}
         </div>

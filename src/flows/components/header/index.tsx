a82b770770e85608df99f3fbf8f073f6ef52b1c2
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2025-02-14 09:37:39 +0700

    feat: remove notebook share button

--- src/flows/components/header/index.tsx
+++ src/flows/components/header/index.tsx
@@ -1,6 +1,5 @@
 // Libraries
-import React, {FC, useCallback, useContext, useState, useEffect} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
+import React, {FC, useCallback, useContext, useEffect} from 'react'
 
 // Contexts
 import {FlowContext} from 'src/flows/context/flow.current'
@@ -12,11 +11,10 @@ import {AppSettingProvider} from 'src/shared/contexts/app'
 
 // Components
 import {
+  ComponentColor,
+  IconFont,
   Page,
   SquareButton,
-  IconFont,
-  ComponentColor,
-  ComponentStatus,
 } from '@influxdata/clockface'
 
 import AutoRefreshButton from 'src/flows/components/header/AutoRefreshButton'
@@ -29,41 +27,15 @@ import {FeatureFlag} from 'src/shared/utils/featureFlag'
 import MenuButton from 'src/flows/components/header/MenuButton'
 
 // Utility
-import {getNotebooksShare, postNotebooksShare} from 'src/client/notebooksRoutes'
-import {event} from 'src/cloud/utils/reporting'
 import {serialize} from 'src/flows/context/flow.list'
-import {getOrg} from 'src/organizations/selectors'
-import {showOverlay} from 'src/overlays/actions/overlays'
 
 // Types
-import {RemoteDataState} from 'src/types'
-
 // Constants
-import {DEFAULT_PROJECT_NAME, PROJECT_NAME} from 'src/flows'
-
-interface Share {
-  id: string
-  accessID: string
-}
+import {DEFAULT_PROJECT_NAME} from 'src/flows'
 
 const FlowHeader: FC = () => {
   const {flow, updateOther} = useContext(FlowContext)
   const {handlePublish} = useContext(VersionPublishContext)
-  const {id: orgID} = useSelector(getOrg)
-  const [share, setShare] = useState<Share>()
-  const [linkLoading, setLinkLoading] = useState(RemoteDataState.NotStarted)
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    getNotebooksShare({query: {orgID: '', notebookID: flow.id}})
-      .then(res => {
-        if (!!res?.data?.[0]) {
-          // TODO: handle there being multiple links?
-          setShare({id: res.data[0].id, accessID: res.data[0].accessID})
-        }
-      })
-      .catch(err => console.error('failed to get notebook share', err))
-  }, [flow.id])
 
   const handleSave = useCallback(
     event => {
@@ -86,49 +58,6 @@ const FlowHeader: FC = () => {
   const handleRename = (name: string) => {
     updateOther({name})
   }
-
-  const generateLink = async () => {
-    event('Show Share Menu', {share: !!share ? 'sharing' : 'not sharing'})
-
-    setLinkLoading(RemoteDataState.Loading)
-    try {
-      const response = await postNotebooksShare({
-        data: {
-          notebookID: flow.id,
-          orgID,
-          region: window.location.hostname,
-        },
-      })
-      if (response.status !== 200) {
-        throw new Error(response.data.message)
-      }
-      setLinkLoading(RemoteDataState.Done)
-      const shareObj = {
-        id: response.data.id,
-        accessID: response.data.accessID,
-      }
-      setShare(shareObj)
-      openShareLinkOverlay(shareObj)
-    } catch (error) {
-      console.error('failed to create share', error)
-      setLinkLoading(RemoteDataState.Error)
-    }
-    event('Notebook Share Link Created')
-  }
-
-  const openShareLinkOverlay = (shareObj: Share) => {
-    dispatch(
-      showOverlay(
-        'share-overlay',
-        {
-          share: shareObj,
-          onSetShare: setShare,
-        },
-        () => {}
-      )
-    )
-  }
-
   const printJSON = () => {
     /* eslint-disable no-console */
     console.log(JSON.stringify(serialize(flow), null, 2))
@@ -167,24 +96,7 @@ const FlowHeader: FC = () => {
           />
           {flow?.id && (
             <>
-              <SquareButton
-                icon={IconFont.Share}
-                onClick={
-                  !!share
-                    ? () => openShareLinkOverlay(share)
-                    : () => generateLink()
-                }
-                color={
-                  !!share ? ComponentColor.Primary : ComponentColor.Secondary
-                }
-                status={
-                  linkLoading === RemoteDataState.Loading
-                    ? ComponentStatus.Loading
-                    : ComponentStatus.Default
-                }
-                titleText={`Share ${PROJECT_NAME}`}
-              />
-              <MenuButton handleResetShare={() => setShare(null)} />
+              <MenuButton />
             </>
           )}
           <FeatureFlag name="flow-snapshot">

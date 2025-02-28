28dc4710d3821b0a194466d5ea926f16bb53672e
Daniel Campbell <metalwhirlwind@gmail.com>
2025-02-28 14:52:31 -0800

    chore(ui): refactor python wizard to use selector

--- src/homepageExperience/containers/PythonWizard.tsx
+++ src/homepageExperience/containers/PythonWizard.tsx
@@ -1,188 +1,46 @@
-import React, {PureComponent} from 'react'
-import classnames from 'classnames'
+// Libraries
+import React, {FC, useState} from 'react'
+import {useSelector} from 'react-redux'
 
-import {
-  Button,
-  ComponentColor,
-  ComponentSize,
-  ComponentStatus,
-  Page,
-  SubwayNav,
-} from '@influxdata/clockface'
-
+// Contexts
 import WriteDataDetailsContextProvider from 'src/writeData/components/WriteDataDetailsContext'
 
-import {InstallDependencies} from 'src/homepageExperience/components/steps/python/InstallDependencies'
-import {InstallDependenciesSql} from 'src/homepageExperience/components/steps/python/InstallDependenciesSql'
-import {Overview} from 'src/homepageExperience/components/steps/Overview'
-import {Tokens} from 'src/homepageExperience/components/steps/Tokens'
-import {InitializeClient} from 'src/homepageExperience/components/steps/python/InitializeClient'
-import {InitializeClientSql} from 'src/homepageExperience/components/steps/python/InitializeClientSql'
-import {WriteData} from 'src/homepageExperience/components/steps/python/WriteData'
-import {WriteDataSql} from 'src/homepageExperience/components/steps/python/WriteDataSql'
+// Components
+import {ExecuteAggregateQuery} from 'src/homepageExperience/components/steps/python/ExecuteAggregateQuery'
+import {ExecuteAggregateQuerySql} from 'src/homepageExperience/components/steps/python/ExecuteAggregateQuerySql'
 import {ExecuteQuery} from 'src/homepageExperience/components/steps/python/ExecuteQuery'
 import {ExecuteQuerySql} from 'src/homepageExperience/components/steps/python/ExecuteQuerySql'
 import {Finish} from 'src/homepageExperience/components/steps/Finish'
-import {ExecuteAggregateQuery} from 'src/homepageExperience/components/steps/python/ExecuteAggregateQuery'
-import {ExecuteAggregateQuerySql} from 'src/homepageExperience/components/steps/python/ExecuteAggregateQuerySql'
-
-import {normalizeEventName} from 'src/cloud/utils/reporting'
-
+import {InitializeClient} from 'src/homepageExperience/components/steps/python/InitializeClient'
+import {InitializeClientSql} from 'src/homepageExperience/components/steps/python/InitializeClientSql'
+import {InstallDependencies} from 'src/homepageExperience/components/steps/python/InstallDependencies'
+import {InstallDependenciesSql} from 'src/homepageExperience/components/steps/python/InstallDependenciesSql'
+import {Overview} from 'src/homepageExperience/components/steps/Overview'
 import {PythonIcon} from 'src/homepageExperience/components/HomepageIcons'
+import {Tokens} from 'src/homepageExperience/components/steps/Tokens'
+import {WizardContainer} from 'src/homepageExperience/containers/WizardContainer'
+import {WriteData} from 'src/homepageExperience/components/steps/python/WriteData'
+import {WriteDataSql} from 'src/homepageExperience/components/steps/python/WriteDataSql'
 
 // Utils
-import {event} from 'src/cloud/utils/reporting'
-import {isFlagEnabled} from 'src/shared/utils/featureFlag'
+import {isOrgIOx} from 'src/organizations/selectors'
+import {HOMEPAGE_NAVIGATION_STEPS} from 'src/homepageExperience/utils'
 
-import {
-  scrollNextPageIntoView,
-  HOMEPAGE_NAVIGATION_STEPS,
-} from 'src/homepageExperience/utils'
+export const PythonWizard: FC<{}> = () => {
+  const [selectedBucket, setSelectedBucket] = useState<string>('sample-bucket')
+  const [finishStepCompleted, setFinishStepCompleted] = useState<boolean>(false)
+  const [tokenValue, setTokenValue] = useState<string | null>(null)
+  const [finalFeedback, setFinalFeedback] = useState<number | null>(null)
 
-interface State {
-  currentStep: number
-  selectedBucket: string
-  finishStepCompleted: boolean
-  tokenValue: string
-  finalFeedback: number
-}
+  const isIOxOrg = useSelector(isOrgIOx)
+  const subwayNavSteps = HOMEPAGE_NAVIGATION_STEPS
 
-export class PythonWizard extends PureComponent<null, State> {
-  state = {
-    currentStep: 1,
-    selectedBucket: 'my-bucket',
-    finishStepCompleted: false,
-    tokenValue: null,
-    finalFeedback: null,
+  const handleMarkStepAsCompleted = () => {
+    setFinishStepCompleted(true)
   }
 
-  subwayNavSteps = HOMEPAGE_NAVIGATION_STEPS
-
-  private handleSelectBucket = (bucketName: string) => {
-    this.setState({selectedBucket: bucketName})
-  }
-
-  private handleMarkStepAsCompleted = () => {
-    this.setState({finishStepCompleted: true})
-  }
-
-  private setTokenValue = (tokenValue: string) => {
-    this.setState({tokenValue})
-  }
-
-  private setFinalFeedback = (feedbackValue: number) => {
-    this.setState({finalFeedback: feedbackValue})
-  }
-
-  handleNextClick = () => {
-    this.setState(
-      {
-        currentStep: Math.min(
-          this.state.currentStep + 1,
-          this.subwayNavSteps.length
-        ),
-      },
-      () => {
-        event(
-          'firstMile.pythonWizard.next.clicked',
-          {},
-          {
-            clickedButtonAtStep: normalizeEventName(
-              this.subwayNavSteps[this.state.currentStep - 2].name
-            ),
-            currentStep: normalizeEventName(
-              this.subwayNavSteps[this.state.currentStep - 1].name
-            ),
-          }
-        )
-        scrollNextPageIntoView()
-      }
-    )
-  }
-
-  handlePreviousClick = () => {
-    this.setState(
-      {currentStep: Math.max(this.state.currentStep - 1, 1)},
-      () => {
-        event(
-          'firstMile.pythonWizard.previous.clicked',
-          {},
-          {
-            clickedButtonAtStep: normalizeEventName(
-              this.subwayNavSteps[this.state.currentStep].name
-            ),
-            currentStep: normalizeEventName(
-              this.subwayNavSteps[this.state.currentStep - 1].name
-            ),
-          }
-        )
-        scrollNextPageIntoView()
-      }
-    )
-  }
-
-  handleNavClick = (clickedStep: number) => {
-    this.setState({currentStep: clickedStep})
-    event(
-      'firstMile.pythonWizard.subNav.clicked',
-      {},
-      {
-        currentStep: normalizeEventName(
-          this.subwayNavSteps[clickedStep - 1].name
-        ),
-      }
-    )
-    scrollNextPageIntoView()
-  }
-
-  renderStep = () => {
-    switch (this.state.currentStep) {
-      case 1: {
-        return <Overview wizard="pythonWizard" />
-      }
-      case 2: {
-        return <InstallDependencies />
-      }
-      case 3: {
-        return (
-          <Tokens
-            wizardEventName="pythonWizard"
-            setTokenValue={this.setTokenValue}
-            tokenValue={this.state.tokenValue}
-          />
-        )
-      }
-      case 4: {
-        return <InitializeClient />
-      }
-      case 5: {
-        return <WriteData onSelectBucket={this.handleSelectBucket} />
-      }
-      case 6: {
-        return <ExecuteQuery bucket={this.state.selectedBucket} />
-      }
-      case 7: {
-        return <ExecuteAggregateQuery bucket={this.state.selectedBucket} />
-      }
-      case 8: {
-        return (
-          <Finish
-            wizardEventName="pythonWizard"
-            markStepAsCompleted={this.handleMarkStepAsCompleted}
-            finishStepCompleted={this.state.finishStepCompleted}
-            finalFeedback={this.state.finalFeedback}
-            setFinalFeedback={this.setFinalFeedback}
-          />
-        )
-      }
-      default: {
-        return <Overview wizard="pythonWizard" />
-      }
-    }
-  }
-
-  renderSqlStep = () => {
-    switch (this.state.currentStep) {
+  const renderSqlStep = (currentStep: number) => {
+    switch (currentStep) {
       case 1: {
         return <Overview wizard="pythonWizard" />
       }
@@ -193,8 +51,8 @@ export class PythonWizard extends PureComponent<null, State> {
         return (
           <Tokens
             wizardEventName="pythonWizard"
-            setTokenValue={this.setTokenValue}
-            tokenValue={this.state.tokenValue}
+            setTokenValue={setTokenValue}
+            tokenValue={tokenValue}
           />
         )
       }
@@ -202,22 +60,22 @@ export class PythonWizard extends PureComponent<null, State> {
         return <InitializeClientSql />
       }
       case 5: {
-        return <WriteDataSql onSelectBucket={this.handleSelectBucket} />
+        return <WriteDataSql onSelectBucket={setSelectedBucket} />
       }
       case 6: {
-        return <ExecuteQuerySql bucket={this.state.selectedBucket} />
+        return <ExecuteQuerySql bucket={selectedBucket} />
       }
       case 7: {
-        return <ExecuteAggregateQuerySql bucket={this.state.selectedBucket} />
+        return <ExecuteAggregateQuerySql bucket={selectedBucket} />
       }
       case 8: {
         return (
           <Finish
-            wizardEventName="pythonSqlWizard"
-            markStepAsCompleted={this.handleMarkStepAsCompleted}
-            finishStepCompleted={this.state.finishStepCompleted}
-            finalFeedback={this.state.finalFeedback}
-            setFinalFeedback={this.setFinalFeedback}
+            wizardEventName="pythonWizard"
+            markStepAsCompleted={handleMarkStepAsCompleted}
+            finishStepCompleted={finishStepCompleted}
+            finalFeedback={finalFeedback}
+            setFinalFeedback={setFinalFeedback}
           />
         )
       }
@@ -227,77 +85,64 @@ export class PythonWizard extends PureComponent<null, State> {
     }
   }
 
-  render() {
-    const {currentStep} = this.state
-
-    return (
-      <Page>
-        <Page.Header fullWidth={false} />
-        <Page.Contents scrollable={true}>
-          <div className="homepage-wizard-container">
-            <aside className="homepage-wizard-container--subway">
-              <div
-                className="homepage-wizard-container--subway-inner"
-                data-testid="subway-nav"
-              >
-                <SubwayNav
-                  currentStep={this.state.currentStep}
-                  onStepClick={this.handleNavClick}
-                  navigationSteps={this.subwayNavSteps}
-                  settingUpIcon={PythonIcon}
-                  settingUpText="Python"
-                  setupTime="5 minutes"
-                />
-              </div>
-            </aside>
-            <div className="homepage-wizard-container--main">
-              <div
-                className={classnames(
-                  'homepage-wizard-container--main-wrapper',
-                  {
-                    verticallyCentered:
-                      this.state.currentStep === 1 ||
-                      this.state.currentStep === this.subwayNavSteps.length,
-                  }
-                )}
-              >
-                <WriteDataDetailsContextProvider>
-                  {isFlagEnabled('ioxOnboarding')
-                    ? this.renderSqlStep()
-                    : this.renderStep()}
-                </WriteDataDetailsContextProvider>
-              </div>
-
-              <div className="homepage-wizard-container-footer">
-                <Button
-                  onClick={this.handlePreviousClick}
-                  text="Previous"
-                  size={ComponentSize.Large}
-                  color={ComponentColor.Tertiary}
-                  status={
-                    currentStep > 1
-                      ? ComponentStatus.Default
-                      : ComponentStatus.Disabled
-                  }
-                  testID="python-prev-button"
-                />
-                <Button
-                  onClick={this.handleNextClick}
-                  text="Next"
-                  size={ComponentSize.Large}
-                  color={ComponentColor.Primary}
-                  status={
-                    currentStep < this.subwayNavSteps.length
-                      ? ComponentStatus.Default
-                      : ComponentStatus.Disabled
-                  }
-                  testID="python-next-button"
-                />
-              </div>
-            </div>
-          </div>
-        </Page.Contents>
-      </Page>
-    )
+  const renderFluxStep = (currentStep: number) => {
+    switch (currentStep) {
+      case 1: {
+        return <Overview wizard="pythonWizard" />
+      }
+      case 2: {
+        return <InstallDependencies />
+      }
+      case 3: {
+        return (
+          <Tokens
+            wizardEventName="pythonWizard"
+            setTokenValue={setTokenValue}
+            tokenValue={tokenValue}
+          />
+        )
+      }
+      case 4: {
+        return <InitializeClient />
+      }
+      case 5: {
+        return <WriteData onSelectBucket={setSelectedBucket} />
+      }
+      case 6: {
+        return <ExecuteQuery bucket={selectedBucket} />
+      }
+      case 7: {
+        return <ExecuteAggregateQuery bucket={selectedBucket} />
+      }
+      case 8: {
+        return (
+          <Finish
+            wizardEventName="pythonWizard"
+            markStepAsCompleted={handleMarkStepAsCompleted}
+            finishStepCompleted={finishStepCompleted}
+            finalFeedback={finalFeedback}
+            setFinalFeedback={setFinalFeedback}
+          />
+        )
+      }
+      default: {
+        return <Overview wizard="pythonWizard" />
+      }
+    }
   }
+
+  return (
+    <WriteDataDetailsContextProvider>
+      <WizardContainer
+        icon={PythonIcon}
+        subwayNavSteps={subwayNavSteps}
+        language="python"
+        languageTitle="Python"
+      >
+        {currentStep =>
+          isIOxOrg ? renderSqlStep(currentStep) : renderFluxStep(currentStep)
+        }
+      </WizardContainer>
+    </WriteDataDetailsContextProvider>
+  )
 }

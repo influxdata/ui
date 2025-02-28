da16ed269b4ab7ba8427e29d785ad6713cdabd09
Daniel Campbell <metalwhirlwind@gmail.com>
2025-02-28 14:52:23 -0800

    chore(ui): refactor nodejs wizard to use selector

--- src/homepageExperience/containers/NodejsWizard.tsx
+++ src/homepageExperience/containers/NodejsWizard.tsx
@@ -1,142 +1,50 @@
-import React, {PureComponent} from 'react'
-import classnames from 'classnames'
+// Libraries
+import React, {FC, useState} from 'react'
+import {useSelector} from 'react-redux'
 
-import {
-  Button,
-  ComponentColor,
-  ComponentSize,
-  ComponentStatus,
-  Page,
-  SubwayNav,
-} from '@influxdata/clockface'
+// Contexts
+import WriteDataDetailsContextProvider from 'src/writeData/components/WriteDataDetailsContext'
 
+// Components
+import {ExecuteAggregateQuery} from 'src/homepageExperience/components/steps/nodejs/ExecuteAggregateQuery'
+import {ExecuteQuery} from 'src/homepageExperience/components/steps/nodejs/ExecuteQuery'
+import {ExecuteQuerySql} from '../components/steps/nodejs/ExecuteQuerySql'
+import {Finish} from 'src/homepageExperience/components/steps/Finish'
+import {InitializeClient} from 'src/homepageExperience/components/steps/nodejs/InitializeClient'
+import {InitializeClientSql} from '../components/steps/nodejs/InitializeClientSql'
 import {InstallDependencies} from 'src/homepageExperience/components/steps/nodejs/InstallDependencies'
+import {InstallDependenciesSql} from '../components/steps/nodejs/InstallDependenciesSql'
+import {NodejsIcon} from 'src/homepageExperience/components/HomepageIcons'
 import {Overview} from 'src/homepageExperience/components/steps/Overview'
 import {Tokens} from 'src/homepageExperience/components/steps/Tokens'
-import {InitializeClient} from 'src/homepageExperience/components/steps/nodejs/InitializeClient'
+import {WizardContainer} from 'src/homepageExperience/containers/WizardContainer'
 import {WriteData} from 'src/homepageExperience/components/steps/nodejs/WriteData'
-import {ExecuteQuery} from 'src/homepageExperience/components/steps/nodejs/ExecuteQuery'
-import {Finish} from 'src/homepageExperience/components/steps/Finish'
-import {ExecuteAggregateQuery} from 'src/homepageExperience/components/steps/nodejs/ExecuteAggregateQuery'
-import WriteDataDetailsContextProvider from 'src/writeData/components/WriteDataDetailsContext'
-import {normalizeEventName} from 'src/cloud/utils/reporting'
-
-import {NodejsIcon} from 'src/homepageExperience/components/HomepageIcons'
+import {WriteDataSql} from '../components/steps/nodejs/WriteDataSql'
 
 // Utils
-import {event} from 'src/cloud/utils/reporting'
-
+import {isOrgIOx} from 'src/organizations/selectors'
 import {
-  scrollNextPageIntoView,
   HOMEPAGE_NAVIGATION_STEPS,
   HOMEPAGE_NAVIGATION_STEPS_SQL,
 } from 'src/homepageExperience/utils'
-import {isFlagEnabled} from 'src/shared/utils/featureFlag'
-import {InstallDependenciesSql} from '../components/steps/nodejs/InstallDependenciesSql'
-import {InitializeClientSql} from '../components/steps/nodejs/InitializeClientSql'
-import {WriteDataSql} from '../components/steps/nodejs/WriteDataSql'
-import {ExecuteQuerySql} from '../components/steps/nodejs/ExecuteQuerySql'
 
-interface State {
-  currentStep: number
-  selectedBucket: string
-  finishStepCompleted: boolean
-  tokenValue: string
-  finalFeedback: number
-}
+export const NodejsWizard: FC<{}> = () => {
+  const [selectedBucket, setSelectedBucket] = useState<string>('sample-bucket')
+  const [finishStepCompleted, setFinishStepCompleted] = useState<boolean>(false)
+  const [tokenValue, setTokenValue] = useState<string | null>(null)
+  const [finalFeedback, setFinalFeedback] = useState<number | null>(null)
 
-export class NodejsWizard extends PureComponent<null, State> {
-  state = {
-    currentStep: 1,
-    selectedBucket: 'my-bucket',
-    finishStepCompleted: false,
-    tokenValue: null,
-    finalFeedback: null,
-  }
-
-  private handleSelectBucket = (bucketName: string) => {
-    this.setState({selectedBucket: bucketName})
-  }
-
-  private handleMarkStepAsCompleted = () => {
-    this.setState({finishStepCompleted: true})
-  }
-
-  private setTokenValue = (tokenValue: string) => {
-    this.setState({tokenValue})
-  }
-
-  private setFinalFeedback = (feedbackValue: number) => {
-    this.setState({finalFeedback: feedbackValue})
-  }
-
-  subwayNavSteps = isFlagEnabled('ioxOnboarding')
+  const isIOxOrg = useSelector(isOrgIOx)
+  const subwayNavSteps = isIOxOrg
     ? HOMEPAGE_NAVIGATION_STEPS_SQL
     : HOMEPAGE_NAVIGATION_STEPS
 
-  handleNextClick = () => {
-    this.setState(
-      {
-        currentStep: Math.min(
-          this.state.currentStep + 1,
-          this.subwayNavSteps.length
-        ),
-      },
-      () => {
-        event(
-          'firstMile.nodejsWizard.next.clicked',
-          {},
-          {
-            clickedButtonAtStep: normalizeEventName(
-              this.subwayNavSteps[this.state.currentStep - 2].name
-            ),
-            currentStep: normalizeEventName(
-              this.subwayNavSteps[this.state.currentStep - 1].name
-            ),
-          }
-        )
-        scrollNextPageIntoView()
-      }
-    )
+  const handleMarkStepAsCompleted = () => {
+    setFinishStepCompleted(true)
   }
 
-  handlePreviousClick = () => {
-    this.setState(
-      {currentStep: Math.max(this.state.currentStep - 1, 1)},
-      () => {
-        event(
-          'firstMile.nodejsWizard.previous.clicked',
-          {},
-          {
-            clickedButtonAtStep: normalizeEventName(
-              this.subwayNavSteps[this.state.currentStep].name
-            ),
-            currentStep: normalizeEventName(
-              this.subwayNavSteps[this.state.currentStep - 1].name
-            ),
-          }
-        )
-        scrollNextPageIntoView()
-      }
-    )
-  }
-
-  handleNavClick = (clickedStep: number) => {
-    this.setState({currentStep: clickedStep})
-    event(
-      'firstMile.nodejsWizard.subNav.clicked',
-      {},
-      {
-        currentStep: normalizeEventName(
-          this.subwayNavSteps[clickedStep - 1].name
-        ),
-      }
-    )
-    scrollNextPageIntoView()
-  }
-
-  renderSqlStep = () => {
-    switch (this.state.currentStep) {
+  const renderSqlStep = (currentStep: number) => {
+    switch (currentStep) {
       case 1: {
         return <Overview wizard="nodejsWizard" />
       }
@@ -147,8 +55,8 @@ export class NodejsWizard extends PureComponent<null, State> {
         return (
           <Tokens
             wizardEventName="nodejsWizard"
-            setTokenValue={this.setTokenValue}
-            tokenValue={this.state.tokenValue}
+            setTokenValue={setTokenValue}
+            tokenValue={tokenValue}
           />
         )
       }
@@ -156,19 +64,19 @@ export class NodejsWizard extends PureComponent<null, State> {
         return <InitializeClientSql />
       }
       case 5: {
-        return <WriteDataSql onSelectBucket={this.handleSelectBucket} />
+        return <WriteDataSql onSelectBucket={setSelectedBucket} />
       }
       case 6: {
-        return <ExecuteQuerySql bucket={this.state.selectedBucket} />
+        return <ExecuteQuerySql bucket={selectedBucket} />
       }
       case 7: {
         return (
           <Finish
             wizardEventName="nodejsWizard"
-            markStepAsCompleted={this.handleMarkStepAsCompleted}
-            finishStepCompleted={this.state.finishStepCompleted}
-            finalFeedback={this.state.finalFeedback}
-            setFinalFeedback={this.setFinalFeedback}
+            markStepAsCompleted={handleMarkStepAsCompleted}
+            finishStepCompleted={finishStepCompleted}
+            finalFeedback={finalFeedback}
+            setFinalFeedback={setFinalFeedback}
           />
         )
       }
@@ -178,8 +86,8 @@ export class NodejsWizard extends PureComponent<null, State> {
     }
   }
 
-  renderFluxStep = () => {
-    switch (this.state.currentStep) {
+  const renderFluxStep = (currentStep: number) => {
+    switch (currentStep) {
       case 1: {
         return <Overview wizard="nodejsWizard" />
       }
@@ -190,8 +98,8 @@ export class NodejsWizard extends PureComponent<null, State> {
         return (
           <Tokens
             wizardEventName="nodejsWizard"
-            setTokenValue={this.setTokenValue}
-            tokenValue={this.state.tokenValue}
+            setTokenValue={setTokenValue}
+            tokenValue={tokenValue}
           />
         )
       }
@@ -199,22 +107,22 @@ export class NodejsWizard extends PureComponent<null, State> {
         return <InitializeClient />
       }
       case 5: {
-        return <WriteData onSelectBucket={this.handleSelectBucket} />
+        return <WriteData onSelectBucket={setSelectedBucket} />
       }
       case 6: {
-        return <ExecuteQuery bucket={this.state.selectedBucket} />
+        return <ExecuteQuery bucket={selectedBucket} />
       }
       case 7: {
-        return <ExecuteAggregateQuery bucket={this.state.selectedBucket} />
+        return <ExecuteAggregateQuery bucket={selectedBucket} />
       }
       case 8: {
         return (
           <Finish
             wizardEventName="nodejsWizard"
-            markStepAsCompleted={this.handleMarkStepAsCompleted}
-            finishStepCompleted={this.state.finishStepCompleted}
-            finalFeedback={this.state.finalFeedback}
-            setFinalFeedback={this.setFinalFeedback}
+            markStepAsCompleted={handleMarkStepAsCompleted}
+            finishStepCompleted={finishStepCompleted}
+            finalFeedback={finalFeedback}
+            setFinalFeedback={setFinalFeedback}
           />
         )
       }
@@ -224,70 +132,18 @@ export class NodejsWizard extends PureComponent<null, State> {
     }
   }
 
-  render() {
-    return (
-      <Page>
-        <Page.Header fullWidth={false} />
-        <Page.Contents scrollable={true}>
-          <div className="homepage-wizard-container">
-            <aside className="homepage-wizard-container--subway">
-              <div className="homepage-wizard-container--subway-inner">
-                <SubwayNav
-                  currentStep={this.state.currentStep}
-                  onStepClick={this.handleNavClick}
-                  navigationSteps={this.subwayNavSteps}
-                  settingUpIcon={NodejsIcon}
-                  settingUpText="Nodejs"
-                  setupTime="5 minutes"
-                />
-              </div>
-            </aside>
-            <div className="homepage-wizard-container--main">
-              <div
-                className={classnames(
-                  'homepage-wizard-container--main-wrapper',
-                  {
-                    verticallyCentered:
-                      this.state.currentStep === 1 ||
-                      this.state.currentStep === this.subwayNavSteps.length,
-                  }
-                )}
-              >
-                <WriteDataDetailsContextProvider>
-                  {isFlagEnabled('ioxOnboarding')
-                    ? this.renderSqlStep()
-                    : this.renderFluxStep()}
-                </WriteDataDetailsContextProvider>
-              </div>
-
-              <div className="homepage-wizard-container-footer">
-                <Button
-                  onClick={this.handlePreviousClick}
-                  text="Previous"
-                  size={ComponentSize.Large}
-                  color={ComponentColor.Tertiary}
-                  status={
-                    this.state.currentStep > 1
-                      ? ComponentStatus.Default
-                      : ComponentStatus.Disabled
-                  }
-                />
-                <Button
-                  onClick={this.handleNextClick}
-                  text="Next"
-                  size={ComponentSize.Large}
-                  color={ComponentColor.Primary}
-                  status={
-                    this.state.currentStep < this.subwayNavSteps.length
-                      ? ComponentStatus.Default
-                      : ComponentStatus.Disabled
-                  }
-                />
-              </div>
-            </div>
-          </div>
-        </Page.Contents>
-      </Page>
-    )
-  }
+  return (
+    <WriteDataDetailsContextProvider>
+      <WizardContainer
+        icon={NodejsIcon}
+        subwayNavSteps={subwayNavSteps}
+        language="nodejs"
+        languageTitle="Node.js"
+      >
+        {currentStep =>
+          isIOxOrg ? renderSqlStep(currentStep) : renderFluxStep(currentStep)
+        }
+      </WizardContainer>
+    </WriteDataDetailsContextProvider>
+  )
 }
